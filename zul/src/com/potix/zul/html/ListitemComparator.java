93be930eb76d4255adacea9d1ac71ece60d3ae22
tomyeh <tomyeh@zkoss.org>
2006-05-25 15:02:47 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@138 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/com/potix/zul/html/ListitemComparator.java
+++ zul/src/com/potix/zul/html/ListitemComparator.java
@@ -1,7 +1,7 @@
 /* ListitemComparator.java
 
 {{IS_NOTE
-	$Id: ListitemComparator.java,v 1.1 2006/05/25 14:11:13 tomyeh Exp $
+	$Id: ListitemComparator.java,v 1.2 2006/05/25 15:02:25 tomyeh Exp $
 	Purpose:
 		
 	Description:
@@ -26,23 +26,25 @@ import java.util.Comparator;
  * A comparator used to compare {@link Listitem}.
  *
  * @author <a href="mailto:tomyeh@potix.com">tomyeh@potix.com</a>
- * @version $Revision: 1.1 $ $Date: 2006/05/25 14:11:13 $
+ * @version $Revision: 1.2 $ $Date: 2006/05/25 15:02:25 $
  */
 public class ListitemComparator implements Comparator {
 	private final int _index;
-	/** null-as-largest. */
-	private final boolean _nal;
+	/** Ascending. */
+	private final boolean _asc;
+	/** Ignore case. */
+	private final boolean _igcase;
 
 	/** Compares with {@link Listitem#getValue}.
 	 *
 	 * <p>It assumes the value implements Comparable.
 	 *
-	 * <p>Note: null is assumed to be smallest. If you want null to be
-	 * largest, use {@link #ListitemComparator(int, boolean)}.
+	 * <p>Note: It assumes the ascending order and case-insensitive.
+	 * If not, use {@link #ListitemComparator(int, boolean, boolean)}
+	 * instead.
 	 */
 	public ListitemComparator() {
-		_index = -1; //compare value
-		_nal = false;
+		this(-1, true, true);
 	}
 	/** Compares with the cell of the specified index.
 	 *
@@ -51,12 +53,12 @@ public class ListitemComparator implements Comparator {
 	 * <p>Note: -1 for {@link Listitem#getValue} and it assumes
 	 * the value implements Comparable.
 	 *
-	 * <p>Note: null is assumed to be smallest. If you want null to be
-	 * largest, use {@link #ListitemComparator(int, boolean)}.
+	 * <p>Note: It assumes the ascending order and case-insensitive.
+	 * If not, use {@link #ListitemComparator(int, boolean, boolean)}
+	 * instead.
 	 */
 	public ListitemComparator(int index) {
-		_index = index;
-		_nal = false;
+		this(index, true, true);
 	}
 	/** Compares with the cell of the specified index.
 	 *
@@ -65,34 +67,47 @@ public class ListitemComparator implements Comparator {
 	 * <p>Note: -1 for {@link Listitem#getValue} and it assumes
 	 * the value implements Comparable.
 	 *
-	 * @param nullAsLargest whether null is assumed to be largest.
+	 * @param ascending whether to sort as ascending (or descending).
+	 * @param ignoreCase whether to sort case-insensitive
 	 */
-	public ListitemComparator(int index, boolean nullAsLargest) {
+	public ListitemComparator(int index, boolean ascending,
+	boolean ignoreCase) {
 		_index = index;
-		_nal = nullAsLargest;
+		_asc = ascending;
+		_igcase = ignoreCase;
 	}
 
 	//Comparator//
 	public int compare(Object o1, Object o2) {
 		final Listitem li1 = (Listitem)o1, li2 = (Listitem)o2;
 
+		Comparable v1, v2;
 		if (_index < 0) {
-			final Object v1 = li1.getValue(), v2 = li2.getValue();
-			if (v1 == null)
-				return v2 == null ? 0: _nal ? 1: -1;
-			return v2 == null ? _nal ? -1: 1: ((Comparable)v1).compareTo(v2);
+			v1 = handleCase((Comparable)li1.getValue());
+			v2 = handleCase((Comparable)li2.getValue());
+		} else {
+			final List lc1 = li1.getChildren(), lc2 = li2.getChildren();
+			v1 = _index >= lc1.size() ? null:
+				handleCase(((Listcell)lc1.get(_index)).getLabel());
+			v2 = _index >= lc2.size() ? null:
+				handleCase(((Listcell)lc2.get(_index)).getLabel());
 		}
 
-		final List lc1 = li1.getChildren(), lc2 = li2.getChildren();
-		final String
-			v1 = _index >= lc1.size() ? null:
-				((Listcell)lc1.get(_index)).getLabel(),
-			v2 = _index >= lc2.size() ? null:
-				((Listcell)lc2.get(_index)).getLabel();
-		if (v1 == null)
-			return v2 == null ? 0: _nal ? 1: -1;
-		return v2 == null ? _nal ? -1: 1: v1.compareTo(v2);
+		if (v1 == null) return v2 == null ? 0: _asc ? -1: 1;
+		if (v2 == null) return _asc ? 1: -1;
+		final int v = ((Comparable)v1).compareTo(v2);
+		return _asc ? v: -v;
 	}
+	private Comparable handleCase(Comparable c) {
+		if (_igcase) {
+			if (c instanceof String)
+				return ((String)c).toUpperCase();
+			if (c instanceof Character)
+				return ((Character)c).toUpperCase();
+		}
+		return c;
+	}
+
 	public boolean equals(Object o) {
 		return o instanceof ListitemComparator &&
 			((ListitemComparator)o)._index == _index;
