33f27ba06954e4ce533ab13253654249ec870147
tomyeh <tomyeh@zkoss.org>
2006-06-22 02:41:07 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@403 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- pxweb/src/com/potix/web/servlet/StyleSheet.java
+++ pxweb/src/com/potix/web/servlet/StyleSheet.java
@@ -26,6 +26,8 @@ import com.potix.lang.Objects;
  * @author <a href="mailto:tomyeh@potix.com">tomyeh@potix.com</a>
  */
 public class StyleSheet implements java.io.Serializable {
+    private static final long serialVersionUID = 20060622L;
+
 	private final String _href, _type, _content;
 	/** Creates by specifying the file to contain the style sheets.
 	 *
