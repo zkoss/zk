1d58bc32a2732d68574b0fde77b1ad7d33f54c59
tomyeh <tomyeh@zkoss.org>
2006-07-01 18:00:16 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@499 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/html/splt.js
+++ zul/src/archive/web/js/zul/html/splt.js
@@ -98,7 +98,7 @@ zkSplt._startDrag = function (cmp) {
 	var drag = zkSplt._drags[cmp.id];
 	if (drag) {
 		var run = drag.run = {};
-		run.org = Position.cumulativeOffset(cmp);
+		run.org = zk.cumulativeOffset(cmp);
 		var nd = $(cmp.id + "!chdextr");
 		var tn = zk.tagName(nd);
 		run.prev = zk.previousSibling(nd, tn);
@@ -117,7 +117,7 @@ zkSplt._snap = function (cmp, x, y) {
 	var drag = zkSplt._drags[cmp.id];
 	if (drag) {
 		var run = drag.run;
-		var ofs = Position.cumulativeOffset(run.box);
+		var ofs = zk.cumulativeOffset(run.box);
 		ofs = zk.toStylePos(cmp, ofs[0], ofs[1]);
 		if (drag.vert) {
 			if (y <= ofs[1]) {
@@ -145,7 +145,7 @@ zkSplt._dragging = function (drag) {
 	var drag = zkSplt._drags[cmp.id];
 	if (drag) {
 		var run = drag.run;
-		var ofs = Position.cumulativeOffset(cmp);
+		var ofs = zk.cumulativeOffset(cmp);
 		if (drag.vert) {
 			var diff = ofs[1] - run.org[1];
 			if (run.next) zkSplt._adj(run.next, "height", -diff);
