3fa4c7d93c70162d981f182d9560087c2cfc589c
tomyeh <tomyeh@zkoss.org>
2006-07-21 04:12:27 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@642 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/com/potix/zk/ui/impl/RequestInfoImpl.java
+++ zk/src/com/potix/zk/ui/impl/RequestInfoImpl.java
@@ -18,12 +18,16 @@ Copyright (C) 2006 Potix Corporation. All Rights Reserved.
 */
 package com.potix.zk.ui.impl;
 
+import javax.servlet.ServletContext;
+
 import com.potix.util.resource.Locator;
 
+import com.potix.zk.ui.Execution;
 import com.potix.zk.ui.WebApp;
 import com.potix.zk.ui.Desktop;
 import com.potix.zk.ui.Session;
 import com.potix.zk.ui.sys.RequestInfo;
+import com.potix.zk.ui.http.PageDefinitions;
 
 /**
  * An implementation of {@link RequestInfo}.
@@ -38,6 +42,13 @@ public class RequestInfoImpl implements RequestInfo {
 	private final Object _request;
 
 	/** Constructor
+	 *
+	 * @param wapp the Web application, never null.
+	 * @param sess the session, never null.
+	 * @param desktop the desktop, or null if not created yet.
+	 * @param request the request, or null if not available.
+	 * @param locator the locator used to locate taglib and other resources,
+	 * never null.
 	 */
 	public RequestInfoImpl(WebApp wapp, Session sess,
 	Desktop desktop, Object request, Locator locator) {
@@ -48,6 +59,30 @@ public class RequestInfoImpl implements RequestInfo {
 		_desktop = desktop;
 		_request = request;
 		_locator = locator;
+	}
+	/** Constructor.
+	 *
+	 * @param desktop the desktop, never null.
+	 * @param request the request, or null if not available.
+	 * @param locator the locator used to locate taglib and other resources,
+	 * never null.
+	 */
+	public RequestInfoImpl(Desktop desktop, Object request, Locator locator) {
+		this(desktop.getWebApp(), desktop.getSession(), desktop, request, locator);
+	}
+	/** Construcotr.
+	 *
+	 * @param exec the current execution, never null
+	 * @param path the related path used to get the locator, or null.
+	 */
+	public RequestInfoImpl(Execution exec, String path) {
+		this(exec.getDesktop(), exec.getNativeRequest(), getLocator(exec, path));
+	}
+	private static Locator getLocator(Execution exec, String path) {
+		final Object ctx = exec.getDesktop().getWebApp().getNativeContext();
+		if (ctx instanceof ServletContext)
+			return PageDefinitions.getLocator((ServletContext)ctx, path);
+		throw new UnsupportedOperationException("Unknown context: "+ctx);
 	}
 
 	public final WebApp getWebApp() {
