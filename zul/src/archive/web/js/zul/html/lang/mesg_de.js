0aef6a89dde9b5a477e1eff36e64dc8e3527f71c
tomyeh <tomyeh@zkoss.org>
2006-07-28 10:36:10 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@702 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/html/lang/mesg_de.js
@@ -0,0 +1,38 @@
+/* mesg.js
+
+{{IS_NOTE
+  Purpose:
+
+  Description:
+
+  History:
+    Fri Jul  8 14:25:41     2005, Created by tomyeh@potix.com
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+  This program is distributed under GPL Version 2.0 in the hope that
+  it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+function msgzul() {};
+
+//msgzul.ITEM_NOT_FOUND = "Treeitem not found: ";
+msgzul.ITEM_NOT_FOUND = "Knoten nicht gefunden: ";
+
+// HDS-060727 - Tab or Tabel (Tabelle in german)
+//msgzul.TAB_NOT_FOUND = "Tab not found: ";
+msgzul.TAB_NOT_FOUND = "Tab nicht gefunden: ";
+
+//msgzul.FAILED_TO_CREATE_MASK = "Unable to create Modal Mask";
+msgzul.FAILED_TO_CREATE_MASK = "Kann keine modale Maske erzeugen";
+
+//msgzul.UNKNOWN_TYPE = "Unknown component type: ";
+msgzul.UNKNOWN_TYPE = "Unbekannte Komponente: ";
+
+//msgzul.DATE_REQUIRED = "You must specify a date. Format: ";
+msgzul.DATE_REQUIRED = "Sie müssen ein Datum angeben. Format: ";
+
+//msgzul.NO_AUDIO_SUPPORT = "Your browser doesn't support dynamic audio";
+msgzul.NO_AUDIO_SUPPORT = "Ihr Browser unterstützt keine Tonwiedergabe";
