72eff54d0c51d8e8c26f8d5b09b13b76025b007d
tomyeh <tomyeh@zkoss.org>
2006-08-11 02:12:49 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@796 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/html/lang/mesg_es.js
@@ -0,0 +1,26 @@
+/* mesg.js
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Aug	07 16:43:18     2006, Created by: Daniel Octavio Ruiz Rodriguez aztekinetix@gmail.com
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+function msgzul() {};
+
+msgzul.ITEM_NOT_FOUND = "Item no encontrado en el Arbol: ";
+msgzul.TAB_NOT_FOUND = "Panel (Tab) no encontrado: ";
+msgzul.FAILED_TO_CREATE_MASK = "No es posible crear una mascada modal (Modal Mask) ";
+msgzul.UNKNOWN_TYPE = "Tipo de componente desconocido: ";
+msgzul.DATE_REQUIRED = "Debe especificar una fecha. Formato: ";
+msgzul.NO_AUDIO_SUPPORT = "Tu explorador (browser) no soporta audio dinamico ";
