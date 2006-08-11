6940f755fdc8034b7802b01514abfb42b5e25cb2
tomyeh <tomyeh@zkoss.org>
2006-08-11 01:56:56 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@791 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/html/lang/mesg_it.js
@@ -0,0 +1,35 @@
+/* mesg_it.js
+
+{{IS_NOTE
+	Purpose:
+		Locale dependent message
+	Description:
+		
+	History:
+		Aug 10, 2006, Created by Matteo Barbieri
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+function mesg() {};
+
+mesg.NOT_FOUND = "Non trovato: ";
+mesg.UNSUPPORTED = "Non ancora supportato: ";
+mesg.FAILED_TO_SEND = "Invio della richiesta al server fallito: ";
+mesg.FAILED_TO_RESPONSE = "Il server ha fallito nel elaborare la tua richiesta: ";
+mesg.UNSUPPORTED_BROWSER = "Browser non supportato, per favore utilizza Mozilla o Internet Explorer: ";
+mesg.ILLEGAL_RESPONSE = "Risposta sconosciuta inviata dal server. Per favore ricarica la pagina e riprova.\n";
+mesg.FAILED_TO_PROCESS = "Errore nell'elaborazione di ";
+mesg.UUID_REQUIRED = "L'UUID è obbligatorio";
+mesg.INVALID_STRUCTURE = "Struttura errata: ";
+mesg.COMP_OR_UUID_REQUIRED = "Il componente od il suo UUID sono obbligatori";
+mesg.NUMBER_REQUIRED = "Devi inserire un numero al posto di ";
+mesg.INTEGER_REQUIRED = "Devi inserire un intero al posto di ";
+mesg.EMPTY_NOT_ALLOWED = "Non è permesso il campo vuoto.\nNon puoi neanche inserire solamente spazi.";
+mesg.GOTO_ERROR_FIELD = "Arrivato al campo sbagliato";
+mesg.PLEASE_WAIT = "Attendere...";
