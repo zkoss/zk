81746a1c0629c2cc7f5379fefdaf90386e03d29f
tomyeh <tomyeh@zkoss.org>
2006-08-11 02:12:38 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@795 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/html/lang/mesg_es.js
@@ -0,0 +1,35 @@
+/* mesg.js
+
+{{IS_NOTE
+	Purpose:
+		Locale dependent message
+	Description:
+		
+	History:
+		Mon Aug	07 16:43:18     2006, Created by: Daniel Octavio Ruiz Rodriguez aztekinetix@gmail.com
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+function mesg() {};
+
+mesg.NOT_FOUND = "No encontrado: ";
+mesg.UNSUPPORTED = "No es soportado por esta version: "
+mesg.FAILED_TO_SEND = "Fallo al enviar peticiones (request) al servidor: ";
+mesg.FAILED_TO_RESPONSE = "El servidor fallo al procesar la peticion (request): ";
+mesg.UNSUPPORTED_BROWSER = "Explorador (Browser) no compatible, use Internet Explorer o Mozilla, por ejemplo: ";
+mesg.ILLEGAL_RESPONSE = "Respuesta no valida por parte del servidor. Por favor actualice la pagina (reload) e intente de nuevo.\n";
+mesg.FAILED_TO_PROCESS = "Fallo al procesar ";
+mesg.UUID_REQUIRED = "UUID es requerido ";
+mesg.INVALID_STRUCTURE = "Estructura invalida: ";
+mesg.COMP_OR_UUID_REQUIRED = "Componente o su UUID es requerido ";
+mesg.NUMBER_REQUIRED = "Debe especificar un numero, en lugar de ";
+mesg.INTEGER_REQUIRED = "Debe especificar un numero entero, en lugar de ";
+mesg.EMPTY_NOT_ALLOWED = "Vacio o espacios en blanco no son permitidos.\n Debe especificar un valor diferente ";
+mesg.GOTO_ERROR_FIELD = "Ir al campo que contiene el error ";
+mesg.PLEASE_WAIT = "Procesando...";
