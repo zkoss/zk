287e04e7e74bde61844070a69c80c642f4cdac7d
tomyeh <tomyeh@zkoss.org>
2006-08-11 01:57:09 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@792 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/html/lang/mesg_it.js
@@ -0,0 +1,26 @@
+/* mesg_it.js
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Aug 10, 2006, Created by Matteo Barbieri
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+function msgzul() {};
+
+msgzul.ITEM_NOT_FOUND = "Treeitem non trovato: ";
+msgzul.TAB_NOT_FOUND = "Tab non trovato: ";
+msgzul.FAILED_TO_CREATE_MASK = "Impossibile creare la Modal Mask";
+msgzul.UNKNOWN_TYPE = "Tipo di componente sconosciuto: ";
+msgzul.DATE_REQUIRED = "Devi inserire una data. Formato: ";
+msgzul.NO_AUDIO_SUPPORT = "Il tuo browser non supporta la funzione di audio dinamico";
