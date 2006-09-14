18e4d94afaf8ff880a55332799c0c5a2c558ca1f
henrichen <henrichen@zkoss.org>
2006-09-14 07:39:30 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1067 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/com/potix/zkplus/hibernate/HibernateUtil.java
+++ zkplus/src/com/potix/zkplus/hibernate/HibernateUtil.java
@@ -20,51 +20,82 @@ import com.potix.zk.ui.WebApp;
 import com.potix.zk.ui.Executions;
 import com.potix.util.logging.Log;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.ConnectionReleaseMode;
+import org.hibernate.HibernateException;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * <p>Utitlity to access Hibernate Session. This implemenation works with the Hibernate's 
+ * thread session context (version 3.1+). That is, you have to specified 
+ * hibernate's configuration file "hibernate.cfg.xml" to as follows:</p>
+ *
+ * <pre><code>
+ *  <session-factory>
+ *		...
+ *		<property name="current_session_context_class">thread</property>
+ *  </session-factory>
+ * </code><pre>
+ *
+ * <p> Also notice that the zkplus.jar must be put under application's WEB-INF/lib because
+ * the SessionFactory is stored as a class static member.
+ *
+ * @author <a href="mailto:henrichen@potix.com">henrichen@potix.com</a>
+ */
 public class HibernateUtil {
-	private static final String HIBERNATE_SESSION_FACTORY = "ZK_HIBERNATE_SESSION_FACTORY";
 	private static final Log log = Log.lookup(HibernateUtil.class);
+	
+	private static SessionFactory _factory;
+
 	/**
-	 * Get the singleton hibernate Session Factory of the WebApp. This method
-	 * must be called within an ZK Execution.
+	 * Get the singleton hibernate Session Factory.
 	 */
 	public static SessionFactory getSessionFactory() {
-		return initSessionFactory(Executions.getCurrent().getDesktop().getWebApp());
+		return initSessionFactory();
+	}
+	
+	/**
+	 * Wrapping HibernateUtil.getSessionFactory().getCurrentSession() into a simple API.
+	 */
+	public static Session currentSession() throws HibernateException {
+		return getSessionFactory().getCurrentSession();
+	}
+	
+	/**
+	 * Wrapping HibernateUtil.getSessionFactory().getCurrentSession().close() into a simple API.
+	 */
+	public static void closeSession() throws HibernateException {
+		currentSession().close();
 	}
 	
 	/**
 	 * Used in {@link HibernateSessionWebAppListener} to init
 	 * Hibernate SessionFactory.
 	 */
-	/* package */ static SessionFactory initSessionFactory(WebApp app) {
-		SessionFactory factory = (SessionFactory) app.getAttribute(HIBERNATE_SESSION_FACTORY);
-		if (factory == null) {
+	/* package */ static SessionFactory initSessionFactory() {
+		if (_factory == null) {
 			try {
 			    // Create the SessionFactory from hibernate.cfg.xml
-			    factory = new Configuration().configure().buildSessionFactory();
+			    _factory = new Configuration().configure().buildSessionFactory();
 			} catch (Throwable ex) {
 			    // Make sure you log the exception, as it might be swallowed
 			    log.error("Initial SessionFactory creation failed." + ex);
 			    throw new ExceptionInInitializerError(ex);
 			}
 
-			app.setAttribute(HIBERNATE_SESSION_FACTORY, factory);
 		}
-		return factory;
+		return _factory;
 	}
 	
 	/**
 	 * Used in {@link HibernateSessionWebAppListener} to init
 	 * Hibernate SessionFactory.
 	 */
-	/* package */ static void cleanupSessionFactory(WebApp app) {
-		SessionFactory factory = (SessionFactory) app.getAttribute(HIBERNATE_SESSION_FACTORY);
-		if (factory != null) {
-			factory.close(); // Free all resources
-			app.removeAttribute(HIBERNATE_SESSION_FACTORY);
+	/* package */ static void cleanupSessionFactory() {
+		if (_factory != null) {
+			_factory.close(); // Free all resources
+			_factory = null;
 		}
 	}
 }
