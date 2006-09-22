b61f8c1eef4933b8d51fc8e8c303887cb1e1d1ce
tomyeh <tomyeh@zkoss.org>
2006-09-22 04:18:58 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1206 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/html/lang/mesg_fr.js
@@ -0,0 +1,35 @@
+/* mesg_fr.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		Locale dependent message
+	Description:
+		
+	History:
+    sept 21, 2006,Created by Jérôme Vergereau
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+function mesg() {};
+
+mesg.NOT_FOUND = "Pas trouvé: ";
+mesg.UNSUPPORTED = "Pas encore supporté: "
+mesg.FAILED_TO_SEND = "Echec de l'envoie d'une requete vers le serveur: ";
+mesg.FAILED_TO_RESPONSE = "Le serveur à echouer dans le traitement de votre requête: ";
+mesg.UNSUPPORTED_BROWSER = "Navigateur non supporté,veuillez utilisez Internet Explorer ou Mozilla: ";
+mesg.ILLEGAL_RESPONSE = "Non connaissance de la réponse envoyer depuis le serveur . Veuillez recharger et réessayer de nouveau.\n";
+mesg.FAILED_TO_PROCESS = "Echec du processus ";
+mesg.UUID_REQUIRED = "UUID requis";
+mesg.INVALID_STRUCTURE = "Invalide structure: ";
+mesg.COMP_OR_UUID_REQUIRED = "Le Composant ou son UUID est requis";
+mesg.NUMBER_REQUIRED = "Vous devez spécifiez un nombre, plutot que";
+mesg.INTEGER_REQUIRED = "Vous devez spécifiez un entier, plutot que";
+mesg.EMPTY_NOT_ALLOWED = "Champ vide non autorisé.\nVous ne pouvez pas n'indiquer rien";
+mesg.GOTO_ERROR_FIELD = "Allez sur le mauvais champ";
+mesg.PLEASE_WAIT = "Chargement...";
