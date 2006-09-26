5cf6cf7e6ed1ff0f2e33dd49a422a6a652e3b0e0
henrichen <henrichen@zkoss.org>
2006-09-26 04:12:25 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1245 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/org/zkoss/zkplus/acegi/AcegiSecurityContextListener.java
+++ zkplus/src/org/zkoss/zkplus/acegi/AcegiSecurityContextListener.java
@@ -16,7 +16,7 @@ Copyright (C) 2006 Potix Corporation. All Rights Reserved.
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
-package org.zkoss.zkplus.hibernate;
+package org.zkoss.zkplus.acegi;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.UiException;
