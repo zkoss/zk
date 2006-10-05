dea1ac29e25f6e88521b838320a12af55be3e04c
tomyeh <tomyeh@zkoss.org>
2006-10-05 07:52:16 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1330 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkdemo/src/org/zkoss/zkdemo/TestRichlet.java
@@ -0,0 +1,33 @@
+/* TestRichlet.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Oct  5 12:29:35     2006, Created by tomyeh@potix.com
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zkdemo;
+
+import org.zkoss.zk.ui.Richlet;
+import org.zkoss.zk.ui.RichletConfig;
+
+/**
+ * Used to test richlet.
+ *
+ * @author <a href="mailto:tomyeh@potix.com">tomyeh@potix.com</a>
+ */
+public class TestRichlet implements Richlet {
+	//Richlet//
+	public void init(RichletConfig config) {
+	}
+	public void destroy() {
+	}
+}
