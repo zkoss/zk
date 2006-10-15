c1512c6093bcce17724d3ea04e512a6c6a48725f
tomyeh <tomyeh@zkoss.org>
2006-10-15 07:53:23 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1413 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/au.js
+++ zk/src/archive/web/js/zk/html/au.js
@@ -16,7 +16,7 @@ Copyright (C) 2005 Potix Corporation. All Rights Reserved.
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
-function zkau() {}
+zkau = {};
 
 if (!zkau._reqs) {
 	zkau._reqs = new Array(); //Ajax requests
@@ -1365,7 +1365,7 @@ zkau._ghostdrag = function (dg, ghosting) {
 
 //////////////
 /// ACTION ///
-function action() {}
+action = {};
 
 /** Makes a component visible.
  * @param bShow false means hide; otherwise (including undefined) is show
