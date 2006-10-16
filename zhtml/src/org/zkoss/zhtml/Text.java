e816b89f29f68cd0069906ede1002491bd134380
tomyeh <tomyeh@zkoss.org>
2006-10-16 16:47:38 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1434 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zhtml/src/org/zkoss/zhtml/Text.java
+++ zhtml/src/org/zkoss/zhtml/Text.java
@@ -71,7 +71,7 @@ public class Text extends AbstractComponent implements RawId {
 	}
 	private static boolean isRawLabel(Component comp) {
 		final LanguageDefinition langdef =
-			((ComponentCtrl)comp).getMillieu().getLanguageDefinition();
+			((ComponentCtrl)comp).getMilieu().getLanguageDefinition();
 		return langdef != null && langdef.isRawLabel();
 	}
 
