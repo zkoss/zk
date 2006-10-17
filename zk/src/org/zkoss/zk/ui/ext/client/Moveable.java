90f063cf46042822b3bf4ac6c1238c37d07f97e0
tomyeh <tomyeh@zkoss.org>
2006-10-17 06:39:27 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1441 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/org/zkoss/zk/ui/ext/client/Moveable.java
@@ -0,0 +1,46 @@
+/* Moveable.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Aug 12 08:44:39     2005, Created by tomyeh@potix.com
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zk.ui.ext.client;
+
+/**
+ * Implemented by the object returned by {@link org.zkoss.zk.ui.sys.ComponentCtrl#getClientCtrl},
+ * a component is moveable by the client.
+ *
+ * <p>Once it is moved by the user, {@link #setLeftByClient} and
+ * {@link #setTopByClient}
+ * are called and {@link org.zkoss.zk.ui.event.MoveEvent} is sent to notify the component.
+ * 
+ * @author <a href="mailto:tomyeh@potix.com">tomyeh@potix.com</a>
+ */
+public interface Moveable {
+	/** Sets the left of the component, caused by user's activity at
+	 * the client.
+	 * <p>This method is designed to be used by engine.
+	 * Don't invoke it directly. Otherwise, the client and server
+	 * might mismatch.
+	 */
+	public void setLeftByClient(String left);
+	/** Sets the top of the component, caused by user's activity at
+	 * the client.
+	 * <p>This method is designed to be used by engine.
+	 * Don't invoke it directly. Otherwise, the client and server
+	 * might mismatch.
+	 */
+	public void setTopByClient(String top);
+}
