1fdce97e04d3e556daceb6a95f909978dde7f914
tomyeh <tomyeh@zkoss.org>
2006-10-25 08:06:58 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1512 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/org/zkoss/zk/ui/annotation/Annotation.java
@@ -0,0 +1,25 @@
+/* Annotation.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Wed Oct 25 16:05:13     2006, Created by tomyeh@potix.com
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zk.ui.annotation;
+
+/**
+ * The common interface extended by all annotation types.
+ *
+ * @author <a href="mailto:tomyeh@potix.com">tomyeh@potix.com</a>
+ */
+public interface Annotation {
+}
