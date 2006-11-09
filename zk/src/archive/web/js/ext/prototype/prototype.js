5b5579ff6d178df6e0f72f765da8caa0dc79ac79
tomyeh <tomyeh@zkoss.org>
2006-11-09 13:12:41 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1612 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/ext/prototype/prototype.js
+++ zk/src/archive/web/js/ext/prototype/prototype.js
@@ -2037,8 +2037,11 @@ var Position = {
   realOffset: function(element) {
     var valueT = 0, valueL = 0;
     do {
+//Tom M. Yeh, Potix: fix opera bug (see the page function)
+if (!window.opera || element.tagName=='BODY') { 
       valueT += element.scrollTop  || 0;
       valueL += element.scrollLeft || 0;
+}
       element = element.parentNode;
     } while (element);
     return [valueL, valueT];
