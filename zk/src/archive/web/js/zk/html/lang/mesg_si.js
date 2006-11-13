c0537503a7ae809c399882eb6277a9a9e9b3713e
tomyeh <tomyeh@zkoss.org>
2006-11-13 10:29:40 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1643 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/html/lang/mesg_si.js
@@ -0,0 +1,40 @@
+/* mesg.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		Locale dependent message
+	Description:
+		
+	History:
+		Created by Ziga (zgantar@users.sourceforge.net) 11 November 2006
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+mesg = {};
+mesg.NOT_FOUND = "Ni mogoèe najti: ";
+mesg.UNSUPPORTED = "Še ni podporto: "
+mesg.FAILED_TO_SEND = "Napaka pri pošiljanju zahteve strežniku: ";
+mesg.FAILED_TO_RESPONSE = "Strežnik je sporoèil napako pri obdelavi vaše zahteve: ";
+mesg.UNSUPPORTED_BROWSER = "Brskalnik ni prepoznan. Prosim uporabite Internet Explorer, Firefox or Mozilla: ";
+mesg.ILLEGAL_RESPONSE = "Strežnik je poslal neznan odgovor. Prosim osvežite stran .\n";
+mesg.FAILED_TO_PROCESS = "Napaka pri obdelavi";
+mesg.UUID_REQUIRED = "UUID je zahtevana";
+mesg.INVALID_STRUCTURE = "Neveljavna oblika: ";
+mesg.COMP_OR_UUID_REQUIRED = "Komponenta in UUID sta zahtevana";
+mesg.NUMBER_REQUIRED = "Podati morate številko in ne ";
+mesg.INTEGER_REQUIRED = "Podati morate številko in ne ";
+mesg.EMPTY_NOT_ALLOWED = "Prazno polje ni dovoljeno.\nTudi sami presledki niso dovoljeni";
+mesg.GOTO_ERROR_FIELD = "Pojdite na napaèno polje";
+mesg.PLEASE_WAIT = "Procesiram...";
+
+mesg.FILE_SIZE = "Velikost datoteke: ";
+mesg.KBYTES = "KB";
+
+mesg.CANCEL="Preklièi";
+
---
+++ zul/src/archive/web/js/zul/lang/mesg_si.js
@@ -0,0 +1,24 @@
+/* mesg.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Created by Ziga (zgantar@users.sourceforge.net) 11 November 2006
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.FAILED_TO_CREATE_MASK = "Ni bilo mogoèe narediti oblikovne maske";
+msgzul.UNKNOWN_TYPE = "Neznan tip komponente: ";
+msgzul.DATE_REQUIRED = "Vnesti je potrebno datum. Oblika: ";
+msgzul.NO_AUDIO_SUPPORT = "Vaš brskalnik ne podpira dinamiène glasbe";
+
