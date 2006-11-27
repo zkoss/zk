414c153d2bd42437638b879bbef3615003510dfd
tomyeh <tomyeh@zkoss.org>
2006-11-27 11:35:35 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1736 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkmob/src/org/zkoss/zkmob/Mainlet.java
@@ -0,0 +1,25 @@
+/* Mainlet.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Nov 27 17:22:06     2006, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zkmob;
+
+/**
+ * The main MIDlet of ZK Mobile Client.
+ *
+ * @author tomyeh
+ */
+public class Mainlet {
+}
