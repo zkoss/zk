5739d64c478ca26ca9aff47340a2018b737e4bb8
tomyeh <tomyeh@zkoss.org>
2006-11-30 10:53:46 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1771 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/html/lang/mesg_in.js
@@ -0,0 +1,39 @@
+/* mesg.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		Locale dependent message
+	Description:
+		
+	History:
+		Created on Nov 30, 2006, by James Liam Supangkat, Jakarta, Indonesia
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+mesg = {};
+mesg.NOT_FOUND = "Tidak Diketemui: ";
+mesg.UNSUPPORTED = "Belum didukung :: "
+mesg.FAILED_TO_SEND = "Gagal untuk mengirim request ke server: ";
+mesg.FAILED_TO_RESPONSE = "Server gagal memproses request Anda: ";
+mesg.UNSUPPORTED_BROWSER = "Browser tidak didukung, mohon gunakan Internet Explorer, Firefox atau Mozilla: ";
+mesg.ILLEGAL_RESPONSE = "Response yang terkirim dari server tidak diketahui. Mohon diulangi lagi.\n";
+mesg.FAILED_TO_PROCESS = "Gagal untuk di-proses";
+mesg.UUID_REQUIRED = "UIID di-perlukan";
+mesg.INVALID_STRUCTURE = "Struktur tidak valid: ";
+mesg.COMP_OR_UUID_REQUIRED = "Komponen atau UUID-nya diperlukan";
+mesg.NUMBER_REQUIRED = "Anda harus memberikan sebuah angka, bukan ";
+mesg.INTEGER_REQUIRED = "Anda harus memberikan sebuah integer, bukan ";
+mesg.EMPTY_NOT_ALLOWED = "Kosong tidak di-ijin-kan.\nAnda tidak dapat memberikan apa-apa selain spasi, atau";
+mesg.GOTO_ERROR_FIELD = "Menuju field yang error";
+mesg.PLEASE_WAIT = "Diproses...";
+
+mesg.FILE_SIZE = "Ukuran File: ";
+mesg.KBYTES = "KB";
+
+mesg.CANCEL="Batal";
---
+++ zul/src/archive/web/js/zul/lang/mesg_in.js
@@ -0,0 +1,23 @@
+/* mesg.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Created on Nov 30, 2006, by James Liam Supangkat, Jakarta, Indonesia
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.FAILED_TO_CREATE_MASK = "Tidak dapat membuat Modal Mask";
+msgzul.UNKNOWN_TYPE = "Tipe komponen tidak diketahui: ";
+msgzul.DATE_REQUIRED = "Anda harus menentukan tanggal. Dengan Format: ";
+msgzul.NO_AUDIO_SUPPORT = "Browser Anda tidak mendukung audio dinamis";
