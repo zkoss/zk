79ed0de444b564dc53c0ec2ca7f4cf96366ab331
henrichen <henrichen@zkoss.org>
2006-12-04 04:31:23 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1788 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkplus/src/org/zkoss/zkplus/databind/ListModelConverter.java
@@ -0,0 +1,69 @@
+/* ListModelConverter.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Dec  1 16:55:36     2006, Created by Henri Chen
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zkplus.databind;
+
+import org.zkoss.zul.ListModelSet;
+import org.zkoss.zul.ListModelMap;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zk.ui.UiException;
+
+import java.util.Set;
+import java.util.Map;
+import java.util.List;
+
+/**
+ * The {@link TypeConverter} implmentation for converting collection to ListModel and vice versa.
+ *
+ * @author Henri Chen
+ */
+public class ListModelConverter implements TypeConverter {
+	/** Convert a Set, Map, or List to associated ListModel.
+	 * @param val must be instanceof Set, Map, or List.
+	 */
+	public Object coerceToUi(Object val) {
+		if (val == null) {
+			throw new NullPointerException("val");
+		}
+		if (val instanceof Set) {
+			return ListModelSet.instance((Set)val);
+		} else if (val instanceof List) {
+			return ListModelList.instance((List)val);
+		} else if (val instanceof Map) {
+			return ListModelMap.instance((Map)val);
+		} else {
+			throw new UiException("Expects only java.util.Set, java.util.List, java.util.Map only. "+val.getClass());
+		}
+	}
+
+	/** Convert a ListModel to Set, Map, or List.
+	 * @param val must be ListModelSet, ListModelList, or ListModelMap
+	 */
+	public Object coerceToBean(Object val) {
+		if (val == null) {
+			throw new NullPointerException("val");
+		}
+		if (val instanceof ListModelSet) {
+			return ((ListModelSet)val).getInnerSet();
+		} else if (val instanceof List) {
+			return ((ListModelList)val).getInnerList();
+		} else if (val instanceof Map) {
+			return ((ListModelMap)val).getInnerMap();
+		} else {
+			throw new UiException("Expects ListModelSet, ListModelList, or ListModelMap only."+val.getClass());
+		}
+	}
+}
