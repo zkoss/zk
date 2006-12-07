8c4a4b4dac59969f24bddca88262a7514ca548a0
tomyeh <tomyeh@zkoss.org>
2006-12-07 07:49:11 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1827 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/grid.js
+++ zul/src/archive/web/js/zul/grid.js
@@ -125,11 +125,9 @@ zk.Grid.prototype = {
 		//Bug 1553937: wrong sibling location
 		//IE: otherwise, element's width will be extended to fit body
 		if (zk.ie && !this.paging) { //note: we don't solve this bug for paging yet
-			var wd = this.element.style.width;
-			if (wd && wd != "auto" && wd.indexOf('%') < 0) {
-				this.body.style.width = wd;
-				if (this.head) this.head.style.width = wd;
-			}
+			var wd = this.element.clientWidth + "px";
+			this.body.style.width = wd;
+			if (this.head) this.head.style.width = wd;
 		}
 	},
 	cleanup: function ()  {
