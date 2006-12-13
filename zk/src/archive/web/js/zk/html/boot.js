b928dc7f576ba31009cbec3ffe32e27ab10df053
tomyeh <tomyeh@zkoss.org>
2006-12-13 07:10:51 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1868 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/boot.js
+++ zk/src/archive/web/js/zk/html/boot.js
@@ -23,7 +23,7 @@ zk = {};
 /** Default version used for all modules that don't define their individual
  * version.
  */
-zk.build = "33"; //increase this if we want the browser to reload JavaScript
+zk.build = "35"; //increase this if we want the browser to reload JavaScript
 zk.mods = {}; //ZkFns depends on it
 
 /** Browser info. */
