e399b0e3841036c83f473c1f46a9a3aa191da0e7
henrichen <henrichen@zkoss.org>
2006-12-28 10:00:59 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1939 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkplus/src/org/zkoss/zkplus/acegi/ZkTokenBasedRememberMeServices.java
@@ -0,0 +1,41 @@
+/* ZkTokenBasedRememberMeServices.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Dec 28 09:26:17     2006, Created by Henri
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zkplus.acegi;
+
+import org.acegisecurity.ui.rememberme.TokenBasedRememberMeServices;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <p>get rememberme from request's attribute rather than request's parameter.</p>
+ *
+ * @author Henri
+ */
+public class ZkTokenBasedRememberMeServices extends TokenBasedRememberMeServices {
+    protected boolean rememberMeRequested(HttpServletRequest request, String param) {
+    	if (isAlwaysRemember()) {
+    		return true;
+    	}
+    	
+    	if (param == null) {
+    		return false;
+    	}
+    	
+    	Boolean yesno = (Boolean) request.getAttribute(param);
+    	return yesno != null && yesno.booleanValue();
+    }
+}
