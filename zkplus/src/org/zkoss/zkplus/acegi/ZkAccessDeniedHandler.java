177195b643cbd26c3c1e532ab699985098eb9558
henrichen <henrichen@zkoss.org>
2006-12-28 09:58:23 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1934 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/org/zkoss/zkplus/acegi/ZkAccessDeniedHandler.java
+++ zkplus/src/org/zkoss/zkplus/acegi/ZkAccessDeniedHandler.java
@@ -18,8 +18,20 @@ package org.zkoss.zkplus.acegi;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.event.EventListener;
 
+import org.acegisecurity.ui.AccessDeniedHandler;
+import org.acegisecurity.ui.AccessDeniedHandlerImpl;
 import org.acegisecurity.AccessDeniedException;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Used by {@link ZkExceptionTranslationHandler} to handle an
@@ -27,13 +39,32 @@ import org.acegisecurity.AccessDeniedException;
  * 
  * @author Henri
  */
-public interface ZkAccessDeniedHandler {
-    /**
-     * Handles an access denied failure.
-     *
-     * @param comp the component that cause this invocation.
-     * @param evt the event that cause this invocation.
-     * @param accessDeniedException the exception occured within the event of the component.
-     */
-    public void handle(Component comp, Event evt, AccessDeniedException accessDeniedException);
+public class ZkAccessDeniedHandler implements AccessDeniedHandler {
+	private static final String ON_ACCESSDENIED = "onAccessDenied";
+	private String _errorPage;
+	
+	public void setErrorPage(String url) {
+		_errorPage = url;
+	}
+	
+	public String getErrorPage() {
+		return _errorPage;
+	}
+	
+    public void handle(ServletRequest request, ServletResponse response, AccessDeniedException accessDeniedException)
+        throws IOException, ServletException {
+
+        // Put exception into request scope (perhaps of use to a view)
+        ((HttpServletRequest) request).setAttribute(AccessDeniedHandlerImpl.ACEGI_SECURITY_ACCESS_DENIED_EXCEPTION_KEY,
+            accessDeniedException);
+
+    	final Component comp = (Component) request.getAttribute(ZkEventExceptionFilter.COMPONENT);
+		if (!comp.isListenerAvailable(ON_ACCESSDENIED, true)) {
+			final EventListener listener = new ShowWindowEventListener();
+			comp.setAttribute(ON_ACCESSDENIED, listener);
+			comp.addEventListener(ON_ACCESSDENIED, listener);
+		}
+		final String url = getErrorPage();
+    	Events.postEvent(new Event(ON_ACCESSDENIED, comp, url != null ? url : "~./accessDenied.zul"));
+    }
 }
