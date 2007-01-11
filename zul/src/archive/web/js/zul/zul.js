a5ebfea400f6395c0b7ab0d7aaedca3eaaf8359e
tomyeh <tomyeh@zkoss.org>
2007-01-11 12:43:43 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@2002 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/zul.js
+++ zul/src/archive/web/js/zul/zul.js
@@ -16,7 +16,7 @@ Copyright (C) 2005 Potix Corporation. All Rights Reserved.
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
-zk.load("zul.lang.mesg*");
+zk.load("zul.lang.msgzul*");
 
 ////
 zul = {};
