34e1ce71511f90495e4f358a9ad939b4c5785c10
tomyeh <tomyeh@zkoss.org>
2007-01-11 03:49:35 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@1994 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/lang/mesg_bg.js
@@ -0,0 +1,25 @@
+﻿/* mesg.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+　　Tranlated by Boril Yonchev (yonchev@gmail.com) 10 January 2007
+}}IS_NOTE
+
+Copyright (C) 2005-2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.FAILED_TO_CREATE_MASK = "Невъзмоно е да се създаде модална маска";
+msgzul.UNKNOWN_TYPE = "Непознат тип на компонента: ";
+msgzul.DATE_REQUIRED = "Трябва да зададете дата в формат: ";
+msgzul.NO_AUDIO_SUPPORT = "Броузерът ви неподдържа динамично аудио";
+
+
