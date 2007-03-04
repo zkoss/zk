1bae8dcc3d0bc07f67492115433f4d82ca37a30d
tomyeh <tomyeh@zkoss.org>
2007-03-04 04:02:43 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@2397 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/lang/msgzul_ro.js
@@ -0,0 +1,23 @@
+/* mesg_ro.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Feb 02, 2007, Created by Calin Vaida
+}}IS_NOTE
+
+Copyright (C) 2005-2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.FAILED_TO_CREATE_MASK = "Imposibil de creat Modal Mask";
+msgzul.UNKNOWN_TYPE = "Tip necunoscut de componentă: ";
+msgzul.DATE_REQUIRED = "Trebuie să specificaţi o dată. Formatul: ";
+msgzul.NO_AUDIO_SUPPORT = "Browser-ul dumneavoastră nu suportă audio dinamic";
