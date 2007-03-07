cf960ee43ace090c810118097bb545de9ff15c30
henrichen <henrichen@zkoss.org>
2007-03-07 00:54:22 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@2406 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/org/zkoss/zkplus/databind/AnnotateDataBinderInit.java
+++ zkplus/src/org/zkoss/zkplus/databind/AnnotateDataBinderInit.java
@@ -18,13 +18,15 @@ package org.zkoss.zkplus.databind;
 
 import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.Path;
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.Initiator;
 
 /**
  * <p>This initiator class do following things:</p>
  * <ol>
  * <li>new an {@link AnnotateDataBinder} instance.</li>
- * <li>set the AnnotateDataBinder instance as a page variable "binder".</li>
+ * <li>set the AnnotateDataBinder instance as a variable "binder" in the ID Space of the root 
+ *  component as specified in arg0 "component-path".(if arg0 is not specified, use Page instead.)</li>
  * <li>call {@link AnnotateDataBinder#loadAll()} to initiate all UI components from the associated data bean.</li>
  * </ol>
  * <p>Put the init PI as follows:</p>
@@ -34,7 +36,7 @@ import org.zkoss.zk.ui.util.Initiator;
  * <p>Where the arg0 is the component path that specifies the component the AnnotateDataBinder covers. If the 
  * arg0 is not specified or set to "page", the AnnotateDataBinder will default to cover the whole page.</p>
  * <p>Where the arg1 is used to decide whether to load default binding configuration defined in lang-addon.xml. 
- * If the arg1 is not specified it is default to true.</p>
+ * If the arg1 is not specified it is default to true. Note that you have to specify arg0 if you want to specify arg1.</p>
  *
  * @author Henri Chen
  * @see AnnotateDataBinder
@@ -43,15 +45,26 @@ public class AnnotateDataBinderInit implements Initiator {
 	private String _compPath;
 	private String _defaultConfig;
 	
+	/** The AnnotateDataBinder created in doAfterCompose() */
+	protected AnnotateDataBinder _binder; 
+	
 	//-- Initator --//
 	public void doAfterCompose(Page page) {
 		boolean b = _defaultConfig != null ? Boolean.valueOf(_defaultConfig).booleanValue() : true;
-		AnnotateDataBinder binder = _compPath == null || "page".equals(_compPath) ?
-			new AnnotateDataBinder(page, b) :  _compPath.startsWith("/") ? 
-			new AnnotateDataBinder(Path.getComponent(_compPath), b) : 
-			new AnnotateDataBinder(page.getFellow(_compPath), b);
-		page.setVariable("binder", binder);
-	    binder.loadAll(); //load data bean properties into UI components
+		
+		if (_compPath == null || "page".equals(_compPath)) {
+			_binder = new AnnotateDataBinder(page, b);
+			page.setVariable("binder", _binder);
+		} else if (_compPath.startsWith("/")) {
+			final Component comp = Path.getComponent(_compPath);
+			_binder = new AnnotateDataBinder(comp, b);
+			comp.setVariable("binder", _binder, true);
+		} else {
+			final Component comp = page.getFellow(_compPath);
+			_binder = new AnnotateDataBinder(comp, b);
+			comp.setVariable("binder", _binder, true);
+		}
+    _binder.loadAll(); //load data bean properties into UI components
 	}
  	public void doCatch(java.lang.Throwable ex) {
  		// do nothing
