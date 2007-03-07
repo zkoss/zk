b7198e63fbdb7ad9a3a3eda00749654f968e4a07
tomyeh <tomyeh@zkoss.org>
2007-03-07 05:09:54 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@2416 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/lang/msgzul_ja_JP.js
@@ -0,0 +1,23 @@
+/* mesg.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Created on Mar. 7, 2007, by Poli Lee
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.FAILED_TO_CREATE_MASK = "Unable to create Modal Mask";
+msgzul.UNKNOWN_TYPE = "Unknown component type: ";
+msgzul.DATE_REQUIRED = "日にちしかインプットできない。フォーマット: ";
+msgzul.NO_AUDIO_SUPPORT = "Your browser doesn't support dynamic audio";
