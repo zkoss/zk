c8811ef6ea4e3bff0dba71e26825ac263f8ea168
tomyeh <tomyeh@zkoss.org>
2007-03-21 12:56:13 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@2599 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zcommon/src/org/zkoss/lang/JVMs.java
@@ -0,0 +1,66 @@
+/* JVMs.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Wed Mar 21 20:29:16     2007, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.lang;
+
+/**
+ * Utilities of Java Virtual Machine.
+ *
+ * @author tomyeh
+ */
+public class JVMs {
+	private JVMs() {}
+
+	private static final int _major, _minor;
+	private static final boolean _j5;
+	static {
+		final String s = System.getProperty("java.version");
+		System.out.println(s);
+		int major = 0, minor = 0;
+		if (s != null) {
+			final int j = s.indexOf('.'), k = s.indexOf('.', j + 1);
+			if (k > j) {
+				try {
+					major = Integer.parseInt(s.substring(0, j));
+					minor = Integer.parseInt(s.substring(j + 1, k));
+				} catch (Throwable ex) { //ignore
+					System.err.println("Warning: unable to parse java.versin: "+s);
+				}
+			}
+		}
+		_major = major;
+		_minor = minor;
+		_j5 = (_major == 1 && _minor >= 5) || _major > 1;
+	}
+
+	/** Returns whether JVM is 5.0 or above
+	 */
+	public static final boolean isJava5() {
+		return _j5;
+	}
+	/** Returns the major version.
+	 */
+	public static final int getMajorVersion() {
+		return _major;
+	}
+	/** Returns the minor version.
+	 */
+	public static final int getMinorVersion() {
+		return _minor;
+	}
+}
