f5f2bccabdec0e79388890ca392813d39b4e118e
tomyeh <tomyeh@zkoss.org>
2007-04-19 08:53:08 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@2796 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkdemo/src/org/zkoss/zkdemo/test/DumbFailoverManager.java
@@ -0,0 +1,100 @@
+/* DumbFailoverManager.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Apr 19 14:36:14     2007, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zkdemo.test;
+
+import java.util.Iterator;
+
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.Desktop;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.sys.WebAppCtrl;
+import org.zkoss.zk.ui.sys.DesktopCtrl;
+import org.zkoss.zk.ui.sys.PageCtrl;
+import org.zkoss.zk.ui.sys.FailoverManager;
+import org.zkoss.zk.ui.impl.PageImpl;
+
+/**
+ * A dumb failover manager useless but to test the failover mechanism.
+ *
+ * <p>To test, call {@link #dropDesktop} first to remove a desktop.
+ * And, then clicks or enters something on the client to see
+ * whether it behaves as usual.
+ *
+ * <p>It is usually tested with test/failover.zul.
+ *
+ * @author tomyeh
+ */
+public class DumbFailoverManager implements FailoverManager {
+	private Desktop _killed;
+
+	/** Drops the current desktop.
+	 *
+	 * @param recoverable whether the dropped desktop is recovable.
+	 */
+	public void dropDesktop(boolean recoverable) {
+		final Desktop desktop = Executions.getCurrent().getDesktop();
+		((WebAppCtrl)desktop.getWebApp())
+			.getDesktopCache(desktop.getSession())
+			.removeDesktop(desktop);
+		if (recoverable)
+			_killed = desktop;
+	}
+
+	//FailoverManager//
+	public boolean isRecoverable(Session sess, String desktopId) {
+		return _killed != null && desktopId.equals(_killed.getId());
+	}
+	public void recover(Session sess, Execution exec, Desktop desktop) {
+		System.out.println("Recover "+_killed.getId());
+
+		//recover desktop
+		((DesktopCtrl)desktop).setId(_killed.getId()); //required
+		desktop.setCurrentDirectory(_killed.getCurrentDirectory()); //optional
+		desktop.setClientType(_killed.getClientType()); //optional
+			//optional: copy _killed's attributes to desktop
+
+		//recover pages
+		for (Iterator it = _killed.getPages().iterator(); it.hasNext();)
+			recover((Page)it.next());
+		_killed = null;
+	}
+	private static void recover(Page killed) {
+		final Page page = new PageImpl(
+			killed.getLanguageDefinition(), //required; never null
+			killed.getComponentDefinitionMap(), //If unknown, just pass null
+			killed.getRequestPath(), //If unknown, just pass null
+			killed.getZScriptLanguage()); //If unkown, just pass null (Java assumed)
+		((PageCtrl)page).init(
+			killed.getId(), //required; never null
+			killed.getTitle(), //if unknown, just pass null
+			killed.getStyle(), //if unknown, just pass null
+			((PageCtrl)killed).getHeaders(), //if unknown, just pass null
+			killed.getUuid()); //required; never null
+		//optional: copy killed's attrs to page
+
+		for (Iterator it = killed.getRoots().iterator(); it.hasNext();) {
+			final Component comp = (Component)it.next();
+			((Component)comp.clone()).setPage(page);
+		}
+	}
+}
