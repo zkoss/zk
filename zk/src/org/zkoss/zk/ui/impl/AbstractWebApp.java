f0c98410c0af6b86dd5b7e13b7c4fdcef8270e1b
tomyeh <tomyeh@zkoss.org>
2007-06-07 10:07:00 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3182 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/impl/AbstractWebApp.java
+++ zk/src/org/zkoss/zk/ui/impl/AbstractWebApp.java
@@ -66,7 +66,7 @@ abstract public class AbstractWebApp implements WebApp, WebAppCtrl {
 	}
 
 	public final String getVersion() {
-		return "2.4.0-FL";
+		return "2.4.0";
 	}
 
 	public final Configuration getConfiguration() {
