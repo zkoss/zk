aaa00c270c0f7cd78c0961822e945a1e1546476c
tomyeh <tomyeh@zkoss.org>
2007-07-05 06:32:50 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3391 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/boot.js
+++ zk/src/archive/web/js/zk/html/boot.js
@@ -48,7 +48,7 @@ if (!window.Boot_progressbox) { //not customized
 /////
 // zk
 zk = {};
-zk.build = "6N"; //increase this if we want the browser to reload JavaScript
+zk.build = "6O"; //increase this if we want the browser to reload JavaScript
 zk.voidf = Prototype.emptyFunction;
 
 /** Browser info. */
