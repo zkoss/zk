fcce30238c4bdaded9fec76c3857df70dab56a5a
tomyeh <tomyeh@zkoss.org>
2007-07-06 04:02:32 +0000

    Bug 1748680
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3410 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/au/AuCmd.java
+++ zk/src/org/zkoss/zk/au/AuCmd.java
@@ -26,14 +26,21 @@ import org.zkoss.zk.ui.Component;
  *
  * <p>data[0]: the component UUID
  * data[1]: the client function name (i.e., JavaScript function)<br/>
- * data[2]: the first argument<br/>
- * data[3]: the second argument...
+ * data[2]: the second argument<br/>
+ * data[3]: the third argument...
+ *
+ * <p>Note: the first argument is always the component itself.
  * 
  * @author tomyeh
  * @since 2.4.1
  */
 public class AuCmd extends AuResponse {
-	/** Construct AuCmd to call a client function without argument.
+	/** Construct AuCmd to call a client function with one argument,
+	 * the component itself.
+	 *
+	 * <p>In other words, it invokes (assume Type is the comp's z.type)<br/>
+	 * <code>zkType.function(comp)</code>
+	 *
 	 * @param comp the component that this script depends on.
 	 * It cannot be null.
 	 * @param function the function name
@@ -41,33 +48,48 @@ public class AuCmd extends AuResponse {
 	public AuCmd(Component comp, String function) {
 		super("cmd", comp, new String[] {comp.getUuid(), function});
 	}
-	/** Construct AuCmd to call a client function with one argument.
+	/** Construct AuCmd to call a client function with two arguments,
+	 * the component itself and arg.
+	 *
+	 * <p>In other words, it invokes (assume Type is the comp's z.type)<br/>
+	 * <code>zkType.function(comp, arg)</code>
+	 *
 	 * @param comp the component that this script depends on.
 	 * It cannot be null.
 	 * @param function the function name
-	 * @param arg
+	 * @param arg the additional argument
 	 */
 	public AuCmd(Component comp, String function, String arg) {
 		super("cmd", comp,
 			new String[] {comp.getUuid(), function, arg});
 	}
-	/** Construct AuCmd to call a client function with two arguments.
+	/** Construct AuCmd to call a client function with two arguments,
+	 * the component itself, arg1 and arg2.
+	 *
+	 * <p>In other words, it invokes (assume Type is the comp's z.type)<br/>
+	 * <code>zkType.function(comp, arg1, arg2)</code>
+	 *
 	 * @param comp the component that this script depends on.
 	 * It cannot be null.
 	 * @param function the function name
 	 */
-	public AuCmd(Component comp, String function, String arg0, String arg1) {
+	public AuCmd(Component comp, String function, String arg1, String arg2) {
 		super("cmd", comp, new String[] {comp.getUuid(), function,
-			arg0, arg1});
+			arg1, arg2});
 	}
-	/** Construct AuCmd to call a client function with three arguments.
+	/** Construct AuCmd to call a client function with two arguments,
+	 * the component itself, arg1, arg2 and arg3.
+	 *
+	 * <p>In other words, it invokes (assume Type is the comp's z.type)<br/>
+	 * <code>zkType.function(comp, arg1, arg2, arg3)</code>
+	 *
 	 * @param comp the component that this script depends on.
 	 * It cannot be null.
 	 * @param function the function name
 	 */
-	public AuCmd(Component comp, String function, String arg0, String arg1,
-	String arg2) {
+	public AuCmd(Component comp, String function, String arg1, String arg2,
+	String arg3) {
 		super("cmd", comp, new String[] {comp.getUuid(), function,
-			arg0, arg1, arg2});
+			arg1, arg2, arg3});
 	}
 }
