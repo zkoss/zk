f17b74fe9cb73c16d5335e099effde5e520a972f
tomyeh <tomyeh@zkoss.org>
2007-07-20 04:17:56 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3471 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul2jsp/src/org/zkoss/zul/jsp/PageTag.java
@@ -0,0 +1,30 @@
+/* PageTag.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Jul 20 11:32:56     2007, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zul.jsp;
+
+import org.zkoss.zul.jsp.impl.AbstractTag;
+import org.zkoss.zul.jsp.impl.Utils;
+
+/**
+ * Defines a ZK page.
+ *
+ * @author tomyeh
+ */
+public class PageTag extends AbstractTag {
+}
---
+++ zul2jsp/src/org/zkoss/zul/jsp/impl/Utils.java
@@ -0,0 +1,28 @@
+/* Utils.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Jul 20 11:33:21     2007, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zul.jsp.impl;
+
+/**
+ * Utilities for implementing JSP tags for ZK components.
+ *
+ * @author tomyeh
+ */
+public class Utils {
+	private Utils() {} //prevent from instantiated
+}
