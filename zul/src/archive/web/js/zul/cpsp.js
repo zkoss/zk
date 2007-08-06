e41f1c4057d4438c4c4899266a1244d3b27c1e6a
tomyeh <tomyeh@zkoss.org>
2007-08-06 11:45:29 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3614 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/cpsp.js
@@ -0,0 +1,22 @@
+/* cpsp.js
+
+{{IS_NOTE
+	Purpose:
+		Client-Polling-based Server Push
+	Description:
+		
+	History:
+		Mon Aug  6 14:26:09     2007, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+zkCpsp = {};
+
+zkCpsp.start = function (dtid) {
+};
+zkCpsp.stop = function (dtid) {
+};
