e5f762fca0283f64a49dd792b717304248d3f365
jebberwocky <jebberwocky@potix.com>
2007-08-10 12:08:51 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3690 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/SimpleTreeModel.java
+++ zul/src/org/zkoss/zul/SimpleTreeModel.java
@@ -31,12 +31,20 @@ import org.zkoss.zul.event.TreeDataEvent;
  */
 public class SimpleTreeModel extends AbstractTreeModel{
 	
+	/** Constructor.
+	 *
+	 * @param root - the root of tree
 	
-	
+	 * However, it is not a good idea to modify <code>node</code>
+	 * once it is passed to this method with live is true,
+	 * since {@link Tree} is not smart enough to hanle it.
+	 * @since 2.4.1
+	 */
 	public SimpleTreeModel(Object obj){
 		super.setRoot(obj);
 	}
 	
+	//-- TreeModel --//
 	public int getChildCount(Object parent) {
 		if(isLeaf(parent))
 			return -1;
@@ -45,7 +53,8 @@ public class SimpleTreeModel extends AbstractTreeModel{
 			return al.size();
 		}
 	}
-
+	
+	//-- TreeModel --//
 	public boolean isLeaf(Object node) {
 		boolean isLeaf =!(node instanceof ArrayList);
 		if(!isLeaf){
@@ -53,16 +62,25 @@ public class SimpleTreeModel extends AbstractTreeModel{
 		}
 		return isLeaf;
 	}
-
+	
+	//-- TreeModel --//
 	public Object getChild(Object parent, int index) {
 		ArrayList al = (ArrayList)parent;
 		return al.get(index);
 	}
-
+	
+	//-- AbstractTreeModel --//
 	public Object getRoot() {
 		return super.getRoot();
 	}
 	
+	/**
+	 * Modify the node which parent is <code>parent</code> with index <code>index</code> by
+	 *  value
+	 * @param parent The parent of node is modified
+	 * @param index The index of node is modified
+	 * @param value The new value of node if modified
+	 */
 	public void set(Object parent, int index, Object value)
 	{
 		ArrayList al = (ArrayList)parent;
@@ -70,6 +88,11 @@ public class SimpleTreeModel extends AbstractTreeModel{
 		fireEvent(parent,index,TreeDataEvent.CONTENTS_CHANGED);
 	}
 	
+	/**
+	 * remove the node which parent is <code>parent</code> with index <code>index</code>
+	 * @param parent The parent of node is removed
+	 * @param index The index of node is removed
+	 */
 	public void remove(Object parent, int index){
 		ArrayList al = (ArrayList)parent;	
 		try{
@@ -82,12 +105,25 @@ public class SimpleTreeModel extends AbstractTreeModel{
 		
 	}
 	
+	/**
+	 * append a new node which parent is <code>parent</code>
+	 * by new node <code>newNode</code>
+	 * @param parent The parent of node is appended
+	 * @param newNode New node which is appended
+	 */
 	public void add(Object parent, Object newNode){
 		ArrayList al = (ArrayList)parent;
 		al.add(newNode);
 		fireEvent(parent,al.size()-1,TreeDataEvent.NODE_ADDED);
 	}
 	
+	/**
+	 * insert new node which parent is <code>parent</code> with index <code>index</code>
+	 * by new node <code>newNode</code>
+	 * @param parent The parent of node is inserted
+	 * @param index The index of node is inserted
+	 * @param newNode New node which is inserted
+	 */
 	public void insert(Object parent, int index, Object newNode){
 		ArrayList al = (ArrayList)parent;
 		al.add(index, newNode);
