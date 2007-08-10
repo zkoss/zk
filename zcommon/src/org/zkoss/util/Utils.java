b8180fdb1b3df080e588a665001f362e908bfcfb
tomyeh <tomyeh@zkoss.org>
2007-08-10 17:41:20 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3691 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zcommon/src/org/zkoss/util/Utils.java
+++ zcommon/src/org/zkoss/util/Utils.java
@@ -32,8 +32,11 @@ public class Utils {
 	 * (2 in "2.4.0"), and getSubversion(1) returns the so-called
 	 * minor version (4 in "2.4.0").
 	 *
+	 * <p>Note: alpha is consider as -500, beta -300, and rc -100.
+	 * Moreover, beta3 is -497 (= -500 + 3) and rc5 -95 (-100 + 5)
+	 *
 	 * @param version the version. The version is assumed to
-	 * a series of integer separated by a non-digit separator.
+	 * a series of integer separated by a non-alphanemric separator.
 	 * @param portion which portion of the version; starting from 0.
 	 * If you want to retrieve the major verion, specify 0.
 	 * @since 2.5.0
@@ -48,17 +51,40 @@ public class Utils {
 			j = nextVerSeparator(version, j) + 1;
 			if (j >= len) return 0; //no such portion
 		}
+
+		String s = version.substring(j, nextVerSeparator(version, j));
 		try {
-			return Integer.parseInt(
-				version.substring(j, nextVerSeparator(version, j)));
-		} catch (Throwable ex) {
-			return 0; //unknow
+			return Integer.parseInt(s);
+		} catch (Throwable ex) { //eat
 		}
+
+		s = s.toLowerCase();
+		final int base;
+		if (s.startsWith("rc")) {
+			base = -100;
+			j = 2;
+		} else if (s.startsWith("beta")) {
+			base = -300;
+			j = 4;
+		} else if (s.startsWith("alpha")) {
+			base = -500;
+			j = 5;
+		} else {
+			return 0; //unknown
+		}
+		if (j < s.length()) {
+			try {
+				return base + Integer.parseInt(s.substring(j));
+			} catch (Throwable ex) { //eat
+			}
+		}
+		return base;
 	}
 	private static final int nextVerSeparator(String version, int from) {
 		for (final int len = version.length(); from < len; ++from) {
 			final char cc = version.charAt(from);
-			if (cc < '0' || cc > '9')
+			if ((cc < '0' || cc > '9') && (cc < 'a' || cc > 'z')
+			&& (cc < 'A' || cc > 'Z'))
 				break;
 		}
 		return from;
