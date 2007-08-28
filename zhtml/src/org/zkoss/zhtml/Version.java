7ea5d74d6a93ccc396fba566b07e1aac17fe97a5
tomyeh <tomyeh@zkoss.org>
2007-08-28 04:38:14 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@3916 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zhtml/src/org/zkoss/zhtml/Version.java
+++ zhtml/src/org/zkoss/zhtml/Version.java
@@ -17,8 +17,9 @@ Copyright (C) 2006 Potix Corporation. All Rights Reserved.
 package org.zkoss.zhtml;
 
 /**
- * Specified this in lang.xml, such that ZK knows what lang.xml is associated
- * with ZHTML.
+ * The version of the ZK ZHTML component set.
+ * {@link #UID} must be the same as the version specified in lang.xml
+ * and config.xml.
  * 
  * @author tomyeh
  */
