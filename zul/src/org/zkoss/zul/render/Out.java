5e3cf1b03c45bbab036c421424c33c007f89eba2
jumperchen <jumperchen@potix.com>
2007-09-07 10:35:43 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@4149 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/render/Out.java
+++ zul/src/org/zkoss/zul/render/Out.java
@@ -1,4 +1,4 @@
-/* RenderFns.java
+/* Out.java
 
  {{IS_NOTE
  Purpose:
@@ -28,133 +28,124 @@ import org.zkoss.xml.XMLs;
  * @author jumperchen
  * 
  */
-public class RenderFns {
-	protected RenderFns() {
+public class Out {
+	private String _value = null;
+
+	private int _maxlength = 0;
+
+	private boolean _escapeXML = true;
+
+	private boolean _nbsp = false;
+
+	private Writer _writer;
+
+	public Out(Writer writer) {
+		_writer = writer;
 	}
 
-	public static final Out getOut(Writer writer) throws IOException {
-		return new RenderFns().new Out(writer);
+	/**
+	 * Returns whether to escape XML. Default: true.
+	 */
+	public boolean getEscapeXML() {
+		return _escapeXML;
 	}
 
-	class Out {
-		private String _value = null;
+	/**
+	 * Sets whether to escape XML.
+	 */
+	public Out setEscapeXML(boolean escapeXML) {
+		_escapeXML = escapeXML;
+		return this;
+	}
 
-		private int _maxlength = 0;
+	/**
+	 * Returns whether to generate &amp;nbsp; if the content is empty.
+	 * Default: true.
+	 */
+	public boolean getNbsp() {
+		return _escapeXML;
+	}
 
-		private boolean _escapeXML = true;
+	/**
+	 * Sets whether to generate &amp;nbsp; if the content is empty.
+	 */
+	public Out setNbsp(boolean nbsp) {
+		_nbsp = nbsp;
+		return this;
+	}
 
-		private boolean _nbsp = false;
+	/**
+	 * Returns the value. Default: null.
+	 */
+	public String getValue() {
+		return _value;
+	}
 
-		private Writer _writer;
+	/**
+	 * Sets the value.
+	 */
+	public Out setValue(String value) {
+		_value = value;
+		return this;
+	}
 
-		public Out(Writer writer) {
-			_writer = writer;
+	/**
+	 * Returns the maxlength of bytes to output.
+	 * <p>
+	 * Default: 0 (no limit).
+	 */
+	public int getMaxlength() {
+		return _maxlength;
+	}
+
+	/**
+	 * Sets the maxlength to output.
+	 */
+	public Out setMaxlength(int maxlength) {
+		_maxlength = maxlength;
+		return this;
+	}
+
+	// -- Action --//
+	public void render() throws IOException {
+
+		int len = _value != null ? _value.length() : 0;
+		if (len == 0 || (_nbsp && _value.trim().length() == 0)) {
+			if (_nbsp)
+				_writer.write("&nbsp;");
+			return;
 		}
 
-		/**
-		 * Returns whether to escape XML. Default: true.
-		 */
-		public boolean getEscapeXML() {
-			return _escapeXML;
+		String value = _value;
+		if (_maxlength > 0 && len > _maxlength) {
+			int j = _maxlength;
+			while (j > 0 && Character.isWhitespace(value.charAt(j - 1)))
+				--j;
+			value = value.substring(0, j) + "...";
 		}
 
-		/**
-		 * Sets whether to escape XML.
-		 */
-		public Out setEscapeXML(boolean escapeXML) {
-			_escapeXML = escapeXML;
-			return this;
-		}
+		if (_escapeXML) {
+			StringBuffer sb = null;
+			len = value.length();
+			for (int j = 0; j < len; ++j) {
+				final char cc = value.charAt(j);
+				final String replace = _escapeXML ? XMLs.escapeXML(cc)
+						: null;
 
-		/**
-		 * Returns whether to generate &amp;nbsp; if the content is empty.
-		 * Default: true.
-		 */
-		public boolean getNbsp() {
-			return _escapeXML;
-		}
-
-		/**
-		 * Sets whether to generate &amp;nbsp; if the content is empty.
-		 */
-		public Out setNbsp(boolean nbsp) {
-			_nbsp = nbsp;
-			return this;
-		}
-
-		/**
-		 * Returns the value. Default: null.
-		 */
-		public String getValue() {
-			return _value;
-		}
-
-		/**
-		 * Sets the value.
-		 */
-		public Out setValue(String value) {
-			_value = value;
-			return this;
-		}
-
-		/**
-		 * Returns the maxlength of bytes to output.
-		 * <p>
-		 * Default: 0 (no limit).
-		 */
-		public int getMaxlength() {
-			return _maxlength;
-		}
-
-		/**
-		 * Sets the maxlength to output.
-		 */
-		public Out setMaxlength(int maxlength) {
-			_maxlength = maxlength;
-			return this;
-		}
-
-		// -- Action --//
-		public void render() throws IOException {
-
-			int len = _value != null ? _value.length() : 0;
-			if (len == 0 || (_nbsp && _value.trim().length() == 0)) {
-				if (_nbsp)
-					_writer.write("&nbsp;");
-				return;
-			}
-
-			String value = _value;
-			if (_maxlength > 0 && len > _maxlength) {
-				int j = _maxlength;
-				while (j > 0 && Character.isWhitespace(value.charAt(j - 1)))
-					--j;
-				value = value.substring(0, j) + "...";
-			}
-
-			if (_escapeXML) {
-				StringBuffer sb = null;
-				len = value.length();
-				for (int j = 0; j < len; ++j) {
-					final char cc = value.charAt(j);
-					final String replace = _escapeXML ? XMLs.escapeXML(cc)
-							: null;
-
-					if (replace != null) {
-						if (sb == null) {
-							sb = new StringBuffer(value.length() + 10);
-							sb.append(value.substring(0, j));
-						}
-						sb.append(replace);
-					} else if (sb != null) {
-						sb.append(cc);
+				if (replace != null) {
+					if (sb == null) {
+						sb = new StringBuffer(value.length() + 10);
+						sb.append(value.substring(0, j));
 					}
+					sb.append(replace);
+				} else if (sb != null) {
+					sb.append(cc);
 				}
-				if (sb != null)
-					value = sb.toString();
 			}
-
-			_writer.write(value);
+			if (sb != null)
+				value = sb.toString();
 		}
+
+		_writer.write(value);
 	}
 }
