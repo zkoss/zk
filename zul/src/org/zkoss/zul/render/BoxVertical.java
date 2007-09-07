6f87d9f679573a1f138022ce549f57b089e7c2b8
robbiecheng <robbiecheng@potix.com>
2007-09-07 12:21:26 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@4166 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/render/BoxVertical.java
+++ zul/src/org/zkoss/zul/render/BoxVertical.java
@@ -25,7 +25,6 @@ import java.util.Iterator;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.ComponentRenderer;
 import org.zkoss.zul.Box;
-import org.zkoss.zul.fn.ZulFns;
 
 /**
  * {@link Box}'s vertical mold.
@@ -38,7 +37,10 @@ public class BoxVertical implements ComponentRenderer {
 <c:set var="self" value="${requestScope.arg.self}"/>
 <table id="${self.uuid}" z.type="zul.box.Box"${self.outerAttrs}${self.innerAttrs} cellpadding="0" cellspacing="0">
 	<c:forEach var="child" items="${self.children}">
-	<tr id="${child.uuid}!chdextr"${u:getBoxChildOuterAttrs(child)}><td${u:getBoxChildInnerAttrs(child)}>${z:redraw(child, null)}</td></tr>
+	<tr id="${child.uuid}!chdextr"${u:getBoxChildOuterAttrs(child)}>
+	<td${u:getBoxChildInnerAttrs(child)}>
+	${z:redraw(child, null)}
+	</td></tr>
 	</c:forEach>
 </table>
 	 */
@@ -50,16 +52,15 @@ public class BoxVertical implements ComponentRenderer {
 		
 		wh.write("<table id=\"").write(uuid).write("\" ");
 		wh.write("z.type=\"zul.box.Box\"").write(self.getOuterAttrs()).write(self.getInnerAttrs());
-		wh.write(" cellpadding=\"0\" cellspacing=\"0\">");
-		wh.write("<tr valign=\"").write(self.getValign()).write("\" id=\"").write(uuid).write("\">");
+		wh.write(" cellpadding=\"0\" cellspacing=\"0\">");		
 		for (Iterator it = self.getChildren().iterator(); it.hasNext();) {
 			final Component child = (Component)it.next();
-			wh.write("<tr valign=\"").write("\" id=\"").write(uuid).write("!chdextr\">");
-			wh.write("<td id=\"").write(child.getUuid()).write("!chdextr\"");
-			wh.write(ZulFns.getBoxChildOuterAttrs(child));
-			wh.write(ZulFns.getBoxChildInnerAttrs(child));
+			wh.write("<tr id=\"").write(uuid).write("!chdextr\"");
+			wh.write(self.getChildOuterAttrs(child)).write(">");
+			wh.write("<td");			
+			wh.write(self.getChildInnerAttrs(child));
 			wh.write(">");
-			child.redraw(out);
+			wh.write(child);
 			wh.write("</td></tr>");
 		}		
 		wh.writeln("</table>");
