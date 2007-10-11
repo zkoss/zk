9c722fbc94a7e2e2858d48a6e80815853744f50f
jumperchen <jumperchen@potix.com>
2007-10-11 08:05:12 +0000

    add some rendering classes for ZK Layout
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@4517 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkmax/src/org/zkoss/zkmax/zul/render/LayoutregionDefault.java
@@ -0,0 +1,72 @@
+/* LayoutregionDefault.java
+
+ {{IS_NOTE
+ Purpose:
+ 
+ Description:
+ 
+ History:
+ Oct 11, 2007 11:23:05 AM , Created by jumperchen
+ }}IS_NOTE
+
+ Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+ {{IS_RIGHT
+ This program is distributed under GPL Version 2.0 in the hope that
+ it will be useful, but WITHOUT ANY WARRANTY.
+ }}IS_RIGHT
+ */
+package org.zkoss.zkmax.zul.render;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.render.ComponentRenderer;
+import org.zkoss.zk.ui.render.SmartWriter;
+import org.zkoss.zulex.LayoutRegion;
+
+/**
+ * {@link LayoutRegion}'s default mold.
+ * 
+ * @author jumperchen
+ * @since 3.0.0
+ * 
+ */
+public class LayoutregionDefault implements ComponentRenderer {
+
+	public void render(Component comp, Writer out) throws IOException {
+		final SmartWriter wh = new SmartWriter(out);
+		final LayoutRegion self = (LayoutRegion) comp;
+		final Execution exec = Executions.getCurrent();
+		wh.write("<div id=\"").write(self.getUuid()).write('"')
+			.write(" z.type=\"zulex.layout.LayoutRegion\">").write("<div id=\"")
+			.write(self.getUuid()).write("!real\"").write(self.getOuterAttrs())
+			.write(self.getInnerAttrs()).write(">").write("<div id=\"").write(self.getUuid())
+			.write("!cave\" class=\"layout-region-body\">").writeChildren(self)
+			.write("</div></div>");
+		if (!self.getPosition().equals("center")) {
+			wh.write("<div id=\"").write(self.getUuid()).write("!split\" class=\"layout-split ");
+			if (self.getPosition().equals("north")
+					|| self.getPosition().equals("south"))
+				wh.write("layout-split-v");
+			if (self.getPosition().equals("west")
+					|| self.getPosition().equals("west"))
+				wh.write("layout-split-h");
+			wh.write("\"><img id=\"").write(self.getUuid()).write(
+					"!splitbtn\" class=\"layout-split-button\" src=\"");
+			if (self.getPosition().equals("north"))
+				wh.write(exec.encodeURL("~./zulex/img/layout/colps-t.png"));
+			else if (self.getPosition().equals("south"))
+				wh.write(exec.encodeURL("~./zulex/img/layout/colps-b.png"));
+			else if (self.getPosition().equals("west"))
+				wh.write(exec.encodeURL("~./zulex/img/layout/colps-l.png"));
+			else wh.write(exec.encodeURL("~./zulex/img/layout/colps-r.png"));
+			wh.write("\"/></div>");
+		}
+		wh.write("</div>");
+	}
+
+}
