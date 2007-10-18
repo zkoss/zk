317dbcdcf4158912af40b2408f3b613c35f2e4c1
tomyeh <tomyeh@zkoss.org>
2007-10-18 10:34:33 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@4660 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/au/out/AuPopup.java
+++ zul/src/org/zkoss/zul/au/out/AuPopup.java
@@ -16,7 +16,7 @@ Copyright (C) 2007 Potix Corporation. All Rights Reserved.
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
-package org.zkoss.zul.au;
+package org.zkoss.zul.au.out;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.au.AuResponse;
