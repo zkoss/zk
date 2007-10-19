0160141f4a5f1639510d3ddbe63e47aac3af46ba
tomyeh <tomyeh@zkoss.org>
2007-10-19 07:32:38 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@4682 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/flash.js
+++ zul/src/archive/web/js/zul/flash.js
@@ -20,43 +20,29 @@ zkFlash = {};
 
 zkFlash.setAttr = function (cmp, name, value) {
 		
-	switch(name)
-	{
-		case "style.height":
-		{
-			//$e(cmp.id+"!obj").height = value;
-			$e(cmp.id).height = value;
-			$e(cmp.id+"!emb").height = value;
-			return true;
-		}
-		case "style.width":
-		{
-			//$e(cmp.id+"!obj").width = value;
-			$e(cmp.id).width = value;
-			$e(cmp.id+"!emb").width = value;
-			return true;
-		}
-		case "z:play":
-		{
-			$e(cmp.id+"!emb").play = value;
-			return true;
-		}
-		case "z:loop":
-		{
-			$e(cmp.id+"!emb").loop = value;
-			return true;
-		}
-		case "z:wmode":
-		{
-			$e(cmp.id+"!emb").wmode = value;
-			return true;
-		}
-		case "z:bgcolor":
-		{
-			$e(cmp.id+"!emb").bgcolor = value;
-			return true;
-		}
-		
+	switch(name) {
+	case "style.height":
+		//$e(cmp.id+"!obj").height = value;
+		$e(cmp.id).height = value;
+		$e(cmp.id+"!emb").height = value;
+		return true;
+	case "style.width":
+		//$e(cmp.id+"!obj").width = value;
+		$e(cmp.id).width = value;
+		$e(cmp.id+"!emb").width = value;
+		return true;
+	case "z:play":
+		$e(cmp.id+"!emb").play = value;
+		return true;
+	case "z:loop":
+		$e(cmp.id+"!emb").loop = value;
+		return true;
+	case "z:wmode":
+		$e(cmp.id+"!emb").wmode = value;
+		return true;
+	case "z:bgcolor":
+		$e(cmp.id+"!emb").bgcolor = value;
+		return true;
 	}
     return false;
 
