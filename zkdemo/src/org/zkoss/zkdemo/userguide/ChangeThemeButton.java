2603198c2a0b04ee2cd270cc478cdb863745007c
dennischen <atticcat@gmail.com>
2007-11-02 03:09:03 +0000

    add font size theme demo
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@5091 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkdemo/src/org/zkoss/zkdemo/userguide/ChangeThemeButton.java
@@ -0,0 +1,52 @@
+/* ChangeThemeButton.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Nov 2, 2007 9:08:20 AM     2007, Created by Dennis.Chen
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zkdemo.userguide;
+
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Radiogroup;
+
+/**
+ * @author Dennis.Chen
+ *
+ */
+public class ChangeThemeButton extends Button{
+	
+	
+	public void onCreate(){
+		Radiogroup frg = (Radiogroup)getFellow("frg");
+		Execution exe = Executions.getCurrent();
+		String fs = FontSizeThemeProvider.getFontSizeCookie(exe);
+		if("lg".equals(fs)){
+			frg.setSelectedIndex(2);
+		}else if("sm".equals(fs)){
+			frg.setSelectedIndex(0);
+		}else{
+			frg.setSelectedIndex(1);
+		}
+	}
+	
+	public void onClick() {
+		Radiogroup frg = (Radiogroup)getFellow("frg");
+		Execution exe = Executions.getCurrent();
+		FontSizeThemeProvider.setFondSizeCookie(exe,frg.getSelectedItem().getValue());
+		exe.sendRedirect("/userguide/index.zul?id=mi9");
+	}
+}
