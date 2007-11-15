855f1a96c6efee0437e3961db2e626a83fa09f60
jumperchen <jumperchen@potix.com>
2007-11-15 08:46:24 +0000

    Bug #1832214
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@5386 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkex/src/org/zkoss/zkex/zul/LayoutRegion.java
+++ zkex/src/org/zkoss/zkex/zul/LayoutRegion.java
@@ -23,6 +23,7 @@ import org.zkoss.xml.HTMLs;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.UiException;
 import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.ext.client.Openable;
 import org.zkoss.zul.impl.XulElement;
 import org.zkoss.zul.impl.Utils;
 
@@ -350,5 +351,19 @@ public abstract class LayoutRegion extends XulElement {
 		HTMLs.appendAttribute(sb, "z.mins", getMinsize());
 		HTMLs.appendAttribute(sb, "z.autoscl", isAutoscroll());
 		return sb.toString();
+	}
+	//-- ComponentCtrl --//
+	protected Object newExtraCtrl() {
+		return new ExtraCtrl();
+	}
+	/** A utility class to implement {@link #getExtraCtrl}.
+	 * It is used only by component developers.
+	 */
+	protected class ExtraCtrl extends XulElement.ExtraCtrl
+	implements Openable {
+		//-- Openable --//
+		public void setOpenByClient(boolean open) {
+			_open = open;
+		}
 	}
 }
