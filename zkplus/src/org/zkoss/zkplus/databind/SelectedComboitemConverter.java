ff26fc761aa6cc3e5162b8cddc2408f866756e28
jumperchen <jumperchen@potix.com>
2008-01-10 10:01:30 +0000

    Feature #1796281 and #1598234
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6044 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/org/zkoss/zkplus/databind/SelectedComboitemConverter.java
+++ zkplus/src/org/zkoss/zkplus/databind/SelectedComboitemConverter.java
@@ -18,7 +18,14 @@
  */
 package org.zkoss.zkplus.databind;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.UiException;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.event.SelectEvent;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Comboitem;
 import org.zkoss.zul.ListModel;
@@ -29,7 +36,6 @@ import org.zkoss.zul.ListModel;
  * @since 3.0.2
  */
 public class SelectedComboitemConverter implements TypeConverter {
-
 	/**
 	 * @since 3.0.2
 	 */
@@ -44,11 +50,41 @@ public class SelectedComboitemConverter implements TypeConverter {
 	}
 	
 	/**
-	 * Unimplemented
 	 * @since 3.0.2
 	 */
 	public Object coerceToUi(Object val, Component comp) {
-		return null;
+		final Combobox cbbox = (Combobox) comp;
+	  	if (val != null) {
+	  		final ListModel xmodel = cbbox.getModel();
+	  		if (xmodel instanceof BindingListModel) {
+	  			final BindingListModel model = (BindingListModel) xmodel;
+	  			int index = model.indexOf(val);
+	  			if (index >= 0) {
+	    			final Comboitem item = (Comboitem) cbbox.getItemAtIndex(index);
+	    			final int selIndex = cbbox.getSelectedIndex();
+	    			
+	    			//We need this to support load-when:onSelect when first load 
+					//the page in (so it is called only once).
+	  				if (item != null && selIndex != index) { // bug 1647817, avoid endless-loop
+	    				Set items = new HashSet();
+	    				items.add(item);
+	    				Events.postEvent(new SelectEvent("onSelect", cbbox, items, item));
+	    			}		
+	  				return item;
+	  			}
+	  		} else if (xmodel == null) { //no model case, assume Comboitem.value to be used with selectedItem
+	  			//iterate to find the selected item assume the value (select mold)
+	  			for (final Iterator it = cbbox.getItems().iterator(); it.hasNext();) {
+	  				final Comboitem li = (Comboitem) it.next();
+	  				if (val.equals(li.getValue())) {
+	  					return li;
+	  				}
+	  			}
+	  		} else {
+	  			throw new UiException("model of the databind combobox "+cbbox+" must be an instanceof of org.zkoss.zkplus.databind.BindingListModel." + xmodel);
+	  		}
+	  	}
+	  	return null;
 	}
 
 }
