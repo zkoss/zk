df62c2f8c270eda14d1bae4876ea076172293f72
tomyeh <tomyeh@zkoss.org>
2008-01-22 06:40:24 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6254 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/org/zkoss/zul/Attributes.java
@@ -0,0 +1,31 @@
+/* Attributes.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Jan 22 12:51:42     2008, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2008 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zul;
+
+/**
+ * Common attributes used for implementation.
+ *
+ * @author tomyeh
+ * @since 3.0.2
+ */
+/*package*/ class Attributes {
+	/** Indicates the component being render shall generate z.skipsib.
+	 */
+	/*package*/ static final String SKIP_SIBLING = "org.zkoss.zul.SkipSibling";
+}
