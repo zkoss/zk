f963a284d1950f175e01e9c0b613fabdb371197d
tomyeh <tomyeh@zkoss.org>
2008-01-31 09:43:39 +0000

    Russian and Ukrainian translations by Denis Yarkovoy
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6446 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/lang/msgzul_ru.js
@@ -0,0 +1,8 @@
+/* mesg.js "charset=utf-8"
+ * created by Denis Yarkovoy (dyarkovoy@aruna.com.ua) 30 January 2008
+ * Copyright (C) 2005,2008 Potix Corporation
+ */
+msgzul={};
+msgzul.UNKNOWN_TYPE="Неизвестный тип компонента: ";
+msgzul.DATE_REQUIRED="Необходимо указать дату. Формат: ";
+msgzul.NO_AUDIO_SUPPORT="Ваш браузер не поддерживает динамическое аудио"
