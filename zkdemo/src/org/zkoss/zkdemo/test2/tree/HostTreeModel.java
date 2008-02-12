26035318207052a25fa90919fa01d2182b1a118f
jebberwocky <jebberwocky@potix.com>
2008-02-12 04:17:45 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6535 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkdemo/src/org/zkoss/zkdemo/test2/tree/HostTreeModel.java
+++ zkdemo/src/org/zkoss/zkdemo/test2/tree/HostTreeModel.java
@@ -36,7 +36,7 @@ public class HostTreeModel extends AbstractTreeModel{
 	 * @param tree the list is contained all data of nodes.
 	 */
 	public HostTreeModel(){
-		super(new FakeRoot("Room1",5));
+		super(new FakeRoot("Root",5));
 		fakeRoot = (FakeRoot)this.getRoot();
 	}
 	
@@ -55,11 +55,32 @@ public class HostTreeModel extends AbstractTreeModel{
 		return ((TreeNode)node).isLeaf();
 	}
 	
-	public void updateProcessType(int hostindex,int processindex,String type){
-		FakeHost host = (FakeHost)fakeRoot.getChild(hostindex);
+	public void updateProcessType(int groupindex,int hostindex,int processindex,String type){
+		FakeGroup group = (FakeGroup)fakeRoot.getChild(groupindex);
+		FakeHost host = (FakeHost)group.getChild(hostindex);
 		FakeProcess process = (FakeProcess)host.getChild(processindex);
 		process.type = type;
+		process.name ="AAAAA";		
 		this.fireEvent(host,processindex, processindex, TreeDataEvent.CONTENTS_CHANGED);
+
+	}
+	
+	public void addProcessType(int groupindex,int hostindex,int processindex,String type){
+		FakeGroup group = (FakeGroup)fakeRoot.getChild(groupindex);
+		FakeHost host = (FakeHost)group.getChild(hostindex);
+		String id = "p1000_10000";
+		FakeProcess process = new FakeProcess(id, id, type);
+		host.addProcess(process);
+		this.fireEvent(host,processindex, processindex, TreeDataEvent.INTERVAL_ADDED);
+	}
+	
+	public void removeProcessType(int groupindex,int hostindex,int processindex,String type){
+		FakeGroup group = (FakeGroup)fakeRoot.getChild(groupindex);
+		FakeHost host = (FakeHost)group.getChild(hostindex);
+		FakeProcess process = (FakeProcess)host.getChild(processindex);
+		process.type = type;	
+		this.fireEvent(host,processindex, processindex, TreeDataEvent.INTERVAL_REMOVED);
+
 	}
 	
 	
@@ -72,12 +93,47 @@ public class HostTreeModel extends AbstractTreeModel{
 	static public class FakeRoot implements TreeNode{
 		String name;
 		int count;
-		ArrayList hosts = new ArrayList();
+		ArrayList groups = new ArrayList();
 		public FakeRoot(String name,int count){
 			this.name = name;
 			this.count = count;
 			for(int i=0;i<count;i++){
-				hosts.add(new FakeHost("host"+i,"host-"+i,i,10));
+				groups.add(new FakeGroup("Group"+i,i,4));
+			}
+		}
+		
+		public String getName(){
+			return name;
+		}
+		
+		public FakeGroup[] getGroups(){
+			return (FakeGroup[])groups.toArray(new FakeGroup[0]); 
+		}
+
+		public Object getChild(int index) {
+			return groups.get(index);
+		}
+
+		public int getChildCount() {
+			return groups.size();
+		}
+
+		public boolean isLeaf() {
+			return groups.size()==0?true:false;
+		}
+		
+	}
+	
+	
+	static public class FakeGroup implements TreeNode{
+		String name;
+		int count;
+		ArrayList hosts = new ArrayList();
+		public FakeGroup(String name,int index,int count){
+			this.name = name;
+			this.count = count;
+			for(int i=0;i<count;i++){
+				hosts.add(new FakeHost("host"+index+"_"+i,"host-"+i,i,10));
 			}
 		}
 		
@@ -144,6 +200,9 @@ public class HostTreeModel extends AbstractTreeModel{
 			return processes.size()==0?true:false;
 		}
 		
+		public void addProcess(FakeProcess fp){
+			processes.add(fp);
+		}
 	}
 	
 	static public class FakeProcess  implements TreeNode{
--- zkdemo/src/org/zkoss/zkdemo/test2/tree/HostTreeRenderer.java
+++ zkdemo/src/org/zkoss/zkdemo/test2/tree/HostTreeRenderer.java
@@ -28,7 +28,7 @@ import org.zkoss.zul.Treerow;
  * @author Dennis.Chen
  *
  */
-public class HostIconTreeRenderer implements TreeitemRenderer {
+public class HostTreeRenderer implements TreeitemRenderer {
 
 
 	public void render(Treeitem treeitem, Object data) throws Exception {
@@ -40,28 +40,12 @@ public class HostIconTreeRenderer implements TreeitemRenderer {
 			row = treeitem.getTreerow(); 
 			row.getChildren().clear();
 		}
-		if(data instanceof HostTreeModel.FakeRoot){
-			treeitem.getTreerow().appendChild(new Treecell(((HostTreeModel.FakeRoot)data).getName()));
-		}else if(data instanceof HostTreeModel.FakeGroup){
+		if(data instanceof HostTreeModel.FakeGroup){
 			treeitem.getTreerow().appendChild(new Treecell(((HostTreeModel.FakeGroup)data).getName()));
 		}else if(data instanceof HostTreeModel.FakeHost){
-			HostTreeModel.FakeHost host = ((HostTreeModel.FakeHost)data);
-			Treecell cell = new Treecell(host.getName());
-			cell.setImage("/img/add.gif");
-			cell.setId(host.getId());
-			treeitem.getTreerow().appendChild(cell);
+			treeitem.getTreerow().appendChild(new Treecell(((HostTreeModel.FakeHost)data).getName()));
 		}else if(data instanceof HostTreeModel.FakeProcess){
-			HostTreeModel.FakeProcess process = ((HostTreeModel.FakeProcess)data);
-			Treecell cell = new Treecell(process.getName());
-			if(process.getType().equals("A")){
-				cell.setImage("/img/caldrbtn.gif");
-			}else if(process.getType().equals("B")){
-				cell.setImage("/img/bandbtn.gif");
-			}else if(process.getType().equals("C")){
-				cell.setImage("/img/upload.gif");
-			}
-			cell.setId(process.getId());
-			treeitem.getTreerow().appendChild(cell);
+			treeitem.getTreerow().appendChild(new Treecell(((HostTreeModel.FakeProcess)data).getName()));
 		}
 		
 		treeitem.setOpen(false);// set open false to show correct icon.
