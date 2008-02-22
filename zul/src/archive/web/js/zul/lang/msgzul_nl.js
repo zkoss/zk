5da054249a0002d41b0ed5979d0f692417efd6fa
jumperchen <jumperchen@potix.com>
2008-02-22 03:26:23 +0000

    Feature #1899307
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6755 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/lang/msgzul_nl.js
@@ -0,0 +1,22 @@
+/* mesg.js "charset=utf-8"
+
+{{IS_NOTE
+  Purpose:
+
+  Description:
+
+  History:
+    Feb 15, 2008, Wido Jansen
+}}IS_NOTE
+
+Copyright (C) 2006 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+  This program is distributed under GPL Version 2.0 in the hope that
+  it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.UNKNOWN_TYPE = "Onbekende component: ";
+msgzul.DATE_REQUIRED = "U moet een datum ingeven. Formaat: ";
+msgzul.NO_AUDIO_SUPPORT = "Uw browser ondersteunt geen geluidsweergave";
