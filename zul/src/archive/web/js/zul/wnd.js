9e504aeae9c405fed0ecb079896e15a9743848c1
tomyeh <tomyeh@zkoss.org>
2008-03-03 11:01:48 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6840 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/wnd.js
+++ zul/src/archive/web/js/zul/wnd.js
@@ -52,10 +52,11 @@ zkWnd.init = function (cmp) {
 	
 	//Bug #1840866
 	zkWnd._initMode(cmp);
-		// Bug #1830668 we have to invoke initMode later. 
 		// But, for a Sun's bug, we need to invoke initMode directly to prevent 
-		// that the outline of page is gone. However, the #1830668 bug seems to 
-		// disappear in ZK 3.0.4 version.
+		// that the outline of page is gone.
+		// Note: we fixed bug #1830668 bug by using addInitLater to invoke
+		// _initMode later, but, with ZK 3.0.4, the problem is already resolved
+		// without using addInitLater
 };
 zkWnd.cleanup = function (cmp) {
 	zkWnd.setSizable(cmp, false);
