922b23837bd2128d4d91536a9c62470d9827c4f3
jumperchen <jumperchen@potix.com>
2008-03-05 11:50:55 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6895 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/boot.js
+++ zk/src/archive/web/js/zk/html/boot.js
@@ -66,7 +66,7 @@ if (!window.Boot_progressbox) { //not customized
 /////
 // zk
 zk = {};
-zk.build = "8v"; //increase this if we want the browser to reload JavaScript
+zk.build = "8w"; //increase this if we want the browser to reload JavaScript
 zk.voidf = Prototype.emptyFunction;
 zk.booting = true; //denote ZK is booting
 
