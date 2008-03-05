8ee860280332c494e1aad64a3f0576176dac9661
tomyeh <tomyeh@zkoss.org>
2008-03-05 04:29:58 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@6865 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zkdemo/src/org/zkoss/zkdemo/test2/B1906892.java
@@ -0,0 +1,28 @@
+/* B1906892.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Wed Mar  5 11:36:59     2008, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2008 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zkdemo.test2;
+
+/**
+ * Used to retrieve special characters.
+ *
+ * @author tomyeh
+ */
+public class B1906892 {
+	public static final char C05 = (char)5;
+}
