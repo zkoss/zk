76eae1c88a56e5ebb126d257cb10b4c9e84b14d0
jumperchen <jumperchen@potix.com>
2008-04-10 03:17:09 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@7722 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/lang/msgzul_zh.js
@@ -0,0 +1,22 @@
+/* mesg_zh.js "charset=utf-8"
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Jul  8 14:25:41     2005, Created by tomyeh
+}}IS_NOTE
+
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.UNKNOWN_TYPE = "未知元件類型：";
+msgzul.DATE_REQUIRED = "只能輸入日期。格式：";
+msgzul.NO_AUDIO_SUPPORT = "你的瀏覽器不支援動態音效";
