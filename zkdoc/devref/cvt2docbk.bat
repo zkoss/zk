57c48443b2316b5ad622fe1c0728d5a78397f282
robbiecheng <robbiecheng@potix.com>
2008-04-18 07:15:47 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@7853 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkdoc/devref/cvt2docbk.bat
+++
@@ -1,3 +0,0 @@
-REM Remember to include full path with directory
-FOR /F %%G IN ('DIR /b %CD%\*.odt') DO soffice -invisible macro:///Standard.Module1.SaveAsDocbook(%CD%\%%G)
-FOR /F %%G IN ('DIR /b %CD%\ch4\*.odt') DO soffice -invisible macro:///Standard.Module1.SaveAsDocbook(%CD%\ch4\%%G)
