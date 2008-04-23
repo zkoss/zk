b67b66669cbb026b69595d47bf7776ea9fc02aa7
jumperchen <jumperchen@potix.com>
2008-04-23 00:59:55 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@7905 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/lang/mesg_es.js
+++ zk/src/archive/web/js/zk/html/lang/mesg_es.js
@@ -43,4 +43,3 @@ mesg.CANCEL="Cancelar";
 mesg.FAILED_TO_LOAD="Fallo al cargar ";
 mesg.FAILED_TO_LOAD_DETAIL="Puede ser a causa de un tráfico erroneo. Podria refrescar de nuevo la página.";
 mesg.CAUSE="Causa: ";
-
