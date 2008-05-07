5ff2434465df31248b069fcaac1721614c50d222
jumperchen <jumperchen@potix.com>
2008-05-07 07:21:45 +0000

    Bug  1958774: cannot create tree with all treeitem branches opened
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@8145 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/Tree.java
+++ zul/src/org/zkoss/zul/Tree.java
@@ -1167,7 +1167,7 @@ public class Tree extends XulElement {
 			ti.setParent(parent);
 			Object childNode = _model.getChild(node, i);
 			renderer.render(ti, childNode);
-			if(!_model.isLeaf(childNode)){	
+			if(!_model.isLeaf(childNode) && ti.getTreechildren() == null){	
 				Treechildren tc = new Treechildren();
 				tc.setParent(ti);
 			}
