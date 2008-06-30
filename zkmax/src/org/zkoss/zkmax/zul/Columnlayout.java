5e30f3231ba03ce4f9918e600f8f0acd5bbb7bd6
liwn <liwn@potix.com>
2008-06-30 04:59:24 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@8732 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkmax/src/org/zkoss/zkmax/zul/Columnlayout.java
+++ zkmax/src/org/zkoss/zkmax/zul/Columnlayout.java
@@ -25,7 +25,7 @@ import org.zkoss.zk.ui.sys.ComponentCtrl;
 
 /**
  * A columnlayout lays out a container which can have multiple columns, and each
- * column may contain no more than one panel.<br>
+ * column may contain one or more panel.<br>
  * Use Columnlayout need assign width (either present or pixel) on every
  * Columnchildren, or we cannot make sure about layout look.
  * 
