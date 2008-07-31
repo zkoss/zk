05a103b480e5732d4d3b83a26fc191fa8a8e07de
tomyeh <tomyeh@zkoss.org>
2008-07-31 09:37:20 +0000

    Feature 2033582
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@8974 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/impl/PageImpl.java
+++ zk/src/org/zkoss/zk/ui/impl/PageImpl.java
@@ -855,11 +855,8 @@ public class PageImpl implements Page, PageCtrl, java.io.Serializable {
 				//set first to avoid dead loop if script calls interpret again
 
 			final String script = _langdef.getInitScript(zslang);
-			if (script != null) {
-				_ns.setVariable("log", _zklog, true);
-				_ns.setVariable("page", this, true);
+			if (script != null)
 				ip.interpret(script, _ns);
-			}
 
 			//evaluate deferred zscripts, if any
 			try {
@@ -1148,9 +1145,10 @@ public class PageImpl implements Page, PageCtrl, java.io.Serializable {
 	}
 	private final static Set _nonSerNames = new HashSet();
 	static {
-		final String[] nms = {"log", "page", "desktop", "pageScope", "desktopScope",
+		final String[] nms = {
+			"log", "page", "desktop", "pageScope", "desktopScope",
 			"applicationScope", "requestScope", "spaceOwner",
-		"session", "sessionScope"};
+			"session", "sessionScope", "execution"};
 		for (int j = 0; j < nms.length; ++j)
 			_nonSerNames.add(nms[j]);
 	}
