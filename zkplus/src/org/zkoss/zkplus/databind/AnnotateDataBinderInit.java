9665abf09f12505e0adcf9bd5d3c9350637d3bd0
henrichen <henrichen@zkoss.org>
2008-08-21 02:24:42 +0000

    2045810 AnnotatedDataBinderInit accept relative path in arg0
    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@9084 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/org/zkoss/zkplus/databind/AnnotateDataBinderInit.java
+++ zkplus/src/org/zkoss/zkplus/databind/AnnotateDataBinderInit.java
@@ -21,28 +21,35 @@ import org.zkoss.zk.ui.Path;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.UiException;
 import org.zkoss.zk.ui.util.Initiator;
+import org.zkoss.zk.ui.util.InitiatorExt;
 
 /**
  * <p>This initiator class do following things:</p>
  * <ol>
  * <li>new an {@link AnnotateDataBinder} instance.</li>
- * <li>set the AnnotateDataBinder instance as a variable "binder" in the ID Space of the root 
- *  component as specified in arg0 "component-path".(if arg0 is not specified, use Page instead.)</li>
- * <li>call {@link AnnotateDataBinder#loadAll()} to initiate all UI components from the associated data bean.</li>
+ * <li>set the AnnotateDataBinder instance as a variable with the name "binder" 
+ * stored in the component as specified in arg0 "component-path".(if arg0 is not 
+ * specified, use Page instead.)</li>
+ * <li>call {@link AnnotateDataBinder#loadAll()} to initiate all UI components 
+ * from the associated data bean.</li>
  * </ol>
  * <p>Put the init PI as follows:</p>
  * <pre>
  * &lt;?init class="org.zkoss.zkplus.databind.AnnotateDataBinderInit" [arg0="component|component-path"] [arg1="true|false"]?>
  * </pre>
- * <p>Where the arg0 is the component itself (via EL expression) or the component path that specifies the component the AnnotateDataBinder covers. If the 
- * arg0 is not specified or set to string "page", the AnnotateDataBinder will default to cover the whole page.</p>
+ * <p>Where the arg0 is the component itself (via EL expression) or the component path that 
+ * specifies the component the AnnotateDataBinder covers. You can use absolute path that 
+ * starts with "//" (search from Desktop) or "/" (search from Page); or you can use relative
+ * path(supported since ZK 3.0.8) that starts with "./" or "../" (relative to the Id Space of 
+ * the root components of the zul page). If the arg0 is not specified or set to string "page", 
+ * the AnnotateDataBinder will default to cover the whole page.</p>
  * <p>Where the arg1 is used to decide whether to load default binding configuration defined in lang-addon.xml. 
  * If the arg1 is not specified it is default to true. Note that you have to specify arg0 if you want to specify arg1.</p>
  *
  * @author Henri Chen
  * @see AnnotateDataBinder
  */
-public class AnnotateDataBinderInit implements Initiator {
+public class AnnotateDataBinderInit implements Initiator, InitiatorExt {
 	private Component _comp;
 	private String _compPath;
 	private String _defaultConfig;
@@ -52,23 +59,7 @@ public class AnnotateDataBinderInit implements Initiator {
 	
 	//-- Initator --//
 	public void doAfterCompose(Page page) {
-		boolean b = _defaultConfig != null ? Boolean.valueOf(_defaultConfig).booleanValue() : true;
-		if (_comp instanceof Component) {
-			_binder = new AnnotateDataBinder(_comp, b);
-			_comp.setVariable("binder", _binder, true);
-		} else if (_compPath == null || "page".equals(_compPath)) {
-			_binder = new AnnotateDataBinder(page, b);
-			page.setVariable("binder", _binder);
-		} else if (_compPath.startsWith("/")) {
-			final Component comp = Path.getComponent(_compPath);
-			_binder = new AnnotateDataBinder(comp, b);
-			comp.setVariable("binder", _binder, true);
-		} else {
-			final Component comp = page.getFellow(_compPath);
-			_binder = new AnnotateDataBinder(comp, b);
-			comp.setVariable("binder", _binder, true);
-		}
-		_binder.loadAll(); //load data bean properties into UI components
+		//will not be called since we implements InitiatorExt
 	}
  	public boolean doCatch(java.lang.Throwable ex) {
  		return false; // do nothing
@@ -92,4 +83,46 @@ public class AnnotateDataBinderInit implements Initiator {
 			_defaultConfig = (String)args[1];
 		}
 	}
+	
+	//-- InitiatorExt --//
+	public void doAfterCompose(Page page, Component[] comps) throws Exception {
+		boolean b = _defaultConfig != null ? Boolean.valueOf(_defaultConfig).booleanValue() : true;
+		if (_comp instanceof Component) { //a specified component instance
+			_binder = new AnnotateDataBinder(_comp, b);
+			_comp.setVariable("binder", _binder, true);
+		} else if (_compPath == null || "page".equals(_compPath)) { //page
+			_binder = new AnnotateDataBinder(page, b);
+			if (page.getVariable("binder") != null) { //already a binder on the page
+				throw new UiException("Page is already covered by another Data Binder. Cannot be covered by this Data Binder again. Page:"+page.getId());
+			} else {
+				page.setVariable("binder", _binder);
+			}
+		} else if (_compPath.startsWith("/")) { //absolute path
+			final Component comp = Path.getComponent(_compPath);
+			if (comp == null) {
+				throw new UiException("Cannot find the specified component. Absolute Path:"+_compPath);
+			}
+			_binder = new AnnotateDataBinder(comp, b);
+			comp.setVariable("binder", _binder, true);
+		} else if (_compPath.startsWith("./") || _compPath.startsWith("../")) { //relative path
+			for (int j = 0; j < comps.length; ++j) {
+				final Component vroot = comps[j];
+				final Component comp = Path.getComponent(vroot.getSpaceOwner(), _compPath);
+				if (comp != null) { //found
+					_binder = new AnnotateDataBinder(comp, b);
+					comp.setVariable("binder", _binder, true);
+					break;
+				}
+			}
+			if (_binder == null) {
+				throw new UiException("Cannot find the specified component. Relative Path:"+_compPath);
+			}
+		} else {
+			final Component comp = page.getFellow(_compPath);
+			_binder = new AnnotateDataBinder(comp, b);
+			comp.setVariable("binder", _binder, true);
+		}
+		_binder.loadAll(); //load data bean properties into UI components
+	}
+
 }
