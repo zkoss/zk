8c8e4161420b8c9d8d610edebfd43911510a0a3d
henrichen <henrichen@zkoss.org>
2008-08-27 08:06:08 +0000

    2077792 GenericAutowireComposer shall support implicit objects "arg"
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@9146 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/util/GenericAutowireComposer.java
+++ zk/src/org/zkoss/zk/ui/util/GenericAutowireComposer.java
@@ -149,6 +149,160 @@ abstract public class GenericAutowireComposer extends GenericComposer {
 	 * @since 3.0.7
 	 */
 	protected Execution execution;
+/* GenericAutowireComposer.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Jun 11, 2008 10:56:06 AM, Created by henrichen
+}}IS_NOTE
+
+Copyright (C) 2008 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zk.ui.util;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import org.zkoss.idom.Document;
+import org.zkoss.lang.Classes;
+import org.zkoss.xel.VariableResolver;
+import org.zkoss.zk.au.AuResponse;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Components;
+import org.zkoss.zk.ui.Desktop;
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.IdSpace;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.UiException;
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.metainfo.PageDefinition;
+import org.zkoss.zk.xel.Evaluator;
+
+/**
+ * <p>An abstract composer that you can extend and write intuitive onXxx 
+ * event handler methods with "auto-wired" accessible variable objects such
+ * as implicit objects, components, and external resolvable variables in a ZK 
+ * zuml page; this class will registers onXxx events to the supervised 
+ * component and wire all accessible variable objects to this composer by 
+ * calling setXxx() method or set xxx field value directly per the variable 
+ * name. Since 3.0.7, this composer has wired all implicit objects 
+ * such as self, spaceOwner, page, desktop, session, application, 
+ * componentScope, spaceScope, pageScope, desktopScope, sessionScope, 
+ * applicationScope, and requestScope, so you can use them directly. Besides 
+ * that, it also provides alert(String message) method, so you can call alert() 
+ * without problems.</p>
+ * 
+ * <p>Notice that since this composer kept references to the components, single
+ * instance composer object cannot be shared by multiple components.</p>
+ *  
+ * <p>The following is an example. The onOK event listener is registered into 
+ * the target window, and the Textbox component with id name "mytextbox" is
+ * injected into the "mytextbox" field automatically (so you can use 
+ * mytextbox variable directly in onOK).</p>
+ * 
+ * <pre><code>
+ * MyComposer.java
+ * 
+ * public class MyComposer extends GenericAutowireComposer {
+ *     private Textbox mytextbox;
+ *     
+ *     public void onOK() {
+ *         mytextbox.setValue("Enter Pressed");
+ *         alert("Hi!");
+ *     }
+ * }
+ * 
+ * test.zul
+ * 
+ * &lt;window id="mywin" apply="MyComposer">
+ *     &lt;textbox id="mytextbox"/>
+ * &lt;/window>
+ * </code></pre>
+ * 
+ * @author henrichen
+ * @since 3.0.6
+ * @see org.zkoss.zk.ui.Components#wireFellows
+ */
+abstract public class GenericAutowireComposer extends GenericComposer {
+	/** Implicit Object; the applied component itself. 
+	 * @since 3.0.7
+	 */ 
+	protected Component self;
+	/** Implicit Object; the space owner of the applied component.
+	 * @since 3.0.7
+	 */
+	protected IdSpace spaceOwner;
+	/** Implicit Object; the page.
+	 * @since 3.0.7
+	 */
+	protected Page page;
+	/** Implicit Object; the desktop.
+	 * @since 3.0.7
+	 */
+	protected Desktop desktop;
+	/** Implicit Object; the session.
+	 * @since 3.0.7
+	 */
+	protected Session session;
+	/** Implicit Object; the web application.
+	 * @since 3.0.7
+	 */
+	protected WebApp application;
+	/** Implicit Object; a map of attributes defined in the applied component.
+	 * @since 3.0.7
+	 */
+	protected Map componentScope;
+	/** Implicit Object; a map of attributes defined in the ID space contains the applied component.
+	 * @since 3.0.7
+	 */
+	protected Map spaceScope;
+	/** Implicit Object; a map of attributes defined in the page.
+	 * @since 3.0.7
+	 */
+	protected Map pageScope;
+	/** Implicit Object; a map of attributes defined in the desktop.
+	 * @since 3.0.7
+	 */
+	protected Map desktopScope;
+	/** Implicit Object; a map of attributes defined in the session.
+	 * @since 3.0.7
+	 */
+	protected Map sessionScope;
+	/** Implicit Object; a map of attributes defined in the web application.
+	 * @since 3.0.7
+	 */
+	protected Map applicationScope;
+	/** Implicit Object; a map of attributes defined in the request.
+	 * @since 3.0.7
+	 */
+	protected Map requestScope;
+	/** Implicit Object; the current execution.
+	 * @since 3.0.7
+	 */
+	protected Execution execution;
+	/** Implicit Object; the arg argument passed to the createComponents method. It is never null.
+	 * @since 3.0.8
+	 */
+	protected Map arg;
 	
 	/**
 	 * Auto wire accessible variables of the specified component into a 
@@ -175,7 +329,8 @@ abstract public class GenericAutowireComposer extends GenericComposer {
 		applicationScope = application.getAttributes();
 		requestScope = REQUEST_SCOPE;
 		execution = EXECUTION;
-		
+		arg = desktop.getExecution().getArg();
+
 		//wire variables to reference fields
 		Components.wireVariables(comp, this);
 	}
