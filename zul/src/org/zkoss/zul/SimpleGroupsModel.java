cc8d855ecec16c684edeb8cb79ac90f91c3b3c55
dennischen <atticcat@gmail.com>
2008-09-02 12:22:21 +0000

    simplegroupsmodel
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@9282 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/org/zkoss/zul/SimpleGroupsModel.java
@@ -0,0 +1,142 @@
+/* SimpleGroupsModel.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Sep 2, 2008 9:50:12 AM     2008, Created by Dennis.Chen
+}}IS_NOTE
+
+Copyright (C) 2007 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zul;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.zkoss.zul.event.GroupsDataEvent;
+
+/**
+ * A simple implementation of {@link GroupsModel}.
+ * Note: It assumes the content is immutable.
+ * @author Dennis.Chen
+ * @since 3.5.0
+ * @see GroupsModel
+ */
+public class SimpleGroupsModel extends AbstractGroupsModel implements GroupsModelExt{
+	
+	/**
+	 * member field to store group data
+	 */
+	protected Object[][] _data;
+	
+	/**
+	 * member field to store group head data
+	 */
+	protected Object[] _gheads;
+	
+	/**
+	 * member field to store group foot data
+	 */
+	protected Object[] _gfoots;
+	
+	/**
+	 * Constructor.
+	 * When using this constructor , {@link #getGroup(int)} will return the corresponding Object[] of data. 
+	 * {@link #hasGroupfoot(int)} will always false 
+	 * @param data a 2 dimension array to represent groups data
+	 */
+	public SimpleGroupsModel(Object[][] data){
+		this(data,null,(Object[])null);
+	}
+	
+	/**
+	 * Constructor
+	 * When using this constructor , 
+	 * {@link #getGroup(int)} will return the corresponding Object depends on heads. 
+	 * {@link #hasGroupfoot(int)} will always return false
+	 * @param data a 2 dimension array to represent groups data
+	 * @param heads an array to represent head data of group
+	 */
+	public SimpleGroupsModel(Object[][] data,Object[] heads){
+		this(data,heads,(Object[])null);
+	}
+	
+	/**
+	 * Constructor
+	 * When using this constructor , 
+	 * {@link #getGroup(int)} will return the corresponding Object depends on heads.  
+	 * The return value of {@link #hasGroupfoot(int)} and {@link #getGroupfoot(int)} 
+	 * are depends on foots. 
+	 *   
+	 * @param data a 2 dimension array to represent groups data
+	 * @param heads an array to represent head data of group
+	 * @param foots an array to represent foot data of group, if an element in this array is null, then 
+	 * {@link #hasGroupfoot(int)} will return false in corresponding index.
+	 */
+	public SimpleGroupsModel(Object[][] data,Object[] heads,Object[] foots){
+		if (data == null)
+			throw new NullPointerException();
+		_data = data;
+		_gheads = heads;
+		_gfoots = foots;
+	}
+
+	public Object getChild(int groupIndex, int index) {
+		return _data[groupIndex][index];
+	}
+
+
+	public int getChildCount(int groupIndex) {
+		return _data[groupIndex].length;
+	}
+
+
+	public Object getGroup(int groupIndex) {
+		return  _gheads==null?_data[groupIndex]:_gheads[groupIndex];
+	}
+
+
+	public int getGroupCount() {
+		return _data.length;
+	}
+
+	public Object getGroupfoot(int groupIndex) {
+		return _gfoots == null ? null:_gfoots[groupIndex];
+	}
+
+	public boolean hasGroupfoot(int groupIndex) {
+		return _gfoots == null ? false:_gfoots[groupIndex]!=null;
+	}
+
+
+	/**
+	 * Do nothing in default implementation, however developer can override it to 
+	 * re-group by manipulating {@link #_data},{@link #_gheads},{@link #_gfoots}
+	 */
+	public void group(Comparator cmpr, boolean ascending, int colIndex) {
+	}
+
+	/**
+	 * Sort each data in each group by Comparator, developer could override {@link #sortGroupItem(Object, Object[], Comparator, boolean, int)
+	 * to customize.
+	 */
+	public void sort(Comparator cmpr, boolean ascending, int colIndex) {
+		for(int i=0;i<_data.length;i++){
+			sortGroupItem(_gheads==null?_data[i]:_gheads[i],_data[i],cmpr,ascending,colIndex);
+		}
+		fireEvent(GroupsDataEvent.GROUPS_CHANGED,-1,0,_data.length-1);
+	}
+
+	protected void sortGroupItem(Object group,Object[] items,Comparator cmpr,boolean ascending, int colIndex){
+		Arrays.sort(items,cmpr);
+	}
+
+}
