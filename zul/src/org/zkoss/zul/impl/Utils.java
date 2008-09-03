490ed323da76cf3d5a434a24aecfb678b807c4eb
tomyeh <tomyeh@zkoss.org>
2008-09-03 10:36:52 +0000

    finetune
    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@9334 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/impl/Utils.java
+++ zul/src/org/zkoss/zul/impl/Utils.java
@@ -157,7 +157,12 @@ public class Utils {
 			sb.append('/');
 			boolean bExtRequired = true;
 			if (name != null && name.length() != 0) {
-				sb.append(name.replace('\\', '/'));
+				name = name.replace('\\', '/');
+				if (name.charAt(0) == '/') {
+					name = name.substring(1);
+					if (name.length() == 0) name = "ua";
+				}
+				sb.append(name);
 				bExtRequired = name.lastIndexOf('.') < 0;
 			} else {
 				sb.append(comp.getId());
