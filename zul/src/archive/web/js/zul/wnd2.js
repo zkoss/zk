10c8f59144f6604b17d9ca150d0d1eb5c2126291
jumperchen <jumperchen@potix.com>
2008-09-08 02:24:59 +0000

    fine tune.
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@9430 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/wnd2.js
+++ zul/src/archive/web/js/zul/wnd2.js
@@ -521,11 +521,11 @@ zkWnd2.setAttr = function (cmp, nm, val) {
 				zkWnd2.syncShadow(cmp);
 				return true;
 			}
-		} else 
-			zkau.setAttr(cmp, nm, val);
-			if (!zkWnd2._embedded(cmp))
-				zkWnd2.syncShadow(cmp);
-			return true;
+		}
+		zkau.setAttr(cmp, nm, val);
+		if (!zkWnd2._embedded(cmp))
+			zkWnd2.syncShadow(cmp);
+		return true;
 	case "z.maximized":
 		zkau.setAttr(cmp, nm, val);
 		if (getZKAttr(cmp, "minimized") == "true") {
