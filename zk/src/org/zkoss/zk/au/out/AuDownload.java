ee79fbd6ca2f682168bd56b56687b24d7a963c3e
tomyeh <tomyeh@zkoss.org>
2008-09-24 04:07:42 +0000

    Bug 2114380
    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@9596 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/au/out/AuDownload.java
+++ zk/src/org/zkoss/zk/au/out/AuDownload.java
@@ -19,6 +19,7 @@ Copyright (C) 2007 Potix Corporation. All Rights Reserved.
 package org.zkoss.zk.au.out;
 
 import org.zkoss.zk.au.AuResponse;
+import org.zkoss.zk.ui.util.DeferredValue;
 
 /**
  * A response to ask the client to download the specified URI.
@@ -29,10 +30,25 @@ import org.zkoss.zk.au.AuResponse;
  * @since 3.0.0
  */
 public class AuDownload extends AuResponse {
-	/**
+	/** Constructs with an encoded URL.
+	 * To work with WebSphere 5.1 and some other old server, it is
+	 * suggested to use {@link #AuDownload(DeferredValue)} and
+	 * then invoke encodeURL in the rendering phase.
 	 * @param url the URI of the file to download, never null.
 	 */
 	public AuDownload(String url) {
 		super("download", new String[] {url});
+	}
+	/** Constructs with a deferred URI.
+	 * To work with WebSphere 5.1 and some other old server, it is
+	 * suggested to use {@link #AuDownload(DeferredValue)} and
+	 * then invoke encodeURL in the rendering phase.
+	 *
+	 * @param deferredURI the URI but the value will be evaluated
+	 * later.
+	 * @since 3.0.8
+	 */
+	public AuDownload(DeferredValue deferredURI) {
+		super("download", deferredURI);
 	}
 }
--- zul/src/org/zkoss/zul/Filedownload.java
+++ zul/src/org/zkoss/zul/Filedownload.java
@@ -31,6 +31,7 @@ import org.zkoss.zk.ui.Desktop;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.sys.WebAppCtrl;
 import org.zkoss.zk.au.out.AuDownload;
+import org.zkoss.zk.ui.util.DeferredValue;
 
 /**
  * File download utilities.
@@ -53,21 +54,9 @@ public class Filedownload {
 	 */
 	public static void save(Media media, String flnm) {
 		final Desktop desktop = Executions.getCurrent().getDesktop();
-		if (flnm == null) flnm = media.getName();
-
-		final StringBuffer sb = new StringBuffer(32);
-		if (flnm != null && flnm.length() != 0) {
-			sb.append('/');
-			sb.append(flnm);
-			if (flnm.lastIndexOf('.') < 0) {
-				final String format = media.getFormat();
-				if (format != null)
-					sb.append('.').append(format);
-			}
-		}
-		final String uri = desktop.getDownloadMediaURI(media, sb.toString());
 		((WebAppCtrl)desktop.getWebApp())
-			.getUiEngine().addResponse(null, new AuDownload(uri));
+			.getUiEngine().addResponse(null,
+				new AuDownload(new DownloadURL(media, flnm))); //Bug 2114380
 	}
 	/** Open a download dialog to save the specified content at the client
 	 * with the suggested file name.
@@ -119,6 +108,7 @@ public class Filedownload {
 	}
 	/** Open a download dialog to save the specified file at the client.
 	 *
+	 * @param file the file to download to the client
 	 * @param contentType the content type, e.g., application/pdf.
 	 * Unlike other save methods, it is optional. If null, the file name's
 	 * extension is used to determine the content type.
@@ -166,5 +156,31 @@ public class Filedownload {
 		if (url == null)
 			throw new FileNotFoundException(path);
 		save(url, contentType);
+	}
+
+	private static class DownloadURL implements DeferredValue {
+		private final Media _media;
+		private final String _path;
+		private DownloadURL(Media media, String flnm) {
+			_media = media;
+
+			if (flnm == null) flnm = media.getName();
+
+			final StringBuffer sb = new StringBuffer(32);
+			if (flnm != null && flnm.length() != 0) {
+				sb.append('/');
+				sb.append(flnm);
+				if (flnm.lastIndexOf('.') < 0) {
+					final String format = media.getFormat();
+					if (format != null)
+						sb.append('.').append(format);
+				}
+			}
+			_path = sb.toString();
+		}
+		public String getValue() {
+			return Executions.getCurrent().getDesktop()
+				.getDownloadMediaURI(_media, _path);
+		}
 	}
 }
