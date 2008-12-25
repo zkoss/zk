556b468fa6edf99b02e6fb1777cf5e74bda84737
jumperchen <jumperchen@potix.com>
2008-12-25 06:16:34 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@10449 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/box/mold/hbox.js
+++ zul/src/archive/web/js/zul/box/mold/hbox.js
@@ -12,7 +12,7 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 This program is distributed under GPL Version 2.0 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
-function () {
+function (out) {
 	out.push('<table', this.domAttrs_(), zUtl.cellps0, '><tr');
 	
 	var	v = this.getAlign();
