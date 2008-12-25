4bcc1ff9f239d3741e8dc1f42674a2967041dc32
tomyeh <tomyeh@zkoss.org>
2008-12-25 01:22:36 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@10429 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/wgt/mold/div.js
+++ zul/src/archive/web/js/zul/wgt/mold/div.js
@@ -12,9 +12,9 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 	This program is distributed under GPL Version 2.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 */
-function () {
-	var html = '<div' + this.domAttrs_() + '>';
+function (out) {
+	out.push('<div', this.domAttrs_(), '>');
 	for (var w = this.firstChild; w; w = w.nextSibling)
-		html += w.redraw();
-	return html + '</div>';
+		w.redraw(out);
+	out.push('</div>');
 }
