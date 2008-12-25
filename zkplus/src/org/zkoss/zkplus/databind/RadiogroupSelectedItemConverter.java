9fff8b95c2ac20c33674f6d6da41bc3b91733c3a
henrichen <henrichen@zkoss.org>
2008-12-25 02:24:31 +0000

    bug 2464484: Binding selectedItem of Radiogroup causes StackOverflowExcep
    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@10434 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/org/zkoss/zkplus/databind/RadiogroupSelectedItemConverter.java
+++ zkplus/src/org/zkoss/zkplus/databind/RadiogroupSelectedItemConverter.java
@@ -40,7 +40,8 @@ public class RadiogroupSelectedItemConverter implements TypeConverter, java.io.S
 						return child;
 					}
 				} else if (!(child instanceof Radiogroup)) { //skip nested radiogroup
-					return coerceToUi(val, comp);
+					//bug 2464484
+					return coerceToUi(val, child);  //recursive
 				}
 			}
 		}
