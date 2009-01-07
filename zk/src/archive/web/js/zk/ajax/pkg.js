c12e12c0ffabe8cac1b074ddcec0a8b8be2b84af
tomyeh <tomyeh@zkoss.org>
2009-01-07 07:57:42 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@10538 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/ajax/pkg.js
+++ zk/src/archive/web/js/zk/ajax/pkg.js
@@ -17,19 +17,30 @@ zPkg = {
 
 	/** Called after the whole package is declared. */
 	end: function (pkg) {
-		if (zPkg._lding.$remove(pkg)) {
-			if (!zPkg._updCnt()) {
-				try {
-					zEvt.enableESC();
-					zUtl.destroyProgressbox("zk_loadprog");
-				} catch (ex) {
-				}
-
-				for (var fn, aflds = zPkg._aflds; fn = aflds.shift();)
-					fn();
-			}
-		} else //specified in lang.xml (or in HTML directly)
+		if (!zPkg._lding.$remove(pkg))
 			zPkg._lded[pkg] = true;
+			//specified in lang.xml (or in HTML directly)
+
+		var al2 = zPkg._afld2[pkg];
+		if (al2) {
+			delete zPkg._afld2[pkg];
+			for (var fn, aflds = zPkg._aflds; fn = al2.pop();)
+				aflds.unshift(fn);
+		}
+
+		if (!zPkg._updCnt()) {
+			try {
+				zEvt.enableESC();
+				zUtl.destroyProgressbox("zk_loadprog");
+			} catch (ex) {
+			}
+
+			for (var fn, aflds = zPkg._aflds; fn = aflds.shift();)
+				fn();
+		}
+	},
+	isLoaded: function (pkg) {
+		return zPkg._lded[pkg];
 	},
 	load: function (pkg, dt) {
 		if (!pkg) return;
@@ -39,9 +50,15 @@ zPkg = {
 
 		pkglds[pkg] = true;
 
+		var deps = zPkg._deps[pkg];
+		if (deps) {
+			delete zPkg._deps[pkg];
+			for (var pn; pn = deps.unshift();)
+				zPkg.load(pn);
+		}
+
 		//We don't use e.onload since Safari doesn't support t
-		//See also
-		//Bug 1815074: IE bug: zk.ald might be called before appendChild returns
+		//See also Bug 1815074
 
 		zPkg._lding.unshift(pkg);
 		if (zPkg._updCnt() == 1) {
@@ -68,6 +85,8 @@ zPkg = {
 	_lded: {'zk': true}, //loaded
 	_lding: [], //loading
 	_aflds: [],
+	_afld2: {},
+	_deps: {},
 
 	_ldmsg: function () {
 		var msg = '';
@@ -102,19 +121,32 @@ zPkg = {
 	setVersion: function (pkg, ver) {
 		zPkg._pkgVers[pkg] = ver;
 	},
+	depends: function (a, b) {
+		if (a && b) //a depends on b
+			if (zPkg._lded[a]) zPkg.load(b);
+			else {
+				var deps = zPkg._deps;
+				if (deps[a]) deps[a].push(b);
+				else deps[a] = [b];
+			}
+	},
 	_pkgVers: {}
 };
-zk.afterLoad = function () { //part of zk
-	var ret;
-	for (var len = arguments.length, j = 0; j < len; ++j) {
-		var arg = arguments[j];
-		if (typeof arg == 'string')
-			zPkg.load(arg);
-		else if (ret || zPkg.loading) {
-			zPkg._aflds.push(arg);
-			ret = true;
-		} else
-			arg();
+zk.afterLoad = function (a, b) { //part of zk
+	if (typeof a == 'string') {
+		if (!b) return;
+		if (zPkg._lded[a]) a = b;
+		else {
+			var al2 = zPkg._afld2;
+			if (al2[a]) al2[a].push(b);
+			else al2[a] = [b];
+			return;
+		}
 	}
-	return ret;
+
+	if (zPkg.loading) {
+		zPkg._aflds.push(a);
+		return true;
+	}
+	a();
 };
