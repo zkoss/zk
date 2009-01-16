727cdd6fea99461dc096be6a7993877f259834f2
henrichen <henrichen@zkoss.org>
2009-01-16 06:29:50 +0000

    feature 2512555 Converter convert date to String with annotated format enhance FieldComparator
    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@10632 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/FieldComparator.java
+++ zul/src/org/zkoss/zul/FieldComparator.java
@@ -19,6 +19,7 @@ Copyright (C) 2009 Potix Corporation. All Rights Reserved.
 
 package org.zkoss.zul;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
@@ -42,49 +43,46 @@ import org.zkoss.zk.ui.UiException;
 public class FieldComparator implements Comparator {
 	/** The field names collection. */
 	private Collection _fieldnames;
+	/** The cached field name string. */
+	private String _orderBy;
 	/** Ascending. */
 	private final boolean _asc;
-	/** Ignore case. */
-	private boolean _igcase;
 	/** Whether to treat null as the maximum value. */
 	private boolean _maxnull;
 	
-	/** Compares with the fields per the given field names.
-	 * <p>Note: It assumes the case-insensitive and deem null as minimum value.
-	 *  If not, use {@link #FieldComparator(String, boolean, boolean, boolean)}
+	/** Compares with the fields per the given "ORDER BY" clause.
+	 * <p>Note: It assumes null as minimum value.
+	 *  If not, use {@link #FieldComparator(String, boolean, boolean)}
 	 * instead.</p>
 	 * 
-	 * @param fieldnames the fields to be compared upon for the given object in {@link #compare}.
+	 * @param orderBy the "ORDER BY" clause to be compared upon for the given object in {@link #compare}.
 	 * @param ascending whether to sort as ascending (or descending).
 	 */
-	public FieldComparator(String fieldnames, boolean ascending) {
-		this(fieldnames, ascending, true, false);
+	public FieldComparator(String orderBy, boolean ascending) {
+		this(orderBy, ascending, false);
 	}
 	
-	/** Compares with the fields per the given field names.
+	/** Compares with the fields per the given "ORDER BY" clause.
 	 *
-	 * @param fieldnames the fields to be compared upon for the given object in {@link #compare}.
+	 * @param orderBy the "ORDER BY" clause to be compared upon for the given object in {@link #compare}.
 	 * @param ascending whether to sort as ascending (or descending).
-	 * @param ignoreCase whether to sort case-insensitive
 	 * @param nullAsMax whether to consider null as the maximum value.
 	 * If false, null is considered as the minimum value.
 	 */
-	public FieldComparator(String fieldnames, boolean ascending,
-			boolean ignoreCase, boolean nullAsMax) {
-		if (Strings.isBlank(fieldnames)) {
-			throw new UiException("Empty fieldnames: "+ fieldnames);
+	public FieldComparator(String orderBy, boolean ascending, boolean nullAsMax) {
+		if (Strings.isBlank(orderBy)) {
+			throw new UiException("Empty fieldnames: "+ orderBy);
 		}
-		_fieldnames = CollectionsX.parse(new ArrayList(), fieldnames, ',');
+		_fieldnames = parseFieldNames(orderBy, ascending);
 		_asc = ascending;
-		_igcase = ignoreCase;
 		_maxnull = nullAsMax;
 	}
 	
 	public int compare(Object o1, Object o2) {
 		try {
 			for(final Iterator it = _fieldnames.iterator(); it.hasNext();) {
-				final String fieldname = ((String) it.next()).trim();
-				final int res = compare0(o1, o2, fieldname);
+				final FieldInfo fi = (FieldInfo) it.next();
+				final int res = compare0(o1, o2, fi.fieldname, fi.asc, fi.func);
 				if (res != 0) {
 					return res;
 				}
@@ -95,26 +93,119 @@ public class FieldComparator implements Comparator {
 		}
 	}
 	
-	private int compare0(Object o1, Object o2, String fieldname) throws NoSuchMethodException { 
+	public String getOrderBy() {
+		if (_orderBy == null) {
+			final StringBuffer sb = new StringBuffer(_fieldnames.size() * 16);
+			final Iterator it = _fieldnames.iterator();
+			if (it.hasNext()) {
+				appendField(sb, (FieldInfo) it.next());
+			}
+			while(it.hasNext()) {
+				sb.append(",");
+				appendField(sb, (FieldInfo) it.next());
+			}
+			_orderBy = sb.toString();
+		}
+		return _orderBy;
+	}
+	
+	private void appendField(StringBuffer sb, FieldInfo fi) {
+		if (fi.func != null) {
+			sb.append(fi.func).append('(').append(fi.fieldname).append(')');
+		} else {
+			sb.append(fi.fieldname);
+		}
+		sb.append(fi.asc ? " ASC" : " DESC");
+	}
+	
+	private int compare0(Object o1, Object o2, String fieldname, boolean asc, String func) throws NoSuchMethodException { 
 		final Object f1 = Fields.getByCompound(o1, fieldname);
 		final Object f2 = Fields.getByCompound(o2, fieldname);
-		final Object v1 = handleCase(f1);
-		final Object v2 = handleCase(f2);
+		final Object v1 = handleFunction(f1, func);
+		final Object v2 = handleFunction(f2, func);
 		
 		if (v1 == null) return v2 == null ? 0: _maxnull ? 1: -1;
 		if (v2 == null) return _maxnull  ? -1: 1;
 		final int v = ((Comparable)v1).compareTo(v2);
-		return _asc ? v: -v;
+		return asc ? v: -v;
 	}
 
-	private Object handleCase(Object c) {
-		if (_igcase) {
+	private Object handleFunction(Object c, String func) {
+		if ("UPPER".equals(func)) {
 			if (c instanceof String)
 				return ((String)c).toUpperCase();
 			if (c instanceof Character)
 				return new Character(Character.toUpperCase(
 					((Character)c).charValue()));
+		} else if ("LOWER".equals(func)) {
+			if (c instanceof String)
+				return ((String)c).toLowerCase();
+			if (c instanceof Character)
+				return new Character(Character.toLowerCase(
+					((Character)c).charValue()));
 		}
 		return c;
+	}
+
+	private Collection parseFieldNames(String fieldnames, boolean ascending) {
+		final Collection fields = CollectionsX.parse(new ArrayList(), fieldnames, ',');
+		final List results = new ArrayList(fields.size());
+		for (final Iterator it = fields.iterator(); it.hasNext();) {
+			final String field = ((String) it.next()).trim();
+			String fieldname;
+			String ascstr = "asc";
+			//whether a String
+			String func = null;
+			final String ufn = field.toUpperCase(); 
+			if (ufn.startsWith("UPPER(") || ufn.startsWith("LOWER(")) { //with function
+				final int k = field.lastIndexOf(')');
+				if (k == 0) {
+					throw new UiException("No closing function ')' mark: "+field);
+				} else if (k == 6) {
+					throw new UiException("No data inside function: "+field);
+				} else {
+					fieldname = field.substring(6, k);
+					if ((k+1) < field.length()) { //with asc
+						ascstr = field.substring(k+1);
+						if (Strings.isBlank(ascstr)) {
+							ascstr = "asc";
+						}
+					}
+					func = ufn.substring(0,5);
+				}
+			} else {
+				final int j = field.indexOf(' ');
+				if (j < 0) {
+					fieldname = field;
+				} else {
+					fieldname = field.substring(0, j);
+					ascstr = field.substring(j+1);
+				}
+			}
+			
+			boolean asc;
+			if ("asc".equalsIgnoreCase(ascstr)) {
+				asc = ascending;
+			} else if ("desc".equalsIgnoreCase(ascstr)) {
+				asc = !ascending;
+			} else {
+				throw new UiException("field must be in the form of \"field ASC\" or \"field DESC\":" + ascstr);
+			}
+				
+			results.add(new FieldInfo(fieldname, asc, func));
+		}
+		return results;
+	}
+		
+	private static class FieldInfo {
+		private String fieldname;
+		private boolean asc;
+		private String func;
+		
+		public FieldInfo(String fieldname, boolean asc, String func) {
+			this.fieldname = fieldname;
+			this.asc = asc;
+			this.func = func;
+		}
 	}
 }
