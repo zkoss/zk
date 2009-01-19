208b59320d3dce82bd2f2053c1af40faab0b93b2
flyworld <flyworld@gmail.com>
2009-01-19 07:15:26 +0000

    bulgarian localization for ZK
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@10654 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/ajax/lang/mesg_bg.js
+++ zk/src/archive/web/js/zk/ajax/lang/mesg_bg.js
@@ -1,16 +1,20 @@
 /* mesg.js "charset=utf-8"
 
+{{IS_NOTE
 	Purpose:
 		Locale dependent message
 	Description:
 		
 	History:
-　　Tranlated by Boril Yonchev (yonchev@gmail.com) 28 March 2007
+　　Tranlated by Boril Yonchev (yonchev@gmail.com) 17 January 2009
+}}IS_NOTE
 
 Copyright (C) 2005-2007 Potix Corporation. All Rights Reserved.
 
+{{IS_RIGHT
 	This program is distributed under GPL Version 2.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
 */
 mesg = {};
 mesg.NOT_FOUND = "Не е открит: ";
@@ -21,11 +25,20 @@ mesg.TRY_AGAIN = "Try again?";
 mesg.UNSUPPORTED_BROWSER = "Този броузер не се поддържа: ";
 mesg.ILLEGAL_RESPONSE = "Проблем с отговора на сървъра. Презаредете страницата наново.\n";
 mesg.FAILED_TO_PROCESS = "Възникна проблем в процеса на работа ";
+mesg.UUID_REQUIRED = "UUID е задължителен";
+mesg.INVALID_STRUCTURE = "Грешна структура: ";
+mesg.COMP_OR_UUID_REQUIRED = "Компонента или неговия UUID са задължителни";
+mesg.NUMBER_REQUIRED = "Числото трябва да е по-голямо от ";
+mesg.INTEGER_REQUIRED = "Изберете по-голямо целочислено число от";
+mesg.EMPTY_NOT_ALLOWED = "Не може да е празно или само интервали.";
 mesg.GOTO_ERROR_FIELD = "Ползване на грешно поле";
 mesg.PLEASE_WAIT = "Зарежда се...";
+mesg.VALUE_NOT_MATCHED = "Трябва да посочите една от стойностите в падащия списък.";
 
 mesg.FILE_SIZE = "Размер на файла: ";
 mesg.KBYTES = "KB";
+
+mesg.CANCEL="Откажи";
 
 mesg.FAILED_TO_LOAD="Проблем при зареждане ";
 mesg.FAILED_TO_LOAD_DETAIL="Възможно е да се дължи на лоша връзка със сървъра. Опитайте да презаредите страницата отново.";
