5b9913742f1b8a5ad6b7da48bd3a40e8bc829f6e
jumperchen <jumperchen@potix.com>
2009-01-23 03:09:14 +0000

    Feature 2530133: Column support default comparator like listheader
    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.5@10681 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/Column.java
+++ zul/src/org/zkoss/zul/Column.java
@@ -28,6 +28,7 @@ import java.util.List;
 
 import org.zkoss.lang.Objects;
 import org.zkoss.lang.Classes;
+import org.zkoss.lang.Strings;
 import org.zkoss.mesg.Messages;
 import org.zkoss.xml.HTMLs;
 
@@ -128,6 +129,51 @@ public class Column extends HeaderElement implements org.zkoss.zul.api.Column{
 		}
 	}
 
+	/** Sets the type of the sorter.
+	 * You might specify either "auto", "auto(FIELD_NAME1[,FIELD_NAME2] ...)"(since 3.0.9) or "none".
+	 *
+	 * <p>If "auto" is specified,
+	 * {@link #setSortAscending} and/or {@link #setSortDescending} 
+	 * are called with {@link ListitemComparator}, if
+	 * {@link #getSortDescending} and/or {@link #getSortAscending} are null.
+	 * If you assigned a comparator to them, it won't be affected.
+	 * The auto created comparator is case-insensitive.
+	 *
+	 * <p>If "auto(FIELD_NAME1, FIELD_NAME2, ...)" is specified,
+	 * {@link #setSortAscending} and/or {@link #setSortDescending} 
+	 * are called with {@link FieldComparator}, if
+	 * {@link #getSortDescending} and/or {@link #getSortAscending} are null.
+	 * If you assigned a comparator to them, it won't be affected.
+	 * The auto created comparator is case-insensitive.
+
+	 * <p>If "none" is specified, both {@link #setSortAscending} and
+	 * {@link #setSortDescending} are called with null.
+	 * Therefore, no more sorting is available to users for this column.
+	 */
+	public void setSort(String type) {
+		if ("auto".equals(type)) {
+			if (getSortAscending() == null)
+				setSortAscending(new RowComparator(this, true, false, false));
+			if (getSortDescending() == null)
+				setSortDescending(new RowComparator(this, false, false, false));
+		} else if (!Strings.isBlank(type) && type.startsWith("auto")) {
+			final int j = type.indexOf('(');
+			final int k = type.lastIndexOf(')');
+			if (j >= 0 && k >= 0) {
+				final String fieldnames = type.substring(j+1, k);
+				if (getSortAscending() == null)
+					setSortAscending(new FieldComparator(fieldnames, true));
+				if (getSortDescending() == null)
+					setSortDescending(new FieldComparator(fieldnames, false));
+			} else {
+				throw new UiException("Unknown sort type: "+type);
+			}
+		} else if ("none".equals(type)) {
+			setSortAscending((Comparator)null);
+			setSortDescending((Comparator)null);
+		}
+	}
+	
 	/** Returns the ascending sorter, or null if not available.
 	 */
 	public Comparator getSortAscending() {
