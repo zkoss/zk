7ddd2cdd14902058d098c93ddfc173d2768cfde6
tomyeh <tomyeh@zkoss.org>
2009-02-12 09:05:06 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.5@10785 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zcommon/src/org/zkoss/idom/util/IDOMs.java
+++ zcommon/src/org/zkoss/idom/util/IDOMs.java
@@ -11,7 +11,7 @@ C91/01/08 10:37:16, reate, Tom M. Yeh
 Copyright (C) 2001 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
+	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
