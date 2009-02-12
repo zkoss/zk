ffd2754cde7825e7a1792fe9c36c821bb89e2d11
tomyeh <tomyeh@zkoss.org>
2009-02-12 11:44:21 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@10791 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zcommon/src/org/zkoss/idom/impl/AbstractTextual.java
+++ zcommon/src/org/zkoss/idom/impl/AbstractTextual.java
@@ -11,7 +11,7 @@ C2001/10/22 20:48:47, reate, Tom M. Yeh
 Copyright (C) 2001 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
+	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
