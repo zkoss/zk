1115672a62b701c0920dd40af07d7d9000ff65bf
tomyeh <tomyeh@zkoss.org>
2009-02-13 10:05:30 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/3.0@10794 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/lang/mesg_ro.js
+++ zk/src/archive/web/js/zk/html/lang/mesg_ro.js
@@ -12,7 +12,7 @@
 Copyright (C) 2005-2007 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
+	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
