8c123df4a82c961cf420583a9e3ffc82c87cbe08
tomyeh <tomyeh@zkoss.org>
2009-02-16 09:36:13 +0000

    Merge 3.5
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@10822 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkmax/src/org/zkoss/zkmax/zul/Portalchildren.java
+++ zkmax/src/org/zkoss/zkmax/zul/Portalchildren.java
@@ -12,7 +12,7 @@
 Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
+	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
@@ -40,7 +40,7 @@ public class Portalchildren extends XulElement implements org.zkoss.zkmax.zul.ap
 	}
 
 	public String getZclass() {
-		return _zclass == null ? "z-portal-children" : super.getZclass();
+		return _zclass == null ? "z-portal-children" : _zclass;
 	}
 	
 	public void setParent(Component parent) {
--- zkmax/src/org/zkoss/zkmax/zul/Portallayout.java
+++ zkmax/src/org/zkoss/zkmax/zul/Portallayout.java
@@ -12,7 +12,7 @@
 Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
+	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
@@ -90,7 +90,7 @@ public class Portallayout extends XulElement implements org.zkoss.zkmax.zul.api.
 	}
 
 	public String getZclass() {
-		return _zclass == null ? "z-portal-layout" : super.getZclass();
+		return _zclass == null ? "z-portal-layout" : _zclass;
 	}
 	public boolean insertBefore(Component child, Component insertBefore) {
 		if (!(child instanceof Portalchildren))
