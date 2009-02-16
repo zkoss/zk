3c85898c8310f1776f18d4bd851d8d4271419565
flyworld <flyworld@gmail.com>
2009-02-16 11:28:26 +0000

    fix
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@10827 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkex/src/org/zkoss/zkex/zul/api/Columnlayout.java
+++ zkex/src/org/zkoss/zkex/zul/api/Columnlayout.java
@@ -1,40 +1,3 @@
-<<<<<<< .working
-/* Columnlayout.java
-
-{{IS_NOTE
-	Purpose:
-		
-	Description:
-		
-	History:
-		Tue Oct 23 09:22:13     2008, Created by Flyworld
-}}IS_NOTE
-
-Copyright (C) 2008 Potix Corporation. All Rights Reserved.
-
-{{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
-	it will be useful, but WITHOUT ANY WARRANTY.
-}}IS_RIGHT
- */
-package org.zkoss.zkex.zul.api;
-
-/**
- * A columnlayout lays out a container which can have multiple columns, and each
- * column may contain one or more panel.<br>
- * Use Columnlayout need assign width (either present or pixel) on every
- * Columnchildren, or we cannot make sure about layout look.
- * 
- * <p>
- * Default {@link org.zkoss.zkex.zul.Columnlayout#getZclass}: z-column-layout.
- * 
- * @author gracelin
- * @since 3.5.0
- */
-public interface Columnlayout extends org.zkoss.zul.impl.api.XulElement {
-
-}
-=======
 /* Columnlayout.java
 
 {{IS_NOTE
@@ -70,4 +33,3 @@ package org.zkoss.zkex.zul.api;
 public interface Columnlayout extends org.zkoss.zul.impl.api.XulElement {
 
 }
->>>>>>> .merge-right.r10815
