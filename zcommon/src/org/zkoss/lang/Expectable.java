a22bc2e90245d89d490642f7e9be57722a5e90e9
tomyeh <tomyeh@zkoss.org>
2009-02-19 07:38:59 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@10908 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zcommon/src/org/zkoss/lang/Expectable.java
+++ zcommon/src/org/zkoss/lang/Expectable.java
@@ -11,7 +11,7 @@
 Copyright (C) 2002 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
+	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
--- zhtml/src/org/zkoss/zhtml/Td.java
+++ zhtml/src/org/zkoss/zhtml/Td.java
@@ -1,4 +1,4 @@
-/* Td.java
+m/* Td.java
 
 {{IS_NOTE
 	Purpose:
@@ -12,7 +12,7 @@
 Copyright (C) 2005 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
-	This program is distributed under GPL Version 2.0 in the hope that
+	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
