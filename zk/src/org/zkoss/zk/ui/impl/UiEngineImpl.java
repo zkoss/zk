f20a306aecff72e03e005b5d6119a3e63ba6481b
tomyeh <tomyeh@zkoss.org>
2009-03-06 03:00:44 +0000

    Avoid activate invalid desktop
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@11143 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/impl/UiEngineImpl.java
+++ zk/src/org/zkoss/zk/ui/impl/UiEngineImpl.java
@@ -1510,6 +1510,9 @@ public class UiEngineImpl implements UiEngine {
 				}
 			}
 
+			if (!desktop.isAlive())
+				throw new org.zkoss.zk.ui.DesktopUnavailableException("Unable to activate destroyed desktop, "+desktop);
+
 			//grant
 			eis.put(desktop, uv = new UiVisualizer(exec, asyncupd, recovering));
 			((DesktopCtrl)desktop).setExecution(exec);
