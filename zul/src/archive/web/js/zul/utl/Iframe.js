f882e1943dc4b8eaf437f77e9958188f1c0ca1eb
tomyeh <tomyeh@zkoss.org>
2009-03-20 04:10:29 +0000

    move wgt's pkg
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11238 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/utl/Iframe.js
+++ zul/src/archive/web/js/zul/utl/Iframe.js
@@ -12,7 +12,7 @@ Copyright (C) 2009 Potix Corporation. All Rights Reserved.
 This program is distributed under GPL Version 3.0 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
-zul.rpt.Iframe = zk.$extends(zul.Widget, {
+zul.utl.Iframe = zk.$extends(zul.Widget, {
 	_scrolling: "auto",
 
 	getSrc: function () {
