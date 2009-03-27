29453a5630e3faaaeed3d96985d3a5d6c1822739
tomyeh <tomyeh@zkoss.org>
2009-03-27 07:01:11 +0000

    captcha ready
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11277 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/wgt/Captcha.js
@@ -0,0 +1,16 @@
+/* Captcha.js
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Mar 27 14:53:11     2009, Created by tomyeh
+
+Copyright (C) 2009 Potix Corporation. All Rights Reserved.
+
+This program is distributed under GPL Version 3.0 in the hope that
+it will be useful, but WITHOUT ANY WARRANTY.
+*/
+zul.wgt.Captcha = zk.$extends(zul.wgt.Image, {
+});
