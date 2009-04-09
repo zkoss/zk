acedf64fd96e978d26b6c69e45aca17a890ac55f
tomyeh <tomyeh@zkoss.org>
2009-04-09 03:33:41 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11338 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/inp/Combobox.js
+++ zul/src/archive/web/js/zul/inp/Combobox.js
@@ -22,41 +22,48 @@ zul.inp.Combobox = zk.$extends(zul.inp.ComboWidget, {
 
 	//called by SimpleConstraint
 	validateStrict: function (val) {
-		return this._findItem(val.toLowerCase(), true) ?
-			null: mesg.VALUE_NOT_MATCHED;
+		return this._findItem(val, true) ? null: mesg.VALUE_NOT_MATCHED;
 	},
 	_findItem: function (val, strict) {
-		val = val.toLowerCase();
-		for (var item = this.firstChild; item; item = item.nextSibling)
-			if ((!strict || !item.isDisabled()) && item.isVisible()
-			&& val == item.getLabel().toLowerCase())
-				return item;
+		var fchild = this.firstChild;
+		if (fchild && val) {
+			val = val.toLowerCase();
+			var sel = this._sel;
+			if (!sel || sel.parent != this) sel = fchild;
+
+			for (var item = sel;;) {
+				if ((!strict || !item.isDisabled()) && item.isVisible()
+				&& val == item.getLabel().toLowerCase())
+					return item;
+				if (!(item = item.nextSibling)) item = fchild;
+				if (item == sel) break;
+			}
+		}
 	},
 	_hilite: function (opts) {
+		this._hilite2(
+			this._findItem(this.getInputNode().value, this._isStrict()), opts);
+	},
+	_hilite2: function (sel, opts) {
 		if (!opts) opts = {};
 
-		var sel = this._sel, item;
-		if (sel && sel.parent == this) { //we don't clear _sel precisely, so...
-			var n = sel.getNode();
-			if (n) zDom.rmClass(n, sel.getZclass() + '-seld');
+		var oldsel = this._sel;
+		this._sel = sel;
+
+		if (oldsel && oldsel.parent == this) { //we don't clear _sel precisely, so...
+			var n = oldsel.getNode();
+			if (n) zDom.rmClass(n, oldsel.getZclass() + '-seld');
 		}
 
-		this._sel = null;
-		if (this._open || opts.sendOnSelect) {
-			var inp = this.getInputNode(),
-				val = inp.value;
-			if (val.length) {
-				var strict = this.getConstraint();
-				strict = strict && strict._flags && strict._flags.STRICT;
-				item = this._findItem(val.toLowerCase(), strict);
-			}
-
-			if ((this._sel = item) && this._open)
-				zDom.addClass(item.getNode(), item.getZclass() + '-seld');
-		}
+		if (sel && this._open)
+			zDom.addClass(sel.getNode(), sel.getZclass() + '-seld');
 
 		if (opts.sendOnSelect)
-			this.fire('onSelect', {items: item?[item]:[], reference: item});
+			this.fire('onSelect', {items: sel?[sel]:[], reference: sel});
+	},
+	_isStrict: function () {
+		var strict = this.getConstraint();
+		return strict && strict._flags && strict._flags.STRICT;
 	},
 
 	//super
@@ -65,8 +72,57 @@ zul.inp.Combobox = zk.$extends(zul.inp.ComboWidget, {
 		this._hilite(); //after _open is set
 	},
 	downPressed_: function (evt) {
+		this._updnSel();
 	},
 	upPressed_: function (evt) {
+		this._updnSel(true);
+	},
+	_updnSel: function (bUp) {
+		var inp = this.getInputNode(),
+			val = inp.value, sel;
+		if (val) {
+			val = val.toLowerCase();
+			var beg = this._sel;
+			if (!beg || beg.parent != this) beg = this._next(null, bUp);
+
+			//Note: we always assume strict when handling up/dn
+			for (var item = beg;;) {
+				if (!item.isDisabled() && item.isVisible()
+				&& val == item.getLabel().toLowerCase()) {
+					sel = item;
+					break;
+				}
+				if ((item = this._next(item, bUp)) == beg)
+					break;
+			}
+
+			if (sel) { //exact match
+				var ofs = zDom.getSelectionRange(inp);
+				if (ofs[0] == 0 && ofs[1] == val.length) //full selected
+					sel = this._next(sel, bUp); //next
+			} else {
+				for (var item = beg;;) {
+					if (!item.isDisabled() && item.isVisible()
+					&& item.getLabel().toLowerCase().startsWith(val)) {
+						sel = item;
+						break;
+					}
+					if ((item = this._next(item, bUp)) == beg)
+						break;
+				}
+			}
+		} else
+			sel = this._next(null, bUp);
+
+		val = inp.value = sel ? sel.getLabel(): '';
+		if (val) zDom.setSelectionRange(inp, 0, val.length)
+
+		this._hilite2(sel, {sendOnSelect:true});
+	},
+	_next: function (item, bUp) {
+		if (item)
+			item = bUp ? item.previousSibling: item.nextSibling;
+		return item ? item: bUp ? this.lastChild: this.firstChild;
 	},
 	otherPressed_: function (evt) {
 	},
