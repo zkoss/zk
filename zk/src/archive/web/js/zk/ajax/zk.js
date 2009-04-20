0cbc4b059bbb29fa7da759342df7bb386754859e
tomyeh <tomyeh@zkoss.org>
2009-04-20 09:01:32 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11397 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/ajax/zk.js
+++ zk/src/archive/web/js/zk/ajax/zk.js
@@ -352,6 +352,7 @@ if (zk.gecko) {
 	if (zk.ie) {
 		j = zk.parseInt(zk.agent.substring(j + 5));
 		zk.ie7 = j >= 7; //ie7 or later
+		zk.ie8All = j >= 8; //ie8 or later (including compatible)
 		zk.ie8 = j >= 8 && document.documentMode >= 8; //ie8 or later
 		zk.ie6Only = !zk.ie7;
 
