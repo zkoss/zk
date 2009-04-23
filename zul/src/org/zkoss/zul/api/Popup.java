7ecda3d5e71487384fdc8414e6cb1ff651f01c8e
jumperchen <jumperchen@potix.com>
2009-04-23 08:26:07 +0000

    update API
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11437 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/api/Popup.java
+++ zul/src/org/zkoss/zul/api/Popup.java
@@ -19,6 +19,7 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 package org.zkoss.zul.api;
 
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zul.impl.XulElement;
 
 /**
  * A container that is displayed as a popup. The popup window does not have any
@@ -84,6 +85,42 @@ public interface Popup extends org.zkoss.zul.impl.api.XulElement {
 	 *            the reference component to position the popup. It cannot be
 	 */
 	public void open(Component ref);
+	
+	/**
+	 * Opens this popup right below the specified component at the client.
+	 * <p>In most cases, the popup is shown automatically when specified
+	 * in the tooltip, popup and context properties
+	 * ({@link XulElement#setTooltip}, {@link XulElement#setPopup},
+	 * and {@link XulElement#setContext}).
+	 * However, if you want to show it manually, you can invoke this
+	 * method directly.
+	 * <p> Possible values for the position attribute are:
+	 * <ul>
+	 * 	<li><b>before_start</b><br/> the popup appears above the anchor, aligned on the left.</li>
+	 *  <li><b>before_end</b><br/> the popup appears above the anchor, aligned on the right.</li>
+	 *  <li><b>after_start</b><br/> the popup appears below the anchor, aligned on the left.</li>
+	 *  <li><b>after_end</b><br/> the popup appears below the anchor, aligned on the right.</li>
+	 *  <li><b>start_before</b><br/> the popup appears to the left of the anchor, aligned on the top.</li>
+	 *  <li><b>start_after</b><br/> the popup appears to the left of the anchor, aligned on the bottom.</li>
+	 *  <li><b>end_before</b><br/> the popup appears to the right of the anchor, aligned on the top.</li>
+	 *  <li><b>end_after</b><br/> the popup appears to the right of the anchor, aligned on the bottom.</li>
+	 *  <li><b>overlap</b><br/> the popup overlaps the anchor, with the topleft 
+	 *  	corners of both the anchor and popup aligned.</li>
+	 *  <li><b>after_pointer</b><br/> the popup appears with the top aligned with
+	 *  	the bottom of the anchor, with the topleft corner of the popup at the horizontal position of the mouse pointer.</li>
+	 * </ul></p>
+	 * 
+	 * @param ref the reference component to position the popup.
+	 * It cannot be null. 
+	 * @param position the descriptions above are for a locale where the UI is 
+	 * displayed left to right and top to bottom. In this case, before is the top,
+	 * after is the bottom, start is the left and end is the right. For right to left locales,
+	 * start is the right and end is the left. 
+	 * 
+	 * @since 5.0.0
+	 */
+	public void open(Component ref, String position);
+	
 
 	/**
 	 * Closes this popup at the client.
