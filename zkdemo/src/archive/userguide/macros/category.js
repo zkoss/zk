99f879ee4b767cb7edf69a27eb7c8a3595d0d040
tomyeh <tomyeh@zkoss.org>
2009-05-08 10:44:41 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11523 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkdemo/src/archive/userguide/macros/category.js
+++ zkdemo/src/archive/userguide/macros/category.js
@@ -1,13 +1,5 @@
-/* category.js
+// category.js
 
-History:
-	Fri May  8 18:14:07     2009, Created by tomyeh
-
-Copyright (C) 2009 Potix Corporation. All Rights Reserved.
-
-This program is distributed under GPL Version 3.0 in the hope that
-it will be useful, but WITHOUT ANY WARRANTY.
-*/
 zk.$package('userguide');
 zPkg.load('zul.wgt', function () {
 userguide.Categorybar = zk.$extends(zul.wgt.Div, {
