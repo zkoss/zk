19aa6f800e351efef9adcdd45678c13a69e2192e
kindalu <kindalu@potix.com>
2009-05-14 01:25:19 +0000

    progressmeter add onShow & onVisi
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11566 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/wgt/Progressmeter.js
+++ zul/src/archive/web/js/zul/wgt/Progressmeter.js
@@ -5,25 +5,30 @@ zul.wgt.Progressmeter = zk.$extends(zul.Widget, {
 		var zcls = this._zclass;
 		return zcls != null ? zcls: "z-progressmeter";
 	},
-	_fixImgWidth: function(val) {
+	_fixImgWidth: _zkf=function() {
 		var n=this.getNode();
 		var img = this.getSubnode("img");
 		if (img) {
 			if (zk.ie6Only) img.style.width = ""; //Bug 1899749
-				img.style.width = Math.round((n.clientWidth * val) / 100) + "px";
+				img.style.width = Math.round((n.clientWidth * this._value) / 100) + "px";
 		}
 	},
+	onSize: _zkf,
+	onShow: _zkf,
 	bind_: function () {//after compose
 		this.$supers('bind_', arguments); 
 		this._fixImgWidth(this._value);
+		zWatch.listen('onSize', this);
+		zWatch.listen('onShow', this);
 	},
 	unbind_: function () {
+		zWatch.unlisten('onSize', this);
+		zWatch.unlisten('onShow', this);
 		this.$supers('unbind_', arguments);
 	},
 	setWidth : function (val){
 		this.$supers('setWidth', arguments);
-		if(this.getNode()) 
-			this._fixImgWidth(this._value);
+		this._fixImgWidth();
 	}
 });
 
@@ -31,7 +36,7 @@ zul.wgt.Progressmeter = zk.$extends(zul.Widget, {
 zk.def(zul.wgt.Progressmeter,{
 	value: function (val) {
 		if(this.getNode()) 
-			this._fixImgWidth(val);
+			this._fixImgWidth();
 	}
 });
 
