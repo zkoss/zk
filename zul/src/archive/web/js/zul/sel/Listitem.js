0dce2da973b576a603b9a828b93f02ee6688ce94
jumperchen <jumperchen@potix.com>
2009-05-15 10:04:37 +0000

    update listitem
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11595 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/Listitem.js
+++ zul/src/archive/web/js/zul/sel/Listitem.js
@@ -12,9 +12,27 @@ Copyright (C) 2009 Potix Corporation. All Rights Reserved.
 This program is distributed under GPL Version 3.0 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
-zul.sel.Listitem = zk.$extends(zul.Widget, {
+zk.def(zul.sel.Listitem = zk.$extends(zul.Widget, {
 	//super//
 	getZclass: function () {
 		return this._zclass == null ? "z-listitem" : this._zclass;
+	},
+	doClick_: function(evt) {
+		this.parent.fire('onSelect', {
+			items: [this.uuid],
+			reference: this.uuid,
+			keys: zEvt.metaData(evt)
+		});
+		this.$supers('doClick_', arguments);
+	}
+}), { // zk.def
+	checkable: function () {
+		this.rerender();
+	},
+	selected: function () {
+//		if (this.parent)
+//			this.parent.toggleItemSelection(this);
+	},
+	disabled: function () {
 	}
 });
