3e203ebe8c440661dd1f260dd9323ae30e38cfbe
flyworld <flyworld@gmail.com>
2009-05-19 06:47:19 +0000

    update message from http://docs.zkoss.org/wiki/ZK_Mesgs
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@11618 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/lang/mesg_it.js
+++ zk/src/archive/web/js/zk/html/lang/mesg_it.js
@@ -1,16 +1,16 @@
 /* mesg_it.js "charset=utf-8"
-
+ 
 {{IS_NOTE
 	Purpose:
 		Locale dependent message
 	Description:
-		
+ 
 	History:
 		Aug 10, 2006, Created by Matteo Barbieri
 }}IS_NOTE
-
+ 
 Copyright (C) 2006 Potix Corporation. All Rights Reserved.
-
+ 
 {{IS_RIGHT
 	This program is distributed under GPL Version 3.0 in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY.
@@ -19,29 +19,29 @@ Copyright (C) 2006 Potix Corporation. All Rights Reserved.
 mesg = {};
 mesg.NOT_FOUND = "Non trovato: ";
 mesg.UNSUPPORTED = "Non ancora supportato: ";
-mesg.FAILED_TO_SEND = "Invio della richiesta al server fallito.";
-mesg.FAILED_TO_RESPONSE = "Il server ha fallito nel elaborare la tua richiesta.";
+mesg.FAILED_TO_SEND = "Invio della richiesta al server non riuscito.";
+mesg.FAILED_TO_RESPONSE = "Il server non è riuscito ad elaborare la richiesta.";
 mesg.TRY_AGAIN = "Riprovare?";
 mesg.UNSUPPORTED_BROWSER = "Browser non supportato: ";
-mesg.ILLEGAL_RESPONSE = "Risposta sconosciuta inviata dal server. Per favore ricarica la pagina e riprova.\n";
+mesg.ILLEGAL_RESPONSE = "Risposta sconosciuta inviata dal server. Ricaricare la pagina e riprovare.\n";
 mesg.FAILED_TO_PROCESS = "Errore nell'elaborazione di ";
 mesg.UUID_REQUIRED = "L'UUID è obbligatorio";
 mesg.INVALID_STRUCTURE = "Struttura errata: ";
-mesg.COMP_OR_UUID_REQUIRED = "Il componente od il suo UUID sono obbligatori";
-mesg.NUMBER_REQUIRED = "Devi inserire un numero al posto di ";
-mesg.INTEGER_REQUIRED = "Devi inserire un intero al posto di ";
-mesg.EMPTY_NOT_ALLOWED = "Non è permesso il campo vuoto.\nNon puoi neanche inserire solamente spazi.";
+mesg.COMP_OR_UUID_REQUIRED = "Il componente o il suo UUID sono obbligatori";
+mesg.NUMBER_REQUIRED = "Occorre inserire un numero invece di ";
+mesg.INTEGER_REQUIRED = "Occorre inserire un numero intero invece di ";
+mesg.EMPTY_NOT_ALLOWED = "Campo vuoto non consentito.\nNon è consentito neanche inserire soli spazi.";
 mesg.GOTO_ERROR_FIELD = "Arrivato al campo sbagliato";
-mesg.PLEASE_WAIT = "Attendere...";
-mesg.VALUE_NOT_MATCHED = "You must specify one of values in the drop-down list.";
-
+mesg.PLEASE_WAIT = "Elaborazione in corso...";
+mesg.VALUE_NOT_MATCHED = "Occorre selezionare uno dei valori nell'elenco a discesa.";
+ 
 mesg.FILE_SIZE = "Dimensione file: ";
 mesg.KBYTES = "KB";
-
+ 
 mesg.CANCEL="Annulla";
-
-mesg.FAILED_TO_LOAD="Failed to load ";
-mesg.FAILED_TO_LOAD_DETAIL="It may be caused by bad traffic. You could reload this page and try again.";
+ 
+mesg.FAILED_TO_LOAD="Impossibile caricare";
+mesg.FAILED_TO_LOAD_DETAIL="Ciò può essere dovuto a traffico errato. Ricaricare la pagina e riprovare.";
 mesg.CAUSE="Cause: ";
-
+ 
 mesg.LOADING = "Attendere...";
--- zk/src/archive/web/js/zk/html/lang/mesg_nl.js
+++ zk/src/archive/web/js/zk/html/lang/mesg_nl.js
@@ -1,16 +1,17 @@
 /* mesg.js "charset=utf-8"
-
+ 
 {{IS_NOTE
   Purpose:
     Locale dependent message
   Description:
-
+ 
   History:
     Feb 21, 2008 Wido Jansen
+    May 15, 2009 Maarten Hogendoorn (corrections)
 }}IS_NOTE
-
+ 
 Copyright (C) 2006 Potix Corporation. All Rights Reserved.
-
+ 
 {{IS_RIGHT
   This program is distributed under GPL Version 3.0 in the hope that
   it will be useful, but WITHOUT ANY WARRANTY.
@@ -21,27 +22,27 @@ mesg.NOT_FOUND = "Niet gevonden: ";
 mesg.UNSUPPORTED = "Nog niet ondersteund: ";
 mesg.FAILED_TO_SEND = "Verzoek kan niet aan de server gestuurd worden.";
 mesg.FAILED_TO_RESPONSE = "Server kan verzoek niet verwerken.";
-mesg.TRY_AGAIN = "Try again?";
+mesg.TRY_AGAIN = "Opnieuw proberen?";
 mesg.UNSUPPORTED_BROWSER = "Niet ondersteunde Browser: ";
-mesg.ILLEGAL_RESPONSE = "Unbekend Antwoord van de Server, Aktualiseer aub de pagina en probeer opnieuw.\n";
+mesg.ILLEGAL_RESPONSE = "Onbekend antwoord van de Server. Actualiseer de pagina en probeer het opnieuw.\n";
 mesg.FAILED_TO_PROCESS = "Verwerking is fout gegaan ";
 mesg.UUID_REQUIRED = "UUID is nodig";
 mesg.INVALID_STRUCTURE = "Corrupte Structuur: ";
 mesg.COMP_OR_UUID_REQUIRED = "Component of haar UUID is nodig";
 mesg.NUMBER_REQUIRED = "U moet een getal invoeren, en niet ";
 mesg.INTEGER_REQUIRED = "U moet een geheel getal invoeren, en niet ";
-mesg.EMPTY_NOT_ALLOWED = "Geen invoer is niet toegestaan.\nAleen spaties zijn ook niet toegestaan";
+mesg.EMPTY_NOT_ALLOWED = "Geen invoer is niet toegestaan.\nAlleen spaties zijn ook niet toegestaan";
 mesg.GOTO_ERROR_FIELD = "Ga naar foutief veld";
 mesg.PLEASE_WAIT = "Verwerken...";
-mesg.VALUE_NOT_MATCHED = "U moet een keuze maken uit de drop down lijst.";
-
+mesg.VALUE_NOT_MATCHED = "U moet een keuze maken uit de drop-down-lijst.";
+ 
 mesg.FILE_SIZE = "Bestandsgrootte: ";
 mesg.KBYTES = "Kb";
-
+ 
 mesg.CANCEL="Afbreken";
-
+ 
 mesg.FAILED_TO_LOAD="Het laden is mislukt ";
-mesg.FAILED_TO_LOAD_DETAIL="Het kan veroorzaakt zijn door foutief verkeer. U kunt de pagina opnieuw landen en opnieuw proberen.";
+mesg.FAILED_TO_LOAD_DETAIL="Het kan veroorzaakt zijn door foutief verkeer. U kunt de pagina opnieuw laden en het opnieuw proberen.";
 mesg.CAUSE="Oorzaak: ";
-
+ 
 mesg.LOADING = "Verwerken...";
