6bea53b45425cf815154a308091498b94b31a08b
tomyeh <tomyeh@zkoss.org>
2009-06-27 10:37:43 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11862 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/au/http/AuUploader.java
+++ zk/src/org/zkoss/zk/au/http/AuUploader.java
@@ -30,8 +30,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
-
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
