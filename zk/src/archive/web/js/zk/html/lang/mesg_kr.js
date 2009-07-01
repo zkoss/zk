257ab38c6477021d9cb104e8deec3b525df408ba
flyworld <flyworld@gmail.com>
2009-07-01 04:43:14 +0000

    message Korea update
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@11913 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/html/lang/mesg_kr.js
@@ -0,0 +1,47 @@
+/* mesg.js "charset=utf-8"
+ 
+{{IS_NOTE
+	Purpose:
+		Locale dependent message
+	Description:
+ 
+	History:
+		Fri Jun 10 15:56:48     2005, Created by tomyeh
+}}IS_NOTE
+ 
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+ 
+{{IS_RIGHT
+	This program is distributed under GPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+mesg = {};
+mesg.NOT_FOUND = "존재하지 않습니다.: ";
+mesg.UNSUPPORTED = "아직 지원되지 않습니다.: ";
+mesg.FAILED_TO_SEND = "서버로의 요청 전송에 실패하였습니다.";
+mesg.FAILED_TO_RESPONSE = "서버는 일시적인 정지상태입니다.";
+mesg.TRY_AGAIN = "재시도를 원하시나요?";
+mesg.UNSUPPORTED_BROWSER = "지원되지 않는 브라우저입니다: ";
+mesg.ILLEGAL_RESPONSE = "이해할 수 없는 응답이 수신되었습니다. 새로고침하시고 재시도하세요..\n";
+mesg.FAILED_TO_PROCESS = "처리실패하였습니다.";
+mesg.UUID_REQUIRED = "UUID가 필요합니다.";
+mesg.INVALID_STRUCTURE = "타당치 않은(Invalid) 구조입니다.: ";
+mesg.COMP_OR_UUID_REQUIRED = "컴퍼넌트 또는 그 컴퍼넌트의 UUID가 필요합니다.";
+mesg.NUMBER_REQUIRED = "번호를 기입하세요";
+mesg.INTEGER_REQUIRED = "정수를 기입하세요";
+mesg.EMPTY_NOT_ALLOWED = "공백은 피하세요.\n필수 사항입니다. 기입해주세요.";
+mesg.GOTO_ERROR_FIELD = "에러필드로 이동합니다.";
+mesg.PLEASE_WAIT = "처리중...";
+mesg.VALUE_NOT_MATCHED = "콤보박스에서 적어도 하나의 값을 선택하셔야 합니다.";
+ 
+mesg.FILE_SIZE = "파일 크기: ";
+mesg.KBYTES = "KB";
+ 
+mesg.CANCEL="취소";
+ 
+mesg.FAILED_TO_LOAD = "로드에 실패하였습니다 ";
+mesg.FAILED_TO_LOAD_DETAIL = "트래픽에 의한 문제일 수 있습니다. 페이지를 새로고침하시고 재시도하세요.";
+mesg.CAUSE = "원인: ";
+ 
+mesg.LOADING = "로딩중...";
