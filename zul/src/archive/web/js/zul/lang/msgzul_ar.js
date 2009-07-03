78ec9185ab07daba848144798e558536574b00cd
tomyeh <tomyeh@zkoss.org>
2009-07-03 03:09:47 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/trunk@11946 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/lang/msgzul_ar.js
+++ zul/src/archive/web/js/zul/lang/msgzul_ar.js
@@ -19,6 +19,5 @@ Copyright (C) 2005-2007 Potix Corporation. All Rights Reserved.
 msgzul = {};
 msgzul.UNKNOWN_TYPE = "نوع غير معروف";
 msgzul.DATE_REQUIRED = "يتطلب وجود تاريخ";
+msgzul.OUT_OF_RANGE = "خارج الحدود المسموح بها";
 msgzul.NO_AUDIO_SUPPORT = "لا يوجد دعم للصوت";
-
-
