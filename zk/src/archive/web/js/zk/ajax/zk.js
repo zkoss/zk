37fbfeade8db12d2056b6a406924eef245ab814d
tomyeh <tomyeh@zkoss.org>
2009-07-03 09:28:54 +0000

    sysId
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@11957 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/ajax/zk.js
+++ zk/src/archive/web/js/zk/ajax/zk.js
@@ -58,6 +58,7 @@ zk = function (sel) {
 			throw 'unknown superclass';
 
 		var jclass = function() {
+			this.sysId = zk._sysId++;
 			this.$init.apply(this, arguments);
 
 			var ais = this._$ais;
@@ -91,6 +92,7 @@ zk = function (sel) {
 
 		return jclass;
 	},
+	_sysId: 0,
 	$default: function (opts, defaults) {
 		opts = opts || {};
 		for (var p in defaults)
