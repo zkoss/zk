86448e58357773305acabca8a2ed04632b5b2745
kindalu <kindalu@potix.com>
2009-07-15 03:17:26 +0000

    fix selected item (by keydown in listbox.) always on the top
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@12112 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/SelectWidget.js
+++ zul/src/archive/web/js/zul/sel/SelectWidget.js
@@ -466,6 +466,7 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 		this.$supers('doKeyDown_', arguments);
 	},
 	_doKeyDown: function (evt) {
+		zk.log("_doKeyDown");
 		if (zAu.processing() || this._shallIgnoreEvent(evt))
 			return true;
 
@@ -509,7 +510,7 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 		if (step) {
 			if (shift) this._toggleSelect(row, true, evt);
 			var nrow = row.$n();
-			for (; (nrow = step > 0 ? nrow.nextSibling: nrow.previousSibling);) {
+			for (; nrow && (nrow = step > 0 ? nrow.nextSibling: nrow.previousSibling);) {
 				var r = zk.Widget.$(nrow);
 				if (r.$instanceof(zul.sel.Treerow))
 					r = r.parent;
@@ -531,7 +532,7 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 			if (ctrl) this._focus(lastrow);
 			else this._select(lastrow, evt);
 			this._syncFocus(lastrow);
-			zk(lastrow).scrollIntoView(this.ebody); // Bug #1823947 and #1823278
+			zjq._scrollIntoView(this.body, lastrow);// Bug #1823947 and #1823278
 		}
 
 		switch (data.keyCode) {
