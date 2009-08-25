9839e9647a78a03a907c15631de1c66a93506901
jumperchen <jumperchen@potix.com>
2009-08-25 13:33:17 +0000

    Upgrade Fckeditor
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@12316 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/widget.js
+++ zk/src/archive/web/js/zk/widget.js
@@ -1210,18 +1210,22 @@ zk.Widget = zk.$extends(zk.Object, {
 
 		if (!evt.auStopped) {
 			var toServer = evt.opts && evt.opts.toServer;
-			if (toServer || (this.inServer && this.desktop)){
-				var asap = toServer || this._asaps[evtnm];
-				if (asap == null) {
-					var ime = this.$class._importantEvts;
-					if (ime) {
-						var ime = ime[evtnm];
-						if (ime != null)
-							asap = ime;
+			if (toServer || (this.inServer && this.desktop)) {
+				if (evt.opts.sendAhead) {
+					zAu.sendAhead(evt, timeout >= 0 ? timeout : 38);
+				} else {
+					var asap = toServer || this._asaps[evtnm];
+					if (asap == null) {
+						var ime = this.$class._importantEvts;
+						if (ime) {
+							var ime = ime[evtnm];
+							if (ime != null) 
+								asap = ime;
+						}
 					}
+					if (asap != null) //true or false
+						zAu.send(evt, asap ? timeout >= 0 ? timeout : 38 : -1);
 				}
-				if (asap != null) //true or false
-					zAu.send(evt, asap ? timeout >= 0 ? timeout: 38: -1);
 			}
 		}
 		return evt;
