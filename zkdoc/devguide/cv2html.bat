a98e67754db81c52ec32319f8e18ef0717f54410
tomyeh <tomyeh@zkoss.org>
2009-09-16 01:43:17 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@12431 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkdoc/devguide/cv2html.bat
+++
@@ -1,3 +0,0 @@
-xsltproc --output html\ --stringparam html.stylesheet "devguide.css" ..\docbook\docbook-xsl-1.73.0\html\chunk-devguide.xsl ZK-devguide.xml
-xsltproc --output html_single\index.html --stringparam html.stylesheet "devguide.css" ..\docbook\docbook-xsl-1.73.0\html\docbook-devguide.xsl ZK-devguide.xml
-pause
