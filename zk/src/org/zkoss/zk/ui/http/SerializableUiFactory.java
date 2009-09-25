a3d2ffde28bcc7ce520d27c7e28abefa81c73587
tomyeh <tomyeh@zkoss.org>
2009-09-25 05:28:38 +0000

    Support serializable sessions
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@12580 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/http/SerializableUiFactory.java
+++ zk/src/org/zkoss/zk/ui/http/SerializableUiFactory.java
@@ -1,11 +1,13 @@
 /* SerializableUiFactory.java
 
+{{IS_NOTE
 	Purpose:
 		
 	Description:
 		
 	History:
 		Thu Jul  6 12:38:04     2006, Created by tomyeh
+}}IS_NOTE
 
 Copyright (C) 2006 Potix Corporation. All Rights Reserved.
 
@@ -16,48 +18,21 @@ Copyright (C) 2006 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zk.ui.http;
 
+import javax.servlet.http.HttpSession;
+
 import org.zkoss.zk.ui.WebApp;
 import org.zkoss.zk.ui.Session;
-import org.zkoss.zk.ui.UiException;
-import org.zkoss.zk.ui.sys.UiFactory;
 import org.zkoss.zk.ui.impl.AbstractUiFactory;
 
 /**
  * The serializable implementation of {@link org.zkoss.zk.ui.sys.UiFactory}.
  * The instances returned by {@link #newSession} is serializable, such that
  * session can be stored when the Web server stops and restore after it starts.
- *
- * <p>Since ZK 5.0.0, use org.zkoss.zkmax.ui.http.SerializableUiFactory
- * instead of this class.
  * 
  * @author tomyeh
  */
 public class SerializableUiFactory extends AbstractUiFactory {
-	private UiFactory _uiFactory;
-
-	/** The class of the real UI factory that this UI factory will delegate
-	 * the invocation to.
-	 * @since 5.0.0
-	 */
-	protected static Class _uiFactoryClass;
-
 	public Session newSession(WebApp wapp, Object nativeSess, Object request) {
-		if (_uiFactory == null) {
-			synchronized (this) {
-				if (_uiFactory == null) {
-					if (_uiFactoryClass == null)
-						throw new UnsupportedOperationException("Clustering supported only in the Enterprise edition");
-					final UiFactory uif;
-					try {
-						uif = (UiFactory)_uiFactoryClass.newInstance();
-					} catch (Throwable ex) {
-						throw UiException.Aide.wrap(ex);
-					}
-					_uiFactory = uif;
-				}
-			}
-		}
-
-		return _uiFactory.newSession(wapp, nativeSess, request);
+		return new SerializableSession(wapp, (HttpSession)nativeSess, request);
 	}
 }
