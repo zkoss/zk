c47f51ea88a7b90eaba165d593202d27fd934853
tomyeh <tomyeh@zkoss.org>
2009-10-01 08:15:41 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@12628 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/layout/West.js
+++ zul/src/archive/web/js/zul/layout/West.js
@@ -12,7 +12,7 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 This program is distributed under LGPL Version 3.0 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
-zul.layout.West = zk.$extends(_zkf = zul.layout.LayoutRegion, zul.layout._West = {
+zul.layout.West = zk.$extends(_zkf = zul.layout.LayoutRegion, {
 	setHeight: zk.$void, // readonly
 	sanchor: 'l',
 	
