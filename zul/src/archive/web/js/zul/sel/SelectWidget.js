51d8696e112b03540eabf47de52c528525d5fc38
jumperchen <jumperchen@potix.com>
2009-11-05 08:16:55 +0000

    fine tune.
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@12929 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/SelectWidget.js
+++ zul/src/archive/web/js/zul/sel/SelectWidget.js
@@ -73,11 +73,11 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 			jq(this.efield).children().remove();
 			
 			// don't use jq.newHidden() in this case, because the performance is not good.
-			var data = [];
+			var data = [],
 				tmp = '<input type="hidden" name="' + this._name + '" value="';
-			for (var i = 0, j = this._selItems.length; i < j; i++) {
+			for (var i = 0, j = this._selItems.length; i < j; i++)
 				data.push(tmp, this._selItems[i].getValue(), '"/>');
-			}
+				
 			jq(this.efield).append(data.join(''));
 		} else if (this.efield) {
 			jq(this.efield).remove();
