433e5afb4e2d44ecbc22a7dbbdfd473d4fd99d17
henrichen <henrichen@zkoss.org>
2009-11-10 02:46:38 +0000

    Grid rod implementation.
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@12958 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/org/zkoss/zul/event/DataLoadingEvent.java
@@ -0,0 +1,70 @@
+/* DataLoadingEvent.java
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Oct 29, 2009 12:44:00 PM, Created by henrichen
+}}IS_NOTE
+
+Copyright (C) 2009 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zul.event;
+
+import java.util.Map;
+
+import org.zkoss.zk.au.AuRequest;
+import org.zkoss.zk.au.AuRequests;
+import org.zkoss.zk.mesg.MZk;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.UiException;
+import org.zkoss.zk.ui.event.Event;
+
+/**
+ * Represent onDataLoading event.
+ * 
+ * @author henrichen
+ * @since 5.0.0
+ */
+public class DataLoadingEvent extends Event {
+	private final int _offset;
+	private final int _limit;
+
+	/** Converts an AU request to a data loading event.
+	 */
+	public static final DataLoadingEvent getDataLoadingEvent(AuRequest request, int preload) {
+		final Map data = request.getData();
+		if (data == null)
+			throw new UiException(MZk.ILLEGAL_REQUEST_WRONG_DATA,
+				new Object[] {data, request});
+		return new DataLoadingEvent(request.getCommand(),
+			request.getComponent(),
+			AuRequests.getInt(data, "offset", 0), AuRequests.getInt(data, "limit", 20)+preload);
+	}
+
+	public DataLoadingEvent(String name, Component comp, int offset, int limit) {
+		super(name, comp);
+		_offset = offset;
+		_limit = limit;
+	}
+
+	/** Returns the offset of the data chunk to be loaded.
+	 */
+	public int getOffset() {
+		return _offset;
+	}
+	
+	/**
+	 * Returns the limit of the data chunk to be loaded.
+	 * @return
+	 */
+	public int getLimit() {
+		return _limit;
+	}
+}
