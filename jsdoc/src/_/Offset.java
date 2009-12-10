08b7959480a1655468e6bfeb8b05e7b6df8f82be
tomyeh <tomyeh@zkoss.org>
2009-12-10 03:44:24 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@13219 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ jsdoc/src/_/Offset.java
@@ -0,0 +1,25 @@
+/* Offset.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Wed Dec  9 12:40:11 TST 2009, Created by tomyeh
+
+Copyright (C) 2009 Potix Corporation. All Rights Reserved.
+
+*/
+package _;
+
+/**
+ * An offset is a two-element array, where the first element is the X coordinate, and the second the Y coordinate.
+ * <p>For example, zDom.cmOffset(el); returns an offset.
+ *
+ * <pre><code>var ofs = zDom.cmOffset(el);
+zk.debug("x,y = " + ofs[0] + "," + ofs[1]);</code></pre>
+ *
+ * @author tomyeh
+ */
+public class Offset {
+}
