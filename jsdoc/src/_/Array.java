ef30ed550742159dd880d71d599fa9ee2993a890
tomyeh <tomyeh@zkoss.org>
2009-12-14 04:17:07 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@13236 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- jsdoc/src/_/Array.java
+++ jsdoc/src/_/Array.java
@@ -19,15 +19,39 @@ package _;
  *
  * <p>In additions to <a href="http://www.w3schools.com/jsref/jsref_obj_array.asp">JavaScript Reference</a>
  * that JavaScript provide, ZK extends with the following methods.
+ *
+ * <p>To test if an object is an array, use <code>jq.isArray(obj)</code>
+ * where obj can be null.
+ *
  * @see Map
  * @see String
+ * @see Dimension
+ * @see Offset
  */
 public class Array {
-	/** A read-only field to indicate whether an object is an array. For example,
-<pre><code>
-if ([0].$array) //is true
-	always_do();
-</code></pre>
+	/** Clones this array.
+	 * <p>For example,
+	 * <pre><code>var anotherArray = ary.$clone();</code></pre>
 	 */
-	public boolean $array;
+	public Array $clone();
+	/** Returns the index in this list of the first occurrence of the
+	 * specified element, or -1 if this list does not contain this element.
+	 */
+	public int $indexOf(Object o);
+	/** Returns true if this list contains the specified element.
+	 */
+	public boolean $contains(Object o);
+	/** Removes the specified object from this array. 
+	 * @return whether the object is removed successfully.
+	 */
+	public boolean $remove(Object o);
+
+	/** Returns whether obj is an array, the length is the same, and
+	 * each element in obj is the same as each element of this array.
+	 * It will compare recursively.
+	 * <p>For example,
+	 * <pre><code>[0, ["s"]].$equals([0, ["s"]]) //is true</code></pre>
+	 * <p>Notice that == returns false in the above case. 
+	 */
+	public boolean $equals(Object o);
 }
