0ab0268af28b65f38d6769ff8574e7241f6132e5
samchuang <samchuang@potix.com>
2009-12-22 07:59:23 +0000

    Fixed Bug 2918527: Align property ignored in DIV
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@13294 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/wgt/Div.js
+++ zul/src/archive/web/js/zul/wgt/Div.js
@@ -13,4 +13,17 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 	it will be useful, but WITHOUT ANY WARRANTY.
 */
 zul.wgt.Div = zk.$extends(zul.Widget, {
+	$define: {
+		align: function (v) {
+			var n = this.$n();
+			if (n)
+				n.align = v;
+		}
+	},
+	//super//
+	domAttrs_: function(no) {
+		var align = this._align,
+			attr = this.$supers('domAttrs_', arguments);
+		return align != null ? attr + ' align="' + align + '"' : attr;
+	}
 });
