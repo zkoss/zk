0ad83eadd5a056137c128df71b7dceaf570ae4b1
tomyeh <tomyeh@zkoss.org>
2009-12-28 04:32:52 +0000

    jsdoc
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@13348 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/canvas/canvas.js
@@ -0,0 +1,22 @@
+/* canvas.js
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Dec 28 12:26:39 TST 2009, Created by tomyeh
+
+Copyright (C) 2009 Potix Corporation. All Rights Reserved.
+
+*/
+/** Canvas - 2D command-based drawing.
+ * Specifying this package in the depends attribute if one of your packages
+ * would like to use the canvas function. For example,
+ *<pre><code>//zk.wpd
+ *<package name="foo.whatever" language="xul/html" depends="zk.canvas">
+ *</code></pre>
+ *
+ * <p>This package is based on <a href="http://excanvas.sourceforge.net/">ExplorerCanvas</a>.
+ */
+//zk.$package('zk.canvas');
