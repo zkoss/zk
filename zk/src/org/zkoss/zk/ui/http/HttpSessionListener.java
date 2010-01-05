741e72d1058d0df1ee667624555b0be8e9bb9595
tomyeh <tomyeh@zkoss.org>
2010-01-05 07:57:31 +0000

    2926070: ZK 5 RC doesn't support Servlet 2.3
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@13414 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/http/HttpSessionListener.java
+++ zk/src/org/zkoss/zk/ui/http/HttpSessionListener.java
@@ -31,82 +31,8 @@ import org.zkoss.zk.ui.impl.Attributes;
  *
  * @author tomyeh
  */
-public class HttpSessionListener implements
-javax.servlet.http.HttpSessionListener, ServletRequestAttributeListener,
-HttpSessionAttributeListener, ServletContextAttributeListener {
-	//HttpSessionListener//
-	public void sessionCreated(HttpSessionEvent evt) {
-	}
-	public void sessionDestroyed(HttpSessionEvent evt) {
-		//Note: Session Fixation Protection (such as Spring Security)
-		//might invalidate HTTP session and restore with a new one.
-		//Thus, we use an attribute to denote this case and avoid the callback
-		final HttpSession hsess = evt.getSession();
-		if (hsess.getAttribute(Attributes.RENEW_NATIVE_SESSION) == null)
-			WebManager.sessionDestroyed(hsess);
-	}
-
-	//HttpSessionAttributeListener//
-	public void attributeAdded(HttpSessionBindingEvent evt) {
-		final String name = evt.getName();
-		if (!shallIgnore(name)) {
-			final Session sess = Sessions.getCurrent();
-			if (sess instanceof SimpleSession
-			&& evt.getSession().equals(sess.getNativeSession()))
-				((SimpleSession)sess).getScopeListeners()
-					.notifyAdded(name, evt.getValue());
-		}
-	}
-	public void attributeRemoved(HttpSessionBindingEvent evt) {
-		final String name = evt.getName();
-		if (!shallIgnore(name)) {
-			final Session sess = Sessions.getCurrent();
-			if (sess instanceof SimpleSession
-			&& evt.getSession().equals(sess.getNativeSession()))
-				((SimpleSession)sess).getScopeListeners()
-					.notifyRemoved(name);
-		}
-	}
-	public void attributeReplaced(HttpSessionBindingEvent evt) {
-		final String name = evt.getName();
-		if (!shallIgnore(name)) {
-			final Session sess = Sessions.getCurrent();
-			if (sess instanceof SimpleSession
-			&& evt.getSession().equals(sess.getNativeSession()))
-				((SimpleSession)sess).getScopeListeners()
-					.notifyReplaced(name, evt.getValue());
-		}
-	}
-
-	//ServletContextAttributeListener//
-	public void attributeAdded(ServletContextAttributeEvent evt) {
-		final String name = evt.getName();
-		if (!shallIgnore(name)) {
-			final WebApp wapp = WebManager.getWebAppIfAny(evt.getServletContext());
-			if (wapp instanceof SimpleWebApp)
-				((SimpleWebApp)wapp).getScopeListeners()
-					.notifyAdded(name, evt.getValue());
-		}
-	}
-	public void attributeRemoved(ServletContextAttributeEvent evt) {
-		final String name = evt.getName();
-		if (!shallIgnore(name)) {
-			final WebApp wapp = WebManager.getWebAppIfAny(evt.getServletContext());
-			if (wapp instanceof SimpleWebApp)
-				((SimpleWebApp)wapp).getScopeListeners()
-					.notifyRemoved(name);
-		}
-	}
-	public void attributeReplaced(ServletContextAttributeEvent evt) {
-		final String name = evt.getName();
-		if (!shallIgnore(name)) {
-			final WebApp wapp = WebManager.getWebAppIfAny(evt.getServletContext());
-			if (wapp instanceof SimpleWebApp)
-				((SimpleWebApp)wapp).getScopeListeners()
-					.notifyReplaced(name, evt.getValue());
-		}
-	}
-
+public class HttpSessionListener extends HttpSessionListener23
+implements ServletRequestAttributeListener {
 	// ServletRequestAttributeListener//
 	public void attributeAdded(ServletRequestAttributeEvent evt) {
 		final String name = evt.getName();
@@ -137,8 +63,5 @@ HttpSessionAttributeListener, ServletContextAttributeListener {
 				((ExecutionImpl)exec).getScopeListeners()
 					.notifyReplaced(name, evt.getValue());
 		}
-	}
-	private static boolean shallIgnore(String name) {
-		return name.startsWith("javax.zkoss") || name.startsWith("org.zkoss");
 	}
 }
