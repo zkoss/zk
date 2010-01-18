f63de8a4115c06c8f0a67eb47a53bde89ddaf5fc
tomyeh <tomyeh@zkoss.org>
2010-01-18 03:33:11 +0000

    fix JQueryPatch
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@13571 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkplus/src/org/zkoss/zkplus/liferay/JQueryRenderPatch.java
+++ zkplus/src/org/zkoss/zkplus/liferay/JQueryRenderPatch.java
@@ -21,7 +21,6 @@ import javax.servlet.ServletRequest;
 import org.zkoss.lang.Library;
 import org.zkoss.lang.Strings;
 import org.zkoss.util.logging.Log;
-import org.zkoss.web.servlet.Servlets;
 
 import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.sys.RequestInfo;
@@ -47,56 +46,60 @@ public class JQueryRenderPatch implements PageRenderPatch {
 	private static final Log log = Log.lookup(JQueryRenderPatch.class);
 
 	/** A library property to indicate how to apply the so-called jQuery patch.
-	 * <p>Default: "ie"
+	 * <p>Default: "500" (it means 500 milliseconds)
 	 *
-	 * <p>If the value is "ie", it is applied only to Internet Explorer.
-	 * Refer to {@link org.zkoss.web.servlet.Servlets#isBrowser} for more options.
-	 * If the value is "*", it is applied to all kinds of browsers.
-	 * <p>If you want to specify the number of milliseconds to wait before
-	 * replacing with the correct content, you could append a number
-	 * by separating it with comma. For example, "ie,1000" means to wait
-	 * 1000 miliseconds if the browser is Internet Explorer.
-	 * The default is 500 miliseconds.
+	 * <p>You can specify a number to indicate how many milliseconds to wait
+	 * before replacing with the correct content.
+	 * If negative, the patch is ignored.
 	 */
 	public static final String JQUERY_PATCH = "org.zkoss.zkplus.liferary.jQueryPatch";
 
-	private String _browser;
-	private int _delay;
+	private int _delay = -1;
 
 	public JQueryRenderPatch() {
-		_delay = 500;
-		_browser = Library.getProperty(JQUERY_PATCH);
-		if (_browser != null) {
-			final int j = _browser.indexOf(',');
-			if (j >= 0) {
-				final String v = _browser.substring(j + 1);
-				try {
-					_delay = Integer.parseInt(v);
-					if (_delay < 0)
-						_delay = 0;
-				} catch (Throwable ex) {
-					log.warning("Ignored delay time specified in "+JQUERY_PATCH+": "+_browser);
-				}
-				_browser = _browser.substring(0, j);
-			}
-			if (_browser.length() == 0)
-				_browser = null;
+		final String val = Library.getProperty(JQUERY_PATCH);
+		try {
+			_delay = Integer.parseInt(val);
+		} catch (Throwable ex) {
+			log.warning("Ignored delay time specified in "+JQUERY_PATCH+": "+val);
 		}
 	}
+	/** Returns the number of milliseconds to wait before replacing with
+	 * the correct content.
+	 * <p>Default: depends on the value defined in the {@link #JQUERY_PATCH}
+	 * library property.
+	 */
+	public int getDelay() {
+		return _delay;
+	}
+	/** Sets the number of milliseconds to wait before replacing with
+	 * the correct content.
+	 * @see #JQUERY_PATCH
+	 */
+	public void setDelay(int delay) {
+		_delay = delay;
+	}
+
 	//@Override
+	/** It returns an instance of StringWriter if {@link #getDelay} is non-negative,
+	 * or null if negative (means no patch).
+	 */
 	public Writer beforeRender(RequestInfo reqInfo) {
-		return _browser != null && ("*".equals(_browser)
-		|| Servlets.isBrowser((ServletRequest)reqInfo.getNativeRequest(), _browser)) ?
-			new StringWriter(): null;
+		return _delay >= 0 ? new StringWriter(): null;
+			//we cannot retrieve HTTP request's header so no need to
+			//apply the patch for particular browsers, such as ie
 	}
 	//@Override
 	public void patchRender(RequestInfo reqInfo, Page page, Writer result, Writer out)
 	throws IOException {
+		final String extid = page.getUuid() + "-ext";
 		out.write("<div id=\"");
-		out.write(page.getUuid());
+		out.write(extid);
 		out.write("\"></div><script>setTimeout(function(){\njQuery('#");
-		out.write(page.getUuid());
-		out.write("').replaceWith('");
+		out.write(extid);
+		out.write("').append('");
+			//we have to use append() since it is evaluated synchronously
+			//while replaceWith() is not
 		out.write(Strings.escape(((StringWriter)result).toString(), Strings.ESCAPE_JAVASCRIPT));
 		out.write("');},");
 		out.write(_delay);
