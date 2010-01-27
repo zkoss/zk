1d8634926ab52f91139c2483e3d5114352612ff2
jumperchen <jumperchen@potix.com>
2010-01-27 03:42:57 +0000

    Bug 2939911: datebox display year incorectly(Thai locale)
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@13706 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/html/common.js
+++ zk/src/archive/web/js/zk/html/common.js
@@ -2073,16 +2073,17 @@ zk.parseDate = function (txt, fmt, strict) {
 		hasHour1 = hasAM ? hh > -1 || KK > -1 : false,
 		isAM;
 
-	var	ts = [], mindex = fmt.indexOf("MMM"), aindex = fmt.indexOf("a"), ary = [];
+	var	ts = [], mindex = fmt.indexOf("MMM"), aindex = fmt.indexOf("a"), ary = [],
+		mmindex = mindex + 3;
 	for (var i = 0, j = txt.length; i < j; i++) {
 		var c = txt.charAt(i);
 		if (c.match(/\d/)) {
 			ary.push(c);
-		} else if ((mindex > -1 && mindex <= i) || (aindex > -1 && aindex <= i)) {
+		} else if ((mindex > -1 && mindex <= i && mmindex >= i) || (aindex > -1 && aindex <= i)) {
 			if (c.match(/\w/)) {
 				ary.push(c);
 			} else {
-				if (c.charCodeAt() < 128) {
+				if (c.charCodeAt() < 128 && c.charCodeAt() != 46) {
 					if (ary.length) {
 						ts.push(ary.join(""));
 						ary = [];
