990c48b23e8153037c348235fdc30455f602180c
tomyeh <tomyeh@zkoss.org>
2010-04-28 07:12:34 +0000

    2983365: Able to get application event queue on a worker thread
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@14271 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/event/impl/EventQueueProviderImpl.java
+++ zk/src/org/zkoss/zk/ui/event/impl/EventQueueProviderImpl.java
@@ -19,6 +19,7 @@ import org.zkoss.zk.ui.Execution;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Desktop;
 import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.Session;
 import org.zkoss.zk.ui.ext.Scope;
 import org.zkoss.zk.ui.event.*;
 
@@ -40,22 +41,8 @@ public class EventQueueProviderImpl implements EventQueueProvider {
 
 		final boolean bAppScope = EventQueues.APPLICATION.equals(scope);
 		if (bAppScope || EventQueues.SESSION.equals(scope)) {
-			Map eqs;
-			final Scope ctxscope = bAppScope ?
-				(Scope)exec.getDesktop().getWebApp(): exec.getSession();
-			synchronized (ctxscope) {
-				eqs = (Map)ctxscope.getAttribute(ATTR_EVENT_QUEUES);
-				if (eqs == null)
-					ctxscope.setAttribute(ATTR_EVENT_QUEUES, eqs = new HashMap(4));
-			}
-
-			EventQueue eq;
-			synchronized (eqs) {
-				eq = (EventQueue)eqs.get(name);
-				if (autoCreate && eq == null)
-					eqs.put(name, eq = new ServerPushEventQueue());
-			}
-			return eq;
+			return lookup0(name, bAppScope ?
+				(Scope)exec.getDesktop().getWebApp(): exec.getSession(), autoCreate);
 		} else  if (EventQueues.DESKTOP.equals(scope)) {
 			final Desktop desktop = exec.getDesktop();
 			Map eqs = (Map)desktop.getAttribute(ATTR_EVENT_QUEUES);
@@ -68,6 +55,29 @@ public class EventQueueProviderImpl implements EventQueueProvider {
 			return eq;
 		} else
 			throw new UnsupportedOperationException("Unknown scope: "+scope);
+	}
+	public EventQueue lookup(String name, Session sess, boolean autoCreate) {
+		return lookup0(name, sess, autoCreate);
+	}
+	public EventQueue lookup(String name, WebApp wapp, boolean autoCreate) {
+		return lookup0(name, wapp, autoCreate);
+	}
+	/** Looks up a session or application scoped event queue. */
+	private EventQueue lookup0(String name, Scope ctxscope, boolean autoCreate) {
+		Map eqs;
+		synchronized (ctxscope) {
+			eqs = (Map)ctxscope.getAttribute(ATTR_EVENT_QUEUES);
+			if (eqs == null)
+				ctxscope.setAttribute(ATTR_EVENT_QUEUES, eqs = new HashMap(4));
+		}
+
+		EventQueue eq;
+		synchronized (eqs) {
+			eq = (EventQueue)eqs.get(name);
+			if (autoCreate && eq == null)
+				eqs.put(name, eq = new ServerPushEventQueue());
+		}
+		return eq;
 	}
 	public boolean remove(String name, String scope) {
 		final Execution exec = Executions.getCurrent();
