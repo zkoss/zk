86b0b2c20964db5838eea14b334e4381f7fdf099
tomyeh <tomyeh@zkoss.org>
2010-06-09 04:40:10 +0000

    3007738: The setImage of the comboitem isn't works
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@14552 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/LabelImageWidget.js
+++ zul/src/archive/web/js/zul/LabelImageWidget.js
@@ -41,6 +41,8 @@ zul.LabelImageWidget = zk.$extends(zul.Widget, {
 		image: function (v) {
 			var n = this.getImageNode();
 			if (n) n.src = v || '';
+			else (this.desktop) //<IMG> might not be generated (Bug 3007738)
+				this.updateDomContent_();
 		},
 		/** Sets the image URI.
 		 * The hover image is used when the mouse is moving over this component.
