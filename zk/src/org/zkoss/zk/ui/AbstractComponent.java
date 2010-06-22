2644c5e30fa5c7d79e7d5dbf95e54c911a72b8ab
tomyeh <tomyeh@zkoss.org>
2010-06-22 04:14:15 +0000

    3019383: Failed to change the position of a root component to the end
    git-svn-id: https://svn.potix.com/svn/zk1/trunk@14639 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/AbstractComponent.java
+++ zk/src/org/zkoss/zk/ui/AbstractComponent.java
@@ -454,9 +454,12 @@ implements Component, ComponentCtrl, java.io.Serializable {
 			setPageBefore(page, null); //append
 	}
 	public void setPageBefore(Page page, Component refRoot) {
-		if (refRoot != null && (page == null || refRoot == this
+		if (refRoot != null && (page == null
 		|| refRoot.getParent() != null || refRoot.getPage() != page))
 			refRoot = null;
+		if (refRoot != null /*&& refRoot.getPage() == page (checked)*/
+		&& (refRoot == this || refRoot == _next))
+			return; //nothing to do
 
 		if (_parent != null)
 			throw new UiException("Only the parent of a root component can be changed: "+this);
