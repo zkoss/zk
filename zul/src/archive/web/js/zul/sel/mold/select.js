b93314401306fa18a28d65f8bc1611e1e2a85d7a
samchuang <samchuang@potix.com>
2010-07-07 09:41:12 +0000

    Fixed bug 3025674: When Listbox mold=select, add listhead cause html error, fine-tune code
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@14778 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/mold/select.js
+++ zul/src/archive/web/js/zul/sel/mold/select.js
@@ -16,10 +16,7 @@ function (out) {
 	out.push('<select', this.domAttrs_(), '>');
 	
 	for (var w = this.firstChild; w; w = w.nextSibling) {
-		if (w.$instanceof(zul.sel.Listhead) || w.$instanceof(zul.mesh.Auxhead))
-			continue;
-
-		if (w.isVisible()) w.redraw(out);
+		if (w.$instanceof(zul.sel.Option) && w.isVisible()) w.redraw(out);
 	}
 		
 	out.push('</select>');
