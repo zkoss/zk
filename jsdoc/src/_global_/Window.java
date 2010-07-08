6cf28ddbe4f0efaac06dd0e4517c62f49f70d794
tomyeh <tomyeh@zkoss.org>
2010-07-08 14:36:33 +0000

    jsdoc
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@14788 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ jsdoc/src/_global_/Window.java
@@ -0,0 +1,21 @@
+/* Window.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Jul  8 11:18:35 TST 2010, Created by tomyeh
+
+Copyright (C) 2009 Potix Corporation. All Rights Reserved.
+
+*/
+package _global_;
+
+/**
+ * Window object representing an open window in a browser.
+ * <p>Refer to <a href="http://wwww.w3schools.com/jsref/obj_window.asp">JavaScript Reference</a>.
+ * @author tomyeh
+ */
+public class Window {
+}
---
+++ jsdoc/src/_global_/XMLHttpRequest.java
@@ -0,0 +1,21 @@
+/* XMLHttpRequest.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Jul  5 10:21:13 TST 2010, Created by tomyeh
+
+Copyright (C) 2009 Potix Corporation. All Rights Reserved.
+
+*/
+package _global_;
+
+/**
+ * The XMLHttpRequest specification defines an API that provides scripted client functionality for transferring data between a client and a server.
+ * Refer <a href="http://www.w3schools.com/xml/xml_http.asp">here</a> for details.
+ * @author tomyeh
+ */
+public class XMLHttpRequest {
+}
