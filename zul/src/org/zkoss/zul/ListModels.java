d33cb49e0aa0a61c498aa3f202743218f2826f2a
jumperchen <jumperchen@potix.com>
2010-08-26 00:55:11 +0000

    Fine tune feature 3052857: Provide a utile class for ListModel to support ListSubModel
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@15135 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/ListModels.java
+++ zul/src/org/zkoss/zul/ListModels.java
@@ -33,17 +33,17 @@ import org.zkoss.zul.event.ListDataListener;
  * @author jumperchen
  * @since 5.0.4
  */
-public class ListModels implements ListModel, ListSubModel, java.io.Serializable {
-	private final ListModel _model;
-	private final Comparator _comparator;
-	private final int _nRows;
+public class ListModels {
+	private static final ListModels INSTANCE = new ListModels();
+	
+	private ListModels() {}
 	
 	/**
 	 *  A String comparator for {@link ListSubModel} to match the value of
 	 *  the model from the given key that user typed.
 	 *  It will return 0 when the value starts with the key(no empty). 
 	 */
-	public static Comparator STRING_COMPARATOR = new Comparator() {
+	public final static Comparator STRING_COMPARATOR = new Comparator() {
 		public int compare(Object key, Object value) {
 			String idx = Objects.toString(key);
 			return idx != null && value != null && idx.length() > 0 &&
@@ -56,7 +56,7 @@ public class ListModels implements ListModel, ListSubModel, java.io.Serializable
 	 *  the model from the given key that user typed.
 	 *  It will return 0 when the value starts with the key(no empty). 
 	 */
-	public static Comparator MAP_COMPARATOR = new Comparator() {
+	public final static Comparator MAP_COMPARATOR = new Comparator() {
 		public int compare(Object key, Object value) {
 			String idx = Objects.toString(key);
 			return idx != null && value != null && idx.length() > 0 &&
@@ -64,73 +64,90 @@ public class ListModels implements ListModel, ListSubModel, java.io.Serializable
 		}
 	};
 	
-	private ListModels(ListModel model, Comparator comparator, int nRows) {
-		_model = model;
-		_comparator = comparator;
-		_nRows = nRows;
-	}
-	
 	/**
-	 * Returns a {@link ListSubModel} instance which is a proxy of the given model.
+	 * Returns a proxy instance of the given model that implements
+	 * {@link ListSubModel} and {@link ListModel} interface.
 	 * @param model a {@link ListModel}
 	 * @param comparator if return 0 means the value of the model is matched from
 	 *  the given key that user typed
 	 * @param nRows the maximal allowed number of matched items.
 	 */
-	public static ListModels getListSubModel(ListModel model, Comparator comparator, int nRows) {
-		return new ListModels(model, comparator, nRows);
+	public static ListModel toListSubModel(ListModel model, Comparator comparator, int nRows) {
+		return INSTANCE.new ListModels0(model, comparator, nRows);
 	}
 	
 	/**
-	 * Returns a {@link ListSubModel} instance which is a proxy of the given model.
+	 * Returns a proxy instance of the given model that implements
+	 * {@link ListSubModel} and {@link ListModel} interface.
 	 * <p> The default comparator is depended on the type of the model, if the
 	 * model is a type of {@link ListModelMap}, the {@link #MAP_COMPARATOR} is used,
 	 * unless the {@link #STRING_COMPARATOR} is used.
-	 * <p>Default: <code>nRows < 0 ? 15: nRows</code>.
+	 * <p>Default: <code>nRows is 15</code>.
 	 * @param model a {@link ListModel}
+	 * @see #toListSubModel(ListModel, Comparator, int)
 	 */
-	public static ListModels getListSubModel(ListModel model) {
-		return new ListModels(model, (model instanceof ListModelMap) ? MAP_COMPARATOR
+	public static ListModel toListSubModel(ListModel model) {
+		return INSTANCE.new ListModels0(model, (model instanceof ListModelMap) ? MAP_COMPARATOR
 					: STRING_COMPARATOR, 15);
 	}
-	/**
-	 * Returns the subset of the list model data that matches
-	 * the specified value.
-	 * It is usually used for implementation of auto-complete.
-	 *
-	 * @param value the value to retrieve the subset of the list model.
-	 * It is the key argument when invoking {@link #inSubModel}.
-	 * this string.
-	 * @param nRows the maximal allowed number of matched items.
-	 * If negative, it means the caller allows any number, but the implementation
-	 * usually limits to a certain number (for better performance).
-	 */
-	public ListModel getSubModel(Object value, int nRows) {
-		final LinkedList data = new LinkedList();
-		nRows = nRows < 0 ? _nRows : nRows;
-		for (int i = 0, j = _model.getSize(); i < j; i++) {
-			Object o = _model.getElementAt(i);
-			if (_comparator.compare(value, o) == 0) {
-				data.add(o);
-				if (--nRows <= 0) break; //done
-			}
+	
+	private class ListModels0 implements ListModel, ListSubModel,
+			java.io.Serializable {
+		private final ListModel _model;
+
+		private final Comparator _comparator;
+
+		private final int _nRows;
+
+		private ListModels0(ListModel model, Comparator comparator, int nRows) {
+			_model = model;
+			_comparator = comparator;
+			_nRows = nRows;
 		}
-		return new ListModelList(data);
-	}
 
-	public Object getElementAt(int index) {
-		return _model.getElementAt(index);
-	}
+		/**
+		 * Returns the subset of the list model data that matches the specified
+		 * value. It is usually used for implementation of auto-complete.
+		 * 
+		 * @param value
+		 *            the value to retrieve the subset of the list model. It is
+		 *            the key argument when invoking {@link #inSubModel}. this
+		 *            string.
+		 * @param nRows
+		 *            the maximal allowed number of matched items. If negative,
+		 *            it means the caller allows any number, but the
+		 *            implementation usually limits to a certain number (for
+		 *            better performance).
+		 */
+		public ListModel getSubModel(Object value, int nRows) {
+			final LinkedList data = new LinkedList();
+			nRows = nRows < 0 ? _nRows : nRows;
+			for (int i = 0, j = _model.getSize(); i < j; i++) {
+				Object o = _model.getElementAt(i);
+				if (_comparator.compare(value, o) == 0) {
+					data.add(o);
+					if (--nRows <= 0)
+						break; // done
+				}
+			}
+			return new ListModelList(data);
+		}
 
-	public int getSize() {
-		return _model.getSize();
-	}
+		public Object getElementAt(int index) {
+			return _model.getElementAt(index);
+		}
 
-	public void addListDataListener(ListDataListener l) {
-		_model.addListDataListener(l);
-	}
+		public int getSize() {
+			return _model.getSize();
+		}
 
-	public void removeListDataListener(ListDataListener l) {
-		_model.removeListDataListener(l);
+		public void addListDataListener(ListDataListener l) {
+			_model.addListDataListener(l);
+		}
+
+		public void removeListDataListener(ListDataListener l) {
+			_model.removeListDataListener(l);
+		}
 	}
+	
 }
