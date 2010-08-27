6d338412db0e48b726e7b8b39ba33ac98f358a78
jumperchen <jumperchen@potix.com>
2010-08-27 01:32:59 +0000

    Fine tune
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@15155 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkdemo/src/archive/userguide/macros/category.js
+++ zkdemo/src/archive/userguide/macros/category.js
@@ -6,7 +6,7 @@ userguide.Categorybar = zk.$extends(zul.wgt.Div, {
 	bind_: function () {
 		this.$supers('bind_', arguments);
 		zk(this.$n()).disableSelection();
-		
+
 		zWatch.listen({onSize: this, beforeSize: this});
 		this.onChildAdded_();//for updating sum of category width
 		this.scrollEvent = false;
@@ -15,78 +15,61 @@ userguide.Categorybar = zk.$extends(zul.wgt.Div, {
 		zWatch.unlisten({onSize: this, beforeSize: this});
 		this.$supers('unbind_', arguments);
 	},
-	beforeSize: zk.ie6_ ? function(){
-			this.$n("body").style.width = "";
-			jq(this.$n("body")).removeClass("demo-categorybar-body-scroll");
+	beforeSize: zk.ie6_ ? function () {
+		jq(this.$n("body")).css("width", "").removeClass("demo-categorybar-body-scroll");
 	}: zk.$void,
-	onSize: function(){	
+	onSize: function(){
 		var width = this.$n().offsetWidth;
 		//with scorll or not
 		if(width < this.childWidth){
 			jq(this.$n("left")).addClass("demo-categorybar-left-scroll");
 			jq(this.$n("right")).addClass("demo-categorybar-right-scroll");
 			jq(this.$n("body")).addClass("demo-categorybar-body-scroll")
-								.width((width-40)+"px");
-			this._addScollEvent();	
+								.width(jq.px0(width- zk(this.$n('body')).sumStyles('lr',jq.margins)));
+			this._addScollEvent();
 		}else{
 			jq(this.$n("left")).removeClass("demo-categorybar-left-scroll");
 			jq(this.$n("right")).removeClass("demo-categorybar-right-scroll");
 			jq(this.$n("body")).removeClass("demo-categorybar-body-scroll")
-								.width(width+"px");
+								.width(jq.px0(width));
 			this.$n("cave").style.marginLeft="0px";
 		}
-		
+
 	},
 	_addScollEvent:function(){
 		//update the animiation distance every time it called
 		this.$n("cave").distance = jq(this.$n("body")).width() - this.childWidth;
-		
+
 		if(this.scrollEvent == false){	//only do at first time
-			var cave = this.$n("cave");
-			jq(this.$n("left"))
-			.mouseover(function(){
-				jq(cave).animate({
+			var cave = this.$n("cave"),
+				$cave = jq(cave),
+				stop = function () {
+					$cave.stop(true);
+				};
+			jq(this.$n("left")).mouseover(function () {
+				$cave.animate({
 					marginLeft: "0px"
-				},400);
-			})
-			.mouseout(function(){
-				jq(cave).stop(true);
-			});
-			
-			jq(this.$n("right"))
-			.mouseover(function(){
-				jq(cave).animate({
+				}, Math.abs(zk.parseInt($cave.css('marginLeft'))/80) * 400);
+			}).mouseout(stop);
+			jq(this.$n("right")).mouseover(function () {
+				$cave.animate({
 					marginLeft: cave.distance + "px"
-				},400);
-			})
-			.mouseout(function(){
-				jq(cave).stop(true);
-			});	
+				}, Math.abs((cave.distance - zk.parseInt($cave.css('marginLeft')))/80) * 400);
+			}).mouseout(stop);
 			this.scrollEvent = true;
 		}
 	},
-	onChildAdded_: function (/*child*/) {
+	onChildAdded_: _zkf = function (/*child*/) {
 		if(this.desktop){
-			var childWidth=0;
-			jq(this.$n("cave")).children().each(
-				function(){
-					childWidth += jq(this).width();
+			var childWidth=0, cave = this.$n("cave"), $cave = jq(cave);
+			$cave.children().each(function() {
+				childWidth += jq(this).width();
 			});
-			jq(this.$n("cave")).width(childWidth+"px");
+			$cave.width(jq.px0(childWidth));
 			this.childWidth = childWidth;
 		}
 	},
-	onChildRemoved_: function (/*child*/) {
-		if(this.desktop){
-			var childWidth=0;
-			jq(this.$n("cave")).children().each(
-				function(){
-					childWidth += jq(this).width();
-			});
-			jq(this.$n("cave")).width(childWidth+"px");
-			this.childWidth = childWidth;
-		}
-	},
+	onChildRemoved_: _zkf,
 	redraw: function (out) {
 		var uuid = this.uuid;
 		out.push('<div', this.domAttrs_(), '>',
@@ -98,7 +81,6 @@ userguide.Categorybar = zk.$extends(zul.wgt.Div, {
 			w.redraw(out);
 		out.push('<div class="z-clear"></div></div></div></div>');
 	}
-
 });
 });
 
@@ -106,13 +88,11 @@ zk.load('zul.wgt', function () {
 userguide.Category = zk.$extends(zul.wgt.Button, {
 	redraw: function (out) {
 		var zcls = this.getZclass();
-		out.push('<div', this.domAttrs_(), '>');
-			out.push('<img id="', this.uuid, '-img"','src="',this.getImage() ,'" title="',this.getLabel(),'" class="', zcls,'-img"/>');
-			
-			out.push('<div id="',this.uuid, '-label" class="', zcls,'-text">');
-			
-			out.push(this.getLabel());
-		out.push('</div></div>');
+		out.push('<div', this.domAttrs_(), '>',
+				'<img id="', this.uuid, '-img"','src="',this.getImage() ,
+				'" title="',this.getLabel(),'" class="', zcls,'-img"/>',
+				'<div id="',this.uuid, '-label" class="', zcls,'-text">',
+				this.getLabel(),'</div></div>');
 	},
 	doMouseOver_: function (evt) {
 		jq(this.$n()).addClass("demo-over");
