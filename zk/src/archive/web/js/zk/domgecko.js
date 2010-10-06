8258208d6133ef6623973affd52fc43b3cfc066e
tomyeh <tomyeh@zkoss.org>
2010-10-06 07:46:25 +0000

    3076384: ComponentNotFoundException by closing AMedia Also apply to Safari and Chrome (since it is harmless and the user reported it happens too)
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@15399 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/domgecko.js
+++ zk/src/archive/web/js/zk/domgecko.js
@@ -11,7 +11,7 @@ Copyright (C) 2009 Potix Corporation. All Rights Reserved.
 
 */
 zk.copy(zjq.prototype, {
-	beforeHideOnUnbind: function () {
+	beforeHideOnUnbind: function () { //Bug 3076384
 		return this.jq.each(function () {
 			for (var ns = this.getElementsByTagName("iframe"), j = ns.length; j--;)
 				ns[j].src = zjq._src0;
