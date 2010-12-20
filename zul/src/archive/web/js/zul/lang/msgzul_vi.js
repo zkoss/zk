d15851541f3a316b5a58f71e1e37c0603e7b2329
jumperchen <jumperchen@potix.com>
2010-12-20 09:32:40 +0000

    Feature 3139008: Add Vietnamese language
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@15852 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zul/src/archive/web/js/zul/lang/msgzul_vi.js
@@ -0,0 +1,23 @@
+/* mesg.js "charset=utf-8"
+  
+{{IS_NOTE
+    Purpose:
+          
+    Description:
+          
+    History:
+        Fri Dec 17 15:56:48     2010, Created by thuocthai at gmail dot com
+}}IS_NOTE
+  
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+  
+{{IS_RIGHT
+    This program is distributed under GPL Version 3.0 in the hope that
+    it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzul = {};
+msgzul.UNKNOWN_TYPE = "Khong xac ??nh lo?i thanh ph?n: ";
+msgzul.DATE_REQUIRED = "B?n ph?i ch? ??nh ngay. ??nh d?ng: ";
+msgzul.OUT_OF_RANGE = "Ngoai ph?m vi cho phep";
+msgzul.NO_AUDIO_SUPPORT = "Trinh duy?t c?a b?n khong ???c h? tr? am thanh ??ng";
