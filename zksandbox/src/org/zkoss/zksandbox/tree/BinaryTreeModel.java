03deccacf6f863ef79b0fabf779485a00d957aae
jimmyshiau <jimmyshiau@potix.com>
2010-12-31 09:27:56 +0000

    add unimplemented method in the BinaryTreeModel
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@15891 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zksandbox/src/org/zkoss/zksandbox/tree/BinaryTreeModel.java
+++ zksandbox/src/org/zkoss/zksandbox/tree/BinaryTreeModel.java
@@ -66,4 +66,10 @@ public class BinaryTreeModel extends AbstractTreeModel{
 		return (getChildCount(node) == 0);
 	}
 
+	@Override
+	public int getIndexOfChild(Object parent, Object child) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
