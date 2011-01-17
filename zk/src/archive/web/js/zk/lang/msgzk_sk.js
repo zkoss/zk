325c6ca3f370f4b3f4bfd23675f03859c9415036
jimmyshiau <jimmyshiau@potix.com>
2011-01-17 04:56:58 +0000

    Feature 3159644: Message translated to Slovak. Thank Ondrej Jombik for his contribution http://www.zkoss.org/services/zk/tracker/?F50-3159644
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@15958 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zk/src/archive/web/js/zk/lang/msgzk_sk.js
@@ -0,0 +1,35 @@
+/* msgzk_sk.js "charset=utf-8"
+
+	Purpose:
+		Locale dependent message
+	Description:
+		
+	History: Created by Ondrej Jombík (ojombik@gmail.com) 11 January 2011
+
+Copyright (C) 2005-2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under LGPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+msgzk = {};
+msgzk.NOT_FOUND = "Nenájdené: ";
+msgzk.UNSUPPORTED = "Nepodporované: ";
+msgzk.FAILED_TO_SEND = "Chyba pri komunikácii so serverom.";
+msgzk.FAILED_TO_RESPONSE = "Server je dočasne nedostupný, prosím opakujte akciu neskôr.";
+msgzk.TRY_AGAIN = "Skúsiť znovu?";
+msgzk.UNSUPPORTED_BROWSER = "Váš prehliadač nie je podporovaný: ";
+msgzk.ILLEGAL_RESPONSE = "Neznáma odpoveď od servera, opakujte prosím akciu.\n";
+msgzk.FAILED_TO_PROCESS = "Chyba spracovania ";
+msgzk.GOTO_ERROR_FIELD = "Choď na pole s chybou";
+msgzk.PLEASE_WAIT = "Pracujem...";
+
+msgzk.FILE_SIZE = "Veľkosť súboru: ";
+msgzk.KBYTES = "KB";
+
+msgzk.FAILED_TO_LOAD = "Chyba pri nahrávaní ";
+msgzk.FAILED_TO_LOAD_DETAIL = "Chyba môže byť spôsobená zlým pripojením, skúste obnoviť stránku";
+msgzk.CAUSE = "Príčina: ";
+
+msgzk.LOADING = "Pracujem";
---
+++ zul/src/archive/web/js/zul/lang/msgzul_sk.js
@@ -0,0 +1,18 @@
+/* msgzul_sr.js "charset=utf-8"
+
+	Purpose:
+		
+	Description:
+		
+	History: Created by Ondrej Jombík (ojombik@gmail.com) 11 January 2011
+
+Copyright (C) 2005-2011 Potix Corporation. All Rights Reserved.
+
+	This program is distributed under LGPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+*/
+msgzul={};
+msgzul.UNKNOWN_TYPE="Neznámy typ komponentu: ";
+msgzul.DATE_REQUIRED="Je nutné zadať dátum. Formát: ";
+msgzul.OUT_OF_RANGE = "Hodnota je mimo daný rozsah";
+msgzul.NO_AUDIO_SUPPORT="Váš prehliadač nepodporuje streamované audio";
