3fc0b961d611779e15badbf18fe0ee9a7bebfd48
simonpai <simonpai@potix.com>
2011-01-26 08:35:45 +0000

    fine tune, fixed the case when format is "#%" and value is near zero
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@16057 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/fmt/numfmt.js
+++ zk/src/archive/web/js/zk/fmt/numfmt.js
@@ -266,6 +266,8 @@ zk.fmt.Number = {
 			pre = fmt.substring(0, prej) + this._removePrefixSharps(pre);
 		if (!pre && fmt.charAt(indFmt+1) == '#')
 			pre = '0';
+		if (!suf && (pre == zk.PERCENT || pre == zk.PER_MILL))
+			pre = '0' + pre;
 		var rexp = new RegExp('^0*['+zk.PERCENT+'|'+zk.PER_MILL+']?$'),
 			shownZero = suf? rexp.test(suf) && /^0*$/.test(pre) : rexp.test(pre);
 		return (val < 0 && !shownZero && !useMinsuFmt? zk.MINUS : '') + (suf ? pre + (/[\d]/.test(suf.charAt(0)) ? zk.DECIMAL : '') + suf : pre);
