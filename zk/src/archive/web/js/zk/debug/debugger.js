81b28d7d4d152c391f1eb45b1da9176f8ceb0895
jumperchen <jumperchen@potix.com>
2011-01-31 09:32:24 +0000

    add a debugger function about dumping widget for zul
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@16118 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/archive/web/js/zk/debug/debugger.js
+++ zk/src/archive/web/js/zk/debug/debugger.js
@@ -31,6 +31,116 @@ function _dumpWgt(out, wgt, nLevel, inf) {
 	for (wgt = wgt.firstChild; wgt; wgt = wgt.nextSibling)
 		_dumpWgt(out, wgt, nLevel, inf);
 }
+var _defaultIgnore = {draggable: 'false', droppable: 'false', mold: 'default', colspan: 1,
+		scrollTop: 0, scrollLeft: 0, innerWidth: '100%', cols: 0, model: true,
+		sortDirection: 'natural', sortAscending: 'none', sortDescending: 'none',
+		columnshide: true, columnsgroup: true};
+var _specialIgnore = {
+	treecell: {width: 1},
+	rows: {visibleItemCount: 1},
+	columns: {menupopup: 1},
+	treeitem: {image: 1, label: 1, zclass: 1},
+	listitem: {label: 1, zclass: 1},
+	include: {content: 1},
+	center: {maxsize: 1, minsize: 1, cmargins: 1, margins: 1, open: 1},
+	paging: {pageCount: 1}
+};
+
+function _dumpAttrs(wgt) {
+	var out = [];
+	for (var nm in wgt) {
+		if (nm.startsWith('get') && nm.length > 3 && !nm.endsWith('_')) {
+			var setting = 's' + nm.substring(1);
+			if (typeof wgt[setting] == 'function') {
+    			var key = nm.charAt(3).toLowerCase() + nm.substring(4);
+    			try {
+    				if (_specialIgnore[wgt.widgetName] && _specialIgnore[wgt.widgetName][key])
+    					continue;
+    				
+    				var value = wgt[nm]();
+    				if (typeof value != 'object' && typeof value != 'function' && value != null && value !== '') {
+    					if (_defaultIgnore[key] === undefined) {
+    						if (key != 'zclass' || value != 'z-' + wgt.widgetName) {
+    							if (key == "selectedIndex")
+    								out.push(' onCreate="self.selectedIndex = ', value, '"');
+    							else
+    								out.push(' ', key, '="', zUtl.encodeXML(zUtl.encodeXML(value)), '"');
+    						}
+    					} else if (_defaultIgnore[key] !== value)
+    						out.push(' ', key, '="', value, '"');
+    				}
+    					
+    			} catch (e) {}
+			}
+		}
+	}
+	return out.join('');
+}
+function _dumpWgt4Zul(out, wgt, nLevel, inf) {
+	inf.cnt++;
+	var nm = wgt.widgetName;
+	
+	if (nm == 'native') {
+		nm = wgt.$n() ? 'h:' + wgt.$n().tagName : wgt.epilog ? 'h:' + (wgt.epilog.match(/<\/?(.+)>/)[1]) : wgt.widgetName;
+		if (nm == 'native') {
+			if (wgt.prolog.trim())
+				nm = 'h:' + wgt.prolog.match(/<(.+)\/>/)[1];
+			else
+				nm = 'h:span';
+		}
+	}
+	
+	if (nm == 'script' || nm == 'paging' && wgt.parent.$instanceof(zul.mesh.MeshWidget))
+		return;
+	else if (nm == 'text')
+		nm = 'h:' + nm;
+	else if (nm == 'select')
+		nm = 'listbox';
+	else if (nm == 'option') {
+		nm = 'listitem';
+		var attrs =  _dumpAttrs(wgt).replace('mold="select"', '');
+		out.push(_space(nLevel++), '&lt;', nm, attrs);
+		if (wgt.firstChild) {
+			inf.cnt++;
+			out.push(' label="', zUtl.encodeXML(zUtl.encodeXML(wgt.firstChild.getLabel())), '"/&gt;<br/>');
+		} else
+			out.push('/&gt;<br/>');
+		return;
+	} else if (nm == 'include')
+		out.push('&lt;!--');
+	
+	
+	out.push(_space(nLevel++), '&lt;', nm, _dumpAttrs(wgt));
+	
+	if (wgt.firstChild) {
+		out.push('&gt;');
+		var isPage = nm == 'page' && wgt.parent.$instanceof(zul.wgt.Include)
+				|| wgt.firstChild.widgetName != 'page' && wgt.$instanceof(zul.wgt.Include);
+		
+		if (isPage)
+			out.push('--&gt;');
+		
+		out.push('<br/>');
+		
+		for (wgt = wgt.firstChild; wgt; wgt = wgt.nextSibling)
+			_dumpWgt4Zul(out, wgt, nLevel, inf);
+		
+		if (isPage) {
+			out.push('&lt;!--');
+		}
+		out.push(_space(--nLevel), '&lt;/', nm, '&gt;<br/>');
+		if (nm == 'include') {
+			out.push('--&gt;');
+		}
+	} else {
+		out.push('/&gt;<br/>');
+		if (nm == 'include')
+			out.push('--&gt;');
+		else if (nm == 'page' && wgt.parent.$instanceof(zul.wgt.Include)) {
+			out.push('--&gt;');
+		}
+	}
+}
 function _parseHTML(text, handler) {
 	var begin, content, deep = 0, empty;
 		
@@ -100,6 +210,7 @@ zk.debug.Debugger = zk.$extends(zk.Object, {
 		}
 		return console;
 	},
+	// don't change the function name, otherwise, zkjet will break.
 	dumpDomTree: function (wgt, handler) {
 		var text;
 		if (wgt && typeof wgt.$instanceof == 'function' && wgt.$instanceof(zk.Widget)) {
@@ -122,6 +233,13 @@ zk.debug.Debugger = zk.$extends(zk.Object, {
 	dumpWidgetTree: function (wgt) {
 		var out = [], inf = {cnt: 0};
 		_dumpWgt(out, wgt, 0, inf);
+		this._dump("Total: "+inf.cnt, out.join(''));
+	},
+	// don't change the function name, otherwise, zkjet will break.
+	dumpWidgetTree4Zul: function (wgt) {
+		var out = ['&lt;zk xmlns:h="native"&gt;'], inf = {cnt: 0};
+		_dumpWgt4Zul(out, wgt, 0, inf);
+		out.push('&lt;/zk&gt;');
 		this._dump("Total: "+inf.cnt, out.join(''));
 	},
 	_dump: function (header, content) {
@@ -162,12 +280,15 @@ zk.debug.DefaultHandler = zk.$extends(zk.Object, {
 	},
 	_parseAttribute: function (content, isSingle) {
 		var out = [];
-		for (var odd, start, c, cnt, i = 0, j = content.length; i < j; i++) {
+		for (var odd, start, c, i = 0, j = content.length; i < j; i++) {
 			c = content.charAt(i);
 			switch (c) {
 				case '=':
-					out.push('=<span style="color:#0666FD">');
-					cnt = 0;
+					// fix if src contains a character '='.
+					if (!odd)
+						out.push('=<span style="color:#0666FD">');
+					else
+						out.push('=');
 					if (!odd)
 						odd = false;
 					break;
