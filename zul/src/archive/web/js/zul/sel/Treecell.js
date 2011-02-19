32e52e76a0e39350db8a616a9f816b3dab7934f8
tomyeh <tomyeh@zkoss.org>
2011-02-19 11:20:29 +0000

    finetune
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@16277 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/Treecell.js
+++ zul/src/archive/web/js/zul/sel/Treecell.js
@@ -52,9 +52,8 @@ zul.sel.Treecell = zk.$extends(zul.LabelImageWidget, {
 		return this.parent ? this.parent.getTree() : null;
 	},
 	domStyle_: function (no) {
-		if (no) no.width = true; //bug#3185657: not span content if given width
-		else no = {width:true};
-		var style = this.$super('domStyle_', no),
+		var style = this.$super('domStyle_', zk.copy(no, {width:true})),
+				//bug#3185657: not span content if given width
 			tc = this.getTreecol();
 			return this.isVisible() && tc && !tc.isVisible() ? style +
 				"display:none;" : style;
