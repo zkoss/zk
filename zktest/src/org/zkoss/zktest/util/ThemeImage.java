3ab548d8f220ee5a395df3e9bfbe3daaa243b7de
tomyeh <tomyeh@zkoss.org>
2011-05-04 07:41:54 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@16728 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zktest/src/org/zkoss/zktest/util/ThemeImage.java
+++ zktest/src/org/zkoss/zktest/util/ThemeImage.java
@@ -16,7 +16,7 @@ Copyright (C) 2007 Potix Corporation. All Rights Reserved.
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
-package org.zkoss.zktest.test;
+package org.zkoss.zktest.util;
 
 import org.zkoss.zk.ui.Execution;
 import org.zkoss.zk.ui.Executions;
--- zktest/src/org/zkoss/zktest/util/ThemeSelector.java
+++ zktest/src/org/zkoss/zktest/util/ThemeSelector.java
@@ -16,7 +16,7 @@ Copyright (C) 2009 Potix Corporation. All Rights Reserved.
 	it will be useful, but WITHOUT ANY WARRANTY.
 }}IS_RIGHT
 */
-package org.zkoss.zktest.test;
+package org.zkoss.zktest.util;
 
 import java.util.HashMap;
 
