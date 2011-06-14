6084fff4da51d9849bd89ba6562d466153193eb5
jimmyshiau <jimmyshiau@potix.com>
2011-06-14 11:13:49 +0000

    //Fixed side effect of reversion: 16986
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@16988 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/SelectWidget.js
+++ zul/src/archive/web/js/zul/sel/SelectWidget.js
@@ -406,6 +406,12 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 						tpadoffsethgh = (tpad ? tpad.offsetHeight : 0),
 						tpadhgh = tpadoffsethgh > 0 && this._padsz && this._padsz['tpad'] ? this._padsz['tpad'] : tpadoffsethgh < 0 ? 0 : tpadoffsethgh;
 					if (nRows <= nVisiRows) {
+						//Fixed side effect of reversion: 16986
+						var bdfaker = this.ebdfaker, minWd;
+						if ((bdfaker = this.ebdfaker) && (minWd = this._minWd)) {
+							for (var i = minWd.wds.length;i--;)
+								bdfaker.cells[i].style.width = jq.px0(minWd.wds[i]);
+						}
 						var $midVisiRow = zk(midVisiRow);
 						hgh = $midVisiRow.offsetTop() + $midVisiRow.offsetHeight() - tpadhgh;
 					} else {
