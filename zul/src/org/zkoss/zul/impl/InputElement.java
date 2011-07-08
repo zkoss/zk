ccffcc1c1ee2bcbcebbfc56ed707b9b99240c1c7
peter <whkuoattaiwan@gmail.com>
2011-07-08 07:53:54 +0000

    Bug fixed: 3357931: WrongValueException Thrown On Valid Component http://www.zkoss.org/services/zk/tracker/?B50-3357931
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17133 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/impl/InputElement.java
+++ zul/src/org/zkoss/zul/impl/InputElement.java
@@ -629,7 +629,8 @@ implements Constrainted, Readonly, Disable, org.zkoss.zul.impl.api.InputElement 
 	 * {@link #getText} and {@link #getTargetValue}.
 	 */
 	protected void checkUserError() throws WrongValueException {
-		if (_auxinf != null && _auxinf.errmsg != null)
+		//Bug 3357931: WrongValueException Thrown On Valid Component
+		if (_auxinf != null && _auxinf.errmsg != null && !_auxinf.errmsg.equals("null"))
 			throw new WrongValueException(this, _auxinf.errmsg);
 				//Note: we still throw exception to abort the exec flow
 				//It's client's job NOT to show the error box!
