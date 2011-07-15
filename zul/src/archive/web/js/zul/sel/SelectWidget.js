0930af486116d6de68d4c6ab6903cbfb5544471e
simonpai <simonpai@potix.com>
2011-07-15 01:45:30 +0000

    Bug fix: B50-3337441: Firefox Listbox Multiselect http://www.zkoss.org/services/zk/tracker/?B50-3337441
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17184 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/SelectWidget.js
+++ zul/src/archive/web/js/zul/sel/SelectWidget.js
@@ -878,7 +878,7 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 		}
 
 		var focusfound = false, rowfound = false;
-		for (var it = this.getBodyWidgetIterator(), w; (w = it.next());) {
+		for (var it = this.getBodyWidgetIterator(), si = this.getSelectedItem(), w; (w = it.next());) {
 			if (w.isDisabled()) continue; // Bug: 2030986
 			if (focusfound) {
 				this._changeSelect(w, true);
@@ -888,6 +888,11 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 				this._changeSelect(w, true);
 				if (this._isFocus(w) || w == this._lastSelectedItem)
 					break;
+			} else if (!si) { // Bug: 3337441
+				if (w != row)
+					continue;
+				this._changeSelect(w, true);
+				break;
 			} else {
 				rowfound = w == row;
 				focusfound = this._isFocus(w) || w == this._lastSelectedItem;
