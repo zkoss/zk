26dcfb5c94764b1d9e80eb4c1683f5bed4cacaa1
simonpai <simonpai@potix.com>
2011-07-25 10:18:53 +0000

    Bug fix: B50-ZK-206: Set Hflex="min" to listheader fail, FF5 IE6,7 http://tracker.zkoss.org/browse/ZK-206
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17254 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/mesh/MeshWidget.js
+++ zul/src/archive/web/js/zul/mesh/MeshWidget.js
@@ -129,6 +129,8 @@ it will be useful, but WITHOUT ANY WARRANTY.
 				} else if (zk.ff > 4 || zk.ie == 9) {// firefox4 & IE9 still cause break line in case B50-3147926 column 1
 					++wds[i];
 				}
+				if (zk.ie < 8) // B50-ZK-206
+					wds[i] += 2;
 				width += wds[i]; // using wds[i] instead of wd for B50-3183172.zul
 				if (w) w = w.previousSibling;
 			}
@@ -146,6 +148,8 @@ it will be useful, but WITHOUT ANY WARRANTY.
 						maxj = i;
 					} else if (zk.ff > 4 || zk.ie == 9) // firefox4 & IE9 still cause break line in case B50-3147926 column 1
 						++wds[i];
+					if (zk.ie < 8) // B50-ZK-206
+						wds[i] += 2;
 					width += wds[i]; // using wds[i] instead of wd for B50-3183172.zul
 				}
 			}
