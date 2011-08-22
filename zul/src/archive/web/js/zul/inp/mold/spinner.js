90a5a31698fbae352282c345356e76265ebdea66
simonpai <simonpai@potix.com>
2011-08-22 01:14:09 +0000

    Bug fix: B50-ZK-331: The layout of Timebox and Button is wrong (side effect of previous spinner.js tuning) http://tracker.zkoss.org/browse/ZK-331
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17498 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/inp/mold/spinner.js
+++ zul/src/archive/web/js/zul/inp/mold/spinner.js
@@ -25,7 +25,7 @@ function (out) {
 		out.push(' ', zcls, '-right-edge');
 		
 	out.push('"', this.textAttrs_(),'/>', '<i id="', uuid,'-btn"',
-			'class="', zcls, '-btn');
+			'class="', zcls,'-btn ');
 	
 	if (isRounded) {
 		if (!isButtonVisible)
@@ -34,9 +34,10 @@ function (out) {
 			out.push(' ', zcls, '-btn-readonly');	
 		if (zk.ie6_ && !isButtonVisible && this._readonly)
 			out.push(' ', zcls, '-btn-right-edge-readonly');
-		out.push('">');
 	} else if (!isButtonVisible)
-		out.push('" style="display:none">');
+		out.push('" style="display:none');	
+	
+	out.push('">');
 	//Merge breeze: for splitting timebox/spinner button to two pieces
 	zul.inp.Renderer.renderSpinnerButton(out, this);
 	out.push('</i></i>');
