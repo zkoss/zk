2f656f9c59c3ffa21876b1bb2cdc6f8b9c738aae
benbai <benbai@potix.com>
2011-08-23 12:39:36 +0000

    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17523 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/wnd/Panel.js
+++ zul/src/archive/web/js/zul/wnd/Panel.js
@@ -642,7 +642,7 @@ zul.wnd.Panel = zk.$extends(zul.Widget, {
 		var n = this.$n(),
 			body = pc.$n(),
 			hgh = n.style.height;
-		if (zk.ie6_ && ((hgh && hgh != "auto" )|| body.style.height)) body.style.height = "0";
+		
 		if (hgh && hgh != "auto")
 			zk(body).setOffsetHeight(this._offsetHeight(n));
 		if (zk.ie6_) zk(body).redoCSS();
