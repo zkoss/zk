1705206375bf78db52ce7bbab04fc1106f2fdc24
jumperchen <jumperchen@potix.com>
2011-08-30 02:40:12 +0000

    Bug ZK-373: Listbox/Grid in ROD fail to reset model when scrolling in middle of the list http://tracker.zkoss.org/browse/ZK-373
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17606 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/Listbox.java
+++ zul/src/org/zkoss/zul/Listbox.java
@@ -2979,6 +2979,12 @@ public class Listbox extends MeshElement implements org.zkoss.zul.api.Listbox {
 			_dataLoader = null;
 			smartUpdate("_lastoffset", 0); //reset for bug 3357641
 		}
+
+		// Bug ZK-373
+		smartUpdate("resetDataLoader", true);
+		_currentTop = 0;
+		_currentLeft = 0;
+		_topPad = 0;
 	}
 
 	private class ModelInitListener implements SerializableEventListener {
