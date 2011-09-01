b64c55dfae03566d9682f0f76fdf11fc21898187
jumperchen <jumperchen@potix.com>
2011-09-01 11:51:07 +0000

    revert the fix cause sideeffect on Safari/Chrome
    git-svn-id: https://svn.potix.com/svn/zk1/releases/5.0.8@17641 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/sel/SelectWidget.js
+++ zul/src/archive/web/js/zul/sel/SelectWidget.js
@@ -427,9 +427,10 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 			this.ebody.style.height = hgh + "px";
 			
 			// bug fixed for B50-3315594.zul on safari and chrome latest version
-			if (zk.safari) {
-				zk(this.ebody).redoCSS();	
-			}
+//	fail on B50-ZK-373
+//		if (zk.safari) {
+//				zk(this.ebody).redoCSS();	
+//			}
 			
 			//2007/12/20 We don't need to invoke the body.offsetHeight to avoid a performance issue for FF.
 			if (zk.ie && this.ebody.offsetHeight) {} // bug #1812001.
