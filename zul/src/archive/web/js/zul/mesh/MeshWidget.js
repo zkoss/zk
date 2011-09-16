820458403bf50db828e198ce822cb18d7ed9b53b
tomyeh <tomyeh@zkoss.org>
2011-09-16 01:19:32 +0000

    fine tune
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17746 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/mesh/MeshWidget.js
+++ zul/src/archive/web/js/zul/mesh/MeshWidget.js
@@ -1036,11 +1036,10 @@ zul.mesh.MeshWidget = zk.$extends(zul.Widget, {
 	/* set the height. */
 	_setHgh: function (hgh) {
 		if (this.isVflex() || (hgh && hgh != "auto" && hgh.indexOf('%') < 0)) {
-			if (zk.safari) {
-				// Bug ZK-417, ignore to set the same size
-				if (this.ebody.style.height == jq.px(this._vflexSize(hgh)))
-					return;
-			}
+			if (zk.safari // Bug ZK-417, ignore to set the same size
+			&& this.ebody.style.height == jq.px(this._vflexSize(hgh)))
+				return;
+
 			this.ebody.style.height = ''; //allow browser adjusting to default size
 			var h = this._vflexSize(hgh); 
 			if (h < 0) h = 0;
