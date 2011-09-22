0352f0b241613f81cba8f59219579bfd5569db9b
tomyeh <tomyeh@zkoss.org>
2011-09-22 07:40:33 +0000

    Optimize: no need to generate JS/CSS if a JSP page is included in AU
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17802 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/sys/HtmlPageRenders.java
+++ zk/src/org/zkoss/zk/ui/sys/HtmlPageRenders.java
@@ -239,7 +239,7 @@ public class HtmlPageRenders {
 	 */
 	public static final
 	String outLangJavaScripts(Execution exec, WebApp wapp, String deviceType) {
-		if (exec.getAttribute(ATTR_LANG_JS_GENED) != null)
+		if (exec.isAsyncUpdate(null) || exec.getAttribute(ATTR_LANG_JS_GENED) != null)
 			return ""; //nothing to generate
 		exec.setAttribute(ATTR_LANG_JS_GENED, Boolean.TRUE);
 
@@ -360,7 +360,7 @@ public class HtmlPageRenders {
 	 */
 	public static final String outLangStyleSheets(Execution exec,
 	WebApp wapp, String deviceType) {
-		if (exec.getAttribute(ATTR_LANG_CSS_GENED) != null)
+		if (exec.isAsyncUpdate(null) || exec.getAttribute(ATTR_LANG_CSS_GENED) != null)
 			return ""; //nothing to generate
 		exec.setAttribute(ATTR_LANG_CSS_GENED, Boolean.TRUE);
 
