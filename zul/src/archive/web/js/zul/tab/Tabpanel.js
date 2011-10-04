207c20a79ad3ccecc54abf003b90d32bcba47cd8
tomyeh <tomyeh@zkoss.org>
2011-10-04 11:36:10 +0000

    B50-ZK-473: Vflex attribute on tabpanel cannot works (fine tune) http://www.zkoss.org/services/zk/tracker/?ZK-473
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17907 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/tab/Tabpanel.js
+++ zul/src/archive/web/js/zul/tab/Tabpanel.js
@@ -94,51 +94,44 @@ zul.tab.Tabpanel = zk.$extends(zul.Widget, {
 		var tbx = tabbox.$n(),
 		hgh = tbx.style.height;
 		if (hgh && hgh != "auto") {
-    		if (!tabbox.inAccordionMold()) {
-    			if (this.getTabbox().isHorizontal()) {
-	        		var n = this.$n();
-	        		hgh = zk(n.parentNode).vflexHeight();
-	    			if (zk.ie8)
-	    				hgh -= 1; // show the bottom border
-	        		zk(n).setOffsetHeight(hgh);
-    			} else {
-    				// B50-ZK-473: Tabpanel in vertical Tabbox should always have full height
-    				var n = this.$n();
-    				hgh = n.parentNode.clientHeight;
-	    			if (zk.ie8)
-	    				hgh -= 1; // show the bottom border
-	        		zk(n).setOffsetHeight(hgh);
-    			}
-        		if (zk.ie6_) {
-        			var s = this.$n('cave').style,
-        			z = s.zoom;
-        			s.zoom = 1;
-        			s.zoom = z;
-        		}
-    		} else {
-    			if (this.getTabbox().isHorizontal()) {
-	    			var n = this.$n(),
-	    				hgh = zk(tbx).revisedHeight(tbx.offsetHeight);
-	    			hgh = zk(n.parentNode).revisedHeight(hgh);
-					
-					// fixed Opera 10.5+ bug
-					if (zk.opera) {
-						var parent;
-						if ((parent = tbx.parentNode) && tbx.style.height == '100%')
-							hgh = zk(parent).revisedHeight(parent.offsetHeight);
-					}
-					
-	    			for (var e = n.parentNode.firstChild; e; e = e.nextSibling)
-	    				if (e != n) hgh -= e.offsetHeight;
-	    			hgh -= n.firstChild.offsetHeight;
-	    			hgh = zk(n = n.lastChild).revisedHeight(hgh);
-	    			if (zk.ie8)
-	    				hgh -= 1; // show the bottom border
-	    			var cave = this.getCaveNode(),
-						s = cave.style;
-	    			s.height = jq.px0(hgh);
-    			}
-    		}
+			if (!tabbox.inAccordionMold()) {
+				var n = this.$n();
+				hgh = tabbox.isHorizontal() ?
+					zk(n.parentNode).vflexHeight(): n.parentNode.clientHeight;
+					// B50-ZK-473: Tabpanel in vertical Tabbox should always have full height
+				if (zk.ie8)
+					hgh -= 1; // show the bottom border
+				zk(n).setOffsetHeight(hgh);
+
+				if (zk.ie6_) {
+					var s = this.$n('cave').style,
+					z = s.zoom;
+					s.zoom = 1;
+					s.zoom = z;
+				}
+			} else if (tabbox.isHorizontal()) {
+				var n = this.$n(),
+					hgh = zk(tbx).revisedHeight(tbx.offsetHeight);
+				hgh = zk(n.parentNode).revisedHeight(hgh);
+				
+				// fixed Opera 10.5+ bug
+				if (zk.opera) {
+					var parent;
+					if ((parent = tbx.parentNode) && tbx.style.height == '100%')
+						hgh = zk(parent).revisedHeight(parent.offsetHeight);
+				}
+				
+				for (var e = n.parentNode.firstChild; e; e = e.nextSibling)
+					if (e != n)
+						hgh -= e.offsetHeight;
+				hgh -= n.firstChild.offsetHeight;
+				hgh = zk(n = n.lastChild).revisedHeight(hgh);
+				if (zk.ie8)
+					hgh -= 1; // show the bottom border
+				var cave = this.getCaveNode(),
+					s = cave.style;
+				s.height = jq.px0(hgh);
+			}
 		}
 	},
 	domClass_: function () {
