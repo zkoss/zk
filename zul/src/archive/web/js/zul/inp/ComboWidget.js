ad236cc3bddd05cdd655a2df82b9c01c2c6f84f1
jumperchen <jumperchen@potix.com>
2011-10-04 07:08:17 +0000

    Bug ZK-475: bandpopup tab key works wrong http://www.zkoss.org/services/zk/tracker/?ZK-475
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@17901 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/inp/ComboWidget.js
+++ zul/src/archive/web/js/zul/inp/ComboWidget.js
@@ -494,7 +494,8 @@ zul.inp.ComboWidget = zk.$extends(zul.inp.InputWidget, {
 	_doKeyDown: function (evt) {
 		var keyCode = evt.keyCode,
 			bOpen = this._open;
-		if (keyCode == 9 || (zk.safari && keyCode == 0)) { //TAB or SHIFT-TAB (safari)
+		if ((evt.target == this || !(evt.target.$instanceof(zul.inp.InputWidget)))// Bug ZK-475
+				&& (keyCode == 9 || (zk.safari && keyCode == 0))) { //TAB or SHIFT-TAB (safari)
 			if (bOpen) this.close({sendOnOpen:true});
 			return;
 		}
