ae78867194329a83bfaeefcbefba9e7d090ae3c7
jumperchen <jumperchen@potix.com>
2011-11-22 03:43:35 +0000

    * Bug ZK-597: Tree not rendering correctly after event. * Optimize the performance of Layout.js http://www.zkoss.org/services/zk/tracker/?ZK-597
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@18295 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/box/Layout.js
+++ zul/src/archive/web/js/zul/box/Layout.js
@@ -71,8 +71,19 @@ zul.box.Layout = zk.$extends(zk.Widget, {
 	 */
 	syncSize: function () {
 		this._shallSize = false;
-		if (this.desktop)
-			zUtl.fireSized(this);
+		if (this.desktop) {
+			// only fire when child has h/vflex
+			for (var w = this.firstChild; w; w = w.nextSibling) {
+				if (w._nvflex || w._nhflex) {
+					this._shallSize = true;
+					break;
+				}
+			}
+			if (this._shallSize)
+				zUtl.fireSized(this);
+			
+			this._shallSize = false;
+		}
 	},
 	onResponse: function () {
 		if (this._shallSize)
@@ -185,6 +196,10 @@ zul.box.Layout = zk.$extends(zk.Widget, {
 		return attr == 'h' ? zk(el).offsetHeight() : zjq.minWidth(el); //See also bug ZK-483
 	},
 	beforeChildrenFlex_: function(child) {
+		
+		// optimized for performance
+		this._shallSize = false;
+		
 		if (child._flexFixed || (!child._nvflex && !child._nhflex)) { //other vflex/hflex sibliing has done it!
 			delete child._flexFixed;
 			return false;
