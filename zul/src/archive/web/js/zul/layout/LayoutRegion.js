7b772e4bb2cf98ea9b49c79ef52a990ea6c5c7c1
simonpai <simonpai@potix.com>
2011-12-05 03:33:23 +0000

    Bug fix: B50-ZK-641: Borderlayout label width inconsistent between browsers (fine tune) http://www.zkoss.org/services/zk/tracker/?ZK-641
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@18448 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/layout/LayoutRegion.js
+++ zul/src/archive/web/js/zul/layout/LayoutRegion.js
@@ -762,7 +762,7 @@ zul.layout.LayoutRegion = zk.$extends(zul.Widget, {
 						Math.floor(pn.offsetHeight * zk.parseInt(h.substring(0, pert)) / 100),
 						0) : this.$n('real').offsetHeight
 			};
-			if (zk.ie && (region == 'west' || region == 'east') && !this.getWidth())
+			if (zk.ie == 9 && (region == 'west' || region == 'east') && !this.getWidth())
 				ambit.w++; // B50-ZK-641: text wrap in IE
 		}
 		var split = ignoreSplit ? {offsetHeight:0, offsetWidth:0}: this.$n('split') || {offsetHeight:0, offsetWidth:0};
