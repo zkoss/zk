843698d1da812e4a26a709a8cb168f360b7d6707
tomyeh <tomyeh@zkoss.org>
2011-12-06 06:34:47 +0000

    javadoc
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@18486 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/org/zkoss/zul/ItemRenderer.java
+++ zul/src/org/zkoss/zul/ItemRenderer.java
@@ -19,17 +19,21 @@ package org.zkoss.zul;
 import org.zkoss.zk.ui.Component;
 
 /**
- * Identifies components that can be used to render the content of the html.
+ * Used to generated the HTML fragment for the data associated
+ * with a component, such as {@link Selectbox}.
  * 
  * @author jumperchen
  * @since 6.0.0
  */
 public interface ItemRenderer<T> {
 	
-	/** Renders the data to the specific html.
+	/** Renders the data to the corresponding HTML fragment, and returns
+	 * the HTML fragment.
 	 *
-	 * @param owner the comopnent that the renderer belongs to.
+	 * @param owner the comopnent that this renderer belongs to (never null).
 	 * @param data that is returned from {@link ListModel#getElementAt}
+	 * @return the HTML fragment representing the data. It depends
+	 * on the component this renderer belongs to.
 	 */
 	public String render(Component owner, T data) throws Exception;
 }
