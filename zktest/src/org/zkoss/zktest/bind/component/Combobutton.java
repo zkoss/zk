7d5d948c0a2a0d021466b62dfe950045e0596bf0
hawkchen <hawkchen@potix.com>
2011-12-23 09:42:27 +0000

    add test case: combobox, combobutton, menuitem.
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@18691 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zktest/src/org/zkoss/zktest/bind/component/Combobutton.java
@@ -0,0 +1,20 @@
+package org.zkoss.zktest.bind.component;
+
+
+
+
+
+public class Combobutton{
+	private boolean open = false;
+
+	public void setOpen(boolean open) {
+		this.open = open;
+	}
+
+	public boolean isOpen() {
+		return open;
+	}
+	
+	
+	
+}
---
+++ zktest/src/org/zkoss/zktest/bind/component/Menuitem.java
@@ -0,0 +1,20 @@
+package org.zkoss.zktest.bind.component;
+
+
+
+
+
+public class Menuitem{
+	private boolean checked = false;
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	
+	
+}
