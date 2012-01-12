4a58d3b33ea2413d32bc1dde7db1171c638484e0
dennischen <atticcat@gmail.com>
2012-01-12 01:57:50 +0000

    fix  ZK-768 SelectedTreeitemConverter should care TreeSelectionModel
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@18858 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkbind/src/org/zkoss/bind/converter/sys/SelectedTreeitemConverter.java
+++ zkbind/src/org/zkoss/bind/converter/sys/SelectedTreeitemConverter.java
@@ -12,19 +12,23 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
 
 package org.zkoss.bind.converter.sys;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.zkoss.bind.BindContext;
 import org.zkoss.bind.Converter;
 import org.zkoss.bind.impl.BinderImpl;
+import org.zkoss.bind.sys.LoadPropertyBinding;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.TreeModel;
 import org.zkoss.zul.Tree;
 import org.zkoss.zul.Treeitem;
+import org.zkoss.zul.ext.TreeSelectionModel;
 
 /**
  * Convert tree selected treeitem to bean and vice versa.
  * @author henrichen
+ * @author dennis
  * @since 6.0.0
  */
 public class SelectedTreeitemConverter implements Converter, java.io.Serializable {
@@ -32,52 +36,75 @@ public class SelectedTreeitemConverter implements Converter, java.io.Serializabl
 	
 	public Object coerceToUi(Object val, Component comp, BindContext ctx) {
 		Tree tree = (Tree) comp;
+		final TreeModel<?> model = tree.getModel();
+
+  		final TreeSelectionModel smodel = (model instanceof TreeSelectionModel)?(TreeSelectionModel)model:null;
 	  	if (val != null) {
-	  		final TreeModel<?> model = tree.getModel();
-	  		if (model != null) {
-		  		if (tree.getAttribute(BinderImpl.RENDERER_INSTALLED) != null) { //There is a bind renderer
-		  			//TODO might be done with dependency tracker, bean -> listitem (implicit binding)
-		  			//iterate to find the selected item
-		  			for (final Iterator<?> it = tree.getItems().iterator(); it.hasNext();) {
-		  				final Treeitem li = (Treeitem) it.next();
-		  				final String varnm = (String) li.getAttribute(BinderImpl.VAR);
-		  				final Object bean = li.getAttribute(varnm);
-		  				if (val.equals(bean)) {
-		  					return li;
-		  				}
-		  			}
-		  			//not in the item list
-		  			return null;
+	  		//there is a BUG on tree.getItems (http://tracker.zkoss.org/browse/ZK-766),
+	  		//it doesn't return all descending children if it is not open yet
+	  		//and if user want better performance, he should get the selection from model directly
+		  	for (final Iterator<?> it = tree.getItems().iterator(); it.hasNext();) {
+		  		final Treeitem ti = (Treeitem) it.next();
+		  		final String varnm = (String) ti.getAttribute(BinderImpl.VAR);
+		  		Object bean = null;
+		  		int path[] = null;
+		  		if (varnm != null) { //There is binding on template
+		  			bean = ti.getAttribute(varnm);
+		  		} else if(model!=null){ //no binding
+		  			bean = model.getChild(path = toPath(ti));
+		  		} else{
+		  			bean = ti.getValue();
 		  		}
-	  		}
-	  		//no model or no binding, assume Treeitem.value to be used with selectedItem
-  			//iterate to find the selected item
-  			for (final Iterator<?> it = tree.getItems().iterator(); it.hasNext();) {
-  				final Treeitem li = (Treeitem) it.next();
-  				if (val.equals(li.getValue())) {
-  					return li;
-  				}
-  			}
+
+		  		if (val.equals(bean)) {
+		  			if(smodel!=null){
+		  				if(path==null){
+		  					path = toPath(ti);
+		  				}
+		  				smodel.addSelectionPath(path);
+		  				return LoadPropertyBinding.LOAD_IGNORED;
+		  			}
+		  			return ti;
+		  		}
+		  	}
+		  	//not in the item list
+	  	}
+	  	
+	  	if(smodel!=null){
+	  		if(smodel.getSelectionCount()>0)
+	  			smodel.clearSelection();
+	  		return LoadPropertyBinding.LOAD_IGNORED;
 	  	}
 	  	return null;
 	}
 
 	public Object coerceToBean(Object val, Component comp, BindContext ctx) {
-	  	if (val != null) {
+		if (val != null) {
 		  	final Tree tree = (Tree) comp;
 	  		final TreeModel<?> model = tree.getModel();
-	  		if (model != null) {
-		  		final String varnm = (String) ((Treeitem) val).getAttribute(BinderImpl.VAR);
-		  		if (varnm != null) { //There is binding on template
-		  			return ((Treeitem) val).getAttribute(varnm);
-		  		} else { //no template, assume Treeitem.value to be used with selectedItem
-		  			return ((Treeitem) val).getValue();
-		  		}
-	  		} else { //no model case, assume Treeitem.value to be used with selectedItem
+	  		final String varnm = (String) ((Treeitem)val).getAttribute(BinderImpl.VAR);
+	  		if (varnm != null) { //There is binding on template
+	  			return ((Treeitem)val).getAttribute(varnm);
+	  		} else if(model!=null){ //no binding
+	  			return model.getChild(toPath((Treeitem) val));
+	  		} else{
 	  			return ((Treeitem) val).getValue();
 	  		}
 	  	}
 	 	return null;
+	}
+
+	private int[] toPath(Treeitem item) {
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		while(item!=null){
+			path.add(0,item.getIndex());
+			item = item.getParentItem();
+		}
+		int[] p = new int[path.size()];
+		for(int i=0;i<p.length;i++){
+			p[i] = path.get(i).intValue();
+		}
+		return p;
 	}
 
 }
