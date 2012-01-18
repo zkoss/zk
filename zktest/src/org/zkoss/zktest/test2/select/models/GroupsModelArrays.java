cff8370b9aae7ee051b543657482cef8524aab4e
benbai <benbai@potix.com>
2012-01-18 10:38:45 +0000

    move to ztl
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@18930 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zktest/src/org/zkoss/zktest/test2/select/models/GroupsModelArrays.java
+++ zktest/src/org/zkoss/zktest/test2/select/models/GroupsModelArrays.java
@@ -12,8 +12,17 @@ public class GroupsModelArrays {
 	}
 	private static Object[][] getItems () {
 		Object[][] items = new Object[itemCnt][3];
+		int setCnt = -1;
+		String setStr = "";
 		for (int i = 0; i < itemCnt; i++) {
-			Object[] item = {new String("Itemset "+i/3), new String("Value "+i), new String("Description "+i)};
+			// make the groups order ascending
+			if (setCnt != i/3) {
+				setCnt = i/3;
+				setStr = setCnt + "";
+				while (setStr.length() < 3)
+					setStr = "0"+setStr;
+			}
+			Object[] item = {new String("Itemset "+setStr), new String("Value "+i), new String("Description "+i)};
 			items[i] = item;
 		}
 		return items;
