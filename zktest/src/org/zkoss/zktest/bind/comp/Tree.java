cccdfe689e16cdaa272291a8acf705059327e14c
hawkchen <hawkchen@potix.com>
2012-02-07 03:53:50 +0000

    replace AbstractTreeModel with DefaultTreeModel. Because AbstractTreeModel implementation's cost is higher.
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@19074 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zktest/src/org/zkoss/zktest/bind/comp/Tree.java
+++ zktest/src/org/zkoss/zktest/bind/comp/Tree.java
@@ -2,47 +2,42 @@ package org.zkoss.zktest.bind.comp;
 
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.NotifyChange;
-import org.zkoss.zul.AbstractTreeModel;
+import org.zkoss.zul.DefaultTreeModel;
+import org.zkoss.zul.DefaultTreeNode;
+import org.zkoss.zul.TreeModel;
+import org.zkoss.zul.TreeNode;
 
 
 
 public class Tree{
 
-	public class MyModel extends AbstractTreeModel {
-	    public MyModel() {
-	        super("Root");
-	    }
-	    public boolean isLeaf(Object node) {
-	        return getLevel((String)node) >= 2; //at most 4 levels
-	    }
-	    public Object getChild(Object parent, int index) {
-	        return parent + "." + index;
-	    }
-	    public int getChildCount(Object parent) {
-	        return isLeaf(parent) ? 0: 2; //each node has 5 children
-	    }
-	    public int getIndexOfChild(Object parent, Object child) {
-	        String data = (String)child;
-	        int i = data.lastIndexOf('.');
-	        return Integer.parseInt(data.substring(i + 1));
-	    }
-	    private int getLevel(String data) {
-	        for (int i = -1, level = 0;; ++level)
-	            if ((i = data.indexOf('.', i + 1)) < 0)
-	                return level;
-	    }
-		
-	}
-	private String selected="init";
+	
+	DefaultTreeModel<String> treeModel = new DefaultTreeModel<String>(
+			  new DefaultTreeNode<String>("Root",
+			    new DefaultTreeNode[] {
+			      new DefaultTreeNode<String>("Root.0", 
+			    		  new DefaultTreeNode[] {
+					      new DefaultTreeNode<String>("Root.0.0"),
+					      new DefaultTreeNode<String>("Root.0.1")
+					    }),
+			      new DefaultTreeNode<String>("Root.1",
+	    		   		  new DefaultTreeNode[] {
+					      new DefaultTreeNode<String>("Root.1.0"),
+					      new DefaultTreeNode<String>("Root.1.1")
+					    })
+			    }
+			  ));
+	
+	
+	private TreeNode<String> selected= new DefaultTreeNode<String>("init");
 	private boolean open =false;
+
 	
-	private MyModel model;
-	
-	public String getSelected() {
+	public TreeNode<String> getSelected() {
 		return selected;
 	}
 
-	public void setSelected(String selected) {
+	public void setSelected(TreeNode<String> selected) {
 		this.selected = selected;
 	}
 
@@ -54,9 +49,8 @@ public class Tree{
 		this.open = open;
 	}
 
-	public AbstractTreeModel<String> getModel(){
-		model = new MyModel();
-		return model;
+	public TreeModel getModel(){
+		return treeModel;
 	}
 	// -----------command -----------------
 	@Command @NotifyChange("*")
