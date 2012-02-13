be46d326e6dc6fdcc60eaed8a6a1559ffaf63632
tomyeh <tomyeh@zkoss.org>
2012-02-13 04:38:26 +0000

    ZK-812: Components.wireFellows() error when root component is not IdSpace http://www.zkoss.org/services/zk/tracker/?ZK-812
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.0@19173 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/Components.java
+++ zk/src/org/zkoss/zk/ui/Components.java
@@ -966,15 +966,16 @@ public class Components {
 			//inject space owner ancestors
 			if (idspace instanceof Component) {
 				wireController((Component)idspace, ((Component)idspace).getId());
-				for (IdSpace xidspace = idspace;;) {
-					final Component parent = ((Component)xidspace).getParent();
+				for (IdSpace is = idspace;;) {
+					final Component parent =
+						is instanceof Component ? ((Component)is).getParent(): null;
 					if (parent == null) {//hit page
-						final Page page = ((Component)xidspace).getPage();
+						final Page page = ((Component)idspace).getPage();
 						if (page != null) injectFellow(page);
 						break;
 					}
-					xidspace = parent.getSpaceOwner();
-					injectFellow(xidspace);
+					is = parent.getSpaceOwner();
+					injectFellow(is);
 				}
 			} else {
 				injectFellow((Page) idspace);
