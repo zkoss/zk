9bd40659d08362c18991d449e96007edf7f7b9f1
tomyeh <tomyeh@zkoss.org>
2012-03-08 03:18:12 +0000

    merge
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@19292 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zktest/src/org/zkoss/zktest/test2/B50_ZK_897.java
@@ -0,0 +1,85 @@
+package org.zkoss.zktest.test2;
+import java.util.Comparator;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Grid;
+import org.zkoss.zul.GroupsModelArray;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Row;
+import org.zkoss.zul.RowRenderer;
+import org.zkoss.zul.event.GroupsDataEvent;
+
+public class B50_ZK_897 extends GenericForwardComposer {
+	private static final long serialVersionUID = 7970338470714930637L;
+
+	private Grid grid;
+	
+	private Listbox listbox;
+
+	private Button button;
+
+	private RowRenderer rowRenderer;
+
+	private MyModel model;
+
+	private class MyModel extends GroupsModelArray {
+
+		public MyModel(String[] data, Comparator<String> comp) {
+			super(data, comp);
+		}
+
+		public void update(int group) {			
+			fireEvent(GroupsDataEvent.GROUPS_CHANGED, 0, group, group);
+		}
+
+		@Override
+		protected Object createGroupHead(Object[] groupdata, int index, int col) {
+			return "HEAD";
+		}
+
+		@Override
+		protected Object createGroupFoot(Object[] groupdata, int index, int col) {
+			return "FOOT";
+		}
+	}
+
+
+	@Override
+	public void doAfterCompose(final Component component) throws Exception {
+		super.doAfterCompose(component);
+
+		String[] data = new String[4];
+		data[0] = "test0";
+		data[1] = "test0";
+		data[2] = "test1";
+		data[3] = "test1";
+		
+		model = new MyModel(data, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		
+		if(grid != null)
+			grid.setModel(model);
+		
+		if(listbox != null)
+			listbox.setModel(model);
+		
+
+		button.addEventListener(Events.ON_CLICK, new EventListener() {
+			@Override
+			public void onEvent(Event arg0) throws Exception {
+				model.update(0);
+			}
+		});
+	}
+
+}
