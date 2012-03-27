6a7bf4ed4ded85e83f305f1b50fbf969b15ecd6a
benbai <benbai@potix.com>
2012-03-27 10:23:33 +0000

    B60-ZK-945: The gap between the image and description/text is large in a comboitem http://www.zkoss.org/services/zk/tracker/?ZK-945
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@19437 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zul/src/archive/web/js/zul/inp/mold/comboitem.js
+++ zul/src/archive/web/js/zul/inp/mold/comboitem.js
@@ -13,17 +13,23 @@ This program is distributed under LGPL Version 2.1 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
 function (out) {
-	var zcls = this.getZclass();
-	out.push('<tr', this.domAttrs_({text:true}), '><td class="',
-		zcls, '-img">', this.domImage_(), '</td><td class="',
+	var zcls = this.getZclass(),
+		v = this._description,
+		m = v? '<br/><div class="' + zcls + '-img-bottom">&nbsp;</div>' : '';
+	// ZK-945
+	// merge image and label into one td
+	// add spacer so can customize it easier
+	out.push('<tr', this.domAttrs_({text:true}), '><td><div class="',
+		zcls, '-img">', this.domImage_(), m, '</div><span class="',
+		zcls, '-spacer">&nbsp;</span><div class="',
 		zcls, '-text">', this.domLabel_());
 
-	var v;
-	if (v = this._description)
+	
+	if (v)
 		out.push('<br/><span class="', zcls, '-inner">',
 			zUtl.encodeXML(v), '</span>');
 	if (v = this._content)
 		out.push('<span class="', zcls, '-cnt">', v, '</span>');
 
-	out.push('</td></tr>');
+	out.push('</div></td></tr>');
 }
