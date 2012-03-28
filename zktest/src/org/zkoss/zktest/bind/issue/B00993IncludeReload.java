c3f19c1ef8930feeb0fd0aa7a6673776cb487dfa
dennischen <atticcat@gmail.com>
2012-03-28 10:36:26 +0000

    testcase of ZK-993
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@19445 dd50bd9b-9913-0410-b9ba-e07a3075be86

---
+++ zktest/src/org/zkoss/zktest/bind/issue/B00993IncludeReload.java
@@ -0,0 +1,37 @@
+package org.zkoss.zktest.bind.issue;
+
+import org.zkoss.bind.BindContext;
+import org.zkoss.bind.Converter;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+
+public class B00993IncludeReload {
+	String url = "B00993IncludeReload2.zul";
+	public String getUrl(){
+		return url;
+	}
+	
+	@org.zkoss.bind.annotation.Command
+	@NotifyChange("url")
+	public void reload(){
+		
+	}
+	
+	public Converter getConverter(){
+		return new Converter() {
+			
+			@Override
+			public Object coerceToUi(Object val, Component component, BindContext ctx) {
+				if(val instanceof String){
+					return val +"?tms=" + System.currentTimeMillis();
+				}
+				return val;
+			}
+			
+			@Override
+			public Object coerceToBean(Object val, Component component, BindContext ctx) {
+				return null;
+			}
+		};
+	}
+}
