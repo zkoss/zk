a71394e2912de47d32e5f4e2900bbae212e84388
dennischen <atticcat@gmail.com>
2012-04-02 08:50:03 +0000

    fix DefaultBinder deprecated warn
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@19479 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkbind/src/org/zkoss/bind/DefaultBinder.java
+++ zkbind/src/org/zkoss/bind/DefaultBinder.java
@@ -12,6 +12,8 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
 
 package org.zkoss.bind;
 
+import java.util.Map;
+
 import org.zkoss.bind.impl.BinderImpl;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.EventQueues;
@@ -44,8 +46,8 @@ public class DefaultBinder extends BinderImpl {
 	}
 	
 	@Override
-	public void init(Component comp, Object vm){
-		super.init(comp, vm);
+	public void init(Component comp, Object vm,Map<String, Object> initArgs){
+		super.init(comp, vm, initArgs);
 		//mark this component was handled by binder after init
 		comp.setAttribute(BINDER, this);
 	}
