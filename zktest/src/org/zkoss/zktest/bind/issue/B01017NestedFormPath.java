91d10a3c499964e82b9e51b602c3ee24f7bbc3b8
dennischen <atticcat@gmail.com>
2012-04-05 07:28:15 +0000

    add more case to zk1017
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@19497 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zktest/src/org/zkoss/zktest/bind/issue/B01017NestedFormPath.java
+++ zktest/src/org/zkoss/zktest/bind/issue/B01017NestedFormPath.java
@@ -1,5 +1,7 @@
 package org.zkoss.zktest.bind.issue;
 
+import java.util.HashMap;
+
 import org.zkoss.bind.Form;
 import org.zkoss.bind.ValidationContext;
 import org.zkoss.bind.Validator;
@@ -19,6 +21,7 @@ public class B01017NestedFormPath {
 	public void init(){
 		bean = new Bean();
 		bean.value1 = "A";
+		bean.value2 = "B";
 	}
 
 	public Bean getBean() {
@@ -41,11 +44,16 @@ public class B01017NestedFormPath {
 
 	@Command @NotifyChange("msg")
 	public void update() {
-		msg = "update "+getBean().getValue1();
+		msg = "update value1:"+getBean().getValue1()+",value2:"+getBean().getValue2();
+	}
+	
+	public String getKey2(){
+		return "value2";
 	}
 
 	public class Bean {
 		String value1;
+		String value2;
 
 		public String getValue1() {
 			return value1;
@@ -54,6 +62,16 @@ public class B01017NestedFormPath {
 		public void setValue1(String value1) {
 			this.value1 = value1;
 		}
+
+		public String getValue2() {
+			return value2;
+		}
+
+		public void setValue2(String value2) {
+			this.value2 = value2;
+		}
+
+		
 
 	}
 	
@@ -67,12 +85,32 @@ public class B01017NestedFormPath {
 				String value = (String)ctx.getProperty().getValue();
 				Object base = ctx.getProperty().getBase();
 				String prop = ctx.getProperty().getProperty();
-				System.out.println(">>>>> base "+base+",value "+value+", prop:"+prop);
+				System.out.println(">>validate: base:"+base+",value:"+value+",prop:"+prop);
 				
 				if(!(base instanceof Form)){
 					lab.setValue("base is not a 'Form', is '"+base.getClass()+"'");
 				}else if(!"bean.value1".equals(prop)){
 					lab.setValue("prop is not 'bean.value1', is '"+prop+"'");
+				}
+			}
+		};
+	}
+	public Validator getValidator2(){
+		return new AbstractValidator() {
+			
+			@Override
+			public void validate(ValidationContext ctx) {
+				Label lab = (Label)ctx.getValidatorArg("info");
+				
+				String value = (String)ctx.getProperty().getValue();
+				Object base = ctx.getProperty().getBase();
+				String prop = ctx.getProperty().getProperty();
+				System.out.println(">>validate: base:"+base+",value:"+value+",prop:"+prop);
+				
+				if(!(base instanceof Form)){
+					lab.setValue("base is not a 'Form', is '"+base.getClass()+"'");
+				}else if(!"bean.value2".equals(prop)){
+					lab.setValue("prop is not 'bean.value2', is '"+prop+"'");
 				}
 			}
 		};
