451fee752d3198e31d9dfcdfed0b93a3da930ca8
dennischen <atticcat@gmail.com>
2012-04-27 07:52:08 +0000

    update javadoc
    git-svn-id: https://svn.potix.com/svn/zk1/branches/6.0@19666 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zkbind/src/org/zkoss/bind/Binder.java
+++ zkbind/src/org/zkoss/bind/Binder.java
@@ -90,8 +90,8 @@ public interface Binder {
 	
 	
 	/**
-	 * Load the load-binding of the component
-	 * You should never call this if you use {@link AnnotateBinder} and zk annotation
+	 * Load the load-binding of the component. <br/>
+	 * By calling this method, all load-bindings in the component (including load-bindings in its descendant) will reload the value to components.  
 	 * @param comp the component to reload
 	 * @param loadinit true if should also load the init-binding
 	 */
