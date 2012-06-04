0b93fd8fa3ee83371dd845d203169956020a02d8
jumperchen <jumperchen@potix.com>
2012-06-04 11:06:48 +0100

    fine tune

--- zul/src/archive/web/js/zul/tab/mold/tab.js
+++ zul/src/archive/web/js/zul/tab/mold/tab.js
@@ -47,8 +47,7 @@ function (out) {
 					'<span class="', zcls, '-text">', this.domContent_(),
 					'</span></span></div></div></div></div>');
 		} else {
-			var isFrameRequired = zul.tab.TabRenderer.isFrameRequired(),
-				hm = '<div class="' + zcls + '-hm" >';
+			var isFrameRequired = zul.tab.TabRenderer.isFrameRequired();
 			if (tbx.getPanelSpacing() && this.getIndex())
 				out.push('<div class="', zcls, '-spacing" style="margin:0;display:list-item;width:100%;height:', tbx.getPanelSpacing(), ';"></div>');
 
@@ -57,12 +56,14 @@ function (out) {
 			if (isFrameRequired)
 				out.push('<div class="', zcls, '-tl" ><div class="', zcls, '-tr" ></div></div>',
 						'<div class="', zcls, '-hl" >',
-						'<div class="', zcls, '-hr" >',
-						(isFrameRequired ? hm: ''));
+						'<div class="', zcls, '-hr" >');
+			out.push('<div class="' + zcls + '-hm" >');
+
 			if (this.isClosable())
 				out.push('<a id="', this.uuid, '-close"  class="', zcls, '-close"><div class="', zcls, '-close-icon"></div></a>');
 
-			out.push((!isFrameRequired ? hm: ''), '<span class="', zcls, '-text">', this.domContent_(), '</span></div></div></div>');
+			out.push('<span class="', zcls, '-text">', this.domContent_(), '</span></div></div></div>');
+			
 			if (isFrameRequired)
 				out.push('</div></div>');
 		}
