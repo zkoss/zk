1c1ed511d87f2b04e0e06c4d55e93431dd9f1729
Vincent <vincentjian@potix.com>
2012-06-05 10:09:11 +0800

    Touch issue: Filedownload

--- zk/src/archive/web/js/zk/au.js
+++ zk/src/archive/web/js/zk/au.js
@@ -1041,13 +1041,24 @@ zAu.cmd0 = /*prototype*/ { //no uuid at all
 	 */
 	download: function (url) {
 		if (url) {
-			var ifr = jq('#zk_download')[0];
-			if (ifr) {
-				ifr.src = url; //It is OK to reuse the same iframe
+			if (zk.mobile) {
+				var link = document.createElement('a'),
+					evt = document.createEvent('Event');
+				link.setAttribute('href', url);
+				link.setAttribute('target','_blank');
+				link.setAttribute('id', 'zk_download');
+				evt.initEvent('click', true, false);
+				link.dispatchEvent(evt);
+				jq('#zk_download').detach();
 			} else {
-				var html = '<iframe src="'+url
-				+'" id="zk_download" name="zk_download" style="visibility:hidden;width:0;height:0;border:0" frameborder="0"></iframe>';
-				jq(document.body).append(html);
+				var ifr = jq('#zk_download')[0];
+				if (ifr) {
+					ifr.src = url; //It is OK to reuse the same iframe
+				} else {
+					var html = '<iframe src="'+url
+					+'" id="zk_download" name="zk_download" style="visibility:hidden;width:0;height:0;border:0" frameborder="0"></iframe>';
+					jq(document.body).append(html);
+				}
 			}
 		}
 	},
