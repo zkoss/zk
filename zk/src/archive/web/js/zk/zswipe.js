e97c3945123d49c8bfbfa95f1029652ff0471831
Vincent <vincentjian@potix.com>
2012-07-11 12:20:03 +0800

    1. fine tune test case 2. add test case for panel and groupbox in tablet

--- zk/src/archive/web/js/zk/zswipe.js
+++ zk/src/archive/web/js/zk/zswipe.js
@@ -16,9 +16,7 @@ Copyright (C) 2012 Potix Corporation. All Rights Reserved.
 		endEvt = hastouch ? 'touchend' : 'mouseup',
 		start, stop;
 	
-	jq(document).bind(startEvt, swipeStart)
-		.bind(moveEvt, swipeMove)
-		.bind(endEvt, swipeEnd);
+	jq(document).bind(startEvt, swipeStart);
 	
 	function _doEvt(wevt) {
 		var wgt = wevt.target;
@@ -39,47 +37,51 @@ Copyright (C) 2012 Potix Corporation. All Rights Reserved.
 			time: (new Date()).getTime(),
 			coords: [data.pageX, data.pageY]
 		};
-	}
-	
-	function swipeMove(devt) {
-		if (!start) return;
-		var evt = devt.originalEvent,
-			data = evt.touches ? evt.touches[0] : evt;
-			
-		stop = {
-			time: (new Date()).getTime(),
-			coords: [data.pageX, data.pageY]
-		};
 		
-		// prevent scrolling
-		var dispX = Math.abs(start.coords[0] - stop.coords[0]),
-			dispY = Math.abs(start.coords[1] - stop.coords[1]);
-		if (dispX > 5 || dispY > 5)
-			evt.preventDefault();
-	}
-	
-	function swipeEnd(devt) {
-		if (start && stop) {
-			var wgt = zk.Widget.$(devt, {child:true}),
-				evt = devt.originalEvent,
-				data = evt.touches ? evt.touches[0] : evt,
-				dispT = stop.time - start.time;
+		function swipeMove(devt) {
+			if (!start) return;
+			var evt = devt.originalEvent,
+				data = evt.touches ? evt.touches[0] : evt;
+				
+			stop = {
+				time: (new Date()).getTime(),
+				coords: [data.pageX, data.pageY]
+			};
 			
-			if (dispT < 500) {
-				var deltaX = start.coords[0] - stop.coords[0],
-					deltaY = start.coords[1] - stop.coords[1],
-					dispX = Math.abs(deltaX),
-					dispY = Math.abs(deltaY),
-					dir;
-				if (dispX > 30 && dispY < 75)
-					dir = deltaX > 0 ? 'left' : 'right';
-				else if (dispY > 30 && dispX < 75)
-					dir = deltaY > 0 ? 'up' : 'down';
-			}
-			
-			if (wgt)
-				_doEvt(new zk.Event(wgt, 'onSwipe', data, {start: start, stop: stop, dir: dir}, devt));
+			// prevent scrolling
+			var dispX = Math.abs(start.coords[0] - stop.coords[0]),
+				dispY = Math.abs(start.coords[1] - stop.coords[1]);
+			if (dispX > 5 || dispY > 5)
+				evt.preventDefault();
 		}
-		start = stop = null;
+		
+		function swipeEnd(devt) {
+			jq(document).unbind(moveEvt, swipeMove);
+			if (start && stop) {
+				var wgt = zk.Widget.$(devt, {child:true}),
+					evt = devt.originalEvent,
+					data = evt.touches ? evt.touches[0] : evt,
+					dispT = stop.time - start.time;
+				
+				if (dispT < 500) {
+					var deltaX = start.coords[0] - stop.coords[0],
+						deltaY = start.coords[1] - stop.coords[1],
+						dispX = Math.abs(deltaX),
+						dispY = Math.abs(deltaY),
+						dir;
+					if (dispX > 30 && dispY < 75)
+						dir = deltaX > 0 ? 'left' : 'right';
+					else if (dispY > 30 && dispX < 75)
+						dir = deltaY > 0 ? 'up' : 'down';
+				}
+				
+				if (wgt)
+					_doEvt(new zk.Event(wgt, 'onSwipe', data, {start: start, stop: stop, dir: dir}, devt));
+			}
+			start = stop = null;
+		}
+		
+		jq(document).bind(moveEvt, swipeMove)
+			.one(endEvt, swipeEnd);
 	}
 })();
