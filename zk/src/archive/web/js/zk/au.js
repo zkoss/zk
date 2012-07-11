475cb0ee525e914ffd1a98d4cbe5359ce5ed821b
Neil Lee <neillee@potix.com>
2012-07-11 12:46:18 +0800

    B60-ZK-1205: Bug fixed
    Notification disappears immedately after other widgets gained focus.
    
    Refix by delay the showing of notification.
    
    Signed-off-by: Neil Lee <neillee@potix.com>

--- zk/src/archive/web/js/zk/au.js
+++ zk/src/archive/web/js/zk/au.js
@@ -1114,9 +1114,14 @@ zAu.cmd0 = /*prototype*/ { //no uuid at all
 	 */
 	showNotification: function (msg, type, pid, ref, pos, off, dur) {
 		var notif = (zul && zul.wgt) ? zul.wgt.Notification : null; // in zul
-		if (notif)
-			notif.show(msg, pid, {ref:ref, pos:pos, off:off, dur:dur, type:type});
-		else {
+		if (notif) {
+			// B60-ZK-1205
+			// Delay the showing of notification to avoid being closed accidentally
+			// by autodisable mechanism
+			setTimeout(function() {
+				notif.show(msg, pid, {ref:ref, pos:pos, off:off, dur:dur, type:type});
+			}, 100);
+		} else {
 			// TODO: provide a hook to customize
 			jq.alert(msg); // fall back to alert when zul is not available
 		}
