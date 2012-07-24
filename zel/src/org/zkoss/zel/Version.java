9386877359a2a06ce161c434589c8cffefe462a0
jumperchen <jumperchen@potix.com>
2012-07-24 05:26:35 +0100

    upgrade version to ZK 6.0.3

--- zel/src/org/zkoss/zel/Version.java
+++ zel/src/org/zkoss/zel/Version.java
@@ -23,5 +23,5 @@ package org.zkoss.zel;
 public class Version {
 	/** The version UID used to identify the resources.
 	 */
-	public static final String UID = "6.0.2";
+	public static final String UID = "6.0.3";
 }
