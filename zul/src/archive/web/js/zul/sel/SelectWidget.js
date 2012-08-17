ba69dd9b0b04f5a2238d7d654397633144d66080
Vincent <vincentjian@potix.com>
2012-08-17 18:16:59 +0800

    remove log

--- zul/src/archive/web/js/zul/sel/SelectWidget.js
+++ zul/src/archive/web/js/zul/sel/SelectWidget.js
@@ -1096,7 +1096,6 @@ zul.sel.SelectWidget = zk.$extends(zul.mesh.MeshWidget, {
 	},
 	/* Toggle the selection and notifies server. */
 	_toggleSelect: function (row, toSel, evt, skipFocus) {
-		zk.log('toggleSelect');
 		if (!this._multiple) {
 			var old = this.getSelectedItem();
 			if (row != old && toSel)
