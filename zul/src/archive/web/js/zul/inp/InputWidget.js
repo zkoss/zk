956e1bddddd0b4c27cb721f5caa523dab2d95520
Vincent <vincentjian@potix.com>
2012-08-23 18:11:54 +0800

    Bug ZK-1313: combobox dropdown changes document scroll height on iPad http://tracker.zkoss.org/browse/ZK-1313

--- zul/src/archive/web/js/zul/inp/InputWidget.js
+++ zul/src/archive/web/js/zul/inp/InputWidget.js
@@ -490,7 +490,7 @@ zul.inp.InputWidget = zk.$extends(zul.Widget, {
 			jq(this.getInputNode()).addClass(this.getInplaceCSS());
 		
 		//B65-ZK-1285: scroll window object back when virtual keyboard closed on ipad
-		if (zk.mobile) {		
+		if (zk.ios) {		
 			var x = window.pageXOffset,
 				y = window.pageYOffset,
 				winX = this._windowX,
@@ -499,7 +499,7 @@ zul.inp.InputWidget = zk.$extends(zul.Widget, {
 				window.scrollTo(winX, winY);
 		}
 	},
-	_doTouch: zk.mobile ? function (evt) {
+	_doTouch: zk.ios ? function (evt) {
 		//B65-ZK-1285: get window offset information before virtual keyboard opened on ipad
 		this._windowX = window.pageXOffset;
 		this._windowY = window.pageYOffset;
