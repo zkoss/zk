4bf3d214ce23b3b4a0cd06a64e5751a76277874d
Vincent <vincentjian@potix.com>
2012-09-06 10:44:32 +0800

    add java doc for zswipe

--- zk/src/archive/web/js/zk/zswipe.js
+++ zk/src/archive/web/js/zk/zswipe.js
@@ -26,8 +26,25 @@ Copyright (C) 2012 Potix Corporation. All Rights Reserved.
 				wevt.domEvent.stop();
 		}
 	}
-
+/**
+ * A swipe object used to make a DOM element swipe-able.
+ * @disable(zkgwt)
+ */
 zk.Swipe = zk.$extends(zk.Object, {
+	/**
+	 * Constructor
+	 * @param Object widget the object for swipe.
+	 * It can be anything, but it is usually a widget ({@link zk.Widget}).
+	 * @param DOMElement node [optional] the DOM element that is made to be swipe-able.
+	 * If omitted and widget is a widget, {@link zk.Widget#$n} is assumed.
+	 * @param Map opts [optional] options. Allowed options:
+	 * <ul>
+	 * <li>int scrollThreshold: swipe displacement(pixel). When larger than threshold, prevent page scrolling. Default: 5.</li>
+	 * <li>int duration: swipe start to end time duration(millisecond). When larger than time duration, it is not a swipe action. Default: 500.</li>
+	 * <li>int minDisplacement: the minimum swipe displacement(pixel). When smaller than minimum displacement, it is not a swipe action. Default: 30.</li>
+	 * <li>int maxDisplacement: the maximum swipe displacement(pixel). When larger than maximum displacement, it is not a swipe action. Default: 75.</li>
+	 * </ul>
+	 */
 	$init: function (widget, node, opts) {
 		this.widget = widget;
 		this.node = node = node ? jq(node, zk)[0]: widget.node || (widget.$n ? widget.$n() : null);
