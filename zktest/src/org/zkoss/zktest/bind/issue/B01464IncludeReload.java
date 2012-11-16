3b46b7e883a2b45e56dccba7517daf006be2a164
Dennis <atticcat@gmail.com>
2012-11-16 09:06:25 +0800

    fix zk 1464

---
+++ zktest/src/org/zkoss/zktest/bind/issue/B01464IncludeReload.java
@@ -0,0 +1,43 @@
+package org.zkoss.zktest.bind.issue;
+
+import java.util.Date;
+
+import org.zkoss.bind.BindContext;
+import org.zkoss.bind.Converter;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+
+public class B01464IncludeReload {
+	String url = "/bind/issue/B01464IncludeReload2.zul";
+	public String getUrl(){
+		return url;
+	}
+	
+	@org.zkoss.bind.annotation.Command
+	@NotifyChange("url")
+	public void reload(){
+		
+	}
+	
+	public Date getNow(){
+		return new Date();
+	}
+	
+	public Converter getConverter(){
+		return new Converter() {
+			
+			@Override
+			public Object coerceToUi(Object val, Component component, BindContext ctx) {
+				if(val instanceof String){
+					return val +"?tms=" + System.currentTimeMillis();
+				}
+				return val;
+			}
+			
+			@Override
+			public Object coerceToBean(Object val, Component component, BindContext ctx) {
+				return null;
+			}
+		};
+	}
+}
