6c2b099abfcfeb6109406f1dd0d2628de4b0d2f6
MontyPan <montypan@potix.com>
2012-11-23 17:59:42 +0800

    Fix javascript syntax error which generated by B65_ZK_1345.java

--- zktest/src/org/zkoss/zktest/test2/B65_ZK_1345.java
+++ zktest/src/org/zkoss/zktest/test2/B65_ZK_1345.java
@@ -10,6 +10,6 @@ public class B65_ZK_1345 implements MessageLoader {
 	@Override
 	public void load(StringBuffer out, Execution exec) throws IOException {
 		out.append("B65_ZK_1345 = \"load by B65_ZK_1345.java and " +
-				"locale is " + Locales.getCurrent() +"\"");
+				"locale is " + Locales.getCurrent() +"\";");
 	}
 }
