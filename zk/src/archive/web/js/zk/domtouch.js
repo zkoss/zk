323321560108deb50358739865464bc6fd31ae57
Vincent <vincentjian@potix.com>
2012-12-05 10:43:08 +0800

    fix mouse/touch event mapping

--- zk/src/archive/web/js/zk/domtouch.js
+++ zk/src/archive/web/js/zk/domtouch.js
@@ -13,6 +13,110 @@ This program is distributed under LGPL Version 2.1 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
 (function () {
+function _createMouseEvent (type, button, changedTouch, ofs) {
+	if (!ofs)
+		ofs = {sx: 0,sy: 0,cx: 0,cy: 0};
+	
+	var simulatedEvent = document.createEvent("MouseEvent");
+	simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+		changedTouch.screenX + ofs.sx, changedTouch.screenY + ofs.sy, 
+		changedTouch.clientX + ofs.cx, changedTouch.clientY + ofs.cy,
+		false, false, false, false, button, null);
+	return simulatedEvent;		
+}
+function _createJQEvent (target, type, button, changedTouch, ofs) {
+	//do not allow text
+	//ZK-1011
+	if (target && (target.nodeType === 3 || target.nodeType === 8))
+		target = target.parentNode;
+	
+	var originalEvent = _createMouseEvent(type, button, changedTouch, ofs),
+		props = jQuery.event.props,
+		event = jQuery.Event(originalEvent);
+
+	for ( var i = props.length, prop; i; ) {
+		prop = props[--i];
+		event[prop] = originalEvent[prop];
+	}
+	event.target = target;
+	return event;
+}
+function _toMouseEvent(event, changedTouch) {
+	switch (event.type) {
+	case 'touchstart':
+		return _createJQEvent(changedTouch.target, 'mousedown', 0, changedTouch);
+	case 'touchend':
+		return _createJQEvent(
+			document.elementFromPoint(
+				changedTouch.clientX, 
+				changedTouch.clientY), 
+				'mouseup', 0, changedTouch);
+		break;
+	case 'touchmove':
+		var ele = document.elementFromPoint(changedTouch.clientX,changedTouch.clientY);
+		return (ele && _createJQEvent(ele, 'mousemove', 0, changedTouch)) || null;
+	}
+	return event;
+}
+function _doEvt(type, evt, jqevt) {
+	var eventFuncs = jq.data(evt.currentTarget, 'zk_eventFuncs'),
+		funcs;
+	//store original event for invoke stop
+	jqevt.touchEvent = evt.originalEvent;
+	if (eventFuncs && (funcs = eventFuncs[type])) {
+		for (var i = 0, l = funcs.length; i < l; i++)
+			funcs[i](jqevt);
+	}
+}
+function delegateEventFunc (event) {
+	var touchEvt = event.originalEvent;
+	if (touchEvt.touches.length > 1) return;
+	
+	var evt;
+	
+	if(evt = _toMouseEvent(event, touchEvt.changedTouches[0]))
+		_doEvt(event.type, event, evt);
+}
+zk.copy(zjq, {
+	eventTypes: {
+		zmousedown: 'touchstart',
+		zmouseup: 'touchend',
+		zmousemove: 'touchmove'
+	}
+});
+function _storeEventFunction(elem, type, data, fn) {
+	var eventFuncs = jq.data(elem, 'zk_eventFuncs'),
+		funcs;
+		
+	//store functions in jq data
+	if (!eventFuncs) {
+		eventFuncs = {};
+		jq.data(elem, 'zk_eventFuncs', eventFuncs);
+	}
+	
+	if (funcs = eventFuncs[type]) {
+		funcs.push(fn);
+		return false; //already listen
+	}
+	eventFuncs[type] = [fn];
+	return true;
+}
+function _removeEventFunction(elem, type, fn) {
+	var eventFuncs = jq.data(elem, 'zk_eventFuncs'),
+		funcs;
+	
+	if (eventFuncs && (funcs = eventFuncs[type])) {
+		funcs.$remove(fn);
+		if (!funcs.length) {
+			delete eventFuncs[type];
+			for (var i in eventFuncs)
+				if (i)
+					return true;
+			jq.removeData(elem, 'zk_eventFuncs');
+			return true; //has no listen
+		}
+	}
+}
 var _xWidget = {};
 zk.override(zk.Widget.prototype, _xWidget, {
 	bindSwipe_: function () {
@@ -173,6 +277,44 @@ zk.override(zk.Widget.prototype, _xWidget, {
 			return false;
 		}
 		this._cancelHold();
+	}
+});
+var _jq = {},
+	_jqEvent = {};
+zk.override(jq.fn, _jq, {
+	bind: function(type, data, fn) {
+		var evtType;
+		if (evtType = zjq.eventTypes[type]) {
+			// refer to jquery bind function for reassign args
+			if ( jq.isFunction(data) || data === false ) {
+				fn = data;
+				data = undefined;
+			}
+			if (_storeEventFunction(this[0], evtType, data, fn))
+				this.zbind(evtType, data, delegateEventFunc);
+		} else
+			this.zbind.apply(this, arguments); // Bug ZK-1142: recover back to latest domios.js
+			
+		return this;
+	},
+	unbind: function(type, fn){
+		var evtType;
+		if (evtType = zjq.eventTypes[type]) {
+			if (_removeEventFunction(this[0], evtType, fn))
+				this.zunbind(evtType, delegateEventFunc);
+		} else
+			this.zunbind.apply(this, arguments); // Bug ZK-1142: recover back to latest domios.js
+		return this;
+	}
+});
+zk.override(jq.Event.prototype, _jqEvent, {
+	stop: function () {
+		_jqEvent.stop.apply(this);
+		var tEvt;
+		if (tEvt = this.touchEvent) {
+			tEvt.preventDefault();
+			tEvt.stopPropagation();
+		}
 	}
 });
 })();
