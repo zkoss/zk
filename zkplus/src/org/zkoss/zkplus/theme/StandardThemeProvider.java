91e02c243a25eaf43572ae1266e9b3202a1ef89b
Neil Lee <neillee@potix.com>
2012-12-12 16:26:41 +0800

    Deprecated the theme supporting API prior to 6.5.1

---
+++ zkplus/src/org/zkoss/zkplus/theme/StandardThemeProvider.java
@@ -0,0 +1,36 @@
+/* StandardThemeProvider.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mar 17, 2011 8:49:08 AM
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under LGPL Version 2.1 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zkplus.theme;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.zkoss.lang.Strings;
+import org.zkoss.zk.ui.Execution;
+
+/**
+ * A standard implementation of ThemeProvider, which works with the Breeze series
+ * themes.
+ * @author simonpai
+ * @deprecated Please use org.zkoss.zul.theme.StandardThemeProvider instead after 6.5.2
+ */
+@Deprecated
+public class StandardThemeProvider extends org.zkoss.zul.theme.StandardThemeProvider {
+}
---
+++ zkplus/src/org/zkoss/zkplus/theme/Themes.java
@@ -0,0 +1,28 @@
+/* Themes.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mar 22, 2011 7:33:44 PM , Created by sam
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under LGPL Version 2.1 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.zkplus.theme;
+
+/**
+ * Utilities for managing standard theme. 
+ * @author sam
+ * @deprecated Please use org.zkoss.zul.theme.Themes instead after 6.5.2
+ */
+@Deprecated
+public class Themes extends org.zkoss.zul.theme.Themes {
+}
