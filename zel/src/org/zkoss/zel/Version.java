ead657608940dac2ee6c2edc78e17c4c130688a9
Vincent <vincentjian@potix.com>
2012-12-20 09:08:27 +0800

    update version to 6.0.5

--- zel/src/org/zkoss/zel/Version.java
+++ zel/src/org/zkoss/zel/Version.java
@@ -23,5 +23,5 @@ package org.zkoss.zel;
 public class Version {
 	/** The version UID used to identify the resources.
 	 */
-	public static final String UID = "6.0.4";
+	public static final String UID = "6.0.5";
 }
