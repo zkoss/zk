c37065bf88dc3c06f6c1acfd873b0539d15691d8
Vincent <vincentjian@potix.com>
2013-02-27 12:44:59 +0800

    Fix bug ZK-1642: Tree columns width not expanding if scrollbar without hflex http://tracker.zkoss.org/browse/ZK-1642

--- zul/src/archive/web/js/zul/mesh/HeaderWidget.js
+++ zul/src/archive/web/js/zul/mesh/HeaderWidget.js
@@ -266,8 +266,9 @@ zul.mesh.HeaderWidget = zk.$extends(zul.LabelImageWidget, {
 				if (scroll > 11)
 					w -= scroll;
 
-				if (zk.ie){ //Related bugs: ZK-890 and ZK-242
-					if (scroll > 11 || !mw.ebody.clientWidth) {
+				if (zk.ie) { //Related bugs: ZK-890 and ZK-242
+					//Bug ZK-1642: only IE 6/7 has horizontal scrollbar issue
+					if (zk.ie < 8 && (scroll > 11 || !mw.ebody.clientWidth)) {
 						// For bug #3255116, we have to avoid IE to appear the hor. scrollbar.
 						var hdflex = jq(mw.ehead).find('table>tbody>tr>th:last-child')[0];
 						hdflex.style.width = '';
