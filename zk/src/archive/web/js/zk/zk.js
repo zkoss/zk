d48d3e6be7a24dac7b6fd239fc701d6cd65772f8
jumperchen <jumperchen@potix.com>
2013-03-21 05:53:21 +0100

    add a value of the function name to wgt.__fname__ while invoking the method defined in $define.

--- zk/src/archive/web/js/zk/zk.js
+++ zk/src/archive/web/js/zk/zk.js
@@ -76,23 +76,30 @@ it will be useful, but WITHOUT ANY WARRANTY.
 		return function (v, opts) {
 			var o = this[nm];
 			this[nm] = v;
-			if (o !== v || (opts && opts.force))
+			if (o !== v || (opts && opts.force)) {
+				this.__fname__ = nm.substring(1);
 				after.apply(this, arguments);
+				delete this.__fname__;
+			}
 			return this;
 		};
 	}
 	function defSet10(nm, before) {
 		return function (/*v, opts*/) {
+			this.__fname__ = nm.substring(1);
 			this[nm] = before.apply(this, arguments);
+			delete this.__fname__;
 			return this;
 		};
 	}
 	function defSet11(nm, before, after) {
 		return function (v, opts) {
 			var o = this[nm];
+			this.__fname__ = nm.substring(1);
 			this[nm] = v = before.apply(this, arguments);
 			if (o !== v || (opts && opts.force))
 				after.apply(this, arguments);
+			delete this.__fname__;
 			return this;
 		};
 	}
