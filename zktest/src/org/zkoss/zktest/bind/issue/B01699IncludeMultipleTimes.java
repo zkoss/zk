48136857a57e71f95031bacb5af56441bec0bc43
Dennis <atticcat@gmail.com>
2013-04-08 18:21:37 +0800

    fix zk-1699 performance issue in include.

---
+++ zktest/src/org/zkoss/zktest/bind/issue/B01699IncludeMultipleTimes.java
@@ -0,0 +1,22 @@
+package org.zkoss.zktest.bind.issue;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+
+public class B01699IncludeMultipleTimes {
+	
+	private String src = "/bind/issue/B01699IncludeMultipleTimesInner.zul";
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	
+	@Command @NotifyChange("src")
+	public void page2(){
+		src = "/bind/issue/B01699IncludeMultipleTimesInner2.zul";
+	}
+}
