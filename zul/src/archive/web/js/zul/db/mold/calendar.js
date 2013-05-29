823e3f4138bfe113511c8e5cbf1ec870bbb86f7e
jumperchen <jumperchen@potix.com>
2013-05-29 08:13:32 +0100

    1. add Calendar to support CSS3 2. add a jquery transition plugin (MIT License)

--- zul/src/archive/web/js/zul/db/mold/calendar.js
+++ zul/src/archive/web/js/zul/db/mold/calendar.js
@@ -15,76 +15,43 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 }}IS_RIGHT
 */
 function (out) {
-	var uuid = this.uuid,
-		view = this._view,
-		zcls = this.getZclass(),
-		val = this.getTime(),
-		m = val.getMonth(),
-		d = val.getDate(),
-		y = val.getFullYear(),
-		ydelta = new zk.fmt.Calendar(val, this._localizedSymbols).getYear() - y, 
-		yofs = y - (y % 10 + 1),
-		ydec = zk.parseInt(y/100),
-		tagnm = zk.ie || zk.gecko ? "a" : "button",
-		localizedSymbols = this._localizedSymbols || {
-			DOW_1ST: zk.DOW_1ST,
-				ERA: zk.ERA,    
-			 YDELTA: zk.YDELTA,
-			   SDOW: zk.SDOW,
-			  S2DOW: zk.S2DOW,
-			   FDOW: zk.FDOW,
-			   SMON: zk.SMON,
-			  S2MON: zk.S2MON,
-			   FMON: zk.FMON,
-				APM: zk.APM
-		};
+	var renderer = zul.db.Renderer,
+		uuid = this.uuid,
+		view = this._view, 
+		tagnm = zk.ie || zk.gecko ? 'a' : 'button',
+		localizedSymbols = this.getLocalizedSymbols(),
+		icon = this.$s('icon');
+	
+	// header
+	out.push('<div id="', uuid, '"', this.domAttrs_(), '><div class="',
+			this.$s('header'), '"><a id="', uuid, '-left" href="javascript:;" class="', icon, ' ',
+			this.$s('left'), '"', this.isOutOfRange(true) ? 
+					' disabled="disabled"' : '',
+					'><i class="z-icon-caret-left"></i></a>',
+			'<a id="', uuid, '-title" href="javascript:;" class="', this.$s('title'), '">');
+	
+	renderer.titleHTML(this, out, localizedSymbols);
 
-	out.push('<div id="', this.uuid, '"', this.domAttrs_(), '><table style="table-layout: fixed" width="100%"', zUtl.cellps0, '>',
-			'<tr><td id="', uuid, '-tdl" class="', zcls, '-tdl');
+	out.push('</a><a id="', uuid, '-right" href="javascript:;" class="', icon, ' ',
+			this.$s('right'), '"', this.isOutOfRange() ? 
+					' disabled="disabled"' : '',
+					'><i class="z-icon-caret-right"></i></a></div>');
 	
-	if (view == 'decade' && ydec*100 == 1900)
-		out.push(' ', zcls, '-icon-disd');
-		
-	out.push('"><div  class="', zcls, '-left"><div id="', uuid, '-ly" class="', zcls, '-left-icon"></div></div></td>',
-				'<td><table class="', zcls, '-calctrl" width="100%" border="0" cellspacing="0" cellpadding="0">',
-				'<tr><td id="', uuid, '-title" class="', zcls, '-title">');
 	switch(view) {
 	case "day" :
-		out.push('<span id="', uuid, '-tm" class="', zcls, '-ctrler">', localizedSymbols.SMON[m], '</span> <span id="', uuid, '-ty" class="', zcls, '-ctrler">', y + ydelta, '</span>');
+		renderer.dayView(this, out, localizedSymbols);
 		break;
 	case "month" :
-		out.push('<span id="', uuid, '-tm" class="', zcls, '-ctrler">', localizedSymbols.SMON[m], '</span> <span id="', uuid, '-ty" class="', zcls, '-ctrler">', y + ydelta, '</span>');
+		renderer.monthView(this, out, localizedSymbols);
 		break;
 	case "year" :
-		out.push('<span id="', uuid, '-tyd" class="', zcls, '-ctrler">', yofs + ydelta + 1, '-', yofs + ydelta + 10, '</span>');
+		renderer.yearView(this, out, localizedSymbols);
 		break;
 	case "decade" :
-		out.push('<span id="', uuid, '-tyd" class="', zcls, '-ctrler">', ydec*100 + ydelta, '-', ydec*100 + ydelta+ 99, '</span>');
+		renderer.decadeView(this, out, localizedSymbols);
 		break;
 	}
-	out.push('</td></tr></table></td>',
-		'<td id="', uuid, '-tdr" class="', zcls, '-tdr');
-		
-	if (view == 'decade' && ydec*100 == 2000)
-		out.push(' ', zcls, '-icon-disd');
-	
-	out.push('"><div class="', zcls, '-right"><div id="', uuid, '-ry" class="', zcls, '-right-icon"></div></div></td></tr>');
-	//year view
-	switch(view) {
-	case "day" :
-		zul.db.Renderer.dayView(this, out, localizedSymbols);
-		break;
-	case "month" :
-		zul.db.Renderer.monthView(this, out, localizedSymbols);
-		break;
-	case "year" :
-		zul.db.Renderer.yearView(this, out, localizedSymbols);
-		break;
-	case "decade" :
-		zul.db.Renderer.decadeView(this, out, localizedSymbols);
-		break;
-	}
-	out.push('</table><', tagnm, ' id="', uuid,
+	out.push('<', tagnm, ' id="', uuid,
 		'-a" tabindex="-1" onclick="return false;" href="javascript:;" class="z-focus-a"></',
-		tagnm, '></td></tr></table></div>');
+		tagnm, '></div>');
 }
