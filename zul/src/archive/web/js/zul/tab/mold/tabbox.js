cc4d69a24fa89422b357e67980cd83dfc06ef90d
huangnoah <noahhuang@potix.com>
2013-06-09 22:14:27 +0800

    change arrow class

--- zul/src/archive/web/js/zul/tab/mold/tabbox.js
+++ zul/src/archive/web/js/zul/tab/mold/tabbox.js
@@ -17,29 +17,29 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 function (out) {
 	var iconImg = this.tabs.$s('icon-img'),
 		uuid = this.uuid,
-		zicon = this.tabs.$s('icon'),
+		zicon = this.$s('icon'),
 		getIcon = function(iconClass) {
 			return '<i class="' + zicon + ' z-' + iconClass + '"></i>';
 		};
 	out.push('<div ', this.domAttrs_(), '>');
 	if (this.isHorizontalBottom()){
 		if (this.tabpanels) this.tabpanels.redraw(out);
-		out.push('<div id="', uuid , '-right" class="', iconImg, ' ', this.tabs.$s('right'), '">' , getIcon('icon-caret-right'),  '</div>',
-				'<div id="', uuid , '-left" class="', iconImg, ' ', this.tabs.$s('left'), '">' , getIcon('icon-caret-left'),  '</div>');
+		out.push('<div id="', uuid , '-right" class="', iconImg, ' ', this.$s('right'), '">' , getIcon('icon-caret-right'),  '</div>',
+				'<div id="', uuid , '-left" class="', iconImg, ' ', this.$s('left'), '">' , getIcon('icon-caret-left'),  '</div>');
 		if (this.tabs) this.tabs.redraw(out);
 		if(this.isTabscroll() && this.toolbar) this.toolbar.redraw(out);
 	} else {
 		if (!this.isVertical()) {			
-			out.push('<div id="', uuid , '-right" class="', iconImg, ' ', this.tabs.$s('right'), '">' , getIcon('icon-caret-right'),  '</div>',
-						'<div id="', uuid , '-left" class="', iconImg, ' ', this.tabs.$s('left'), '">' , getIcon('icon-caret-left'),  '</div>');
+			out.push('<div id="', uuid , '-right" class="', iconImg, ' ', this.$s('right'), '">' , getIcon('icon-caret-right'),  '</div>',
+						'<div id="', uuid , '-left" class="', iconImg, ' ', this.$s('left'), '">' , getIcon('icon-caret-left'),  '</div>');
 			if(this.isTabscroll() && this.toolbar) {
 				this.toolbar.redraw(out);
 			}
 		}
 		if (this.tabs) this.tabs.redraw(out);
 		if (this.isVertical()) {
-			out.push('<div id="', uuid , '-up" class="', iconImg, ' ', this.tabs.$s('up'), '">' , getIcon('icon-caret-up'),  '</div>',
-						'<div id="', uuid , '-down" class="', iconImg, ' ', this.tabs.$s('down'), '">' , getIcon('icon-caret-down'),  '</div>');
+			out.push('<div id="', uuid , '-up" class="', iconImg, ' ', this.$s('up'), '">' , getIcon('icon-caret-up'),  '</div>',
+						'<div id="', uuid , '-down" class="', iconImg, ' ', this.$s('down'), '">' , getIcon('icon-caret-down'),  '</div>');
 		}
 		if (this.tabpanels) this.tabpanels.redraw(out);
 		
