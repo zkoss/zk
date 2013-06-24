663b2808780671b243f9caca04a7ecc084526c0c
Huang ShengYu <noahhuang@potix.com>
2013-06-24 12:44:42 +0800

    move redraw to combobutton.js, replace " with '

---
+++ zul/src/archive/web/js/zul/wgt/mold/combobutton.js
@@ -0,0 +1,33 @@
+/* combobutton.js
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon June 16 12:11:07     2013, Created by huangnoah
+
+Copyright (C) 2013 Potix Corporation. All Rights Reserved.
+
+This program is distributed under LGPL Version 2.1 in the hope that
+it will be useful, but WITHOUT ANY WARRANTY.
+*/
+function (out) {
+	var tabi = this._tabindex,
+		uuid = this.uuid;
+		
+	out.push('<span ', this.domAttrs_(), ' >',	
+				'<button id="', uuid, '-real" class="', this.$s('input') ,'" type="', this._type, '"');
+		
+	if (this._disabled) out.push(' disabled="disabled"');
+	if (tabi) out.push(' tabindex="', tabi, '"');
+	out.push('>', this.domContent_(), '</button>', 
+			 '<a id="', uuid, '-btn" class="', this.$s('button'), '">', 
+				'<i id="', uuid, '-icon" class="', this.$s('icon'), ' z-icon-caret-down"></i>', 
+			 '</a>');
+	// pp
+	if (this.firstChild)
+		this.firstChild.redraw(out);
+		
+	out.push('</span>');
+}
