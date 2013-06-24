cecb362494e9c244a1ed7cee20a691ec4dc6a5b6
Huang ShengYu <noahhuang@potix.com>
2013-06-24 11:18:24 +0800

    fine tuning

--- zul/src/archive/web/js/zul/wgt/mold/button.js
+++ zul/src/archive/web/js/zul/wgt/mold/button.js
@@ -14,10 +14,8 @@ it will be useful, but WITHOUT ANY WARRANTY.
 */
 function (out) {
 	out.push('<button type="', this._type, '"', this.domAttrs_());
-	var tabi = this._tabindex,
-		uuid = this.uuid;
+	var tabi = this._tabindex;
 	if (this._disabled) out.push(' disabled="disabled"');
 	if (tabi) out.push(' tabindex="', tabi, '"');
-	out.push('>', this.domContent_(), '');
-	out.push('</button>');
+	out.push('>', this.domContent_(), '</button>');
 }
