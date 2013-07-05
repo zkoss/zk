7031ec80df2bd1a40897c8e358df6e87c0e9e32c
Yuki Takei <yuki@fio.jp>
2013-07-06 00:14:25 +0900

    update Javadoc comment of Messagebox.java

--- zul/src/org/zkoss/zul/Messagebox.java
+++ zul/src/org/zkoss/zul/Messagebox.java
@@ -146,6 +146,7 @@ public class Messagebox {
 	 * recognized. For the default template, typical parameters are<br/>
 	 * <code>width</code>: the width of the dialog.<br/>
 	 * <code>icon</code>: the URI of the icon
+	 * <code>sclass</code>: the CSS class name of the top level Window
 	 * @since 6.0.0
 	 */
 	public static Button show(String message, String title,
