d3459b267394e7f76f5e619e54f7c84557c9cbcc
Vincent <vincentjian@potix.com>
2013-08-05 16:03:56 +0800

    fix tabbox issue and fine tune

--- zul/src/archive/web/js/zul/tab/mold/tabbox.js
+++ zul/src/archive/web/js/zul/tab/mold/tabbox.js
@@ -17,44 +17,46 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 function (out) {
 	var icon = this.$s('icon'),
 		uuid = this.uuid,
+		tabscroll = this.isTabscroll(),
+		tabs = this.tabs,
+		tabpanels = this.tabpanels,
+		toolbar = this.toolbar,
 		getIcon = function(fontIconCls) {
-			return '<i class="z-' + fontIconCls + '"></i>';
+			return '<i class="z-icon-caret-' + fontIconCls + '"></i>';
 		};
 	out.push('<div ', this.domAttrs_(), '>');
-	if (this.isHorizontalBottom()){
-		if (this.tabpanels) this.tabpanels.redraw(out);
-		if (this.isTabscroll()) {
+	if (this.isHorizontal()) { // horizontal
+		if (this.isHorizontalBottom()) {
+			if (tabpanels)
+				tabpanels.redraw(out);
+			if (tabs)
+				tabs.redraw(out);
+		} else {
+			if (tabs)
+				tabs.redraw(out);
+			if (tabpanels)
+				tabpanels.redraw(out);
+		}
+		if (tabscroll) {
 			out.push(
-				'<div id="', uuid , '-right" class="', icon, ' ', this.$s('right'), '">' , getIcon('icon-caret-right'),  '</div>',
-				'<div id="', uuid , '-left" class="', icon, ' ', this.$s('left'), '">' , getIcon('icon-caret-left'),  '</div>');
+				'<div id="', uuid , '-left" class="', icon, ' ', this.$s('left'), '">', getIcon('left'), '</div>',
+				'<div id="', uuid , '-right" class="', icon, ' ', this.$s('right'), '">' , getIcon('right'),  '</div>');
 		}
-		if (this.tabs) this.tabs.redraw(out);
-		if(this.isTabscroll() && this.toolbar) this.toolbar.redraw(out);
-	} else {
-		if (!this.isVertical()) {
-			if (this.isTabscroll()) {
+		if (tabscroll && toolbar)
+			toolbar.redraw(out);
+	} else { // accordion and vertical(-right)
+		if (tabs)
+			tabs.redraw(out);
+		if (tabpanels)
+			tabpanels.redraw(out);
+		if (this.isVertical()) { // only vertical allow tabscroll
+			if (tabscroll) {
 				out.push(
-					'<div id="', uuid , '-right" class="', icon, ' ', this.$s('right'), '">' , getIcon('icon-caret-right'),  '</div>',
-					'<div id="', uuid , '-left" class="', icon, ' ', this.$s('left'), '">' , getIcon('icon-caret-left'),  '</div>');
+					'<div id="', uuid , '-up" class="', icon, ' ', this.$s('up'), '">', getIcon('up'), '</div>',
+					'<div id="', uuid , '-down" class="', icon, ' ', this.$s('down'), '">' , getIcon('down'),  '</div>');
 			}
-			if(this.isTabscroll() && this.toolbar) {
-				this.toolbar.redraw(out);
-			}
+			out.push('<div class="z-clear"></div>');
 		}
-		if (this.tabs)
-			this.tabs.redraw(out);
-		if (this.isVertical()) {
-			if (this.isTabscroll()) {
-				out.push(
-					'<div id="', uuid , '-up" class="', icon, ' ', this.$s('up'), '">' , getIcon('icon-caret-up'),  '</div>',
-					'<div id="', uuid , '-down" class="', icon, ' ', this.$s('down'), '">' , getIcon('icon-caret-down'),  '</div>');
-			}
-		}
-		if (this.tabpanels)
-			this.tabpanels.redraw(out);
 	}
-	
-	if (this.isVertical())
-		out.push('<div class="z-clear"></div>');
 	out.push("</div>");
 }
