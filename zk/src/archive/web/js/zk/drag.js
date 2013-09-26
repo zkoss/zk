2f075e1f529ec135d5352eaae13e320d1044e19f
RaymondChao <raymondchao@potix.com>
2013-09-26 14:40:24 +0800

    Bug fixed B65-ZK-1946: The select mold listbox with multiple attribute in a draggable listitem will be disabled (Chrome, IE10) http://tracker.zkoss.org/browse/ZK-1946

--- zk/src/archive/web/js/zk/drag.js
+++ zk/src/archive/web/js/zk/drag.js
@@ -611,7 +611,7 @@ String scroll; //DOM Element's ID</code></pre>
 
 		this.offset = ofs;
 		_activate(this, devt, pt);
-
+		
 		if ((!zk.ie || zk.ie9) && !zk.mobile) {
 			if (!zk.Draggable.ignoreStop(target)) // Bug B65-ZK-1839 we should ignore select tag on IE9
 				devt.stop();
@@ -810,7 +810,8 @@ String scroll; //DOM Element's ID</code></pre>
 		//Bug 3310017/3309975: if trigger focus() FF and chrome cannot handle input cursor.
 		return zk(target).isInput()
 				// B65-ZK-1839 ignore select tag for IE9
-				|| (zk.ie9 && jq.nodeName(target, 'select'));
+				|| (zk.ie9 && jq.nodeName(target, 'select'))
+				|| jq.nodeName(target, 'option'); // B65-ZK-1946: ignore option tag 
 	}
 });
 })();
