d1ada6e8e4b2f34f75c61e18dba14913c48e2c55
jumperchen <jumperchen@potix.com>
2013-11-14 10:19:04 +0000

    add more details for Javadoc

--- zweb/src/org/zkoss/web/fn/ThemeFns.java
+++ zweb/src/org/zkoss/web/fn/ThemeFns.java
@@ -107,7 +107,7 @@ public class ThemeFns {
 	 * @param direction
 	 *            "ver", "hor", "diag-", "diag+", "rad"
 	 * @param colors
-	 *            the colors, which are separated by semicolon ";"
+	 *            the colors with stops, which are separated by semicolon ";"
 	 * @return A specific CSS gradient rules String
 	 */
 	public static String gradient(String direction, String colors) {
@@ -132,7 +132,7 @@ public class ThemeFns {
 	 * @param direction
 	 *            "ver", "hor", "diag-", "diag+", "rad"
 	 * @param colors
-	 *            the colors, which are separated by semicolon ";"
+	 *            the colors with stops, which are separated by semicolon ";"
 	 * @return A specific CSS gradient rules String without the words
 	 *         "background:".
 	 */
@@ -161,9 +161,9 @@ public class ThemeFns {
 					if (hex)
 						throw new IllegalArgumentException(
 								"The format of hexadecimal is wrong! [" + color
-										+ "]");
+										+ "] or without stops (%)");
 					else throw new IllegalArgumentException(
-							"The format of RGBA is wrong! [" + color + "]");
+							"The format of RGBA is wrong! [" + color + "] or without stops (%)");
 
 				String pos = color.substring(end, color.length());
 				color = color.substring(0, end);
@@ -183,9 +183,9 @@ public class ThemeFns {
 					if (hex)
 						throw new IllegalArgumentException(
 								"The format of hexadecimal is wrong! [" + color
-										+ "]");
+										+ "] or without stops (%)");
 					else throw new IllegalArgumentException(
-							"The format of RGBA is wrong! [" + color + "]");
+							"The format of RGBA is wrong! [" + color + "] or without stops (%)");
 
 				String pos = color.substring(end, color.length());
 				color = color.substring(0, end);
