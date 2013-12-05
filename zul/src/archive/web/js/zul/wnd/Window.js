17a8d460d464586098e1fa40676e5b757ccbe163
Huang ShengYu <noahhuang@potix.com>
2013-12-05 09:57:02 +0800

    Bug fixed ZK-2045: Page becomes blank after detaching a modal window having an iframe loaded with PDF in IE 11 http://tracker.zkoss.org/browse/ZK-2045

--- zul/src/archive/web/js/zul/wnd/Window.js
+++ zul/src/archive/web/js/zul/wnd/Window.js
@@ -1161,9 +1161,9 @@ zul.wnd.Window = zk.$extends(zul.Widget, {
 			this._mask = null;
 		}
 
-		// ZK-1951 Page becomes blank after detaching a modal window having an iframe loaded with PDF in IE 10
+		// ZK-1951, ZK-2045: Page becomes blank after detaching a modal window having an iframe loaded with PDF in IE 10, 11
 		// A workaround is to hide the iframe before remove
-		if (zk.ie == 10) {
+		if (zk.ie10_ || zk.ie11_) {
 			var $jq = jq(this.$n()).find('iframe');
 			if ($jq.length)
 				$jq.hide().remove();
