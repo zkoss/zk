9e22d99f54f8ec3bd2014e4e4bd449fc9ca32780
danchen0720 <danchen@potix.com>
2013-12-17 03:13:54 +0000

    Bug fixed B70-ZK-2003: When resizing using splitter in borderlayout visible popup windows do not close (bandbox popup, combobox popup) http://tracker.zkoss.org/browse/ZK-2003

--- zk/src/archive/web/js/zk/drag.js
+++ zk/src/archive/web/js/zk/drag.js
@@ -615,8 +615,11 @@ String scroll; //DOM Element's ID</code></pre>
 		_activate(this, devt, pt);
 		
 		if ((!(zk.ie < 11) || zk.ie8) && !zk.mobile) {
-			if (!zk.Draggable.ignoreStop(target)) // Bug B65-ZK-1839 we should ignore select tag on IE9
+			if (!zk.Draggable.ignoreStop(target)){ // Bug B65-ZK-1839 we should ignore select tag on IE9
 				devt.stop();
+				//B70-ZK-2003: fire to all the widgets that listen onFloatUp
+				zWatch.fire('onFloatUp', evt.target);
+			}
 			//IE8: if not stop, onclick won't be fired (B50-ZK-909.zul)
 			//IE6: if stop*, onclick won't be fired (unable to select) (test/dragdrop.zul)
 			//FF3: if not stop, IMG cannot be dragged (test/dragdrop.zul) and INPUT not droppable (Bug 3031511)
