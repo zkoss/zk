3a237b5553ee35bbce44abb15b30f6654b0594cd
Huang ShengYu <noahhuang@potix.com>
2014-03-25 11:52:58 +0800

    Feature added ZK-2227: ZK Messages support Polish lang http://tracker.zkoss.org/browse/ZK-2227

---
+++ zk/src/archive/web/js/zk/lang/msgzk_pl.js
@@ -0,0 +1,35 @@
+/* msgzk_pl.js "charset=utf-8"
+
+	Purpose:
+		Locale dependent message
+	Description:
+		
+	History: 
+		Fri Mar 17 14:57:32     2014, Created by Michal Derecki (Michal.Derecki at asseco dot pl) 
+
+Copyright (C) 2005-2014 Potix Corporation. All Rights Reserved.
+
+	This program is distributed under LGPL Version 2.1 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+*/
+msgzk = {
+NOT_FOUND: "Nie znaleziono: ",
+UNSUPPORTED: "Nie wspierane: ",
+FAILED_TO_SEND: "Niepowodzenie wysłania żądania na serwer.",
+FAILED_TO_RESPONSE: "Serwer jest tymczasowo niedostępny.",
+TRY_AGAIN: "Czy chciałbyś spróbować ponownie?",
+UNSUPPORTED_BROWSER: "Nie wspierana przeglądarka: ",
+ILLEGAL_RESPONSE: "Nieznana odpowiedź serwera. Przeładuj stronę i spróbuj ponownie.\n",
+FAILED_TO_PROCESS: "Nie udało się przetworzyć ",
+GOTO_ERROR_FIELD: "Kliknij żeby wprowadzić ponownie dane",
+PLEASE_WAIT: "Przetwarzanie...",
+
+
+FILE_SIZE: "Rozmiar pliku: ",
+KBYTES: "KB",
+ 
+FAILED_TO_LOAD: "Nie udało się załadować ",
+FAILED_TO_LOAD_DETAIL: "Może być to spowodowane zatorem sieciowym. Możesz przeładować stronę i spróbować ponownie.",
+CAUSE: "Przyczyna: ",
+LOADING: "Ładowanie"
+};
