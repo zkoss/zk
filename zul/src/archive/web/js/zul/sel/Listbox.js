b2b77b2abe95ccbfeb486a0cbc9763dadf2a1923
jerrywell <b80203@gmail.com>
2014-07-22 11:13:54 +0100

    Bug ZK-2365: "No items to display" is no longer centered in a Listbox with Firefox and Chrome.

--- zul/src/archive/web/js/zul/sel/Listbox.js
+++ zul/src/archive/web/js/zul/sel/Listbox.js
@@ -34,7 +34,8 @@ it will be useful, but WITHOUT ANY WARRANTY.
 							colspan++;
 				}
 				empty.colSpan = colspan || 1;
-				empty.style.display = 'block';
+				// ZK-2365 table cell needs the "display:table-cell" when colspan is enable.
+				empty.style.display = 'table-cell';
 			}
 		}
 		wgt._shallFixEmpty = false;
