b2a489699b293d4bef66dad910202043e44b359a
jumperchen <jumperchen@potix.com>
2014-08-12 08:53:40 +0100

    Bug ZK-2398: Sortable tree missing sort icon on tree column

--- zul/src/archive/web/js/zul/sel/mold/treecol.js
+++ zul/src/archive/web/js/zul/sel/mold/treecol.js
@@ -15,7 +15,7 @@ it will be useful, but WITHOUT ANY WARRANTY.
 function (out) {
 	out.push('<th', this.domAttrs_(), '><div id="', this.uuid, '-cave" class="',
 			this.$s('content'), '"', this.domTextStyleAttr_(),
-			'><div class="', this.$s('sorticon'), '"></div>', this.domContent_());
+			'><div class="', this.$s('sorticon'), '"><i id="', this.uuid, '-sort-icon"></i></div>', this.domContent_());
 	for (var w = this.firstChild; w; w = w.nextSibling)
 		w.redraw(out);
 	out.push('</div></th>');	
