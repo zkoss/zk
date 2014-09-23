c9b0f9d48fea20ceb9253175545dbbbf4d24ac6d
DevChu <marchuqq@gmail.com>
2014-09-23 16:47:01 +0800

    Bug ZK-2463: dynamically adding columns to frozen grids raises javascript errors

---
+++ zktest/src/org/zkoss/zktest/test2/B70_ZK_2463_Column.java
@@ -0,0 +1,17 @@
+package org.zkoss.zktest.test2;
+
+public class B70_ZK_2463_Column {
+	private String title;
+
+	public B70_ZK_2463_Column(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
