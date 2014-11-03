1844ac7de754ac0f3993aa8380fce1445a1f7a44
hanhsu <hanhsu@potix.com>
2014-11-03 09:13:11 +0000

    Bug ZK-2506: href with mailto: causes session timeout

---
+++ zktest/src/org/zkoss/zktest/test2/B70_ZK_2506_VM.java
@@ -0,0 +1,20 @@
+package org.zkoss.zktest.test2;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+
+public class B70_ZK_2506_VM {
+
+  private String name;
+
+  @Command
+  @NotifyChange("name")
+  public void checkState() {
+     name = "Hello there!";
+  }
+
+  public String getName() {
+     return name;
+  }
+
+}
