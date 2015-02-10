dc6972adb3d35cf2d4bd3f89230d0f257472e5ee
jumperchen <jumperchen@potix.com>
2015-02-10 08:10:05 +0000

    fine tune

--- zk/src/archive/web/js/zk/widget.js
+++ zk/src/archive/web/js/zk/widget.js
@@ -5116,7 +5116,7 @@ zk.Native = zk.$extends(zk.Widget, {
 				}
 			}
 			// B65-ZK-1836 and B70-ZK-2622
-			out.push(this.$class.replaceScriptContent(s));
+			out.push(zk.Native.replaceScriptContent(s));
 			if (this.value && s.startsWith('<textarea'))
 				out.push(this.value);
 		}
@@ -5144,7 +5144,7 @@ zk.Native = zk.$extends(zk.Widget, {
 						// enclose with <script></script>
 						if (substr.length >= 17) {
 							var cnt = substr.substring(8, substr.length - 9);
-							var cnt2 = this.replaceScriptContent(cnt);
+							var cnt2 = zk.Native.replaceScriptContent(cnt);
 							if (cnt != cnt2)
 								str = str.replace(cnt, cnt2);
 						}
