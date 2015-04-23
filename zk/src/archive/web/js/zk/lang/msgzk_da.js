ab8f1746cf36338c91dfda0862483a3d71500629
chunfuchang <chunfuchang@potix.com>
2015-04-23 07:32:58 +0100

    Feature ZK-2725: New ZK message support - Danish translation

---
+++ zk/src/archive/web/js/zk/lang/msgzk_da.js
@@ -0,0 +1,35 @@
+/* msgzk.js "charset=utf-8"
+ 
+    Purpose:
+        Locale dependent message
+    Description:
+         
+    History:
+		Thu Apr  23 14:25:41     2015, Created by sjeffen
+ 
+Copyright (C) 2005 Potix Corporation. All Rights Reserved.
+ 
+    This program is distributed under LGPL Version 3.0 in the hope that
+    it will be useful, but WITHOUT ANY WARRANTY.
+*/
+msgzk = {
+NOT_FOUND: "Ikke fundet: ",
+UNSUPPORTED: "Endnu ikke understøttet: ",
+FAILED_TO_SEND: "Kunne ikke at sende anmodninger til serveren.",
+FAILED_TO_RESPONSE: "Serveren er midlertidigt ude af drift.",
+TRY_AGAIN: "Vil du prøve igen?",
+UNSUPPORTED_BROWSER: "Ikke understøttet browser: ",
+ILLEGAL_RESPONSE: "Ukendt svar fra serveren. Genindlæs og prøv igen.\n",
+FAILED_TO_PROCESS: "Det lykkedes ikke at behandle ",
+GOTO_ERROR_FIELD: "Klik for at indtaste data igen",
+PLEASE_WAIT: "Arbejder...",
+ 
+FILE_SIZE: "Filstørrelse: ",
+KBYTES: "KB",
+ 
+FAILED_TO_LOAD: "Kunne ikke indlæse ",
+FAILED_TO_LOAD_DETAIL: "Det kan være forårsaget af trafik. Genindlæs denne side og prøv igen.",
+CAUSE: "Årsag: ",
+ 
+LOADING: "Indlæser"
+};
