2d2a37f7e04b5464e3758243c9a2496fc50e692d
jumperchen <jumperchen@potix.com>
2015-10-28 12:39:12 +0800

    * Bug fixed for ZK-2934: A control in a grid row does not count as a focusable control when giving focus to the first element of a modal window * Refixed for ZK-2664, which causes the side-effect on ZK-2934

--- zk/src/archive/web/js/zk/widget.js
+++ zk/src/archive/web/js/zk/widget.js
@@ -1653,9 +1653,11 @@ wgt.$f().main.setTitle("foo");
 		}
 		if (cfid) {
 			cf = zk.Widget.$(cfid);
-			if (!cf)
-				_rsFocus({focus: newwgt, range: cfrg}); // restore to outer root
-			else if (zUtl.isAncestor(newwgt, cf))
+			if (!cf) {
+			    // Bug ZK-2664, we should not restore the focus to root component, which
+			    // may not be correct one.
+			    // _rsFocus({focus: newwgt, range: cfrg}); // restore to outer root
+			} else if (zUtl.isAncestor(newwgt, cf))
 				_rsFocus({focus: cf, range: cfrg});
 		}
 	},
