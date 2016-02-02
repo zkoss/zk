aa915878e1bd8beeafa7868615e7246aae6115c0
christopher-szu <christopherszu@potix.com>
2016-02-02 19:37:30 +0800

    refine ZK-3111 test case

--- zktest/test/java/org/zkoss/zktest/zats/test2/B80_ZK_3111Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B80_ZK_3111Test.java
@@ -28,14 +28,152 @@ import org.zkoss.zktest.zats.ztl.JQuery;
  */
 public class B80_ZK_3111Test extends WebDriverTestCase {
 
-	private List<String> locales = Arrays.asList(
-			new String[] { "ar", "bg", "ca", "cs", "da", "de", "es", "fr", "hu",
-					"id", "it", "ja", "ko", "nl", "pl", "pt", "pt_BR", "ro",
-					"ru", "sk", "sl", "sv", "tr", "uk", "vi", "zh", "zh_CN",
-					"zh_SG", "zh_TW" });
+	@Test
+	public void testar() {
+		test("ar");
+	}
 
 	@Test
-	public void test() {
+	public void testbg() {
+		test("bg");
+	}
+
+	@Test
+	public void testca() {
+		test("ca");
+	}
+
+	@Test
+	public void testcs() {
+		test("cs");
+	}
+
+	@Test
+	public void testda() {
+		test("da");
+	}
+
+	@Test
+	public void testde() {
+		test("de");
+	}
+
+	@Test
+	public void testes() {
+		test("es");
+	}
+
+	@Test
+	public void testfr() {
+		test("fr");
+	}
+
+	@Test
+	public void testhu() {
+		test("hu");
+	}
+
+	@Test
+	public void testid() {
+		test("id");
+	}
+
+	@Test
+	public void testit() {
+		test("it");
+	}
+
+	@Test
+	public void testja() {
+		test("ja");
+	}
+
+	@Test
+	public void testko() {
+		test("ko");
+	}
+
+	@Test
+	public void testnl() {
+		test("nl");
+	}
+
+	@Test
+	public void testpl() {
+		test("pl");
+	}
+
+	@Test
+	public void testpt() {
+		test("pt");
+	}
+
+	@Test
+	public void testpt_BR() {
+		test("pt_BR");
+	}
+
+	@Test
+	public void testro() {
+		test("ro");
+	}
+
+	@Test
+	public void testru() {
+		test("ru");
+	}
+
+	@Test
+	public void testsk() {
+		test("sk");
+	}
+
+	@Test
+	public void testsl() {
+		test("sl");
+	}
+
+	@Test
+	public void testsv() {
+		test("sv");
+	}
+
+	@Test
+	public void testtr() {
+		test("tr");
+	}
+
+	@Test
+	public void testuk() {
+		test("uk");
+	}
+
+	@Test
+	public void testvi() {
+		test("vi");
+	}
+
+	@Test
+	public void testzh() {
+		test("zh");
+	}
+
+	@Test
+	public void testzh_CN() {
+		test("zh_CN");
+	}
+
+	@Test
+	public void testzh_SG() {
+		test("zh_SG");
+	}
+
+	@Test
+	public void testzh_TW() {
+		test("zh_TW");
+	}
+
+	private void test(String locale) {
 		connect();
 
 		//check init label, should be english
@@ -45,20 +183,18 @@ public class B80_ZK_3111Test extends WebDriverTestCase {
 			assertTrue(btn.attr("title").equals("Close"));
 		}
 
-		for (String locale : locales) {
-			//switch locale
-			JQuery textbox = jq("@textbox");
-			type(textbox, locale);
-			waitResponse(true);
-			click(jq("button").eq(0));
-			waitResponse(true);
+		//switch locale
+		JQuery textbox = jq("@textbox");
+		type(textbox, locale);
+		waitResponse(true);
+		click(jq("button").eq(0));
+		waitResponse(true);
 
-			//check label again, should be localized label
-			btns = jq(".z-window-close, .z-panel-close").iterator();
-			while (btns.hasNext()) {
-				JQuery btn = btns.next();
-				assertFalse(btn.attr("title").contains("27112700"));
-			}
+		//check label again, should be localized label
+		btns = jq(".z-window-close, .z-panel-close").iterator();
+		while (btns.hasNext()) {
+			JQuery btn = btns.next();
+			assertFalse(btn.attr("title").contains("27112700"));
 		}
 	}
 }
