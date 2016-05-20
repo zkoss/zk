e8e05dedc37923266d82810026e92b8397c32f5c
Wenning Hsu <imwenninghsu@gmail.com>
2016-05-20 19:08:36 +0800

    add javadoc for DigestUtilsHelper

--- zk/src/org/zkoss/zk/ui/sys/DigestUtilsHelper.java
+++ zk/src/org/zkoss/zk/ui/sys/DigestUtilsHelper.java
@@ -11,6 +11,8 @@ Copyright (C) 2016 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zk.ui.sys;
 
+import org.apache.commons.codec.Charsets;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -25,22 +27,47 @@ import java.security.NoSuchAlgorithmException;
  */
 public class DigestUtilsHelper {
 
+	/**
+	 * Calculates the MD5 digest and returns the value as a 32 charactor hex string.
+	 * @param data data to digest
+	 * @return MD5 digest as a hex string
+	 */
 	public static String md5Hex(String data) {
 		return encodeHexString(md5(data));
 	}
 
+	/**
+	 * Calculates the MD5 digest and returns the value as a 16 element <code>byte[]</code>.
+	 * @param data data to digest
+	 * @return MD5 digest
+     */
 	public static byte[] md5(String data) {
 		return md5(getBytesUtf8(data));
 	}
 
+	/**
+	 * Calculates the MD5 digest and returns the value as a 16 element <code>byte[]</code>.
+	 * @param data data to digest
+	 * @return MD5 digest
+     */
 	public static byte[] md5(byte[] data) {
 		return getMd5Digest().digest(data);
 	}
 
+	/**
+	 * Returns an MD5 MessageDigest.
+	 * @return An MD5 digest instance.
+     */
 	public static MessageDigest getMd5Digest() {
 		return getDigest("MD5");
 	}
 
+	/**
+	 * Returns a <code>MessageDigest</code> for the given <code>algorithm</code>.
+	 * @param algorithm
+	 * @return An MD5 digest instance.
+	 * @throws IllegalArgumentException
+     */
 	public static MessageDigest getDigest(String algorithm) {
 		try {
 			return MessageDigest.getInstance(algorithm);
@@ -49,6 +76,14 @@ public class DigestUtilsHelper {
 		}
 	}
 
+	/**
+	 * Encodes the given string into a sequence of bytes using the UTF-8 charset, storing the result into a new byte
+	 * array.
+	 *
+	 * @param string
+	 *            the String to encode, may be {@code null}
+	 * @return encoded bytes, or {@code null} if the input string was {@code null}
+	 */
 	public static byte[] getBytesUtf8(String string) {
 		try {
 			return string == null ? null : string.getBytes("UTF-8");
@@ -59,19 +94,54 @@ public class DigestUtilsHelper {
 		return null;
 	}
 
+	/**
+	 * Converts an array of bytes into a String representing the hexadecimal values of each byte in order. The returned
+	 * String will be double the length of the passed array, as it takes two characters to represent any given byte.
+	 *
+	 * @param data
+	 *            a byte[] to convert to Hex characters
+	 * @return A String containing hexadecimal characters
+	 */
 	public static String encodeHexString(byte[] data) {
 		return new String(encodeHex(data));
 	}
 
+	/**
+	 * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
+	 * The returned array will be double the length of the passed array, as it takes two characters to represent any
+	 * given byte.
+	 *
+	 * @param data
+	 *            a byte[] to convert to Hex characters
+	 * @return A char[] containing hexadecimal characters
+	 */
 	public static char[] encodeHex(byte[] data) {
 		return encodeHex(data, true);
 	}
 
+	/**
+	 * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
+	 * The returned array will be double the length of the passed array, as it takes two characters to represent any
+	 * given byte.
+	 *
+	 * @param data
+	 *            a byte[] to convert to Hex characters
+	 * @param toLowerCase
+	 *            {@code true} converts to lowercase, {@code false} to uppercase
+	 * @return A char[] containing hexadecimal characters
+	 */
 	public static char[] encodeHex(byte[] data, boolean toLowerCase) {
 		return encodeHex(data, toLowerCase ? DIGITS_LOWER : DIGITS_UPPER);
 	}
 
+	/**
+	 * Used to build output as Hex
+	 */
 	private static final char[] DIGITS_LOWER;
+
+	/**
+	 * Used to build output as Hex
+	 */
 	private static final char[] DIGITS_UPPER;
 
 	static {
@@ -79,6 +149,17 @@ public class DigestUtilsHelper {
 		DIGITS_UPPER = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 	}
 
+	/**
+	 * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
+	 * The returned array will be double the length of the passed array, as it takes two characters to represent any
+	 * given byte.
+	 *
+	 * @param data
+	 *            a byte[] to convert to Hex characters
+	 * @param toDigits
+	 *            the output alphabet
+	 * @return A char[] containing hexadecimal characters
+	 */
 	protected static char[] encodeHex(byte[] data, char[] toDigits) {
 		int l = data.length;
 		char[] out = new char[l << 1];
@@ -92,10 +173,28 @@ public class DigestUtilsHelper {
 		return out;
 	}
 
+	/**
+	 * Converts a String into an array of bytes.
+	 *
+	 * @param data
+	 *            a byte[] to convert to Hex characters
+	 * @return A String containing hexadecimal characters
+	 * @throws IllegalArgumentException
+	 */
 	public static byte[] decodeHexString(String data) throws IllegalArgumentException {
 		return decodeHex(data.toCharArray());
 	}
 
+	/**
+	 * Converts an array of characters representing hexadecimal values into an array of bytes of those same values. The
+	 * returned array will be half the length of the passed array, as it takes two characters to represent any given
+	 * byte. An exception is thrown if the passed char array has an odd number of elements.
+	 *
+	 * @param data
+	 *            An array of characters containing hexadecimal digits
+	 * @return A byte array containing binary data decoded from the supplied char array.
+	 * @throws IllegalArgumentException
+	 */
 	public static byte[] decodeHex(final char[] data) throws IllegalArgumentException {
 
 		final int len = data.length;
@@ -118,6 +217,16 @@ public class DigestUtilsHelper {
 		return out;
 	}
 
+	/**
+	 * Converts a hexadecimal character to an integer.
+	 *
+	 * @param ch
+	 *            A character to convert to an integer digit
+	 * @param index
+	 *            The index of the character in the source
+	 * @return An integer
+	 * @throws IllegalArgumentException
+	 */
 	protected static int toDigit(final char ch, final int index) throws IllegalArgumentException {
 		final int digit = Character.digit(ch, 16);
 		if (digit == -1) {
