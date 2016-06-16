0546f21c2d6de4d63e813da00733f863e819c40c
Wenning Hsu <imwenninghsu@gmail.com>
2016-06-16 18:11:37 +0800

    update case for ZTL of 8.0.2 release

--- zktest/src/org/zkoss/zktest/test2/F80_ZK_2959.java
+++ zktest/src/org/zkoss/zktest/test2/F80_ZK_2959.java
@@ -15,16 +15,36 @@ it will be useful, but WITHOUT ANY WARRANTY.
 package org.zkoss.zktest.test2;
 
 import org.zkoss.util.media.MediaTypeResolver;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.WebApps;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Image;
 
 /**
  * 
  * @author wenning
  */
-public class F80_ZK_2959 implements MediaTypeResolver {
+public class F80_ZK_2959 extends SelectorComposer<Component> implements MediaTypeResolver {
+
+	@Wire
+	Image img1;
 
 	public String resolve(String format) {
-		if ("svg".equals(format)) return "image/svg+xml(F80-ZK-2959);";
-		else return null;
+		if ("svg".equals(format)) {
+			WebApps.getCurrent().setAttribute("ZK2959", "ZK2959");
+			return "image/svg+xml;";
+		} else return null;
+	}
+
+	@Listen("onClick=#btn")
+	public void handleBtn(){
+		if ("ZK2959".equals(WebApps.getCurrent().getAttribute("ZK2959").toString())) {
+			Clients.log("done");
+			WebApps.getCurrent().removeAttribute("ZK2959");
+		}
 	}
 
 }
