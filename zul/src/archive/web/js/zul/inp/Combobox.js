b95f34d664a947cd1d937be6447ecfd7de0fc56f
DevChu <marchuqq@gmail.com>
2016-12-12 18:42:03 +0800

    refine combobox onSelect

--- zul/src/archive/web/js/zul/inp/Combobox.js
+++ zul/src/archive/web/js/zul/inp/Combobox.js
@@ -205,7 +205,7 @@ zul.inp.Combobox = zk.$extends(zul.inp.ComboWidget, {
 			if (opts.sendOnChange)
 				this.$supers('updateChange_', []);
 
-			this.fire('onSelect', {items: sel ? [sel] : [], reference: sel, prevSeld: oldsel}, {toServer: true}, 90);
+			this.fire('onSelect', {items: sel ? [sel] : [], reference: sel, prevSeld: oldsel});
 			//spec change (diff from zk 3): onSelect fired after onChange
 			//purpose: onSelect can retrieve the value correctly
 			//If we want to change this spec, we have to modify Combobox.java about _lastCkVal
