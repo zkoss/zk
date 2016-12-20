60b442178c060269044e02a2845a6e2ee61aadc5
DevChu <marchuqq@gmail.com>
2016-12-20 15:48:32 +0800

    ZK-3517: ForEach cause NoSuchElementException when using ListModel

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B80_ZK_3517Test.java
@@ -0,0 +1,36 @@
+/* B80_ZK_3517Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Dec 15 14:14:32 CST 2016, Created by jameschu
+
+Copyright (C) 2016 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import org.zkoss.util.Locales;
+import org.zkoss.util.TimeZones;
+import org.zkoss.zktest.zats.WebDriverTestCase;
+import org.zkoss.zktest.zats.ZATSTestCase;
+
+/**
+ * 
+ * @author jameschu
+ */
+public class B80_ZK_3517Test extends ZATSTestCase{
+    @Test
+    public void test() {
+        connect();
+    }
+}
