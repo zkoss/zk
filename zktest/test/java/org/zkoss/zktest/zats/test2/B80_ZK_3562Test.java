c2cee773ce2e1349a649ab3a7dcba3f3a78a3b4a
DevChu <marchuqq@gmail.com>
2017-01-07 14:18:59 +0800

    refine ZK-3562 test cases

--- zktest/test/java/org/zkoss/zktest/zats/test2/B80_ZK_3562Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B80_ZK_3562Test.java
@@ -1,10 +1,8 @@
 package org.zkoss.zktest.zats.test2;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
-
 import org.zkoss.zats.mimic.DesktopAgent;
 import org.zkoss.zktest.zats.ZATSTestCase;
 
@@ -12,11 +10,9 @@ public class B80_ZK_3562Test extends ZATSTestCase {
 	@Test
 	public void test() {
 		try {
-			DesktopAgent desktop = connect();
-			desktop.query("#btn").click();
+			connect().query("#btn").click();
 		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
+			fail(e.getMessage());
 		}
 	}
 }
