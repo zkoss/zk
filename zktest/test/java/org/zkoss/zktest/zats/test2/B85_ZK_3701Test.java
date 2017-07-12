27e8c7feadddfc2feef3aab4e03274f527e640b9
DevChu <marchuqq@gmail.com>
2017-07-12 16:24:48 +0800

    ZK-3700: detach a script component doesn't remove its DOM element  ZK-3701: set Script's src produce a new <script> instead of changing the existing one

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3701Test.java
@@ -0,0 +1,37 @@
+/* B85_ZK_3701Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Jul 10 12:25:48 CST 2017, Created by jameschu
+
+Copyright (C) 2017 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author jameschu
+ */
+public class B85_ZK_3701Test extends WebDriverTestCase {
+	@Test
+	public void test() throws Exception {
+		connect();
+		waitResponse();
+		Assert.assertEquals("zk-3701-1 zk-3701-d1", getZKLog());
+		closeZKLog();
+		click(jq("@button").eq(0));
+		waitResponse();
+		Assert.assertEquals("zk-3701-2", getZKLog());
+		closeZKLog();
+		click(jq("@button").eq(1));
+		waitResponse();
+		Assert.assertEquals("zk-3701-d2", getZKLog());
+	}
+}
