0b69aff0b857fe3676d97cb4660e5310a253da5c
Rudy Huang <rudyhuang@potix.com>
2017-08-31 17:22:58 +0800

    Scrollbar (embedded) should be hidden if mouseover

--- zul/src/archive/web/js/zul/Scrollbar.js
+++ zul/src/archive/web/js/zul/Scrollbar.js
@@ -19,16 +19,10 @@ Copyright (C) 2013 Potix Corporation. All Rights Reserved.
 		if (bar) {
 			style = bar.style;
 			style.display = isHide ? 'none' : 'block';
-			style.opacity = opacity;
-			if (zk.ie < 9)
-				style.filter = 'alpha(opacity=' + 100 * opacity + ')';
 		}
-		if (embed) { // always show if enabled
+		if (embed) {
 			style = embed.style;
-			style.display = 'block';
-			style.opacity = isHide ? 0.2 : 0;
-			if (zk.ie < 9)
-				style.filter = isHide ? 'alpha(opacity=20)' : 'alpha(opacity=0)';
+			style.display = isHide ? 'block' : 'none';
 		}
 	};
 	function _setScrollPos(position, min, max) {
