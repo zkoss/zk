5c0b17997d3bd65efb79c4e6f5bd11e7de71046a
Wenning Hsu <imwenninghsu@gmail.com>
2017-09-29 18:43:35 +0800

    Bug Fixed ZK-3750: Following zk-3722, hidden cell / row style is incorrect

--- zul/src/archive/web/js/zul/grid/Row.js
+++ zul/src/archive/web/js/zul/grid/Row.js
@@ -204,7 +204,7 @@ zul.grid.Row = zk.$extends(zul.Widget, {
 				if (realIndex < cols.nChildren) {
 					var col = cols.getChildAt(realIndex);
 					// ZK-3600: Prevent the dummy cell from hiding if the first column is invisible
-					visible = headerNotVisible ? 'text-overflow: ellipsis;white-space: nowrap;' : (grid._model && this._loaded) ? 'width: 0.1px' : '';
+					visible = headerNotVisible ? 'text-overflow: ellipsis;white-space: nowrap;' : (grid._model && this._loaded) ? 'width: 0.1px;' : '';
 					hgh = col.getHeight();
 					align = col.getAlign();
 					valign = col.getValign();
