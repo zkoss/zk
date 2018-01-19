00ccf43fb711320773f11232665178ed45854c03
DevChu <marchuqq@gmail.com>
2018-01-19 19:23:51 +0800

    ZK-3812: Nested collections bindings with templates stop firing after ZK 3611

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3812Test.java
@@ -0,0 +1,54 @@
+/* B85_ZK_3812Test.java
+
+	Purpose:
+
+	Description:
+
+	History:
+		Wed Jan 17 11:42:38 CST 2018, Created by jameschu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zats.mimic.ComponentAgent;
+import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zktest.zats.ZATSTestCase;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Label;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jameschu
+ */
+public class B85_ZK_3812Test extends ZATSTestCase {
+	@Test
+	public void test() {
+		DesktopAgent desktop = connect();
+		verifyResult(desktop, "d1");
+		desktop.queryAll("button").get(0).click();
+		verifyResult(desktop, "d2");
+		desktop.queryAll("button").get(0).click();
+		verifyResult(desktop, "d3");
+		desktop.queryAll("button").get(0).click();
+		verifyResult(desktop, "d4");
+		desktop.queryAll("button").get(0).click();
+		verifyResult(desktop, "d5");
+	}
+
+	public void verifyResult(DesktopAgent desktop, String result) {
+		List<Label> labels = new ArrayList<>();
+		labels.add(desktop.query("#h1 label").as(Label.class));
+		labels.add(desktop.query("#h2 label").as(Label.class));
+		labels.add(desktop.query("#h3 label").as(Label.class));
+		labels.add(desktop.query("#h4 label").as(Label.class));
+		for (int i = 0; i < labels.size(); i++) {
+			Assert.assertEquals(result, labels.get(i).getValue());
+		}
+	}
+}
