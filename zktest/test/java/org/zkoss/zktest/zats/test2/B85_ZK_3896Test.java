6b695b96724ff7a2c4c6ecaa6b72ac585400fc80
ZiJia <zga12181991@gmail.com>
2018-03-22 14:55:20 +0800

    ZK-3896: Datebox failing format patterns (containing weekday)

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3896Test.java
@@ -0,0 +1,43 @@
+/* B85_ZK_3896Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Thu Mar 22 12:55:21 CST 2018, Created by charlesqiu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.zkoss.zktest.zats.WebDriverTestCase;
+import org.zkoss.zktest.zats.ztl.JQuery;
+
+public class B85_ZK_3896Test extends WebDriverTestCase {
+
+	@Test
+	public void test() {
+		connect();
+		for (JQuery datebox : jq(".z-datebox"))
+			dateChangeTest(datebox);
+	}
+
+	private void dateChangeTest(JQuery datebox) {
+		String oldDate = datebox.find(".z-datebox-input").val();
+
+		click(datebox.find(".z-datebox-button"));
+		waitResponse(true);
+
+		JQuery today = jq("div.z-datebox-popup.z-datebox-open.z-datebox-shadow")
+				.find("td.z-calendar-cell.z-calendar-weekday.z-calendar-selected");
+		JQuery prevSibling = today.prev();
+		click(prevSibling.exists() ? prevSibling : today.next());
+		waitResponse(true);
+
+		Assert.assertFalse(jq("div.z-errorbox.z-errorbox-open").exists());
+		Assert.assertNotEquals(oldDate, datebox.find(".z-datebox-input").val());
+	}
+}
