360abe5601958453e39f0b0a7b77492a1fd8abeb
klyvechen <klyvechen@potix.com>
2018-04-16 12:02:06 +0800

    refine: ZK-3738 testcase

--- zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3738Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3738Test.java
@@ -15,6 +15,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.zkoss.zats.mimic.ComponentAgent;
 import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zats.mimic.operation.MultipleSelectAgent;
 import org.zkoss.zktest.zats.ZATSTestCase;
 import org.zkoss.zul.Label;
 
@@ -26,10 +27,10 @@ public class B85_ZK_3738Test extends ZATSTestCase {
 	public void test() {
 		DesktopAgent desktop = connect();
 		ComponentAgent selectAllbtn = desktop.query("button");
-		ComponentAgent label = desktop.query("label");
+		ComponentAgent label = desktop.query("#l1");
 		List<ComponentAgent> treeItems = desktop.queryAll("treeitem");
 		selectAllbtn.click();
-		treeItems.get(0).click();
+		treeItems.get(0).as(MultipleSelectAgent.class).deselect();
 		Assert.assertEquals("[1.1, 1.2, 2.1, 2.2, 2.3, 2, 3.1, 3]",label.as(Label.class).getValue());
 
 	}
