afdce87b5ff935cf0c740912a288e9de76a1e219
DevChu <marchuqq@gmail.com>
2018-04-18 09:45:58 +0800

    new component for zk test suite

---
+++ zktest/src/org/zkoss/zktest/comp/Teststeps.java
@@ -0,0 +1,21 @@
+/* Teststeps.java
+	Purpose:
+
+	Description:
+
+	History:
+		Thu Mar 15 18:38:20 CST 2018, Created by jameschu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.comp;
+
+/**
+ * @author jameschu
+ */
+public class Teststeps extends org.zkoss.zul.Label {
+	@Override
+	public boolean isMultiline() {
+		return true;
+	}
+}
