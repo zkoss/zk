4d8207bdfe0ae14d203ae4166387b4c5ba4def0b
ZiJia <zga12181991@gmail.com>
2018-05-25 18:16:48 +0800

    ZK-3529: Add title label to collapsed borderlayout region if title is set

--- zul/src/archive/web/js/zul/layout/Borderlayout.js
+++ zul/src/archive/web/js/zul/layout/Borderlayout.js
@@ -199,6 +199,8 @@ zul.layout.Borderlayout = zk.$extends(zul.Widget, {
 				ambit = region._ambit();
 				_ambit[rs[j]](ambit, center, width, height);
 				this._resizeWgt(region, ambit); //might recursive back
+				var directionToCalculate = region._isVertical() ? 'width' : 'height';
+				jq(region.$n('title')).width(jq(region.$n('colled'))[directionToCalculate]() - jq(region.$n('btned'))[directionToCalculate]());
 			}
 		}
 		if (this.center && zk(this.center.$n()).isVisible()) {
