4c14de558d4b433f7f43ba2ab9c69af3031dbbb5
klyvechen <klyvechen@potix.com>
2018-05-29 18:46:02 +0800

    ZK-3569: Popup slide-in direction change depending on position relative to input

---
+++ zktest/src/org/zkoss/zktest/test2/F85_ZK_3569VM.java
@@ -0,0 +1,23 @@
+/* F85_ZK_3569VM.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Tue May 29 12:37:34 CST 2018, Created by klyve
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.test2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class F85_ZK_3569VM {
+	private List<String> _colors = Arrays.asList(new String[]{"red", "blue", "green"});
+	
+	public List<String> getColors() {
+		return _colors;
+	}
+}
