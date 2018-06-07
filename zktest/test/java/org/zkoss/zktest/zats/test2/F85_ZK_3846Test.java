b17520767736c4440083b9ede39e9d276f260812
ZiJia <zga12181991@gmail.com>
2018-06-07 16:16:53 +0800

    ZK-3846: use date format in data constraint error messages and localize range separator

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/F85_ZK_3846Test.java
@@ -0,0 +1,58 @@
+/* F85_ZK_3846.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Thu Jun 07 15:45:28 CST 2018, Created by charlesqiu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+import org.zkoss.zktest.zats.ztl.JQuery;
+
+public class F85_ZK_3846Test extends WebDriverTestCase {
+	
+	private JQuery datebox;
+	
+	@Test
+	public void test() {
+		connect();
+		
+		datebox = jq(".z-datebox-input");
+		
+		assertBySeparator("~");
+		
+		click(jq("@button"));
+		waitResponse();
+		
+		assertBySeparator("...");
+	}
+	
+	private void assertBySeparator(String separator) {
+		type(datebox.eq(0), "2019/01/01");
+		waitResponse();
+		Assert.assertEquals("2018/01/01 " + separator + " 2018/12/31", getDateOfErrorMsg());
+		
+		type(datebox.eq(1), "01.01.2019");
+		waitResponse();
+		Assert.assertEquals("01.01.2018 " + separator + " 31.12.2018", getDateOfErrorMsg());
+		
+		type(datebox.eq(2), "20190101");
+		waitResponse();
+		Assert.assertEquals("20180101 " + separator + " 20181231", getDateOfErrorMsg());
+	}
+	
+	private String getDateOfErrorMsg() {
+		JQuery errorbox = jq(".z-errorbox-content");
+		String errorMsg = errorbox.text();
+		errorbox.remove();
+		return errorMsg.substring(errorMsg.indexOf(':') + 2);
+	}
+}
