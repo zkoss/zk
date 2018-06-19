cd55fc2384daec2fb65796fe94644a1fc6fa7e09
klyvechen <klyvechen@potix.com>
2018-06-19 11:40:43 +0800

    ZK-3956: popup causes spacing vlayout

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3956Test.java
@@ -0,0 +1,28 @@
+/* B85_ZK_3956Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Tue Jun 19 11:14:59 CST 2018, Created by klyve
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+public class B85_ZK_3956Test extends WebDriverTestCase{
+
+	@Test
+	public void test() {
+		connect();
+		click(jq("@button").get(0));
+		waitResponse();
+		Assert.assertEquals("none", jq(".z-vlayout-inner").eq(1).css("display"));
+	}
+}
