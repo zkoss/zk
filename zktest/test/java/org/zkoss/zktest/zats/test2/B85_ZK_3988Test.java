4a4da7386846680a776d5c2b69fde351d364bed1
klyvechen <klyvechen@potix.com>
2018-07-16 12:17:56 +0800

    ZK-3988: HtmlMacroComponent compose() doesn't resolve @Wire before doAfterCompose

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3988Test.java
@@ -0,0 +1,23 @@
+/* B85_ZK_3988Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Fri Jul 13 18:49:37 CST 2018, Created by klyve
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.ZATSTestCase;
+
+public class B85_ZK_3988Test extends ZATSTestCase {
+	@Test
+	public void test() {
+		connect();
+	}
+}
