287aca1fc119f597674a76077040d4bc235ad499
DevChu <marchuqq@gmail.com>
2018-07-26 13:17:30 +0800

    modify test cases

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3761Test.java
@@ -0,0 +1,23 @@
+package org.zkoss.zktest.zats.test2;
+
+import static org.junit.Assert.assertEquals;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class B85_ZK_3761Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+		click(jq("@button"));
+		waitResponse();
+		sleep(5000);
+		assertEquals("breeze", getEval("zk.themeName"));
+	}
+}
