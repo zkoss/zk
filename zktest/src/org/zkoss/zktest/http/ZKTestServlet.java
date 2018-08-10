d9b42b4cfba5c7676a51779d9f925c47227a26b6
Rudy Huang <rudyhuang@potix.com>
2018-08-10 11:50:59 +0800

    Fixed switch theme by adding url parameter in zktest Usage: test.zul?zktheme=breeze, zktheme=default means the default theme

--- zktest/src/org/zkoss/zktest/http/ZKTestServlet.java
+++ zktest/src/org/zkoss/zktest/http/ZKTestServlet.java
@@ -1,27 +1,30 @@
 package org.zkoss.zktest.http;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.zkoss.lang.Strings;
 import org.zkoss.zk.ui.http.DHtmlLayoutServlet;
 
 public class ZKTestServlet extends DHtmlLayoutServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String zktheme = request.getParameter("zktheme");
-		List<String> themes = Arrays.asList("atlantic", "silvertail", "sapphire");
-		boolean isDefaultTheme = zktheme == null || !themes.contains(zktheme.toLowerCase());
-		if (!isDefaultTheme) { // fixed for HtmlUnit issue
-			Cookie cookie = new Cookie("zktheme",
-					isDefaultTheme ? null : zktheme);
-			cookie.setPath("/zktest/");
-			((HttpServletResponse) response).addCookie(cookie);
+		boolean hasThemeSetting = !Strings.isBlank(zktheme);
+		boolean isDefaultTheme = "default".equalsIgnoreCase(zktheme);
+		if (hasThemeSetting) { // fixed for HtmlUnit issue
+			Cookie cookie = new Cookie("zktheme", zktheme);
+			cookie.setSecure(request.isSecure());
+			String contextPath = request.getContextPath();
+			if (Strings.isEmpty(contextPath)) contextPath = "/";
+			cookie.setPath(contextPath);
+			cookie.setMaxAge(isDefaultTheme ? 0 : -1); // delete cookie if default
+			response.addCookie(cookie);
+			response.sendRedirect(request.getRequestURL().toString());
+			return;
 		}
 		super.doGet(request, response);
 	}
