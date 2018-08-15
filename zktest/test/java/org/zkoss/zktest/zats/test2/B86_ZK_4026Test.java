fe367bca809073f5e335ef41219693c525142436
klyvechen <klyvechen@potix.com>
2018-08-15 11:14:07 +0800

    ZK-4026: shadow <if> re-renders too eagerly

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B86_ZK_4026Test.java
@@ -0,0 +1,35 @@
+/* B86_ZK_4026Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Wed Aug 15 10:48:23 CST 2018, Created by klyve
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zats.mimic.ComponentAgent;
+import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zktest.zats.ZATSTestCase;
+
+public class B86_ZK_4026Test extends ZATSTestCase {
+
+	@Test
+	public void test() {
+		DesktopAgent da = connect();
+		ComponentAgent btn = da.query("#notifyBtn");
+		ComponentAgent lbl = da.query("#testLbl");
+		String uuid1 = lbl.getUuid();
+		btn.click();
+		lbl = da.query("#testLbl");
+		String uuid2 = lbl.getUuid();
+		Assert.assertEquals("The two uuid should be the same:", uuid1, uuid2);
+	}
+
+}
