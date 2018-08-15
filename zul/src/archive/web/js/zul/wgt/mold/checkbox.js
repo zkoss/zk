3babcb7a9805147cbd2e0f36f3ce516694924b53
Charles Qiu <charlesqiu@potix.com>
2018-08-15 14:08:30 +0800

    refine checkbox.js: remove unused variable

--- zul/src/archive/web/js/zul/wgt/mold/checkbox.js
+++ zul/src/archive/web/js/zul/wgt/mold/checkbox.js
@@ -13,7 +13,7 @@ This program is distributed under LGPL Version 2.1 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
 function (out) {
-	var uuid = this.uuid, content = this.domContent_();
+	var uuid = this.uuid;
 	//tabindex attribute will be set in input element
 	out.push('<span', this.domAttrs_({tabindex: 1}), '>');
 	out.push('<input type="checkbox" id="', uuid, '-real"', this.contentAttrs_(), '/>');
