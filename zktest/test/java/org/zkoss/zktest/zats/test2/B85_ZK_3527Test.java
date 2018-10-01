7c1cdeb0f8e9b988f65c9ab9d6239b09bd15c655
Rudy Huang <rudyhuang@potix.com>
2018-10-01 18:49:29 +0800

    refine ZK-3527 for tzdata 2018d Europe/Lisbon changes

--- zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3527Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3527Test.java
@@ -1,11 +1,11 @@
 package org.zkoss.zktest.zats.test2;
 
-import org.junit.Test;
-import org.zkoss.zktest.zats.WebDriverTestCase;
-import org.zkoss.zktest.zats.ztl.JQuery;
+import static org.junit.Assert.assertThat;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
 
 /**
  * @author bob peng
@@ -16,11 +16,7 @@ public class B85_ZK_3527Test extends WebDriverTestCase {
 		connect();
 		for (int i = 0; i < 4; i++) {
 			String str = jq(".z-datebox-input:eq(" + i + ")").val();
-			if (i == 2) {
-				assertTrue("Wrong hours or minutes : " + str, str.contains("00:36"));
-			} else {
-				assertTrue("Wrong hours or minutes : " + str, str.contains("00:00"));
-			}
+			assertThat("Wrong hours or minutes : " + str, str, CoreMatchers.endsWith("00:00"));
 		}
 	}
 }
