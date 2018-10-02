e2d25c7f1c0c38cb61a924f770413c52ec85997f
Charles Qiu <charlesqiu@potix.com>
2018-10-02 10:33:33 +0800

    Refine ZK-3588: frozen + added listheader doesn't update UI properly

--- zktest/src/org/zkoss/zktest/test2/B86_ZK_3588Composer.java
+++ zktest/src/org/zkoss/zktest/test2/B86_ZK_3588Composer.java
@@ -16,18 +16,30 @@ import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Column;
 import org.zkoss.zul.Columns;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listhead;
 import org.zkoss.zul.Listheader;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Row;
 
 public class B86_ZK_3588Composer extends SelectorComposer {
 	@Wire
 	private Listhead listhead;
 	@Wire
+	private Listitem listitem;
+	@Wire
 	private Columns columns;
+	@Wire
+	private Row row;
+	private int count = 4;
 
-	@Listen("onClick = #button")
-	public void addHeader() {
-		listhead.appendChild(new Listheader("xxx"));
-		columns.appendChild(new Column("xxx"));
+	@Listen("onClick = #add")
+	public void add() {
+		String content = String.valueOf(count++);
+		listhead.appendChild(new Listheader(content));
+		listitem.appendChild(new Listcell(content));
+		columns.appendChild(new Column(content));
+		row.appendChild(new Label(content));
 	}
 }
