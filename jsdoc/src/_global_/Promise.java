1d3195bbde811ee43f9613767efb4ea1faaf571b
Rudy Huang <rudyhuang@potix.com>
2019-03-05 10:08:33 +0800

    javadoc: Add Promise

---
+++ jsdoc/src/_global_/Promise.java
@@ -0,0 +1,23 @@
+/* Promise.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Mar 05 10:03:00 CST 2019, Created by rudyhuang
+
+Copyright (C) 2019 Potix Corporation. All Rights Reserved.
+
+*/
+package _global_;
+
+/**
+ * The Promise object represents the eventual completion (or failure) of an asynchronous operation, and its resulting value.
+ *
+ * <p>Refer to <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">Promise - JavaScript | MDN</a>.
+ * @author rudyhuang
+ */
+public class Promise {
+	private Promise() {}
+}
