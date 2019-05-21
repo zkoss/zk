22aecf95aa39d9b6cb9156b408da6811678175ab
Rudy Huang <rudyhuang@potix.com>
2019-05-21 18:05:26 +0800

    ZK-4288: infinite recursion during session deserialization with redis

---
+++ zktest/src/org/zkoss/zktest/test2/B86_ZK_4288VM.java
@@ -0,0 +1,21 @@
+/* B86_ZK_4288VM.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri May 17 10:55:28 CST 2019, Created by rudyhuang
+
+Copyright (C) 2019 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.test2;
+
+import java.io.Serializable;
+
+/**
+ * @author rudyhuang
+ */
+public class B86_ZK_4288VM implements Serializable {
+	private static final long serialVersionUID = 20190517105658L;
+}
