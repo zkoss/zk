6585498db061d0c1d94aeb664c78e024d6d6a2b9
Rudy Huang <rudyhuang@potix.com>
2019-07-04 12:15:56 +0800

    Add F50_3309539Test Convert B70_ZK_2814Test into ZATS

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B70_ZK_2814Test.java
@@ -0,0 +1,34 @@
+/* B70_ZK_2814Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Wed Sep 09 10:16:56 CST 2020, Created by rudyhuang
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class B70_ZK_2814Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+
+		click(jq("@button:eq(0)"));
+		waitResponse();
+		assertNoJSError();
+
+		click(jq("@button:eq(1)"));
+		waitResponse();
+		assertNoJSError();
+	}
+}
