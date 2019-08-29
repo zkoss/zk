f9ff694046daed3c96781770ba29a2945e1aea9f
Rudy Huang <rudyhuang@potix.com>
2019-08-29 18:53:00 +0800

    ZK-4297: Listbox hover and selected style are the same, makes it hard to be distinguished in mobile

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B90_ZK_4297Test.java
@@ -0,0 +1,36 @@
+/* B90_ZK_4297Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu May 30 16:14:05 CST 2019, Created by rudyhuang
+
+Copyright (C) 2019 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.support.Color;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class B90_ZK_4297Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+
+		click(jq("@textbox"));
+		waitResponse();
+
+		Assert.assertNotEquals(
+			Color.fromString(jq("@listitem.z-listitem-selected").find("@listcell").css("backgroundColor")),
+			Color.fromString(jq("@listitem:eq(0)").find("@listcell").css("backgroundColor"))
+		);
+	}
+}
