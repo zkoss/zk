ba28b03891f01c8b7d6d295750ccdd94241f049c
Rudy Huang <rudyhuang@potix.com>
2019-11-13 12:07:55 +0800

    update version for release RC version

--- zkplus-legacy/src/org/zkoss/zkplus/legacy/Version.java
+++ zkplus-legacy/src/org/zkoss/zkplus/legacy/Version.java
@@ -23,5 +23,5 @@ package org.zkoss.zkplus.legacy;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "9.0.0";
+	public static final String UID = "9.0.0-RC";
 }
