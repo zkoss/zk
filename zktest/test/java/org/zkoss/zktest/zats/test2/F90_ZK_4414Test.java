06d23f665be4a0e7a113ff914bbbae4cb672f1c0
Rudy Huang <rudyhuang@potix.com>
2020-01-15 15:29:10 +0800

    ZK-4414: scrolling can change pages in a pdfviewer at any zoom level

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/F90_ZK_4414Test.java
@@ -0,0 +1,38 @@
+/* F90_ZK_4414Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Wed Jan 15 17:45:48 CST 2020, Created by rudyhuang
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class F90_ZK_4414Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+		waitResponse();
+		sleep(2000); // ensure pdf is loaded
+
+		String pageBefore = widget("@pdfviewer").$n("toolbar-page-active").get("value");
+
+		jq(widget("@pdfviewer").$n("container")).scrollTop(10000);
+		eval(jq("@pdfviewer") + ".trigger('mousewheel', [-10])");
+		waitResponse();
+
+		String page = widget("@pdfviewer").$n("toolbar-page-active").get("value");
+		Assert.assertNotEquals("Scrolling down doesn't change page number.", pageBefore, page);
+	}
+}
