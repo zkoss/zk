f676d364d3309d3151f2d2937af5920eebe30f85
Rudy Huang <rudyhuang@potix.com>
2020-02-03 18:15:32 +0800

    refine ZK-3848 test case for ZK-4480

--- zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3848Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3848Test.java
@@ -11,28 +11,6 @@ Copyright (C) 2018 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zktest.zats.test2;
 
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.zkoss.util.resource.ClassLocator;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
-
 import static org.hamcrest.core.StringContains.containsString;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -40,15 +18,32 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.zkoss.util.resource.ClassLocator;
 
 @RunWith (PowerMockRunner.class)
 @PrepareForTest ({LoggerFactory.class})
 public class B85_ZK_3848Test {
-	private static final Logger slf4logger = LoggerFactory.getLogger(ClassLocator.class);
 	private static Logger logger;
-	private static ArrayList<String> results = new ArrayList();
 	private static ClassLocator clToTest;
 	
 	@BeforeClass
@@ -61,7 +56,6 @@ public class B85_ZK_3848Test {
 	
 	@Test
 	public void test1() throws Exception {
-		
 		File src = new File("src/org/zkoss/zktest/test2/ZK3848_lang_addon_Test1.xml");
 		File dst = new File("debug/classes/metainfo/zk/lang-addon.xml");
 		
@@ -92,7 +86,6 @@ public class B85_ZK_3848Test {
 
 	@Test
 	public void test2() throws IOException {
-		
 		File src = new File("src/org/zkoss/zktest/test2/ZK3848_lang_addon_Test2.xml");
 		File dst = new File("debug/classes/metainfo/zk/lang-addon.xml");
 		
@@ -103,25 +96,20 @@ public class B85_ZK_3848Test {
 		Path zkPath = dst.toPath().getParent();
 		if (!Files.exists(zkPath))
 			Files.createDirectories(zkPath);
-		doAnswer(new Answer() {
-			@Override
-			public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
-				Object[] arguments = invocationOnMock.getArguments();
-				String[] args = (String[]) arguments[1];
-				slf4logger.warn((String) arguments[0], args);
-				String f = ((String) arguments[0]).replace("{}", "%s");
-				String result = String.format(f, args[0], args[1], args[2]);
-				results.add(result);
-				return null;
-			}
-		}).when(logger).warn(anyString(), any(String[].class));
+
+		List<String> results = new ArrayList<>();
+		doAnswer(invocationOnMock -> {
+			Object[] args = invocationOnMock.getArguments();
+			String f = ((String) args[0]).replace("{}", "%s");
+			String result = String.format(f, args[1], args[2], args[3]);
+			results.add(result);
+			return null;
+		}).when(logger).warn(anyString(), anyString(), anyString(), anyString());
 		Files.copy(src.toPath(), dst.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		try {
 			clToTest.getDependentXMLResources(name, elName, elDepends);
-			verify(logger, times(1)).warn(anyString(), any(String[].class));
+			verify(logger, times(1)).warn(anyString(), anyString(), anyString(), anyString());
 			Assert.assertThat(results.get(0), containsString("you are extending component decimalbox which is defined in 'zkplus-legacy', 'zkbind', please define <depends> element"));
-		} catch (Exception ex) {
-			throw ex;
 		} finally {
 			Files.delete(dst.toPath());
 			Path metainfoPath = zkPath.getParent();
