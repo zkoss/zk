6c9b4ab5fd58292531112f62694539927d562bac
Rudy Huang <rudyhuang@potix.com>
2020-02-17 15:56:11 +0800

    ZK-4447: grid skips interval added / interval removed in onListDataChange, cause consistency exception

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B90_ZK_4447Test.java
@@ -0,0 +1,26 @@
+/* B90_ZK_4447Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Feb 17 15:52:37 CST 2020, Created by rudyhuang
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.ZATSTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class B90_ZK_4447Test extends ZATSTestCase {
+	@Test
+	public void test() {
+		connect();
+	}
+}
