0d77e30eab54f0196f3761422df976254154acca
DevChu <marchuqq@gmail.com>
2020-04-06 18:57:28 +0800

    refine ZK-4452 for button input misalign

--- zktest/test/java/org/zkoss/zktest/zats/test2/B90_ZK_4452Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B90_ZK_4452Test.java
@@ -36,5 +36,13 @@ public class B90_ZK_4452Test extends WebDriverTestCase {
 		Assert.assertEquals(jq(".z-bandbox-invalid").css("border"), jq(".z-bandbox-button").css("border-left"));
 		Assert.assertEquals(jq(".z-combobox-invalid").css("border"), jq(".z-combobox-button").css("border-left"));
 		Assert.assertEquals(jq(".z-datebox-invalid").css("border"), jq(".z-datebox-button").css("border-left"));
+
+		JQuery alignDiv = jq("$alignDiv");
+		Assert.assertEquals(alignDiv.find(".z-bandbox-input").positionTop(), alignDiv.find(".z-bandbox-button").positionTop());
+		Assert.assertEquals(alignDiv.find(".z-combobox-input").positionTop(), alignDiv.find(".z-combobox-button").positionTop());
+		Assert.assertEquals(alignDiv.find(".z-datebox-input").positionTop(), alignDiv.find(".z-datebox-button").positionTop());
+		Assert.assertEquals(alignDiv.find(".z-doublespinner-input").positionTop(), alignDiv.find(".z-doublespinner-button").positionTop());
+		Assert.assertEquals(alignDiv.find(".z-spinner-input").positionTop(), alignDiv.find(".z-spinner-button").positionTop());
+		Assert.assertEquals(alignDiv.find(".z-timebox-input").positionTop(), alignDiv.find(".z-timebox-button").positionTop());
 	}
 }
