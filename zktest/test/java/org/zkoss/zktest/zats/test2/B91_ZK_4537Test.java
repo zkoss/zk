a811eae07b1503c9b850f3e5283c5266d86f6b8d
leonlee <leonlee@potix.com>
2020-05-20 17:51:59 +0800

    ZK-4537: Chosenbox clear and add selected items cause js error

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B91_ZK_4537Test.java
@@ -0,0 +1,29 @@
+/* B91_ZK_4537Test.java
+
+		Purpose:
+		
+		Description:
+		
+		History:
+				Thu May 21 16:47:36 CST 2020, Created by leon
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+public class B91_ZK_4537Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+		
+		click(jq("@button").eq(0));
+		waitResponse();
+		click(jq("@button").eq(1));
+		waitResponse();
+		Assert.assertFalse(hasError());
+	}
+}
