7ae2afa84a9e745f6bcf1b67e8998e944af80002
Rudy Huang <rudyhuang@potix.com>
2020-08-28 12:50:05 +0800

    ZK-4515: ListProxy dirty status not cleaned after save

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B95_ZK_4515Test.java
@@ -0,0 +1,36 @@
+/* B95_ZK_4515Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Aug 28 12:41:09 CST 2020, Created by rudyhuang
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zktest.zats.ZATSTestCase;
+import org.zkoss.zul.Button;
+
+/**
+ * @author rudyhuang
+ */
+public class B95_ZK_4515Test extends ZATSTestCase {
+	@Test
+	public void test() {
+		DesktopAgent desktop = connect();
+
+		desktop.queryAll("toolbarbutton").get(0).click();
+		desktop.queryAll("textbox").get(1).type("Sun");
+		desktop.queryAll("button").get(1).click();
+
+		Assert.assertTrue("The button should be disabled",
+				desktop.query("button").as(Button.class).isDisabled());
+	}
+}
