0bf909723dcaee44e3360915863760b8f7b11b76
Rudy Huang <rudyhuang@potix.com>
2020-10-27 11:37:09 +0800

    Add a ClassRule ExternalZkXml

--- zktest/test/java/org/zkoss/zktest/zats/test2/B30_1753712Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B30_1753712Test.java
@@ -1,39 +1,18 @@
 package org.zkoss.zktest.zats.test2;
 
-import java.net.InetSocketAddress;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.DefaultHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 
-import org.zkoss.lang.Library;
+import org.zkoss.zktest.zats.ExternalZkXml;
 import org.zkoss.zktest.zats.WebDriverTestCase;
 
 /**
  * @author jameschu
  */
 public class B30_1753712Test extends WebDriverTestCase {
-	@BeforeClass
-	public static void init() throws Exception {
-		Library.setProperty("org.zkoss.zk.config.path", "/test2/B30-1753712-zk.xml");
-		Server server = new Server(new InetSocketAddress(getHost(), 0));
-		final WebAppContext context = new WebAppContext();
-		context.setContextPath(getContextPath());
-		context.setBaseResource(Resource.newResource("./src/archive/"));
-		server.setHandler(new HandlerList(context, new DefaultHandler()));
-		initServer(server);
-	}
-
-	@AfterClass
-	public static void cleanUp() {
-		Library.setProperty("org.zkoss.zk.config.path", null);
-	}
+	@ClassRule
+	public static final ExternalZkXml CONFIG = new ExternalZkXml(B30_1753712Test.class);
 
 	@Test
 	public void test() {
--- zktest/test/java/org/zkoss/zktest/zats/test2/F70_ZK_1919Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/F70_ZK_1919Test.java
@@ -11,38 +11,19 @@ Copyright (C) 2019 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zktest.zats.test2;
 
-import java.net.InetSocketAddress;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.DefaultHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 
-import org.zkoss.lang.Library;
+import org.zkoss.zktest.zats.ExternalZkXml;
 import org.zkoss.zktest.zats.WebDriverTestCase;
 
 /**
  * @author rudyhuang
  */
 public class F70_ZK_1919Test extends WebDriverTestCase {
-	@BeforeClass
-	public static void init() throws Exception {
-		Library.setProperty("org.zkoss.zk.config.path", "/test2/F70-ZK-1919-zk.xml");
-
-		Server server = new Server(new InetSocketAddress(getHost(), 0));
-
-		final WebAppContext context = new WebAppContext();
-		context.setContextPath(getContextPath());
-		context.setBaseResource(Resource.newResource("./src/archive/"));
-		context.getSessionHandler().setSessionIdPathParameterName(null);
-		server.setHandler(new HandlerList(context, new DefaultHandler()));
-		initServer(server);
-	}
+	@ClassRule
+	public static final ExternalZkXml CONFIG = new ExternalZkXml(F70_ZK_1919Test.class);
 
 	@Test
 	public void test() {
@@ -67,10 +48,5 @@ public class F70_ZK_1919Test extends WebDriverTestCase {
 		Assert.assertEquals(uuidWindow, widget(jq("@window")).uuid());
 		Assert.assertEquals(uuidButton, widget(jq("@button")).uuid());
 		Assert.assertEquals(uuidLabel, widget(jq("@label")).uuid());
-	}
-
-	@AfterClass
-	public static void afterClass() {
-		Library.setProperty("org.zkoss.zk.config.path", null);
 	}
 }
