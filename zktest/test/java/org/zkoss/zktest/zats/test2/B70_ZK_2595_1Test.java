b0445b2cd770330ad3ffbf1e7f29fb53c7376606
Rudy Huang <rudyhuang@potix.com>
2020-12-14 16:58:43 +0800

    ZK-4720: show a hidden panel causes Uncaught RangeError: Maximum call stack size exceeded

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B70_ZK_2595_1Test.java
@@ -0,0 +1,33 @@
+/* B70_ZK_2595Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Dec 15 10:05:08 CST 2020, Created by rudyhuang
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Test;
+
+import org.zkoss.zats.ZatsException;
+import org.zkoss.zk.ui.UiException;
+import org.zkoss.zktest.zats.ZATSTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class B70_ZK_2595_1Test extends ZATSTestCase {
+	// Since ZK8 it's not allowed to use both width and hflex together
+	@Test(expected = UiException.class)
+	public void test() throws Throwable {
+		try {
+			connect("/test2/B70-ZK-2595_1.zul");
+		} catch (ZatsException e) {
+			throw e.getCause();
+		}
+	}
+}
---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B95_ZK_4720Test.java
@@ -0,0 +1,49 @@
+/* B95_ZK_4720Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Dec 14 16:53:05 CST 2020, Created by rudyhuang
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class B95_ZK_4720Test extends WebDriverTestCase {
+	@Test
+	public void testLayout() {
+		connect();
+
+		jq("$d1").scrollTop(100);
+		jq("$d2").scrollTop(100);
+		jq("$d3").scrollTop(100);
+		click(jq("@checkbox:eq(0)"));
+		waitResponse();
+		click(jq("@checkbox:eq(0)"));
+		waitResponse();
+		assertNoJSError();
+	}
+
+	@Test
+	public void testBox() {
+		connect();
+
+		jq("$d4").scrollTop(100);
+		jq("$d5").scrollTop(100);
+		jq("$d6").scrollTop(100);
+		click(jq("@checkbox:eq(1)"));
+		waitResponse();
+		click(jq("@checkbox:eq(1)"));
+		waitResponse();
+		assertNoJSError();
+	}
+}
