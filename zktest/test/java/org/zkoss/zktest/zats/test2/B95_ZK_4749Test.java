d7062d7d964f6d15b91b138564f41b552843c8a6
DevChu <marchuqq@gmail.com>
2020-12-21 08:58:27 +0800

    ZK-4749: @ref bindings on form objects only update partially

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B95_ZK_4749Test.java
@@ -0,0 +1,34 @@
+/* B95_ZK_4749Test.java
+
+	Purpose:
+
+	Description:
+
+	History:
+		Fri Dec 18 10:50:21 CST 2020, Created by jameschu
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zktest.zats.ZATSTestCase;
+import org.zkoss.zul.Label;
+
+/**
+ * @author jameschu
+ */
+public class B95_ZK_4749Test extends ZATSTestCase {
+	@Test
+	public void test() {
+		DesktopAgent desktop = connect();
+		desktop.query("#prefillBtn").click();
+		Assert.assertNotEquals("", desktop.query("#l1").as(Label.class).getValue().trim());
+		Assert.assertNotEquals("", desktop.query("#l2").as(Label.class).getValue().trim());
+		Assert.assertNotEquals("", desktop.query("#l2_1").as(Label.class).getValue().trim());
+		Assert.assertNotEquals("", desktop.query("#l3").as(Label.class).getValue().trim());
+	}
+}
