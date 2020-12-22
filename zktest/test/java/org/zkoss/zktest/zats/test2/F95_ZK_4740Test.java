1c7ec23b7afb6a5d2303aeaeb2a0680b8c108f78
Rudy Huang <rudyhuang@potix.com>
2020-12-22 10:35:54 +0800

    ZK-4740: Provide an "auto rotate" feature to support landscapes PDF files

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/F95_ZK_4740Test.java
@@ -0,0 +1,40 @@
+/* F95_ZK_4740Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Dec 22 10:38:33 CST 2020, Created by rudyhuang
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import static org.hamcrest.Matchers.greaterThan;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author rudyhuang
+ */
+public class F95_ZK_4740Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+
+		final WebDriverWait wait = new WebDriverWait(driver, 3);
+		final WebElement pdfCanvas = toElement(jq("canvas"));
+		wait.until(ExpectedConditions.attributeToBeNotEmpty(pdfCanvas, "height"));
+
+		final Dimension pdfSize = pdfCanvas.getSize();
+		MatcherAssert.assertThat("Seems it's portrait", pdfSize.width, greaterThan(pdfSize.height));
+	}
+}
