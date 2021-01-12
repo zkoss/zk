5c59acde5564b643bc176e6860dbb6a39873e6ae
Rudy Huang <rudyhuang@potix.com>
2021-01-12 18:43:46 +0800

    Convert B50-2984235, B50-2984382, B70-ZK-2357, and B80-ZK-1987 to ZATS Provide a static format in B50-3288904 Refine ZK-3518 green background and ZK-1407 CSS class

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B70_ZK_2357Test.java
@@ -0,0 +1,53 @@
+/* B70_ZK_2357Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Jan 14 11:39:13 CST 2021, Created by rudyhuang
+
+Copyright (C) 2021 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+import org.zkoss.zktest.zats.ztl.JQuery;
+
+/**
+ * @author rudyhuang
+ */
+public class B70_ZK_2357Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+
+		final JQuery btn = jq("@button");
+		final String btnBackground = getBackground(btn);
+		getActions().moveToElement(toElement(btn)).perform();
+		Assert.assertNotEquals(btnBackground, getBackground(btn));
+
+		final JQuery toolbtn = jq("@toolbarbutton");
+		final String toolbtnBackground = getBackground(toolbtn);
+		getActions().moveToElement(toElement(toolbtn)).perform();
+		Assert.assertNotEquals(toolbtnBackground, getBackground(toolbtn));
+
+		click(jq("@menu"));
+		waitResponse();
+		final JQuery menu1 = jq("@menupopup .z-menuitem-content");
+		final String menu1Background = getBackground(menu1);
+		final JQuery menu2 = jq(".z-menuitem-content:last");
+		final String menu2Background = getBackground(menu2);
+		getActions().moveToElement(toElement(menu1)).perform();
+		Assert.assertNotEquals(menu1Background, getBackground(menu1));
+		getActions().moveToElement(toElement(menu2)).perform();
+		Assert.assertNotEquals(menu2Background, getBackground(menu2));
+	}
+
+	private String getBackground(JQuery w) {
+		return w.css("backgroundColor");
+	}
+}
