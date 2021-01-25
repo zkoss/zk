e5e2a39095a0f5349b9eee44b6eafa6647b0ec09
Katherine <b952426@gmail.com>
2021-01-25 18:33:36 +0800

    Revert "ZK-4760: default error message box hard coded width (too small)"
    This reverts commit d9088a83c616c700fb8545cbdbc55970102841b9.

--- zul/src/archive/web/js/zul/dom.js
+++ zul/src/archive/web/js/zul/dom.js
@@ -73,6 +73,7 @@ it will be useful, but WITHOUT ANY WARRANTY.
 				var wnd = new zul.wnd.Window({
 					id: 'aualert',
 					closable: true,
+					width: '250pt',
 					sclass: 'z-messagebox-window',
 					title: opts.title || zk.appName,
 					border: 'normal',
@@ -92,6 +93,7 @@ it will be useful, but WITHOUT ANY WARRANTY.
 								new zul.wgt.Div({
 									id: 'content',
 									sclass: 'z-messagebox',
+									style: 'overflow:auto',
 									children: [
 										new zul.wgt.Label({
 											id: 'msg',
