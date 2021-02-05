4ea3a4b90d8388763d13b2a2e285379fa2553705
DevChu <marchuqq@gmail.com>
2021-02-05 15:32:14 +0800

    refine test case ZK-664 (reset button for static value after testing)

--- zktest/src/org/zkoss/zktest/test2/SubmitTest.java
+++ zktest/src/org/zkoss/zktest/test2/SubmitTest.java
@@ -1,9 +1,21 @@
 package org.zkoss.zktest.test2;
 
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Session;
+
 public class SubmitTest {
-	private static int count = 0;
-	public static int getCount () {
+	private static final String ZK664KEY = "ZK-664";
+
+	public static int getCount() {
+		Session session = Executions.getCurrent().getSession();
+		Integer count = (Integer) session.getAttribute(ZK664KEY);
+		if (count == null) count = 0;
 		count++;
+		session.setAttribute(ZK664KEY, count);
 		return count;
+	}
+
+	public static void resetCount() {
+		Executions.getCurrent().getSession().removeAttribute(ZK664KEY);
 	}
 }
