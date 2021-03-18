6105d5c6dad026b21442c8a45772c0dd36966f71
DevChu <marchuqq@gmail.com>
2021-03-18 12:08:33 +0800

    ZK-4795: Grid/Listbox/Tree supports sticky column headers

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/F96_ZK_4795Test.java
@@ -0,0 +1,43 @@
+/* F96_ZK_4795Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Mar 16 10:50:45 CST 2021, Created by jameschu
+
+Copyright (C) 2020 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author jameschu
+ */
+public class F96_ZK_4795Test extends WebDriverTestCase {
+	@Test
+	public void testScroll() {
+		connect();
+		waitResponse();
+		//test grid sticky
+		eval("window.scrollTo(0, " + jq("@row").last().offsetTop() + ")");
+		waitResponse();
+		assertEquals(parseInt(getEval("window.scrollY")), jq(".z-grid-header").offsetTop());
+
+		//test listbox sticky
+		eval("window.scrollTo(0, " + jq("@listitem").last().offsetTop() + ")");
+		waitResponse();
+		assertEquals(parseInt(getEval("window.scrollY")), jq(".z-listbox-header").offsetTop());
+
+		//test tree sticky
+		eval("window.scrollTo(0, document.body.scrollHeight)");
+		waitResponse();
+		assertEquals(parseInt(getEval("window.scrollY")), jq(".z-tree-header").offsetTop());
+	}
+}
