81a469a00f4931d863fd9258cb11ba3e5e25b2b2
DevChu <marchuqq@gmail.com>
2021-03-19 14:52:50 +0800

    ZK-4827: Fail to load JSP with ZK Components

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4827Test.java
@@ -0,0 +1,57 @@
+/* B96_ZK_4827Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Fri Mar 19 10:30:22 CST 2021, Created by jameschu
+
+Copyright (C) 2021 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author jameschu
+ */
+
+public class B96_ZK_4827Test extends WebDriverTestCase {
+	@BeforeClass
+	public static void init() throws Exception {
+		Server server = new Server(new InetSocketAddress(getHost(), 0));
+
+		//config for jsp
+		Configuration.ClassList classlist = Configuration.ClassList
+				.setServerDefault(server);
+		classlist.addBefore(
+				"org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+				"org.eclipse.jetty.annotations.AnnotationConfiguration");
+
+		final WebAppContext context = new WebAppContext();
+		context.setContextPath(getContextPath());
+		context.setBaseResource(Resource.newResource("./src/archive/"));
+		context.getSessionHandler().setSessionIdPathParameterName(null);
+		server.setHandler(new HandlerList(context, new DefaultHandler()));
+		initServer(server);
+	}
+
+	@Test
+	public void test() throws Exception {
+		connect("/test2/B95-ZK-4827.jsp");
+		sleep(3000);
+		assertNoJSError();
+	}
+}
