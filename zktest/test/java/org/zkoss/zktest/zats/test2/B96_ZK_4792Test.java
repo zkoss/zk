f82f67c209ebdb04888d4bde6625010a9dc46b01
Katherine <b952426@gmail.com>
2021-03-31 14:57:06 +0800

    ZK-4792: Chosenbox getSelectedIndex() return wrong index with ListModelList

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4792Test.java
@@ -0,0 +1,35 @@
+/* B96_ZK_4792Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Mar 29 12:23:54 CST 2021, Created by katherinelin
+
+Copyright (C) 2021 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.support.ui.Select;
+import org.zkoss.zktest.zats.WebDriverTestCase;
+import org.zkoss.zul.Label;
+
+public class B96_ZK_4792Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+		Select sb = new Select(toElement(jq("@selectbox")));
+		sb.selectByVisibleText("Name is data 1");
+		waitResponse();
+		click(jq("@button:contains(show selected index)"));
+		waitResponse();
+		Assert.assertEquals("0", jq("$lb1").text());
+		Assert.assertEquals("0", jq("$lb2").text());
+		Assert.assertEquals("0", jq("$lb3").text());
+		Assert.assertEquals("0", jq("$lb4").text());
+		Assert.assertEquals("0", jq("$lb5").text());
+	}
+}
