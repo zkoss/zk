d89c123d076d08b65cdd54823ab0564ddd6f58e5
leonlee <leonlee@potix.com>
2021-04-07 18:19:04 +0800

    ZK-4843: combobox checkPopupPosition performance

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4843Test.java
@@ -0,0 +1,37 @@
+/* B96_ZK_4843Test.java
+
+		Purpose:
+		
+		Description:
+		
+		History:
+				Wed Apr 07 17:49:14 CST 2021, Created by leon
+
+Copyright (C) 2021 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+public class B96_ZK_4843Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+
+		click(jq("@button:eq(0)"));
+		waitResponse();
+		double period = getPeriod();
+
+		MatcherAssert.assertThat("Response was too slow!", period, Matchers
+			.lessThanOrEqualTo(1000.0));
+	}
+
+	private double getPeriod() {
+		double period = Double.parseDouble(getZKLog().replaceAll("[^\\d.]", ""));
+		closeZKLog();
+		return period;
+	}
+}
