b3d2341ac69023895bf4161176b33191df9e6fc0
DevChu <marchuqq@gmail.com>
2021-04-23 11:33:25 +0800

    ZK-4855: some ListProxy methods set _dirty directly instead of calling setDirty, missing updates

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4855Test.java
@@ -0,0 +1,33 @@
+/* B96_ZK_4855Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Apr 20 15:50:21 CST 2021, Created by jameschu
+
+Copyright (C) 2017 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zktest.zats.ZATSTestCase;
+
+/**
+ * @author jameschu
+ */
+public class B96_ZK_4855Test extends ZATSTestCase {
+	@Test
+	public void test() throws Exception {
+		DesktopAgent desktop = connect();
+		desktop.query("#btn1").click();
+		assertEquals(2, desktop.queryAll("listitem").size());
+		desktop.query("#btn2").click();
+		assertEquals(4, desktop.queryAll("listitem").size());
+	}
+}
