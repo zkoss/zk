ae45cd98128514ff5dfa7b9308b1d088747a21aa
Jumper Chen <jumperchen@gmail.com>
2021-12-21 14:51:12 +0800

    move 9.6.1 test cases to gradle test folder

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4509Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4509Test.java
@@ -13,6 +13,7 @@ package org.zkoss.zktest.zats.test2;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 import org.zkoss.zktest.zats.WebDriverTestCase;
 
 public class B96_ZK_4509Test extends WebDriverTestCase {
