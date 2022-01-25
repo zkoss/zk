35205e8416a8138f295bf4a66f32289864f15378
leonlee <leonlee@potix.com>
2022-01-25 18:32:05 +0800

    refine B85_ZK_3848Test

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3848Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B85_ZK_3848Test.java
@@ -59,7 +59,7 @@ public class B85_ZK_3848Test {
 	@Test
 	public void test1() throws Exception {
 		File src = new File("src/test/java/org/zkoss/zktest/zats/test2/ZK3848_lang_addon_Test1.xml");
-		File dst = new File("debug/classes/metainfo/zk/lang-addon.xml");
+		File dst = new File("build/resources/main/metainfo/zk/lang-addon.xml");
 		
 		String name = "metainfo/zk/lang-addon.xml";
 		String elName = "addon-name";
@@ -88,8 +88,8 @@ public class B85_ZK_3848Test {
 
 	@Test
 	public void test2() throws IOException {
-		File src = new File("src/test/java/org/zkoss/zktest/zats/test2/ZK3848_lang_addon_Test1.xml");
-		File dst = new File("debug/classes/metainfo/zk/lang-addon.xml");
+		File src = new File("src/test/java/org/zkoss/zktest/zats/test2/ZK3848_lang_addon_Test2.xml");
+		File dst = new File("build/resources/main/metainfo/zk/lang-addon.xml");
 		
 		String name = "metainfo/zk/lang-addon.xml";
 		String elName = "addon-name";
