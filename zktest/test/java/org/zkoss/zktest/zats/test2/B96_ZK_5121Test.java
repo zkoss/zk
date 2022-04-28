0a147d08450c5bcc56a20967fd3e2f8a1a616eb4
James Chu <marchuqq@gmail.com>
2022-04-28 17:14:30 +0800

    ZK-5121: cascader / searchbox do no clean popup on unbind

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5121Test.java
@@ -0,0 +1,37 @@
+/* B96_ZK_5121Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Apr 28 16:37:12 CST 2022, Created by jameschu
+
+Copyright (C) 2022 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author jameschu
+ */
+public class B96_ZK_5121Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+		click(jq("@button"));
+		click(jq(".z-searchbox-icon.z-icon-caret-down"));
+		waitResponse(4000);
+		assertFalse(jq(".z-searchbox-popup").is(":visible"));
+
+		click(jq("@button"));
+		click(jq(".z-cascader-icon.z-icon-caret-down"));
+		waitResponse(4000);
+		assertFalse(jq(".z-cascader-popup").is(":visible"));
+	}
+}
