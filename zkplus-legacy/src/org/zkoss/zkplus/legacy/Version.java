f2aea753500d7bf0e8966fdc5cba9497df591e2a
James Chu <marchuqq@gmail.com>
2022-05-24 17:54:30 +0800

    upgrade to ZK 9.6.3 version

--- zkplus-legacy/src/org/zkoss/zkplus/legacy/Version.java
+++ zkplus-legacy/src/org/zkoss/zkplus/legacy/Version.java
@@ -23,5 +23,5 @@ package org.zkoss.zkplus.legacy;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "9.6.2";
+	public static final String UID = "9.6.3";
 }
