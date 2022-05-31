52af88fafaeea7b173630765e6b904ac2d311f1b
learningstud <gordonhsu@potix.com>
2022-05-31 11:10:22 +0800

    eslint: allowSingleExtends

--- zk/src/main/resources/web/js/zk/global.d.ts
+++ zk/src/main/resources/web/js/zk/global.d.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable one-var */
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /* globals.d.ts
