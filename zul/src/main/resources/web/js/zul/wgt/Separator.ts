787f72985b88979a7735db3d3dad7b37da1fccd5
learningstud <gordonhsu@potix.com>
2022-06-02 10:20:40 +0800

    js2ts zul/wgt/Separator

--- zul/src/main/resources/web/js/zul/wgt/Separator.ts
+++ zul/src/main/resources/web/js/zul/wgt/Separator.ts
@@ -12,104 +12,126 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 This program is distributed under LGPL Version 2.1 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
-(function () {
 
-	let _shallFixPercent: (wgt: zk.Widget) => boolean =
-		zk.gecko
-			? (wgt) => {
-				let s: string = wgt._spacing;
-				return s != null && s.endsWith('%');
-			}
-			: zk.$void;
+let _shallFixPercent: (wgt: zul.wgt.Separator) => boolean =
+	zk.gecko
+		? (wgt) => {
+			let s = wgt._spacing;
+			return s != null && s.endsWith('%');
+		}
+		: zk.$void;
 
 /**
  * A separator.
  *  <p>Default {@link #getZclass} is "z-separator".
  */
-zul.wgt.Separator = zk.$extends(zul.Widget, {
-	_orient: 'horizontal',
+ export class Separator extends zul.Widget {
+	private _orient = 'horizontal';
+	private _bar = false;
+	public _spacing?: string;
 
-	$define: { //zk.def
-		/** Returns the orient.
-		 * <p>Default: "horizontal".
-		 * @return String
-		 */
-		/** Sets the orient.
-		 * @param String orient either "horizontal" or "vertical".
-		 */
-		orient() {
+	/** Returns the orient.
+	 * <p>Default: "horizontal".
+	 * @return String
+	 */
+	public getOrient(): string {
+		return this._orient;
+	}
+	/** Sets the orient.
+	 * @param String orient either "horizontal" or "vertical".
+	 */
+	public setOrient(v: string, opts?: Record<string, boolean>): this {
+		const o = this._orient;
+		this._orient = v;
+		if (o !== v || (opts && opts.force)) {
 			this.updateDomClass_();
-		},
-		/** Returns whether to display a visual bar as the separator.
-		 * <p>Default: false
-		 * @return boolean
-		 */
-		/** Sets  whether to display a visual bar as the separator.
-		 * @param boolean bar
-		 */
-		bar() {
+		}
+		return this;
+	}
+	/** Returns whether to display a visual bar as the separator.
+	 * <p>Default: false
+	 * @return boolean
+	 */
+	public isBar(): boolean {
+		return this._bar;
+	}
+	/** Sets  whether to display a visual bar as the separator.
+	 * @param boolean bar
+	 */
+	public setBar(v: boolean, opts?: Record<string, boolean>): this {
+		const o = this._bar;
+		this._bar = v;
+		if (o !== v || (opts && opts.force)) {
 			this.updateDomClass_();
-		},
-		/** Returns the spacing.
-		 * <p>Default: null (depending on CSS).
-		 * @return String
-		 */
-		/** Sets the spacing.
-		 * @param String spacing the spacing (such as "0", "5px", "3pt" or "1em")
-		 */
-		spacing() {
+		}
+		return this;
+	}
+	/** Returns the spacing.
+	 * <p>Default: null (depending on CSS).
+	 * @return String
+	 */
+	public getSpacing(): string | undefined {
+		return this._spacing;
+	}
+	/** Sets the spacing.
+	 * @param String spacing the spacing (such as "0", "5px", "3pt" or "1em")
+	 */
+	public setSpacing(v: string, opts?: Record<string, boolean>): this {
+		const o = this._spacing;
+		this._spacing = v;
+		if (o !== v || (opts && opts.force)) {
 			this.updateDomStyle_();
 		}
-	},
+		return this;
+	}
 
 	/** Returns whether it is a vertical separator.
 	 * @return boolean
 	 */
-	isVertical(): boolean {
+	public isVertical(): boolean {
 		return this._orient == 'vertical';
-	},
+	}
 
 	//super//
-	bind_() {
-		this.$supers(zul.wgt.Separator, 'bind_', arguments);
-	},
-	getZclass() {
+	protected override bind_(desktop?: zk.Desktop | null, skipper?: zk.Skipper | null, after?: CallableFunction[]): void {
+		super.bind_(desktop, skipper, after);
+	}
+	public override getZclass(): string {
 		return 'z-separator';
-	},
-	domClass_(no) {
-		var sc = this.$supers('domClass_', arguments),
+	}
+	protected override domClass_(no?: Partial<zk.DomClassOptions>): string {
+		var sc = super.domClass_(no),
 			bar = this.isBar();
 		if (!no || !no.zclass) {
 			sc += ' ' + this.$s((this.isVertical() ? 'vertical' + (bar ? '-bar' : '') :
 				'horizontal' + (bar ? '-bar' : '')));
 		}
 		return sc;
-	},
-	domStyle_() {
-		var s = this.$supers('domStyle_', arguments);
+	}
+	protected override domStyle_(no?: Partial<zk.DomStyleOptions>): string {
+		var s = super.domStyle_(no);
 		if (!_shallFixPercent(this))
 			return s;
 
 		//_spacing contains %
-		var space = this._spacing,
+		var space = this._spacing!,
 			v = zk.parseInt(space.substring(0, space.length - 1).trim());
 		if (v <= 0) return s;
 		let percent = v >= 2 ? (v / 2) + '%' : '1%';
 
 		return 'margin:' + (this.isVertical() ? '0 ' + percent : percent + ' 0')
 			+ ';' + s;
-	},
-	getWidth() {
-		let wd = this.$supers('getWidth', arguments);
+	}
+	public override getWidth(): string | null | undefined {
+		let wd = super.getWidth();
 		return !this.isVertical() || (wd != null && wd.length > 0)
 			|| _shallFixPercent(this) ? wd : this._spacing;
 
-	},
-	getHeight() {
-		let hgh = this.$supers('getHeight', arguments);
+	}
+	public override getHeight(): string | null | undefined {
+		let hgh = super.getHeight();
 		return this.isVertical() || (hgh != null && hgh.length > 0)
 			|| _shallFixPercent(this) ? hgh : this._spacing;
 	}
-});
-
-})();
+}
+zul.wgt.Separator = Separator;
