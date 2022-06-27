a44d39d6c2c29956d6c9913dd5c7ceb78a9dc5e5
learningstud <gordonhsu@potix.com>
2022-06-27 14:17:06 +0800

    Migrate zul/mesh/Auxheader to TS

--- zul/src/main/resources/web/js/zul/mesh/Auxheader.ts
+++ zul/src/main/resources/web/js/zul/mesh/Auxheader.ts
@@ -1,4 +1,4 @@
-/* Auxheader.js
+/* Auxheader.ts
 
 	Purpose:
 
@@ -16,49 +16,72 @@ it will be useful, but WITHOUT ANY WARRANTY.
  * An auxiliary header.
  * <p>Default {@link #getZclass}: z-auxheader.
  */
-zul.mesh.Auxheader = zk.$extends(zul.mesh.HeaderWidget, {
-	_colspan: 1,
-	_rowspan: 1,
+export abstract class Auxheader extends zul.mesh.HeaderWidget {
+	private _colspan = 1;
+	private _rowspan = 1;
 
-	$define: {
-		/** Returns number of columns to span this header.
-		 * Default: 1.
-		 * @return int
-		 */
-		/** Sets the number of columns to span this header.
-		 * <p>It is the same as the colspan attribute of HTML TD tag.
-		 * @param int colspan
-		 */
-		colspan: function (v) {
-			var n = this.$n();
+	/** Returns number of columns to span this header.
+	 * Default: 1.
+	 * @return int
+	 */
+	public getColspan(): number {
+		return this._colspan;
+	}
+
+	/** Sets the number of columns to span this header.
+	 * <p>It is the same as the colspan attribute of HTML TD tag.
+	 * @param int colspan
+	 */
+	public setColspan(v: number, opts?: Record<string, boolean>): this {
+		const o = this._colspan;
+		this._colspan = v;
+
+		if (o !== v || (opts && opts.force)) {
+			var n = this.$n() as HTMLTableCellElement | null | undefined;
 			if (n) {
 				n.colSpan = v;
 				if (zk.ie < 11) this.rerender(); //IE's limitation
 			}
-		},
-		/** Returns number of rows to span this header.
-		 * Default: 1.
-		 * @return int
-		 */
-		/** Sets the number of rows to span this header.
-		 * <p>It is the same as the rowspan attribute of HTML TD tag.
-		 * @param int rowspan
-		 */
-		rowspan: function (v) {
-			var n = this.$n();
+		}
+
+		return this;
+	}
+
+	/** Returns number of rows to span this header.
+	 * Default: 1.
+	 * @return int
+	 */
+	public getRowspan(): number {
+		return this._rowspan;
+	}
+
+	/** Sets the number of rows to span this header.
+	 * <p>It is the same as the rowspan attribute of HTML TD tag.
+	 * @param int rowspan
+	 */
+	public setRowspan(v: number, opts?: Record<string, boolean>): this {
+		const o = this._rowspan;
+		this._rowspan = v;
+
+		if (o !== v || (opts && opts.force)) {
+			var n = this.$n() as HTMLTableCellElement | null | undefined;
 			if (n) {
 				n.rowSpan = v;
 				if (zk.ie < 11) this.rerender(); //IE's limitation
 			}
 		}
-	},
+
+		return this;
+	}
+
 	//super//
-	domAttrs_: function () {
-		var s = this.$supers('domAttrs_', arguments), v;
+	public override domAttrs_(no?: zk.DomAttrsOptions): string {
+		var s = super.domAttrs_(no), v;
 		if ((v = this._colspan) != 1)
 			s += ' colspan="' + v + '"';
 		if ((v = this._rowspan) != 1)
 			s += ' rowspan="' + v + '"';
 		return s;
 	}
-});
+}
+zul.mesh.Auxheader = zk.regClass(Auxheader);
