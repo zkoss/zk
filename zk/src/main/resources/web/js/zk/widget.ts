af585ef8dbd4afdbf916feed2f85af1e40df21ad
Jumper Chen <jumperchen@gmail.com>
2022-06-27 16:50:31 +0800

    fix for B95-ZK-4566.zul

--- zk/src/main/resources/web/js/zk/widget.ts
+++ zk/src/main/resources/web/js/zk/widget.ts
@@ -1377,6 +1377,7 @@ new zul.wnd.Window({
 	 */
 	public setCssflex(cssflex: boolean): void {
 		if (this._cssflex != cssflex) {
+			this._cssflex = cssflex;
 			if (this.desktop) {
 				this.rerender();
 			}
