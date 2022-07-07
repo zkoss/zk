d0ca39637d73f3d0afaaafa5df192dcb1923f498
Jumper Chen <jumperchen@gmail.com>
2022-07-07 12:34:02 +0800

    fix for B95-ZK-4531.zul

--- zk/src/main/resources/web/js/zk/index.ts
+++ zk/src/main/resources/web/js/zk/index.ts
@@ -41,7 +41,9 @@ declare global {
 window.zk = zk;
 
 window.zjq = zjq;
-window.$ = window.jQuery = jq;
+if (!window.jQuery) {
+	window.$ = window.jQuery = jq;
+}
 if (zk.gecko) {
 	require('./domgecko');
 } else if (zk.safari) {
