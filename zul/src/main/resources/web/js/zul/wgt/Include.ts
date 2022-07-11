b3c130f493e42c8fe9df70888cb99bbf818bf61a
learningstud <gordonhsu@potix.com>
2022-07-11 10:09:24 +0800

    Migrate zul/wgt/Include to TS

--- zul/src/main/resources/web/js/zul/wgt/Include.ts
+++ zul/src/main/resources/web/js/zul/wgt/Include.ts
@@ -15,47 +15,62 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 /**
  * An include widget
  */
-zul.wgt.Include = zk.$extends(zul.Widget, {
+@zk.WrapClass('zul.wgt.Include')
+export class Include extends zul.Widget {
 	//F70-ZK-2455: a way to change enclosing tag
-	_enclosingTag: 'div',
-	$init: function () {
-		this._fellows = {};
-		this.$supers('$init', arguments);
-	},
+	private _enclosingTag = 'div';
+	private _comment?: boolean;
+	private _childjs?: (() => void) | null;
+	private _xcnt?: HTMLElement[] | string | null;
 
-	$define: {
-		/** Returns whether to generate the included content inside
-		 * the HTML comment.
-		 * <p>Default: false.
-		 *
-		 * @return boolean
-		 */
-		/** Sets  whether to generate the included content inside
-		 * the HTML comment.
-		 * @param boolean comment
-		 */
-		comment: null
-	},
+	public constructor() {
+		super(); // FIXME: params?
+		this._fellows = {};
+		// NOTE: Prior to TS migration, super is called after `_fellows` is initialized.
+		// In this case, it's safe to initialize `_fellows` after calling super, as
+		// the none in the chain of super constructors will access `_fellows`.
+	}
+
+	/** Returns whether to generate the included content inside
+	 * the HTML comment.
+	 * <p>Default: false.
+	 *
+	 * @return boolean
+	 */
+	public isComment(): boolean | undefined {
+		return this._comment;
+	}
+
+	/** Sets  whether to generate the included content inside
+	 * the HTML comment.
+	 * @param boolean comment
+	 */
+	public setComment(comment: boolean): this {
+		this._comment = comment;
+		return this;
+	}
+
 	/**
 	 * Returns the enclosing tag
 	 * @return String
 	 * @since 7.0.4
 	 */
-	getEnclosingTag: function () {
+	public getEnclosingTag(): string {
 		return this._enclosingTag;
-	},
+	}
+
 	/**
 	 * Sets the enclosing tag
 	 * @param String enclosing tag
 	 * @since 7.0.4
 	 */
-	setEnclosingTag: function (tag) {
+	public setEnclosingTag(tag: string): void {
 		this._enclosingTag = tag;
-	},
+	}
 
 	//super//
-	domStyle_: function (no) {
-		var style = this.$supers('domStyle_', arguments);
+	protected override domStyle_(no?: zk.DomStyleOptions): string {
+		var style = super.domStyle_(no);
 		if (!this.previousSibling && !this.nextSibling) {
 		//if it is only child, the default is 100%
 			if ((!no || !no.width) && !this.getWidth())
@@ -64,24 +79,27 @@ zul.wgt.Include = zk.$extends(zul.Widget, {
 				style += 'height:100%;';
 		}
 		return style;
-	},
-	bind_: function () {
-		this.$supers(zul.wgt.Include, 'bind_', arguments);
-		var ctn;
-		if (ctn = this._childjs) {
+	}
+
+	protected override bind_(desktop?: zk.Desktop | null, skipper?: zk.Skipper | null, after?: CallableFunction[]): void {
+		super.bind_(desktop, skipper, after);
+		const ctn = this._childjs;
+		if (ctn) {
 			ctn();
 			this._childjs = this._xcnt = null;
 				//only once since the content has been created as child widgets
 		}
 
-		if (jq.isArray(ctn = this._xcnt)) //array -> zk().detachChildren() used
-			for (var n = this.$n(), j = 0; j < ctn.length; ++j)
-				n.appendChild(ctn[j]);
-	},
-	unbind_: function () {
-		if (jq.isArray(this._xcnt)) //array -> zk().detachChildren() used
-			for (var n = this.$n(); n.firstChild;)
-				n.removeChild(n.firstChild);
-		this.$supers(zul.wgt.Include, 'unbind_', arguments);
+		const xcnt = this._xcnt;
+		if (jq.isArray(xcnt)) //array -> zk().detachChildren() used
+			for (var n = this.$n_(), j = 0; j < xcnt.length; ++j)
+				n.appendChild(xcnt[j]);
 	}
-});
+
+	protected override unbind_(skipper?: zk.Skipper | null, after?: CallableFunction[], keepRod?: boolean): void {
+		if (jq.isArray(this._xcnt)) //array -> zk().detachChildren() used
+			for (var n = this.$n_(); n.firstChild;)
+				n.removeChild(n.firstChild);
+		super.unbind_(skipper, after, keepRod);
+	}
+}
