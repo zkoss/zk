4ad878ca641d8cda45de8b0e05d3575567fcc012
Jumper Chen <jumperchen@gmail.com>
2022-07-12 10:58:04 +0800

    refine

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4937Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4937Test.java
@@ -11,20 +11,29 @@ Copyright (C) 2021 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zktest.zats.test2;
 
-import org.junit.Assert;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.StringStartsWith;
 import org.junit.Test;
+
 import org.zkoss.util.media.ContentTypes;
 
 public class B96_ZK_4937Test {
 	@Test
 	public void test() {
 		// getContentType shall return the type correctly
-		Assert.assertEquals("image/svg+xml", ContentTypes.getContentType("svg"));
-		Assert.assertEquals("font/ttf", ContentTypes.getContentType("ttf"));
-		Assert.assertEquals("font/otf", ContentTypes.getContentType("otf"));
-		Assert.assertEquals("font/woff", ContentTypes.getContentType("woff"));
-		Assert.assertEquals("font/woff2", ContentTypes.getContentType("woff2"));
-		Assert.assertEquals("application/vnd.ms-fontobject", ContentTypes.getContentType("eot"));
-		Assert.assertEquals("font/sfnt", ContentTypes.getContentType("sfnt"));
+		MatcherAssert.assertThat(ContentTypes.getContentType("svg"),
+				new StringStartsWith(true, "image/svg+xml"));
+		MatcherAssert.assertThat(ContentTypes.getContentType("ttf"),
+				new StringStartsWith(true,  "font/ttf"));
+		MatcherAssert.assertThat(ContentTypes.getContentType("otf"),
+				new StringStartsWith(true, "font/otf"));
+		MatcherAssert.assertThat(ContentTypes.getContentType("woff"),
+				new StringStartsWith(true, "font/woff"));
+		MatcherAssert.assertThat(ContentTypes.getContentType("woff2"),
+				new StringStartsWith(true, "font/woff2"));
+		MatcherAssert.assertThat(ContentTypes.getContentType("eot"),
+				new StringStartsWith(true, "application/vnd.ms-fontobject"));
+		MatcherAssert.assertThat(ContentTypes.getContentType("sfnt"),
+				new StringStartsWith(true, "font/sfnt"));
 	}
 }
