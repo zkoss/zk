65754cab4830aa3cea74424d22d0489fbcd4b112
learningstud <gordonhsu@potix.com>
2022-07-12 12:13:04 +0800

    Migrate zul/fud to TS

--- zul/src/main/resources/web/js/zul/fud/FileuploadDlg.ts
+++ zul/src/main/resources/web/js/zul/fud/FileuploadDlg.ts
@@ -14,39 +14,50 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
  * A FileuploadDlg.
  *
  */
-zul.fud.FileuploadDlg = zk.$extends(zul.wnd.Window, {
-	getZclass: function () { // keep the window's zclass
+@zk.WrapClass('zul.fud.FileuploadDlg')
+export class FileuploadDlg extends zul.wnd.Window {
+	public max?: number;
+
+	public override getZclass(): string { // keep the window's zclass
 		return this._zclass == null ? 'z-window' : this._zclass;
-	},
-	cancel: function (sendToServer) {
+	}
+
+	public cancel(sendToServer?: boolean): void {
 		var fu = this.$f('fileupload');
 		if (fu) {
-			fu._uplder.destroy();
+			fu._uplder!.destroy();
 			if (sendToServer) {
 				zAu.send(new zk.Event(this, 'onClose'));
 				this.detach();
 			}
 		}
-	},
-	submit: function () {
+	}
+
+	public submit(): void {
 		this.cancel();
 		zAu.send(new zk.Event(this, 'onClose', true));
 		this.detach();
 	}
-});
+}
 
-zul.fud.ModalFileViewer = zk.$extends(zk.Object, {
-	updated: null,
-	$init: function (uplder, filenm) {
+@zk.WrapClass('zul.fud.ModalFileViewer')
+export class ModalFileViewer extends zk.Object {
+	public updated: boolean | null = null;
+	private _finish?: boolean;
+	private _uplder: zul.Uploader;
+	public viewer?: HTMLElement;
+
+	public constructor(uplder: zul.Uploader, filenm: string) {
+		super();
 		this._uplder = uplder;
 		filenm = filenm.replace(/\//g, '\\');
 		filenm = filenm.substring(filenm.lastIndexOf('\\') + 1, filenm.length);
 
 		var id = uplder.id,
 			self = this,
-			wgt = uplder.getWidget(),
-			uploaded = wgt.$f('uploaded'),
-			max = wgt.$o().max || 0, //max is stored in FileuploadDlg (i.e., $o())
+			wgt = uplder.getWidget()!,
+			uploaded = wgt.$f('uploaded')!,
+			max = wgt.$o<zul.fud.FileuploadDlg>()!.max || 0, //max is stored in FileuploadDlg (i.e., $o())
 			uri = zk.ajaxURI('/web/zk/img/progress2.gif', {resource: true}),
 			html = '<div id="' + id + '" style="min-height:16px;background:#F4F8FF;border: 1px solid #99AABD;font-family:'
 			+ 'arial,sans-serif;font-size: 11px;padding: 2px;'
@@ -60,38 +71,40 @@ zul.fud.ModalFileViewer = zk.$extends(zk.Object, {
 		uploaded.setVisible(true);
 		jq(uploaded).append(html);
 
-		if (max > 0 && max <= uploaded.$n().childNodes.length)
-			uploaded.$f('fileupload').setVisible(false); // B50-ZK-340: need to skip rerender
+		if (max > 0 && max <= uploaded.$n_().childNodes.length)
+			uploaded.$f('fileupload')!.setVisible(false); // B50-ZK-340: need to skip rerender
 
 		this.viewer = jq('#' + id)[0];
 		jq('#' + id + '-cancel').click(function () {
-			wgt.$f('submit').revert();
+			wgt.$f<zul.fud.Submit>('submit')!.revert();
 			if (!self._finish) uplder.cancel();
 			else {
 				var $n = jq('#' + id),
 					index = $n.parent().children().index($n[0]);
 				zAu.send(new zk.Event(wgt.$o(), 'onRemove', index));
-				jq(self.viewer).remove();
+				jq(self.viewer!).remove();
 			}
 
-			if (max > 0 && max > uploaded.$n().childNodes.length)
-				uploaded.$f('fileupload').setVisible(true); // B50-ZK-340: need to skip rerender
+			if (max > 0 && max > uploaded.$n_().childNodes.length)
+				uploaded.$f('fileupload')!.setVisible(true); // B50-ZK-340: need to skip rerender
 		});
-	},
-	update: function (sent, total) {
+	}
+
+	public update(sent: number, total: number): void {
 		jq('#' + this._uplder.id + '-sent').html(Math.round((total / 1024) * sent / 100) + msgzk.KBYTES);
 		if (!this.updated) {
 			this.updated = true;
 			jq('#' + this._uplder.id + '-total').html(Math.round(total / 1024) + msgzk.KBYTES);
 		}
-	},
-	destroy: function (finish) {
+	}
+
+	public destroy(finish: boolean): void {
 		if (this._finish) return;
 		this._finish = finish;
-		if (!finish) jq(this.viewer).remove();
+		if (!finish) jq(this.viewer!).remove();
 		else {
 			jq('#' + this._uplder.id + '-sent').parent().remove();
-			jq('#' + this._uplder.id)[0].firstChild.src = zk.ajaxURI('/web/zk/img/attachment.gif', {resource: true});
+			(jq('#' + this._uplder.id)[0].firstChild as HTMLImageElement).src = zk.ajaxURI('/web/zk/img/attachment.gif', {resource: true});
 		}
 	}
-});
+}
