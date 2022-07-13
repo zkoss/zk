520a9e50fbc0a9fcaaf41abc1f5671270d19c9c3
learningstud <gordonhsu@potix.com>
2022-07-13 11:03:03 +0800

    Migrate zhtml to TS

--- zhtml/src/main/resources/web/js/zhtml/Text.ts
+++ zhtml/src/main/resources/web/js/zhtml/Text.ts
@@ -12,51 +12,83 @@ Copyright (C) 2008 Potix Corporation. All Rights Reserved.
 This program is distributed under LGPL Version 2.1 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
-zhtml.Text = zk.$extends(zhtml.Widget, {
-	_value: '',
-	_encode: true,
+@zk.WrapClass('zhtml.Text')
+export class Text extends zhtml.Widget {
+	private _value = '';
+	private _encode = true;
+	public idRequired?: boolean;
 
-	$define: {
-		/** Returns whether to encode the text, such as converting &lt;
-		 * to &amp;lt;.
-		 * <p>Default: true.
-		 * @return boolean
-		 * @since 5.0.8
-		 */
-		/** Sets whether to encode the text, such as converting &lt;
-		 * to &amp;lt;.
-		 * <p>Default: true.
-		 * @param boolean encode whether to encode
-		 * @since 5.0.8
-		 */
-		encode: _zkf = function () {
+	/** Returns whether to encode the text, such as converting &lt;
+	 * to &amp;lt;.
+	 * <p>Default: true.
+	 * @return boolean
+	 * @since 5.0.8
+	 */
+	public isEncode(): boolean {
+		return this._encode;
+	}
+
+	/** Sets whether to encode the text, such as converting &lt;
+	 * to &amp;lt;.
+	 * <p>Default: true.
+	 * @param boolean encode whether to encode
+	 * @since 5.0.8
+	 */
+	public setEncode(encode: boolean, opts?: Record<string, boolean>): this {
+		const o = this._encode;
+		this._encode = encode;
+
+		if (o !== encode || (opts && opts.force)) {
 			var n = this.$n();
 			if (n) {
 				var val = this._value;
 				n.innerHTML = this._encode ? zUtl.encodeXML(val) : val;
 				//See Bug 2871080 and ZK-294
 			}
-		},
-		/** Returns the value of this label.
-		 * @return String
-		 */
-		/** Sets the value of this label.
-		 * @param String label the label
-		 */
-		value: _zkf
-	},
+		}
+
+		return this;
+	}
+
+	/** Returns the value of this label.
+	 * @return String
+	 */
+	public getValue(): string {
+		return this._value;
+	}
+
+	/** Sets the value of this label.
+	 * @param String label the label
+	 */
+	public setValue(label: string, opts?: Record<string, boolean>): this {
+		const o = this._value;
+		this._value = label;
+
+		if (o !== label || (opts && opts.force)) {
+			var n = this.$n();
+			if (n) {
+				var val = this._value;
+				n.innerHTML = this._encode ? zUtl.encodeXML(val) : val;
+				//See Bug 2871080 and ZK-294
+			}
+		}
+
+		return this;
+	}
+
 	// ZK 7.5 enable to preserve the line break and comment into XHTML format
 	// so we need to skip them here.
-	_checkContentRequired: function (val) {
+	private _checkContentRequired(val: string): boolean {
 		if (val) {
 			val = val.trim();
 			if (val && !(val.startsWith('<!--') && val.endsWith('-->')))
 				return true;
 		}
 		return false;
-	},
-	redraw: function (out) {
-		var attrs = this.domAttrs_({id: 1, zclass: 1}),
+	}
+
+	public override redraw(out: string[]): void {
+		var attrs = this.domAttrs_({id: true, zclass: true}),
 			val = this._value,
 			span = attrs || (this.idRequired && this._checkContentRequired(val));
 			// Bug 3245960: enclosed text was wrapped with <span>
@@ -65,4 +97,4 @@ zhtml.Text = zk.$extends(zhtml.Widget, {
 			//See Bug 2871080 and ZK-294
 		if (span) out.push('</span>');
 	}
-});
+}
