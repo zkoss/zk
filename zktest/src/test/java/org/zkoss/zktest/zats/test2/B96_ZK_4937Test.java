d3bcc8cef51e4a1ee184ab85c254d8c121b8c862
Jumper Chen <jumperchen@gmail.com>
2022-08-26 16:55:12 +0800

    refine

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4937Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4937Test.java
@@ -17,15 +17,22 @@ import org.junit.jupiter.api.Test;
 import org.zkoss.util.media.ContentTypes;
 
 public class B96_ZK_4937Test {
+	private String trimSemicolon(String str) {
+		if (str.endsWith(";")) {
+			return str.substring(0, str.length() - 1);
+		}
+		return str;
+	}
+
 	@Test
 	public void test() {
 		// getContentType shall return the type correctly
-		Assertions.assertEquals("image/svg+xml", ContentTypes.getContentType("svg"));
-		Assertions.assertEquals("font/ttf", ContentTypes.getContentType("ttf"));
-		Assertions.assertEquals("font/otf", ContentTypes.getContentType("otf"));
-		Assertions.assertEquals("font/woff", ContentTypes.getContentType("woff"));
-		Assertions.assertEquals("font/woff2", ContentTypes.getContentType("woff2"));
-		Assertions.assertEquals("application/vnd.ms-fontobject", ContentTypes.getContentType("eot"));
-		Assertions.assertEquals("font/sfnt", ContentTypes.getContentType("sfnt"));
+		Assertions.assertEquals("image/svg+xml", trimSemicolon(ContentTypes.getContentType("svg")));
+		Assertions.assertEquals("font/ttf", trimSemicolon(ContentTypes.getContentType("ttf")));
+		Assertions.assertEquals("font/otf", trimSemicolon(ContentTypes.getContentType("otf")));
+		Assertions.assertEquals("font/woff", trimSemicolon(ContentTypes.getContentType("woff")));
+		Assertions.assertEquals("font/woff2", trimSemicolon(ContentTypes.getContentType("woff2")));
+		Assertions.assertEquals("application/vnd.ms-fontobject", trimSemicolon(ContentTypes.getContentType("eot")));
+		Assertions.assertEquals("font/sfnt", trimSemicolon(ContentTypes.getContentType("sfnt")));
 	}
 }
