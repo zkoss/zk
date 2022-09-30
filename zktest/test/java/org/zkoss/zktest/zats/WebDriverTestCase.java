df4524ea10ffb5328d2f528353ffe932db8427fd
Jumper Chen <jumperchen@gmail.com>
2022-09-30 18:27:44 +0800

    ZK-5033: infinite loop when firing an event on a detached widget

--- zktest/test/java/org/zkoss/zktest/zats/WebDriverTestCase.java
+++ zktest/test/java/org/zkoss/zktest/zats/WebDriverTestCase.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.logging.Level;
 
 import org.eclipse.jetty.server.Connector;
@@ -67,7 +68,9 @@ public abstract class WebDriverTestCase {
 	private static final Logger log = LoggerFactory.getLogger(WebDriverTestCase.class);
 	private static final String PACKAGE = WebDriverTestCase.class.getPackage().getName();
 	private static final ThreadLocal<Server> server = new ThreadLocal<>();
+	private static Server static_server;
 	private static final ThreadLocal<Integer> port = new ThreadLocal<>();
+	private static Integer static_port;
 	private static final ThreadLocal<WebDriver> _local = new ThreadLocal<WebDriver>();
 	private static final String JS_DROP_FILES = "var c=arguments,b=c[0],k=c[1];c=c[2];for(var d=b.ownerDocument||document,l=0;;){var e=b.getBoundingClientRect(),g=e.left+(k||e.width/2),h=e.top+(c||e.height/2),f=d.elementFromPoint(g,h);if(f&&b.contains(f))break;if(1<++l)throw b=Error('Element not interactable'),b.code=15,b;b.scrollIntoView({behavior:'instant',block:'center',inline:'center'})}var a=d.createElement('INPUT');a.setAttribute('type','file');a.setAttribute('multiple','');a.setAttribute('style','position:fixed;z-index:2147483647;left:0;top:0;');a.onchange=function(b){a.parentElement.removeChild(a);b.stopPropagation();var c={constructor:DataTransfer,effectAllowed:'all',dropEffect:'none',types:['Files'],files:a.files,setData:function(){},getData:function(){},clearData:function(){},setDragImage:function(){}};window.DataTransferItemList&&(c.items=Object.setPrototypeOf(Array.prototype.map.call(a.files,function(a){return{constructor:DataTransferItem,kind:'file',type:a.type,getAsFile:function(){return a},getAsString:function(b){var c=new FileReader;c.onload=function(a){b(a.target.result)};c.readAsText(a)}}}),{constructor:DataTransferItemList,add:function(){},clear:function(){},remove:function(){}}));['dragenter','dragover','drop'].forEach(function(a){var b=d.createEvent('DragEvent');b.initMouseEvent(a,!0,!0,d.defaultView,0,0,0,g,h,!1,!1,!1,!1,0,null);Object.setPrototypeOf(b,null);b.dataTransfer=c;Object.setPrototypeOf(b,DragEvent.prototype);f.dispatchEvent(b)})};d.documentElement.appendChild(a);a.getBoundingClientRect();return a;";
 	private static final boolean IS_JENKINS = System.getenv("JENKINS_HOME") != null;
@@ -88,7 +91,7 @@ public abstract class WebDriverTestCase {
 	protected WebDriver driver;
 
 	protected static int getPort() {
-		return port.get();
+		return Optional.ofNullable(port.get()).orElse(static_port);
 	}
 	protected static String getContextPath() {
 		return "/zktest";
@@ -202,14 +205,14 @@ public abstract class WebDriverTestCase {
 
 	@BeforeClass
 	public static void init() throws Exception {
-		Server server = new Server(new InetSocketAddress(getHost(), 0));
+		static_server = new Server(new InetSocketAddress(getHost(), 0));
 
 		final WebAppContext context = new WebAppContext();
 		context.setContextPath(getContextPath());
 		context.setBaseResource(Resource.newResource("./src/archive/"));
 		context.getSessionHandler().setSessionIdPathParameterName(null);
-		server.setHandler(new HandlerList(context, new DefaultHandler()));
-		initServer(server);
+		static_server.setHandler(new HandlerList(context, new DefaultHandler()));
+		initServer(static_server);
 	}
 
 	protected static void initServer(Server currentServer) throws Exception {
@@ -219,7 +222,8 @@ public abstract class WebDriverTestCase {
 		for (Connector c : currentServer.getConnectors()) {
 			if (c instanceof NetworkConnector) {
 				if (((NetworkConnector)c).getLocalPort() > 0) {
-					port.set(((NetworkConnector) c).getLocalPort());
+					static_port = ((NetworkConnector) c).getLocalPort();
+					port.set(static_port);
 					break;
 				}
 			}
@@ -228,12 +232,13 @@ public abstract class WebDriverTestCase {
 
 	@AfterClass
 	public static void end() throws Exception {
-		Server currentServer = server.get();
+		Server currentServer = Optional.ofNullable(server.get()).orElse(static_server);
 		if (currentServer != null) {
 			currentServer.stop();
 		}
 		server.set(null);
-		port.set(0);
+		port.set(null);
+		static_port = null;
 	}
 
 	/**
