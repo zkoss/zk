4630a2f148adf226c6fd2394142f92dcf8d5c893
Jumper Chen <jumperchen@gmail.com>
2022-10-06 11:12:34 +0800

    merge 9.6.3 ZK-5148: datebox throws a js error with locale specified

---
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5148Test.java
@@ -0,0 +1,29 @@
+/* B96_ZK_5148Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		11:35 AM 2022/10/4, Created by jumperchen
+
+Copyright (C) 2022 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.jupiter.api.Test;
+
+import org.zkoss.test.webdriver.WebDriverTestCase;
+
+/**
+ * @author jumperchen
+ */
+public class B96_ZK_5148Test extends WebDriverTestCase {
+	@Test
+	public void testNoJSError() {
+		connect();
+		click(jq("@button"));
+		waitResponse();
+		assertNoJSError();
+	}
+}
