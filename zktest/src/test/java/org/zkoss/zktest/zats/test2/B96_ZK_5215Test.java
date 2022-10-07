8ac66369211c314742fd791f7474b52a3601d4fe
Jumper Chen <jumperchen@gmail.com>
2022-10-07 18:18:15 +0800

    merge 9.6.3 ZK-5215: cannot load locale-dependent resources in a relative path

---
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5215Test.java
@@ -0,0 +1,34 @@
+/* B96_ZK_5215Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		3:05 PM 2022/10/7, Created by jumperchen
+
+Copyright (C) 2022 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zktest.zats.ZATSTestCase;
+
+/**
+ * @author jumperchen
+ */
+public class B96_ZK_5215Test extends ZATSTestCase {
+
+	@Test
+	public void test() {
+		try {
+			DesktopAgent desktop = connect();
+		} catch (Throwable t) {
+			fail("cannot run into this line");
+		}
+	}
+}
