109d5de1eccda90afb5d64dea7b6fea179baa175
James Chu <marchuqq@gmail.com>
2022-10-31 18:04:35 +0800

    update ZK-4026 test case in zephyr-test

--- zephyr-test/src/main/java/org/zkoss/zktest/test2/B86_ZK_4026VM.java
+++ zephyr-test/src/main/java/org/zkoss/zktest/test2/B86_ZK_4026VM.java
@@ -1,5 +1,9 @@
 package org.zkoss.zktest.test2;
 
+import org.zkoss.bind.BindUtils;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+
 public class B86_ZK_4026VM {
 	private boolean shown = true;
 
@@ -9,5 +13,16 @@ public class B86_ZK_4026VM {
 
 	public void setShown(boolean shown) {
 		this.shown = shown;
+	}
+
+	@Command
+	@NotifyChange("shown")
+	public void toggleShown() {
+		setShown(!isShown());
+	}
+
+	@Command
+	public void notifyShown() {
+		BindUtils.postNotifyChange(null, null, this, "shown");
 	}
 }
