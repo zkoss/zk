64e307176c6c666d269b4074f3aea7bd7e6ffdc1
Jumper Chen <jumperchen@gmail.com>
2023-03-07 16:40:40 +0800

    refine

--- zephyr-test/src/test/java/org/zkoss/zephyr/webdriver/test2/B80_ZK_2983Test.java
+++ zephyr-test/src/test/java/org/zkoss/zephyr/webdriver/test2/B80_ZK_2983Test.java
@@ -13,7 +13,6 @@ package org.zkoss.zephyr.webdriver.test2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.zkoss.test.webdriver.WebDriverTestCase;
@@ -27,6 +26,7 @@ public class B80_ZK_2983Test extends WebDriverTestCase {
     @Test
     public void test() {
 		connect();
+		waitResponse();
 		click(jq("@button:eq(0)"));
 		waitResponse();
 		assertEquals("Value 1", jq("@textbox").val());
