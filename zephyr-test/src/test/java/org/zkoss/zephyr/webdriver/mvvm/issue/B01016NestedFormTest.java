993169d9927808190e7bde839db18b640af99b55
Jumper Chen <jumperchen@gmail.com>
2023-03-24 17:37:12 +0800

    ignore for Client MVVM

--- zephyr-test/src/test/java/org/zkoss/zephyr/webdriver/mvvm/issue/B01016NestedFormTest.java
+++ zephyr-test/src/test/java/org/zkoss/zephyr/webdriver/mvvm/issue/B01016NestedFormTest.java
@@ -2,10 +2,12 @@ package org.zkoss.zephyr.webdriver.mvvm.issue;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.zkoss.test.webdriver.WebDriverTestCase;
 
+@Disabled
 public class B01016NestedFormTest extends WebDriverTestCase {
 	@Test
 	public void test() {
