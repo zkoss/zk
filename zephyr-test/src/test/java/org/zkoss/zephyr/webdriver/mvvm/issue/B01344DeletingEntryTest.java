5bd8bd23d7a9b7837aa778456f880e9913c29a98
Jumper Chen <jumperchen@gmail.com>
2023-03-28 10:53:49 +0800

    minor

--- zephyr-test/src/test/java/org/zkoss/zephyr/webdriver/mvvm/issue/B01344DeletingEntryTest.java
+++ zephyr-test/src/test/java/org/zkoss/zephyr/webdriver/mvvm/issue/B01344DeletingEntryTest.java
@@ -10,6 +10,7 @@ public class B01344DeletingEntryTest extends WebDriverTestCase {
 	@Test
 	public void test() {
 		connect();
+		waitResponse();
 		for (int i = 10; i > 0; i--) {
 			assertEquals(i + "", jq("$lb1").text());
 			click(jq("$btn1"));
