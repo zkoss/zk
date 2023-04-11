f7e7ac4115b28416decafb540dc2c7cfc7e81200
James Chu <marchuqq@gmail.com>
2023-04-11 14:50:03 +0800

    ZK-5251: listbox select mold produces js error in a mobile browser

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5251Test.java
@@ -0,0 +1,45 @@
+/* B96_ZK_5251Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Apr 11 12:32:44 CST 2023, Created by jameschu
+
+Copyright (C) 2023 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.zkoss.zktest.zats.ExternalZkXml;
+import org.zkoss.zktest.zats.ForkJVMTestOnly;
+import org.zkoss.zktest.zats.WebDriverTestCase;
+
+/**
+ * @author jameschu
+ */
+@Category(ForkJVMTestOnly.class)
+public class B96_ZK_5251Test extends WebDriverTestCase {
+	@ClassRule
+	public static final ExternalZkXml CONFIG = new ExternalZkXml("/test2/enable-tablet-ui-zk.xml");
+
+	@Override
+	protected ChromeOptions getWebDriverOptions() {
+		return super.getWebDriverOptions().setExperimentalOption("mobileEmulation", Collections.singletonMap("deviceName", "iPad")).setExperimentalOption("w3c", false); // Temporary workaround for TouchAction
+	}
+
+	@Test
+	public void test() {
+		connect();
+		waitResponse();
+		Assert.assertFalse(hasError());
+	}
+}
