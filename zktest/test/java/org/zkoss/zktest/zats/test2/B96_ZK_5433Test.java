bbc55f86b6320f0501d1279e9701ecb7dda56d08
James Chu <marchuqq@gmail.com>
2023-04-27 10:50:30 +0800

    ZK-5429: tabbox scroll unsynced when resizing with toolbar ZK-5433: tabbox doesn't scroll the selected tab into the current view when shrinking its width

---
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5433Test.java
@@ -0,0 +1,24 @@
+package org.zkoss.zktest.zats.test2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+
+import org.zkoss.zktest.zats.WebDriverTestCase;
+import org.zkoss.zktest.zats.ztl.JQuery;
+
+public class B96_ZK_5433Test extends WebDriverTestCase {
+	@Test
+	public void test() {
+		connect();
+		waitResponse();
+		driver.manage().window().setSize(new Dimension(820, 1080));
+		waitResponse();
+		JQuery tab15 = jq("@tab").eq(14);
+		//in view
+		System.out.println("tab15 offsetLeft: " + tab15.toElement().get("offsetLeft"));
+		System.out.println("tabs rightmost: " + (jq("@tabs").scrollLeft() + jq("@tabs").width()));
+		assertTrue(parseInt(tab15.toElement().get("offsetLeft")) < (jq("@tabs").scrollLeft() + jq("@tabs").width()));
+	}
+}
