ca8b0fb6d8d0857953f2d8a5d5ee7c1530509d7f
James Chu <marchuqq@gmail.com>
2023-05-05 11:50:54 +0800

    refine ZK-5433 test case
    (cherry picked from commit 91d8dfbc6467f03c1547f2eb3ee5b0a88d778188)
    (cherry picked from commit 102e6501408d6cd6237f47e59f8a2632bed6bf46)

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5433Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5433Test.java
@@ -14,7 +14,8 @@ public class B96_ZK_5433Test extends WebDriverTestCase {
 		connect();
 		waitResponse();
 		driver.manage().window().setSize(new Dimension(820, 1080));
-		waitResponse();
+		driver.manage().window().setSize(new Dimension(810, 1080)); // force resizing the browser
+		waitResponse(true);
 		JQuery tab15 = jq("@tab").eq(14);
 		//in view
 		System.out.println("tab15 offsetLeft: " + tab15.toElement().get("offsetLeft"));
