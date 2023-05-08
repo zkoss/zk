7963487a5ed1af42f1284b9936b94def8f84709b
James Chu <marchuqq@gmail.com>
2023-05-08 18:49:17 +0800

    refine ZK-5251 Test case

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5251Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5251Test.java
@@ -13,29 +13,20 @@ package org.zkoss.zktest.zats.test2;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import java.util.Collections;
-
-import org.junit.ClassRule;
-import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import org.zkoss.test.webdriver.ExternalZkXml;
 import org.zkoss.test.webdriver.ForkJVMTestOnly;
-import org.zkoss.test.webdriver.WebDriverTestCase;
+import org.zkoss.test.webdriver.TouchWebDriverTestCase;
 
 /**
  * @author jameschu
  */
-@Category(ForkJVMTestOnly.class)
-public class B96_ZK_5251Test extends WebDriverTestCase {
-	@ClassRule
+@ForkJVMTestOnly
+public class B96_ZK_5251Test extends TouchWebDriverTestCase {
+	@RegisterExtension
 	public static final ExternalZkXml CONFIG = new ExternalZkXml("/test2/enable-tablet-ui-zk.xml");
-
-	@Override
-	protected ChromeOptions getWebDriverOptions() {
-		return super.getWebDriverOptions().setExperimentalOption("mobileEmulation", Collections.singletonMap("deviceName", "iPad")).setExperimentalOption("w3c", false); // Temporary workaround for TouchAction
-	}
 
 	@Test
 	public void test() {
