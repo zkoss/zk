fcc5e70f3944de802c27cb1b8cd446cb01d951c4
Jumper Chen <jumperchen@gmail.com>
2023-07-21 17:49:13 +0800

    Fix ZK-5468: Components with Model only renders one item with wrong result if the EL contains forEachStatus.index

---
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B100_ZK_5468_CascaderTest.java
@@ -0,0 +1,72 @@
+/* B100_ZK_5468_CascaderTest.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		12:17 PM 2023/7/21, Created by jumperchen
+
+Copyright (C) 2023 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.ref.WeakReference;
+import java.util.Iterator;
+
+import org.junit.jupiter.api.Test;
+
+import org.zkoss.test.webdriver.WebDriverTestCase;
+import org.zkoss.test.webdriver.ztl.JQuery;
+
+/**
+ * @author jumperchen
+ */
+public class B100_ZK_5468_CascaderTest extends WebDriverTestCase {
+
+	@Test
+	public void testInsertBefore() {
+		connect();
+
+		click(jq(".z-cascader-icon.z-icon-caret-down"));
+		waitResponse(true);
+		String[] result = {"0 - aaa0", "1 - aaa1", "2 - aaa2"};
+		int index = 0;
+		for (Iterator<JQuery> it = jq(".z-cascader-item").iterator(); it.hasNext(); ) {
+			JQuery jQuery = it.next();
+			assertEquals(result[index++], jQuery.text());
+		}
+		click(jq("button:contains(insert before)"));
+		waitResponse();
+
+		click(jq(".z-cascader-icon.z-icon-caret-down"));
+		waitResponse(true);
+
+		result = new String[]{"0 - ddd", "1 - aaa0", "2 - aaa1", "3 - aaa2"};
+		index = 0;
+		for (Iterator<JQuery> it = jq(".z-cascader-item").iterator(); it.hasNext(); ) {
+			JQuery jQuery = it.next();
+			if (index == 0) {
+				assertThat(jQuery.text(), startsWith(result[index++]));
+				continue;
+			}
+			assertEquals(result[index++], jQuery.text());
+		}
+
+		click(jq("button:contains(remove)"));
+		waitResponse(true);
+
+		click(jq(".z-cascader-icon.z-icon-caret-down"));
+		waitResponse(true);
+		result = new String[]{"0 - aaa0", "1 - aaa1", "2 - aaa2"};
+		index = 0;
+		for (Iterator<JQuery> it = jq(".z-cascader-item").iterator(); it.hasNext(); ) {
+			JQuery jQuery = it.next();
+			assertEquals(result[index++], jQuery.text());
+		}
+	}
+}
