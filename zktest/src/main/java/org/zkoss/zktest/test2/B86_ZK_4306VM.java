b6177c37434a996836797d3e9d4790e4d329039e
Jumper Chen <jumperchen@gmail.com>
2023-08-09 17:55:01 +0800

    Fix new version of com.google.javascript.jscomp.SourceFile$Builder with a private constructor issue for B86_ZK_4306Test

--- zktest/src/main/java/org/zkoss/zktest/test2/B86_ZK_4306VM.java
+++ zktest/src/main/java/org/zkoss/zktest/test2/B86_ZK_4306VM.java
@@ -11,13 +11,13 @@ Copyright (C) 2019 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zktest.test2;
 
-import org.zkoss.bind.annotation.Command;
-import org.zkoss.zk.ui.http.SourceMapManager;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.zk.ui.http.SourceMapManager;
 
 /**
  * @author jameschu
@@ -29,7 +29,7 @@ public class B86_ZK_4306VM {
 		doSerialize0();
 		doDeserialize0();
 	}
-	public void doSerialize0() throws Exception{
+	public void doSerialize0() throws Exception {
 		ByteArrayOutputStream oaos = new ByteArrayOutputStream();
 		ObjectOutputStream oos = new ObjectOutputStream(oaos);
 		oos.writeObject(new SourceMapManager(null, null, null));
@@ -38,7 +38,7 @@ public class B86_ZK_4306VM {
 		_bytes = oaos.toByteArray();
 	}
 
-	public void doDeserialize0() throws Exception{
+	public void doDeserialize0() throws Exception {
 		ByteArrayInputStream oaos = new ByteArrayInputStream(_bytes);
 		ObjectInputStream oos = new ObjectInputStream(oaos);
 		oos.close();
