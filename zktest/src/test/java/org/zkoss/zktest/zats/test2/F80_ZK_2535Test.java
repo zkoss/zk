04e92774fd3ccd12774b85ff04d2dd1370aa3206
Jumper Chen <jumperchen@gmail.com>
2023-08-14 10:51:24 +0800

    Fix a side effect of the fixed ZK-5213 for F80_ZK_2535Test.java

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/F80_ZK_2535Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/F80_ZK_2535Test.java
@@ -27,48 +27,50 @@ public class F80_ZK_2535Test extends WebDriverTestCase{
     @Test
     public void test() {
 		connect();
+		// this case cannot work with A11Y as the fixed ZK-5213: Improve accessibility by add tooltips on icons
+		if (!Boolean.valueOf(getEval("!!window.za11y"))) {
 
-		for (int i = 1; i <= 2; i++) {
-			JQuery p = jq("$p" + i);
+			for (int i = 1; i <= 2; i++) {
+				JQuery p = jq("$p" + i);
 
-			JQuery maximize = p.find(".z-panel-maximize");
-			assertEquals("Maximize", maximize.attr("title"));
+				JQuery maximize = p.find(".z-panel-maximize");
+				assertEquals("Maximize", maximize.attr("title"));
 
-			click(maximize);
-			waitResponse();
-			assertEquals("Restore", maximize.attr("title"));
+				click(maximize);
+				waitResponse();
+				assertEquals("Restore", maximize.attr("title"));
 
-			JQuery expand = p.find(".z-panel-expand");
-			assertEquals("Collapse", expand.attr("title"));
+				JQuery expand = p.find(".z-panel-expand");
+				assertEquals("Collapse", expand.attr("title"));
 
-			click(expand);
-			waitResponse();
-			assertEquals("Expand", expand.attr("title"));
-			assertEquals(false, p.find(".z-panel-body").isVisible());
+				click(expand);
+				waitResponse();
+				assertEquals("Expand", expand.attr("title"));
+				assertEquals(false, p.find(".z-panel-body").isVisible());
 
-			JQuery close = p.find(".z-panel-close");
-			assertEquals("Close", close.attr("title"));
-			click(close);
-			waitResponse();
-			assertEquals(0, jq("$p" + i).length());
-		}
+				JQuery close = p.find(".z-panel-close");
+				assertEquals("Close", close.attr("title"));
+				click(close);
+				waitResponse();
+				assertEquals(0, jq("$p" + i).length());
+			}
 
-		for (int i = 1; i <= 2; i++) {
-			JQuery w = jq("$w" + i);
+			for (int i = 1; i <= 2; i++) {
+				JQuery w = jq("$w" + i);
 
-			JQuery maximize = w.find(".z-window-maximize");
-			assertEquals("Maximize", maximize.attr("title"));
+				JQuery maximize = w.find(".z-window-maximize");
+				assertEquals("Maximize", maximize.attr("title"));
 
-			click(maximize);
-			waitResponse();
-			assertEquals("Restore", maximize.attr("title"));
+				click(maximize);
+				waitResponse();
+				assertEquals("Restore", maximize.attr("title"));
 
-
-			JQuery close = w.find(".z-window-close");
-			assertEquals("Close", close.attr("title"));
-			click(close);
-			waitResponse();
-			assertEquals(0, jq("$w" + i).length());
+				JQuery close = w.find(".z-window-close");
+				assertEquals("Close", close.attr("title"));
+				click(close);
+				waitResponse();
+				assertEquals(0, jq("$w" + i).length());
+			}
 		}
 	}
 }
