187ab0eb41a6697d6ed6a67c28ab23ee216047dc
Jumper Chen <jumperchen@gmail.com>
2023-08-21 17:01:24 +0800

    ZK-5441: Provide a better way to override za11y module

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 //@ts-check
 const path = require('path');
 const webpack = require('webpack');
+const tslib = require('tslib');
 // const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 /** @type { import('webpack').Configuration } */
 module.exports = {
 	resolve: {
 		extensions: ['.ts', '.js', '.json'],
+		alias: {
+			'tslib$': 'tslib/tslib.es6.js',
+		},
 	},
 
 	module: {
@@ -29,11 +33,19 @@ module.exports = {
 		'jquery': 'jq', // for jquery.transit.js
 		'moment': 'zk.mm', // for moment.js
 	},
-
 	plugins: [
 		new webpack.IgnorePlugin({
 			resourceRegExp: /^\.\/locale$/
 		}),
+		new webpack.ProvidePlugin((function () {
+			let options = {};
+			for (let key in tslib) {
+				options[key] = ['tslib', key];
+			}
+			return Object.assign(options, {
+				'__extends': [path.join(__dirname, 'extends.js'), 'default'] // fix ZK-5441
+			});
+		})())
 		// new CircularDependencyPlugin({
 		// 	failOnError: true
 		// })
