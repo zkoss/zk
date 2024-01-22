fc1b84697fc4a7df44fa52f0f569ca0f14fc36fd
jamsonchan <jamsonchan@potix.com>
2024-01-22 14:32:25 +0800

    ZK-3853: Tree supports 3-states selection model

---
+++ zul/src/main/java/org/zkoss/zul/ext/TristateModel.java
@@ -0,0 +1,33 @@
+/* TristateModel.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                2024/1/20 AM 09:29, Created by jumperchen
+
+Copyright (C) 2024 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zul.ext;
+
+import java.util.Set;
+
+/**
+ * Indicate a model that supports tristate checkbox.
+ * @author jumperchen
+ * @since 10.0.0
+ */
+public interface TristateModel<E> {
+
+	/**
+	 * Returns the current partial selection.
+	 */
+	Set<E> getPartials();
+
+	/**
+	 * Returns whether the item is partially selected.
+	 * @param item a data item
+	 */
+	boolean isPartial(E item);
+}
