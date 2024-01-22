42a1e56d8dbeda29a01b5d305c8f64908266fd9f
jamsonchan <jamsonchan@potix.com>
2024-01-22 16:36:07 +0800

    Add javadoc for TristateModel.State

--- zul/src/main/java/org/zkoss/zul/ext/TristateModel.java
+++ zul/src/main/java/org/zkoss/zul/ext/TristateModel.java
@@ -20,11 +20,30 @@ import java.util.Set;
  */
 public interface TristateModel<E> {
 
-
-	/** States of the tri-state selection. */
+	/**
+	 * States of the tri-state selection designed to assist users in defining states effectively.
+	 * This enum is meant to be used in conjunction with an interface that requires the definition
+	 * of tri-state selection states.
+	 * <p>This enum is designed to facilitate the implementation of interfaces where a tri-state selection
+	 * is required. Utilizing these predefined states enables users to clearly define the selection status
+	 * of nodes consistently and meaningfully.
+	 */
 	enum State {
+		/**
+		 * Represents the unselected state.
+		 * Use this state when the node is explicitly not selected.
+		 */
 		UNSELECTED,
+		/**
+		 * Represents the selected state.
+		 * Use this state when the node is explicitly selected.
+		 */
 		SELECTED,
+		/**
+		 * Represents the partial selection state.
+		 * Use this state when the node is partially selected, meaning
+		 * that it is in an intermediate state between fully selected and unselected.
+		 */
 		PARTIAL
 	}
 
