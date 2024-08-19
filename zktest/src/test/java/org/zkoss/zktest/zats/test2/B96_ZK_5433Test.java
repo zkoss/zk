5efe41ce02dbfa426e5e8d104433f060257235d8
Jumper Chen <jumperchen@gmail.com>
2024-08-19 17:23:42 +0800

    fix test case for ZK-5433 about chrome's headless issue.

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5433Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B96_ZK_5433Test.java
@@ -3,19 +3,28 @@ package org.zkoss.zktest.zats.test2;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.zkoss.test.webdriver.WebDriverTestCase;
 import org.zkoss.test.webdriver.ztl.JQuery;
 
 public class B96_ZK_5433Test extends WebDriverTestCase {
+
+	// fix Chrome's headless mode doesn't support resizing the browser
+	protected ChromeOptions getWebDriverOptions() {
+		ChromeOptions options = super.getWebDriverOptions();
+		options.addArguments(new String[]{"window-size=820,1080"});
+		return options;
+	}
+
 	@Test
 	public void test() {
 		connect();
 		waitResponse();
-		driver.manage().window().setSize(new Dimension(820, 1080));
-		driver.manage().window().setSize(new Dimension(810, 1080)); // force resizing the browser
-		waitResponse(true);
+//		Chrome's headless mode doesn't support resizing the browser
+//		driver.manage().window().setSize(new Dimension(820, 1080));
+//		driver.manage().window().setSize(new Dimension(800, 1080)); // force resizing the browser
+//		waitResponse(true);
 		JQuery tab15 = jq("@tab").eq(14);
 		//in view
 		System.out.println("tab15 offsetLeft: " + tab15.toElement().get("offsetLeft"));
