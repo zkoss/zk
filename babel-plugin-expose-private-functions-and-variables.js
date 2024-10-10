ef000daeeb6d0e783827d06b59e8df8b6cf9ef4b
jamsonchan <jamsonchan@potix.com>
2024-10-10 17:43:44 +0800

    tmp

--- babel-plugin-expose-private-functions-and-variables.js
+++ babel-plugin-expose-private-functions-and-variables.js
@@ -21,7 +21,8 @@ module.exports = function ({types: t}) {
 					const jsLoc = dir.findIndex(x => x === 'js'),
 						file = dir[dir.length - 1],
 						privateVars = new Set(),
-						privateFuncs = new Set();
+						privateFuncs = new Set(),
+						funcCallCount = new Map();
 
 					// pass if [not in js folder] or [not ts file] or [is global.d.ts] or [is index.ts]
 					if (jsLoc === -1 || !file.endsWith('ts') || file === 'global.d.ts' || file === 'index.ts') return;
@@ -46,8 +47,9 @@ module.exports = function ({types: t}) {
 						// collect private functions and replace them with `window.PACKAGE._._func = function (args) {...}`
 						} else if (t.isFunctionDeclaration(node) && t.isIdentifier(node.id)) {
 							if (node.id.name === '_zk') return;
-							if (node.id.name === 'doLog') return;
+							// if (node.id.name === 'doLog') return;
 							privateFuncs.add(node.id.name);
+							funcCallCount.set(node.id.name, 0);
 							path.get('body')[index].replaceWith(
 								t.expressionStatement(
 									t.assignmentExpression(
@@ -124,6 +126,7 @@ module.exports = function ({types: t}) {
 							// 如果賦值的右側是一個私有函式名稱，將其替換成 `window.PACKAGE._._func`
 							if (t.isIdentifier(right) && privateFuncs.has(right.name)) {
 								assignPath.node.right = createNestedMemberExpression([right.name, ...dir]);
+								funcCallCount.set(right.name, funcCallCount.get(right.name) + 1);
 							}
 						},
 						CallExpression(callPath) {
@@ -132,6 +135,7 @@ module.exports = function ({types: t}) {
 							// 替換直接的函式呼叫
 							if (t.isIdentifier(callee) && privateFuncs.has(callee.name)) {
 								callPath.node.callee = createNestedMemberExpression([callee.name, ...dir]);
+								funcCallCount.set(callee.name, funcCallCount.get(callee.name) + 1);
 							}
 
 							// 檢查是否是 setTimeout 的函式參數
@@ -147,6 +151,7 @@ module.exports = function ({types: t}) {
 												// 如果內部的呼叫是私有函式
 												if (t.isIdentifier(innerCallee) && privateFuncs.has(innerCallee.name)) {
 													innerCallPath.node.callee = createNestedMemberExpression([innerCallee.name, ...dir]);
+													funcCallCount.set(innerCallee.name, funcCallCount.get(innerCallee.name) + 1);
 												}
 											}
 										});
@@ -157,13 +162,16 @@ module.exports = function ({types: t}) {
 										// 檢查 consequent 和 alternate 是否是 Identifier
 										if (t.isIdentifier(consequent) && privateFuncs.has(consequent.name)) {
 											arg.get('consequent').replaceWith(createNestedMemberExpression([consequent.name, ...dir]));
+											funcCallCount.set(consequent.name, funcCallCount.get(consequent.name) + 1);
 										}
 										if (t.isIdentifier(alternate) && privateFuncs.has(alternate.name)) {
 											arg.get('alternate').replaceWith(createNestedMemberExpression([alternate.name, ...dir]));
+											funcCallCount.set(alternate.name, funcCallCount.get(alternate.name) + 1);
 										}
 									} else if (t.isIdentifier(arg.node) && privateFuncs.has(arg.node.name)) {
 										// 替換 setTimeout 中的函式參數
 										arg.replaceWith(createNestedMemberExpression([arg.node.name, ...dir]));
+										// funcCallCount.set(arg.node.name, funcCallCount.get(arg.node.name) + 1);
 									}
 								});
 							}
@@ -176,6 +184,7 @@ module.exports = function ({types: t}) {
 
 									if (t.isIdentifier(callee) && privateFuncs.has(callee.name)) {
 										innerCallPath.node.callee = createNestedMemberExpression([callee.name, ...dir]);
+										funcCallCount.set(callee.name, funcCallCount.get(callee.name) + 1);
 									}
 								}
 							});
@@ -186,10 +195,24 @@ module.exports = function ({types: t}) {
 								if (t.isIdentifier(property.value) && privateFuncs.has(property.value.name)) {
 									// 替換屬性值
 									property.value = createNestedMemberExpression([property.value.name, ...dir]);
+									// funcCallCount.set(property.value.name, funcCallCount.get(property.value.name) + 1);
 								}
 							});
 						}
 					});
+
+					// 4. 構建一個變數來輸出呼叫次數結果
+					const callCountArray = Array.from(funcCallCount.entries()).map(([name, count]) =>
+						t.objectProperty(t.identifier(name), t.numericLiteral(count))
+					);
+					const callCountVariable = t.variableDeclaration('var', [
+						t.variableDeclarator(
+							t.identifier('dlwlrmaXXX'),
+							t.objectExpression(callCountArray)
+						)
+					]);
+					// 5. 在程式結尾插入計算結果變數
+					path.pushContainer('body', callCountVariable);
 				}
 			}
 		}
