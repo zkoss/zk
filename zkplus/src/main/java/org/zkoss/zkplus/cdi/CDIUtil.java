33a540dcee7793ad0ca9e95c76a58d32b676d739
Jumper Chen <jumperchen@gmail.com>
2025-04-07 14:40:02 +0800

    ZK-5458: zkplus DelegatingVariableResolver not longer works for the normal CDI case or for Quarkus ARC

--- zkplus/src/main/java/org/zkoss/zkplus/cdi/CDIUtil.java
+++ zkplus/src/main/java/org/zkoss/zkplus/cdi/CDIUtil.java
@@ -18,6 +18,7 @@ Copyright (C) 2009 Potix Corporation. All Rights Reserved.
 package org.zkoss.zkplus.cdi;
 
 import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -26,10 +27,10 @@ import org.zkoss.xel.XelException;
 
 /**
  * Some generic utility for use with CDI.
- * <p>Applicable to CDI version 1.0 or later</p>
+ * <p>Applicable to CDI version 1.1 or later</p>
  * <p> To specify a different JNDI name for the bean manager, please specify them
- * in zk.xml as follows. (By default is <tt>java:comp/env/BeanManager</tt>) since
- * 5.0.11.
+ * in zk.xml as follows. (By default is <tt>CDI.current().getBeanManager()</tt>) since
+ * 10.2.0.
  * <pre>{@code
  *	<library-property>
  *		<name>org.zkoss.zkplus.cdi.beanManager.jndiName</name>
@@ -42,19 +43,23 @@ import org.zkoss.xel.XelException;
 public class CDIUtil {
 	private static BeanManager _manager;
 
-	/** Returns the CDI BeanManager. 
-	 * Default implementation use JNDI to lookup "java:comp/env/BeanManager". 
+	/** Returns the CDI BeanManager.
 	 */
 	public static BeanManager getBeanManager() {
 		if (_manager != null)
 			return _manager;
 
 		try {
-			final InitialContext initialContext = new InitialContext();
-			_manager = (BeanManager) initialContext.lookup(
-					Library.getProperty("org.zkoss.zkplus.cdi.beanManager.jndiName", "java:comp/env/BeanManager"));
+			final String property = Library.getProperty(
+					"org.zkoss.zkplus.cdi.beanManager.jndiName");
+			if (property != null) {
+				final InitialContext initialContext = new InitialContext();
+				_manager = (BeanManager) initialContext.lookup(property);
+			} else {
+				_manager = CDI.current().getBeanManager();
+			}
 		} catch (NamingException e) { // Error getting the home interface
-			throw XelException.Aide.wrap(e, "Cannot locate the BeanManager for JavaEE 6.");
+			throw XelException.Aide.wrap(e, "Cannot locate the BeanManager for JavaEE.");
 		}
 		return _manager;
 	}
