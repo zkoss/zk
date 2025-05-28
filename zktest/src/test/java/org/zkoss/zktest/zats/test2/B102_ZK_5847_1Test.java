2c428578770e1681197db0e37da487a3433a4d70
jamsonchan <jamsonchan@potix.com>
2025-05-28 12:47:01 +0800

    refix ZK-5847 for non-rod case

---
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B102_ZK_5847_1Test.java
@@ -0,0 +1,15 @@
+/* B102_ZK_5847_1Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Wed May 28 12:30:37 CST 2025, Created by jamson
+
+Copyright (C) 2025 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+public class B102_ZK_5847_1Test extends B102_ZK_5847Test {
+}
