676508822208696dd87ff18b5023f9bea1574b93
jamsonchan <jamsonchan@potix.com>
2025-06-30 16:19:44 +0800

    refine ZK-5640, print tzdb version mismatched log when WebAppInit (originally print when every session started)

--- zktest/src/test/java/org/zkoss/zktest/zats/test2/F102_ZK_5640Test.java
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/F102_ZK_5640Test.java
@@ -13,6 +13,7 @@ package org.zkoss.zktest.zats.test2;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.time.zone.ZoneRulesProvider;
@@ -20,23 +21,67 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.NetworkConnector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.JarResource;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.resource.ResourceCollection;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.jupiter.api.Assertions;
 
 import org.zkoss.lang.Library;
-import org.zkoss.test.webdriver.WebDriverTestCase;
-import org.zkoss.zk.ui.http.Wpds;
+import org.zkoss.zk.au.http.TimeZoneDataBaseVersionChecker;
 
-public class F102_ZK_5640Test extends WebDriverTestCase {
+public class F102_ZK_5640Test {
 
+    protected static Server server;
+    protected static int port;
+    protected static String getContextPath() {
+        return "/zktest";
+    }
+    protected static String getHost() {
+        return "127.0.0.1";
+    }
+    protected static Logger _logger = null;
+    protected static TestLogHandler _handler = null;
     protected static final String tempFileName = "F102-ZK-5640-temp-tzdb-version.json";
 
+    /**
+     * Init server and all needed variables.
+     */
+    protected static void beforeAll0() throws Exception {
+        _logger = Logger.getLogger("org.zkoss.zk.au.http.TimeZoneDataBaseVersionChecker");
+        _handler = new TestLogHandler();
+        _logger.addHandler(_handler);
+
+        server = new Server(new InetSocketAddress(getHost(), 0));
+        final WebAppContext context = new WebAppContext();
+        context.setContextPath(getContextPath());
+
+        Resource[] res = new Resource[] {Resource.newResource("./src/main/webapp/"), JarResource.newJarResource(Resource.newResource("./src/main/webapp/data/zk-3123-1.0.jar"))};
+        context.setBaseResource(new ResourceCollection(res));
+        server.setHandler(context);
+        server.start();
+
+        for (Connector c : server.getConnectors()) {
+            if (c instanceof NetworkConnector) {
+                if (((NetworkConnector)c).getLocalPort() > 0) {
+                    port = ((NetworkConnector)c).getLocalPort();
+                    break;
+                }
+            }
+        }
+    }
+    
     /**
      * Create temp tzdb version json file,
      * and set library property "org.zkoss.zk.moment.timezone.path".
      * @param version the target file version
      */
-    protected static void beforeAll0(String version) {
+    protected static void createTempTZDBFileAndApplyToLibraryProperty(String version) throws Exception {
         String tempJsonContent = "{\"version\":\"" + version + "\"}";
         File tempFile = new File("src/main/webapp/test2/data/" + tempFileName);
         try {
@@ -51,13 +96,19 @@ public class F102_ZK_5640Test extends WebDriverTestCase {
      * Delete temp tzdb version json file,
      * and set library property "org.zkoss.zk.moment.timezone.path" to null.
      */
-    protected static void afterAll0() {
+    protected static void afterAll0() throws Exception {
         try {
             Files.delete(new File("src/main/webapp/test2/data/" + tempFileName).toPath());
         } catch (IOException e) {
             Assertions.fail("Failed to delete temp tzdb version json.", e);
         }
         Library.setProperty("org.zkoss.zk.moment.timezone.path", null);
+        if (server != null) {
+            server.stop();
+        }
+        _logger.removeHandler(_handler);
+        _logger = null;
+        _handler = null;
     }
 
     protected static class TestLogHandler extends Handler {
@@ -95,6 +146,6 @@ public class F102_ZK_5640Test extends WebDriverTestCase {
     }
 
     protected static String getJsTZDBVersion() {
-        return Wpds.getClientTimeZoneDataBaseVersion();
+        return TimeZoneDataBaseVersionChecker.getClientTimeZoneDataBaseVersion();
     }
 }
