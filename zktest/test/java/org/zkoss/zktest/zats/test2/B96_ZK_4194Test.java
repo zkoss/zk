65bcaf5dc3f7c3bb877ed71b4f1fe019735462df
jamsonchan <jamsonchan@potix.com>
2025-07-07 16:00:12 +0800

    upgrade and fix

--- zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4194Test.java
+++ zktest/test/java/org/zkoss/zktest/zats/test2/B96_ZK_4194Test.java
@@ -22,8 +22,8 @@ import org.junit.experimental.categories.Category;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v121.network.Network;
-import org.openqa.selenium.devtools.v121.network.model.ConnectionType;
+import org.openqa.selenium.devtools.v138.network.Network;
+import org.openqa.selenium.devtools.v138.network.model.ConnectionType;
 
 import org.zkoss.zktest.zats.ChromiumHeadlessDriver;
 import org.zkoss.zktest.zats.ExternalZkXml;
@@ -64,9 +64,9 @@ public class B96_ZK_4194Test extends WebDriverTestCase {
 		try (DevTools devTools = window1.getDevTools()) {
 			devTools.createSession();
 			// network offline
-			devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+			devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty()));
 			devTools.send(Network.emulateNetworkConditions(true, 20, 20, 40, Optional.of(
-					ConnectionType.WIFI)));
+					ConnectionType.WIFI), Optional.empty(), Optional.empty(), Optional.empty()));
 
 			_local.set(window2);
 			for (int i = 0; i < 9; i++) {
@@ -109,10 +109,10 @@ public class B96_ZK_4194Test extends WebDriverTestCase {
 		try (DevTools devTools = window1.getDevTools()) {
 			devTools.createSession();
 			// network offline
-			devTools.send(Network.enable(Optional.empty(), Optional.empty(),
-					Optional.empty()));
+			devTools.send(
+					Network.enable(Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty()));
 			devTools.send(Network.emulateNetworkConditions(true, 20, 20, 40,
-					Optional.of(ConnectionType.WIFI)));
+					Optional.of(ConnectionType.WIFI), Optional.empty(), Optional.empty(), Optional.empty()));
 
 			_local.set(window2);
 			for (int i = 0; i < 19; i++) {
@@ -156,9 +156,9 @@ public class B96_ZK_4194Test extends WebDriverTestCase {
 			devTools.createSession();
 			// network offline
 			devTools.send(Network.enable(Optional.empty(), Optional.empty(),
-					Optional.empty()));
+					Optional.empty(), Optional.empty()));
 			devTools.send(Network.emulateNetworkConditions(true, 20, 20, 40,
-					Optional.of(ConnectionType.WIFI)));
+					Optional.of(ConnectionType.WIFI), Optional.empty(), Optional.empty(), Optional.empty()));
 
 			_local.set(window2);
 			for (int i = 0; i < 19; i++) {
