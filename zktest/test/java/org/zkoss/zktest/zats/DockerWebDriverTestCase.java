f46c91ba731020dbf6d1f123e5e5178491f4d29f
jamsonchan <jamsonchan@potix.com>
2025-07-07 12:49:08 +0800

    upgrade DockerWebDriverTestCase

--- zktest/test/java/org/zkoss/zktest/zats/DockerWebDriverTestCase.java
+++ zktest/test/java/org/zkoss/zktest/zats/DockerWebDriverTestCase.java
@@ -14,14 +14,21 @@ package org.zkoss.zktest.zats;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
 
-import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.DockerComposeExtension;
+import com.palantir.docker.compose.configuration.ShutdownStrategy;
 import com.palantir.docker.compose.connection.waiting.HealthChecks;
-import org.junit.ClassRule;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.parallel.ResourceLock;
 
 /**
  * A local Docker container to support a linux based remote WebDriver for
@@ -29,40 +36,82 @@ import org.junit.ClassRule;
  *
  * @author jumperchen
  */
+@ResourceLock("dockerResource")
 public abstract class DockerWebDriverTestCase extends WebDriverTestCase {
+
 	// enable to use docker env.
-	@Override
 	protected final boolean isUseDocker() {
 		return true;
 	}
 
-	@Override
 	protected String getRemoteWebDriverUrl() {
 		final int externalPort = docker.containers().container("hub").port(4444).getExternalPort();
 		return "http://localhost:" + externalPort + "/wd/hub";
 	}
 
+	protected FileLock globalLock;
+
+	private static final String tempDir = new SimpleDateFormat("yyyyMMdd").format(new java.util.Date());
+
 	// create a temp file for docker compose.yml
-	private static String exportResource(String file) {
+	protected String exportResource(String file) {
 		try {
 			InputStream resourceAsStream = new FileInputStream("test/resources/docker/docker-compose.yml");
-			String dest = Files.createTempDirectory("zkWebdriver").resolve(file).toFile().getAbsolutePath();
-			Path path = Paths.get(dest);
+			Path path = Paths.get(System.getProperty("java.io.tmpdir"), tempDir, "zkWebdriver").resolve(file);
 			if (!Files.isDirectory(path.getParent())) {
 				Files.createDirectories(path.getParent());
 			}
+			if (path.toFile().exists()) {
+				path.toFile().delete();
+			}
+
+			// always copy the docker-compose.yml to the temp file
 			Files.copy(resourceAsStream, path, StandardCopyOption.REPLACE_EXISTING);
-			return dest;
+
+			// try to acquire a global lock for each DockerWebDriver
+			RandomAccessFile files = new RandomAccessFile(path.toFile(), "rw");
+			FileChannel channel = files.getChannel();
+			while (true) {
+				try {
+					globalLock = channel.tryLock();
+					if (globalLock.isValid()) {
+						break;
+					}
+				} catch (Throwable e) {
+					// File is already locked in this thread or virtual machine
+				}
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			}
+			return path.toAbsolutePath().toString();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
+	@AfterAll
+	public void unlockGlobalLock() {
+		if (globalLock != null) {
+			try {
+				globalLock.release();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
 	// remove the docker env. if the bug has fixed - https://tracker.zkoss.org/browse/ZK-5092
-	@ClassRule
-	public final static DockerComposeRule docker = new DockerComposeRule.Builder()
-		.file(exportResource("docker/docker-compose.yml"))
-		.waitingForService("hub", HealthChecks.toRespondOverHttp(4444, (port) -> port.inFormat("http://$HOST:$EXTERNAL_PORT/ui/index.html")))
-		.waitingForService("chrome", HealthChecks.toHaveAllPortsOpen())
-		.build();
+	@RegisterExtension
+	public final DockerComposeExtension docker = DockerComposeExtension.builder()
+			.file(exportResource("test/resources/docker/docker-compose.yml"))
+			.useDockerComposeV2(Boolean.parseBoolean(System.getProperty("useDockerComposeV2", "true")))
+			.waitingForService("hub", HealthChecks.toRespondOverHttp(4444,
+					(port) -> port.inFormat("http://$HOST:$EXTERNAL_PORT/ui/index.html")))
+			.waitingForService("chrome", HealthChecks.toHaveAllPortsOpen())
+			.shutdownStrategy(ShutdownStrategy.KILL_DOWN)
+			.removeConflictingContainersOnStartup(true)
+			.build();
 }
