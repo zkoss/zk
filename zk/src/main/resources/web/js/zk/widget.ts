1dacc18e0f576d8d9643aa9ea030967286985fde
yuehfeng <52810637+yuehfeng@users.noreply.github.com>
2025-11-14 14:02:34 +0800

    ZK-5800: css flex listen then unlisten immediately causes listeners to not be removed (#3370)

--- zk/src/main/resources/web/js/zk/widget.ts
+++ zk/src/main/resources/web/js/zk/widget.ts
@@ -364,23 +364,24 @@ function _listenFlex(wgt: zk.Widget & {_flexListened?: boolean}): void {
 		wgt._flexListened = true;
 	}
 }
+
 function _unlistenFlex(wgt: zk.Widget): void {
 	if (wgt._flexListened) {
-		var cssFlexApplied = wgt._cssFlexApplied;
-		if (cssFlexApplied) {
-			//remove css flex flag
+		var parent = wgt.parent,
+			cssFlexEnabled = wgt._cssflex && parent && (parent instanceof zk.Page || parent.getFlexContainer_() != null);
+		if (cssFlexEnabled)
+			zWatch.unlisten({beforeSize: [wgt, zFlex.beforeSizeClearCachedSize]});
+		wgt.unlistenOnFitSize_();
+		if (cssFlexEnabled) {
 			zWatch.unlisten({onSize: [wgt, zFlex.applyCSSFlex]});
 			delete wgt._cssFlexApplied;
 		} else {
-			zWatch.unlisten({onSize: [wgt, zFlex.onSize]});
 			zWatch.unlisten({
 				_beforeSizeForRead: [wgt, zFlex.beforeSizeForRead],
 				beforeSize: [wgt, zFlex.beforeSize]
 			});
+			zWatch.unlisten({onSize: [wgt, zFlex.onSize]});
 		}
-		wgt.unlistenOnFitSize_();
-		if (cssFlexApplied)
-			zWatch.unlisten({beforeSize: [wgt, zFlex.beforeSizeClearCachedSize]});
 		delete wgt._flexListened;
 	}
 }
