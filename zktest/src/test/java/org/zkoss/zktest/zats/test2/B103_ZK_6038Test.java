7bda2f44276742a5f395610221b1af4287b07d76
peggypeng <peggypeng@potix.com>
2026-01-08 16:23:12 +0800

    ZK-6038: coachmark lose its background and border

---
+++ zktest/src/test/java/org/zkoss/zktest/zats/test2/B103_ZK_6038Test.java
@@ -0,0 +1,32 @@
+/* B103_ZK_6038Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Thu Jan 08 15:50:16 CST 2026, Created by peggypeng
+
+Copyright (C) 2026 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.zats.test2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.Color;
+
+import org.zkoss.test.webdriver.WebDriverTestCase;
+import org.zkoss.test.webdriver.ztl.JQuery;
+
+public class B103_ZK_6038Test extends WebDriverTestCase {
+    @Test
+    public void test() {
+        connect();
+        JQuery coachmarkMask = jq(".z-coachmark-mask");
+
+        Color maskColor = Color.fromString(coachmarkMask.css("background-color"));
+        Color transparent = Color.fromString("rgba(0, 0, 0, 0)");
+
+        Assertions.assertNotEquals(transparent, maskColor);
+    }
+}
